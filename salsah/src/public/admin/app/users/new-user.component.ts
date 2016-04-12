81bcf5763a1e933eee8e0160594a99d66acf2ca5
Ivan Subotic <ibs@subotic.ch>
2016-04-12 12:41:32 +0200

    feature (salsah): angular 2 based admin interface (ongoing)

--- salsah/src/public/admin/app/users/new-user.component.ts
+++ salsah/src/public/admin/app/users/new-user.component.ts
@@ -18,7 +18,7 @@
  License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {Component} from "angular2/core";
+import {Component, OnInit} from "angular2/core";
 import {UserService} from "./user.service";
 import {User} from "./user";
 import {Router} from "angular2/router";
@@ -29,23 +29,43 @@ import {Router} from "angular2/router";
             <h2>Create New User</h2>
             <div>
                 <label for="user-name">Username:</label>
-                <input type="text" id="user-name" ngControl="userName">
+                <input type="text" id="user-name" 
+                    ngControl="userName"
+                    [(ngModel)]="newUser.userName"
+                    required
+                >
             </div>
             <div>
                 <label for="given-name">Given Name:</label>
-                <input type="text" id="given-name" ngControl="givenName">
+                <input type="text" id="given-name"
+                    ngControl="givenName"
+                    [(ngModel)]="newUser.givenName"
+                    required
+                >
             </div>
             <div>
                 <label for="family-name">Family Name:</label>
-                <input type="text" id="family-name" ngControl="familyName">
+                <input type="text" id="family-name"
+                    ngControl="familyName"
+                    [(ngModel)]="newUser.familyName"
+                    required
+                >
             </div>
             <div>
                 <label for="email">Email:</label>
-                <input type="text" id="email" ngControl="email">
+                <input type="text" id="email"
+                    ngControl="email"
+                    [(ngModel)]="newUser.email"
+                    required
+                >
             </div>
             <div>
                 <label for="phone">Phone:</label>
-                <input type="text" id="phone" ngControl="phone">
+                <input type="text" id="phone"
+                    ngControl="phone"
+                    [(ngModel)]="newUser.phone"
+                    required
+                >
             </div>
             <div>
                 <label for="system-admin-status">System Admin:</label>
@@ -71,18 +91,21 @@ import {Router} from "angular2/router";
 })
 export class NewUserComponent {
 
-    newUser: User;
+    newUser: User = {
+        userId: '',
+        userName: '',
+        givenName: '',
+        familyName: '',
+        email: '',
+        phone: '',
+        isActiveUser: false,
+        isSystemAdmin: false
+    };
 
     constructor(private _userService: UserService, private _router: Router) {}
 
-    onCreateUser(userName, givenName, familyName, email, phone, isSystemAdmin, isActiveUser) {
-        let user: User = {userId: "http://data.knora.org/users/" + userName, userName: userName, givenName: givenName, familyName: familyName, email: email, phone: phone, isSystemAdmin: isSystemAdmin, isActiveUser: isActiveUser}
-        this._userService.insertUser(user)
+    onSubmit() {
+        this._userService.insertUser(this.newUser)
         this._router.navigate(['Users'])
     }
-
-    onSubmit() {
-        
-    }
-
 }
