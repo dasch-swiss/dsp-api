a44d258cb7d2591f1ac38d41477b4a324069a003
Ivan Subotic <ibs@subotic.ch>
2016-06-09 11:08:00 +0200

    build (triplestore): add docker versions of graphdb prepare/load scripts with fixed path to KnoraRules.pie
    - additionally, KnoraRules.pie needs to be copied to the GraphDB folder, so that it can be add to the docker image

---
+++ webapi/scripts/graphdb-se-docker-load-test-data.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONSOLE="openrdf-console/bin/console.sh --force --echo --serverURL http://localhost:8080/openrdf-sesame"
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NO_COLOUR='\033[0m'
+DELIMITER="****************************************************************************************************\n* "
+
+printf "${GREEN}${DELIMITER}Deleting repository${NO_COLOUR}\n\n"
+
+cat graphdb-se-drop-knora-test-repository.ttl | $CONSOLE
+
+printf "\n${GREEN}${DELIMITER}Creating repository${NO_COLOUR}\n\n"
+
+# in this docker version of the script, the path to the KnoraRules.pie is fixed
+sed -e 's@PIE_FILE@'"/usr/local/tomcat/KnoraRules.pie"'@' graphdb-se-knora-test-repository-config.ttl.tmpl > graphdb-se-knora-test-repository-config.ttl
+
+curl -X POST -H "Content-Type:application/x-turtle" -T graphdb-se-knora-test-repository-config.ttl "http://localhost:8080/openrdf-sesame/repositories/SYSTEM/rdf-graphs/service?graph=http://www.knora.org/config"
+
+curl -X POST -H "Content-Type:application/x-turtle" -d "<http://www.knora.org/config> a <http://www.openrdf.org/config/repository#RepositoryContext>." http://localhost:8080/openrdf-sesame/repositories/SYSTEM/statements
+
+printf "${GREEN}Repository created.\n\n${DELIMITER}Loading test data${NO_COLOUR}\n\n"
+
+cat graphdb-se-knora-test-load-data.ttl | $CONSOLE
+
+printf "\n${GREEN}${DELIMITER}Creating Lucene index${NO_COLOUR}\n\n"
+
+STATUS=$(curl -s -w '%{http_code}' -S -X POST -H "Content-Type:text/turtle" --data-binary @./graphdb-se-knora-test-index-config.ttl http://localhost:8080/openrdf-sesame/repositories/knora-test/statements)
+
+if [ ${STATUS} -eq 204 ]
+then
+    printf "${GREEN}Lucene index built.${NO_COLOUR}\n\n"
+else
+    printf "${RED}Building of Lucene index failed.${NO_COLOUR}\n\n"
+fi
