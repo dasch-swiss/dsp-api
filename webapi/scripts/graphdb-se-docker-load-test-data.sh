059e37910ef58723af090327ac20c0f91c3191d0
Ivan Subotic <ibs@subotic.ch>
2016-09-23 16:06:58 +0200

    fix (webapi): fix graphdb data loading scripts for the docker case

--- webapi/scripts/graphdb-se-docker-load-test-data.sh
+++ webapi/scripts/graphdb-se-docker-load-test-data.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-CONSOLE="openrdf-console/bin/console.sh --force --echo --serverURL http://localhost:8080/openrdf-sesame"
+GRAPHDB="http://localhost:7200"
+CONSOLE="openrdf-console/bin/console.sh --force --echo --serverURL $GRAPHDB"
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -16,11 +17,11 @@ cat graphdb-se-drop-knora-test-repository.ttl | $CONSOLE
 printf "\n${GREEN}${DELIMITER}Creating repository${NO_COLOUR}\n\n"
 
 # in this docker version of the script, the path to the KnoraRules.pie is fixed
-sed -e 's@PIE_FILE@'"/usr/local/tomcat/KnoraRules.pie"'@' graphdb-se-knora-test-repository-config.ttl.tmpl > graphdb-se-knora-test-repository-config.ttl
+sed -e 's@PIE_FILE@'"/graphdb/KnoraRules.pie"'@' graphdb-se-knora-test-repository-config.ttl.tmpl > graphdb-se-knora-test-repository-config.ttl
 
-curl -X POST -H "Content-Type:application/x-turtle" -T graphdb-se-knora-test-repository-config.ttl "http://localhost:8080/openrdf-sesame/repositories/SYSTEM/rdf-graphs/service?graph=http://www.knora.org/config"
+curl -X POST -H "Content-Type:application/x-turtle" -T graphdb-se-knora-test-repository-config.ttl "$GRAPHDB/repositories/SYSTEM/rdf-graphs/service?graph=http://www.knora.org/config"
 
-curl -X POST -H "Content-Type:application/x-turtle" -d "<http://www.knora.org/config> a <http://www.openrdf.org/config/repository#RepositoryContext>." http://localhost:8080/openrdf-sesame/repositories/SYSTEM/statements
+curl -X POST -H "Content-Type:application/x-turtle" -d "<http://www.knora.org/config> a <http://www.openrdf.org/config/repository#RepositoryContext>." $GRAPHDB/repositories/SYSTEM/statements
 
 printf "${GREEN}Repository created.\n\n${DELIMITER}Loading test data${NO_COLOUR}\n\n"
 
@@ -28,11 +29,12 @@ cat graphdb-se-knora-test-load-data.ttl | $CONSOLE
 
 printf "\n${GREEN}${DELIMITER}Creating Lucene index${NO_COLOUR}\n\n"
 
-STATUS=$(curl -s -w '%{http_code}' -S -X POST -H "Content-Type:text/turtle" --data-binary @./graphdb-se-knora-test-index-config.ttl http://localhost:8080/openrdf-sesame/repositories/knora-test/statements)
+STATUS=$(curl -s -w '%{http_code}' -S -X POST -H "Content-Type:text/turtle" --data-binary @./graphdb-se-knora-test-index-config.ttl $GRAPHDB/repositories/knora-test/statements)
 
-if [ ${STATUS} -eq 204 ]
+if [ "$STATUS" == "204" ]
 then
     printf "${GREEN}Lucene index built.${NO_COLOUR}\n\n"
 else
-    printf "${RED}Building of Lucene index failed.${NO_COLOUR}\n\n"
+    printf "${RED}Building of Lucene index failed: ${STATUS}${NO_COLOUR}\n\n"
 fi
+
