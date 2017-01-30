95a72ee878db33493ed835d8fbf4e62b0966636e
Tobias Schweizer <t.schweizer@unibas.ch>
2017-01-30 12:10:54 +0100

    refactor (standoff): remove obsolete manual test files

--- webapi/_test_data/test_route/ckeditor/createMapping.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -v -X POST --header "Content-Type: multipart/form-data" -u anything-user:test --form "json=@mappingCreationParams.json" --form "xml=@mapping.xml" localhost:3333/v1/mapping
