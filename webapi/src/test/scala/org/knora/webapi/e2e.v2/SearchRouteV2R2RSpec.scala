0d57b3b22d330ddbd913516dba8b122c961bebf6
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2017-07-26 13:46:54 +0200

    test (webapi): Change "ignore" to "should".

--- webapi/src/test/scala/org/knora/webapi/e2e.v2/SearchRouteV2R2RSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e.v2/SearchRouteV2R2RSpec.scala
@@ -110,7 +110,7 @@ class SearchRouteV2R2RSpec extends R2RSpec {
 
     }
 
-    "The Search Endpoint" ignore {
+    "The Search Endpoint" should {
         "perform a fulltext search for 'Narr'" in {
 
             Get("/v2/search/Narr") ~> searchPath ~> check {
