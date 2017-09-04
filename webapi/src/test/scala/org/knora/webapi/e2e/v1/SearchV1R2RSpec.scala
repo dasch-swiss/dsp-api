3f98718dc107eae086852778cb17f76b2f6ef1a5
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2017-09-04 16:19:51 +0200

    Revert "test (webapi): Don't load unused test data."
    This reverts commit 61d8aa0fe9c6c35a51cb6baf7089315e3702c7ad.

--- webapi/src/test/scala/org/knora/webapi/e2e/v1/SearchV1R2RSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e/v1/SearchV1R2RSpec.scala
@@ -62,6 +62,7 @@ class SearchV1R2RSpec extends R2RSpec {
     private val rdfDataObjects = List(
 
         RdfDataObject(path = "_test_data/all_data/incunabula-data.ttl", name = "http://www.knora.org/data/incunabula"),
+        RdfDataObject(path = "_test_data/demo_data/images-demo-data.ttl", name = "http://www.knora.org/data/images"),
         RdfDataObject(path = "_test_data/all_data/anything-data.ttl", name = "http://www.knora.org/data/anything")
 
     )
