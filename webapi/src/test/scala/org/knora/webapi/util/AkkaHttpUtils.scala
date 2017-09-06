271fabd58b05c63e7218d530dd9d8927954cdc0b
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2017-09-06 17:27:31 +0200

    feature (webapi): Copy some JSON-LD infrastructure from #572.

--- webapi/src/test/scala/org/knora/webapi/util/AkkaHttpUtils.scala
+++ webapi/src/test/scala/org/knora/webapi/util/AkkaHttpUtils.scala
@@ -1,11 +1,16 @@
 package org.knora.webapi.util
 
+import java.io.ByteArrayInputStream
+import java.util
+
 import akka.actor.ActorSystem
 import akka.event.LoggingAdapter
 import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+import com.github.jsonldjava.core.{JsonLdOptions, JsonLdProcessor}
+import com.github.jsonldjava.utils.JsonUtils
 import spray.json._
 
 import scala.concurrent.duration._
@@ -37,6 +42,60 @@ object AkkaHttpUtils {
 
         //FIXME: There is probably a better non blocking way of doing it.
         Await.result(jsonFuture, Timeout(10.seconds).duration)
+    }
+
+    /**
+      * Given an [[HttpResponse]] containing json-ld, return the said json-ld in expanded form.
+      *
+      * @param response the [[HttpResponse]] containing json
+      * @return an [[JsObject]]
+      */
+    def httpResponseToJsonLDExpanded(response: HttpResponse)(implicit ec: ExecutionContext, system: ActorSystem, log: LoggingAdapter): Map[String, Any] = {
+
+        implicit val materializer = ActorMaterializer()
+
+        val jsonStringFuture: Future[String] = Unmarshal(response.entity).to[String]
+
+        val jsonString = Await.result(jsonStringFuture, Timeout(1.second).duration)
+
+        val istream = new ByteArrayInputStream(jsonString.getBytes(java.nio.charset.StandardCharsets.UTF_8))
+
+        val jsonObject: AnyRef = JsonUtils.fromInputStream(istream)
+
+        val context = new util.HashMap()
+
+        val options = new JsonLdOptions()
+
+        val normalized: util.Map[String, Object] = JsonLdProcessor.compact(jsonObject, context, options)
+
+
+
+        /*
+        val opts: JsonLdOptions = new JsonLdOptions()
+        val expanded = JsonLdProcessor.expand(httpResponseToJson(response), opts)
+        println("expanded json-ld: " + expanded)
+        */
+
+        convert(normalized).asInstanceOf[Map[String, Any]]
+    }
+
+    /**
+      * Deep conversion of a java collection into a scala collection.
+      *
+      * Usage: val y = convert(x).asInstanceOf[Map[String, Any]]
+      *
+      * @param x
+      * @return
+      */
+    private def convert(x:Any):Any =
+    {
+        import collection.JavaConverters._
+        x match
+        {
+            case x: java.util.HashMap[_,_] => x.asScala.toMap.mapValues(convert)
+            case x: java.util.ArrayList[_] => x.asScala.toList.map(convert)
+            case _ => x
+        }
     }
 
 }
