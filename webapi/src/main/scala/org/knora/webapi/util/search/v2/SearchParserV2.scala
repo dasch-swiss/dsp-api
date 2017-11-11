5d412c83bfaa1b5f57d25de563a973017df8fecc
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2017-11-11 11:53:39 +0100

    fix (webapi): Allow non-Knora IRIs in KnarQL, but not ApiV2WithValueObjects.

--- webapi/src/main/scala/org/knora/webapi/util/search/v2/SearchParserV2.scala
+++ webapi/src/main/scala/org/knora/webapi/util/search/v2/SearchParserV2.scala
@@ -155,8 +155,16 @@ object SearchParserV2 {
         private def makeIri(rdf4jIri: rdf4j.model.IRI): IriRef = {
             val smartIri: SmartIri = rdf4jIri.stringValue.toSmartIriWithErr(() => throw SparqlSearchException(s"Invalid IRI: ${rdf4jIri.stringValue}"))
 
-            if (!(smartIri.isKnoraApiV2OntologyEntityIri && smartIri.getOntologySchema == ApiV2Simple)) {
-                throw SparqlSearchException(s"Invalid IRI in search query: $smartIri")
+            if (smartIri.isKnoraInternalDefinitionIri) {
+                throw SparqlSearchException(s"Internal ontology entity IRI not allowed in search query: $smartIri")
+            }
+
+            if (smartIri.isKnoraOntologyIri) {
+                throw SparqlSearchException(s"Ontology IRI not allowed in search query: $smartIri")
+            }
+
+            if (smartIri.isKnoraApiV2OntologyEntityIri && smartIri.getOntologySchema != ApiV2Simple) {
+                throw SparqlSearchException(s"Ontology schema in not allowed in search query: $smartIri")
             }
 
             IriRef(smartIri)
