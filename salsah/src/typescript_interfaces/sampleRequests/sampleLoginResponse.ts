2d68024950b0a16c2eb817fa33e393526a621a97
Loïc Jaouen <loic.jaouen@unil.ch>
2017-12-01 15:37:28 +0100

    updating test expected data

--- salsah/src/typescript_interfaces/sampleRequests/sampleLoginResponse.ts
+++ salsah/src/typescript_interfaces/sampleRequests/sampleLoginResponse.ts
@@ -16,15 +16,15 @@
 
 import { sessionResponseFormats } from "../sessionResponseFormats"
 
-let login: sessionResponseFormats.loginResponse = {
+let login:sessionResponseFormats.loginResponse = {
     "status":0,
     "message":"credentials are OK",
-    "sid":"bb8e83ce-353d-43da-9913-5d76a0f03f6c",
+    "sid":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3ZWJhcGkiLCJzdWIiOiJodHRwOi8vcmRmaC5jaC91c2Vycy9tdWx0aXVzZXIiLCJhdWQiOiJ3ZWJhcGkiLCJpYXQiOjE1MTIxMzc2ODEsImV4cCI6MTUxNDcyOTY4MSwianRpIjoiZTMzY2M4MzYtYjVmYy00Yzc4LWIyNWYtOGE1OWQ4MmViNmFmIn0.lYv_LOnyE1yGtJcMs8tOjD-LeGZVTiwEEnur0BiWfnw",
     "userProfile":{
         "userData":{
             "email":"multi.user@example.com",
             "firstname":"Multi",
-            "user_id":"http://data.knora.org/users/multiuser",
+            "user_id":"http://rdfh.ch/users/multiuser",
             "lastname":"User",
             "status":true,
             "token":null,
@@ -32,39 +32,42 @@ let login: sessionResponseFormats.loginResponse = {
             "password":null
         },
         "groups":[
-            "http://data.knora.org/groups/images-reviewer"
+            "http://rdfh.ch/groups/00FF/images-reviewer"
         ],
         "projects_info":{
-            "http://data.knora.org/projects/images":{
+            "http://rdfh.ch/projects/00FF":{
                 "shortname":"images",
                 "description":"A demo project of a collection of images",
                 "institution":null,
+                "shortcode":"00FF",
                 "logo":null,
-                "id":"http://data.knora.org/projects/images",
+                "id":"http://rdfh.ch/projects/00FF",
                 "status":true,
                 "selfjoin":false,
                 "keywords":"images, collection",
                 "longname":"Image Collection Demo",
-                "ontologies":["http://www.knora.org/ontology/images"]
+                "ontologies":["http://www.knora.org/ontology/00FF/images"]
             },
-            "http://data.knora.org/projects/77275339":{
+            "http://rdfh.ch/projects/77275339":{
                 "shortname":"incunabula",
                 "description":"<p>Das interdisziplinäre Forschungsprojekt \"<b><em>Die Bilderfolgen der Basler Frühdrucke: Spätmittelalterliche Didaxe als Bild-Text-Lektüre</em></b>\" verbindet eine umfassende kunstwissenschaftliche Analyse der Bezüge zwischen den Bildern und Texten in den illustrierten Basler Inkunabeln mit der Digitalisierung der Bestände der Universitätsbibliothek und der Entwicklung einer elektronischen Edition in der Form einer neuartigen Web-0.2-Applikation.\n</p>\n<p>Das Projekt wird durchgeführt vom <a href=\"http://kunsthist.unibas.ch\">Kunsthistorischen Seminar</a> der Universität Basel (Prof. B. Schellewald) und dem <a href=\"http://www.dhlab.unibas.ch\">Digital Humanities Lab</a> der Universität Basel (PD Dr. L. Rosenthaler).\n</p>\n<p>\nDas Kernstück der digitalen Edition besteht aus rund zwanzig reich bebilderten Frühdrucken aus vier verschiedenen Basler Offizinen. Viele davon sind bereits vor 1500 in mehreren Ausgaben erschienen, einige fast gleichzeitig auf Deutsch und Lateinisch. Es handelt sich um eine ausserordentlich vielfältige Produktion; neben dem Heilsspiegel finden sich ein Roman, die Melusine,  die Reisebeschreibungen des Jean de Mandeville, einige Gebets- und Erbauungsbüchlein, theologische Schriften, Fastenpredigten, die Leben der Heiligen Fridolin und Meinrad, das berühmte Narrenschiff  sowie die Exempelsammlung des Ritters vom Thurn.\n</p>\nDie Internetpublikation macht das digitalisierte Korpus dieser Frühdrucke  durch die Möglichkeiten nichtlinearer Verknüpfung und Kommentierung der Bilder und Texte, für die wissenschaftliche Edition sowie für die Erforschung der Bilder und Texte nutzbar machen. Auch können bereits bestehende und entstehende Online-Editionen damit verknüpft  werden , wodurch die Nutzung von Datenbanken anderer Institutionen im Hinblick auf unser Corpus optimiert wird.\n</p>",
                 "institution":null,
+                "shortcode":null,
                 "logo":"incunabula_logo.png",
-                "id":"http://data.knora.org/projects/77275339",
+                "id":"http://rdfh.ch/projects/77275339",
                 "status":true,
                 "selfjoin":false,
                 "keywords":"Basler Frühdrucke, Inkunabel, Narrenschiff, Wiegendrucke, Sebastian Brant, Bilderfolgen, early print, incunabula, ship of fools, Kunsthistorischs Seminar Universität Basel, Late Middle Ages, Letterpress Printing, Basel, Contectualisation of images",
                 "longname":"Bilderfolgen Basler Frühdrucke",
-                "ontologies": [ "http://www.knora.org/ontology/incunabula" ]
+                "ontologies":["http://www.knora.org/ontology/incunabula"]
             },
-            "http://data.knora.org/projects/anything":{
+            "http://rdfh.ch/projects/anything":{
                 "shortname":"anything",
                 "description":"Anything Project",
                 "institution":null,
+                "shortcode":null,
                 "logo":null,
-                "id":"http://data.knora.org/projects/anything",
+                "id":"http://rdfh.ch/projects/anything",
                 "status":true,
                 "selfjoin":false,
                 "keywords":null,
@@ -76,22 +79,22 @@ let login: sessionResponseFormats.loginResponse = {
         "isSystemUser":false,
         "permissionData":{
             "groupsPerProject":{
-                "http://data.knora.org/projects/77275339":[
+                "http://rdfh.ch/projects/77275339":[
                     "http://www.knora.org/ontology/knora-base#ProjectMember",
                     "http://www.knora.org/ontology/knora-base#ProjectAdmin"
                 ],
-                "http://data.knora.org/projects/images":[
-                    "http://data.knora.org/groups/images-reviewer",
+                "http://rdfh.ch/projects/00FF":[
+                    "http://rdfh.ch/groups/00FF/images-reviewer",
                     "http://www.knora.org/ontology/knora-base#ProjectMember",
                     "http://www.knora.org/ontology/knora-base#ProjectAdmin"
                 ],
-                "http://data.knora.org/projects/anything":[
+                "http://rdfh.ch/projects/anything":[
                     "http://www.knora.org/ontology/knora-base#ProjectMember",
                     "http://www.knora.org/ontology/knora-base#ProjectAdmin"
                 ]
             },
             "administrativePermissionsPerProject":{
-                "http://data.knora.org/projects/77275339":[
+                "http://rdfh.ch/projects/77275339":[
                     {
                         "name":"ProjectAdminAllPermission",
                         "additionalInformation":null,
@@ -103,7 +106,7 @@ let login: sessionResponseFormats.loginResponse = {
                         "v1Code":null
                     }
                 ],
-                "http://data.knora.org/projects/images":[
+                "http://rdfh.ch/projects/00FF":[
                     {
                         "name":"ProjectAdminAllPermission",
                         "additionalInformation":null,
@@ -115,7 +118,7 @@ let login: sessionResponseFormats.loginResponse = {
                         "v1Code":null
                     }
                 ],
-                "http://data.knora.org/projects/anything":[
+                "http://rdfh.ch/projects/anything":[
                     {
                         "name":"ProjectAdminAllPermission",
                         "additionalInformation":null,
