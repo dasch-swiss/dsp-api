71c9496f2cdca43f82d00dd5d250419512ca5ac5
Ivan Subotic <ibs@subotic.ch>
2018-01-29 17:51:46 +0100

    refactor (webapi): renamed store messages to adm

--- webapi/src/main/scala/org/knora/webapi/responders/admin/StoresResponderADM.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/admin/StoresResponderADM.scala
@@ -18,9 +18,9 @@ package org.knora.webapi.responders.admin
 
 import akka.pattern._
 import org.knora.webapi._
+import org.knora.webapi.messages.admin.responder.storesmessages.{ResetTriplestoreContentRequestADM, ResetTriplestoreContentResponseADM}
 import org.knora.webapi.messages.store.triplestoremessages.{RdfDataObject, ResetTriplestoreContent, ResetTriplestoreContentACK}
 import org.knora.webapi.messages.v1.responder.ontologymessages.{LoadOntologiesRequest, LoadOntologiesResponse}
-import org.knora.webapi.messages.v1.responder.storemessages.{ResetTriplestoreContentRequestV1, ResetTriplestoreContentResponseV1}
 import org.knora.webapi.responders.Responder
 import org.knora.webapi.util.ActorUtil._
 
@@ -30,7 +30,7 @@ import scala.concurrent.Future
   * This responder is used by [[org.knora.webapi.routing.admin.StoreRouteADM]], for piping through HTTP requests to the
   * 'Store Module'
   */
-class StoreResponderADM extends Responder {
+class StoresResponderADM extends Responder {
 
     /**
       * A user representing the Knora API server, used in those cases where a user is required.
@@ -38,7 +38,7 @@ class StoreResponderADM extends Responder {
     private val systemUser = KnoraSystemInstances.Users.SystemUser.asUserProfileV1
 
     def receive = {
-        case ResetTriplestoreContentRequestV1(rdfDataObjects: Seq[RdfDataObject]) => future2Message(sender(), resetTriplestoreContent(rdfDataObjects), log)
+        case ResetTriplestoreContentRequestADM(rdfDataObjects: Seq[RdfDataObject]) => future2Message(sender(), resetTriplestoreContent(rdfDataObjects), log)
         case other => handleUnexpectedMessage(sender(), other, log, this.getClass.getName)
     }
 
@@ -46,12 +46,13 @@ class StoreResponderADM extends Responder {
       * This method send a [[ResetTriplestoreContent]] message to the [[org.knora.webapi.store.triplestore.TriplestoreManager]].
       *
       * @param rdfDataObjects the payload consisting of a list of [[RdfDataObject]] send inside the message.
-      * @return a future containing a [[ResetTriplestoreContentResponseV1]].
+      * @return a future containing a [[ResetTriplestoreContentResponseADM]].
       */
-    private def resetTriplestoreContent(rdfDataObjects: Seq[RdfDataObject]): Future[ResetTriplestoreContentResponseV1] = {
+    private def resetTriplestoreContent(rdfDataObjects: Seq[RdfDataObject]): Future[ResetTriplestoreContentResponseADM] = {
 
-        log.debug(s"resetTriplestoreContent called with: ${rdfDataObjects.toString}")
-        log.debug(s"StartupFlags.allowReloadOverHTTP = ${StartupFlags.allowReloadOverHTTP.get}")
+        log.info(s"resetTriplestoreContent - called")
+        log.debug(s"resetTriplestoreContent called with: {}", rdfDataObjects.toString)
+        log.debug(s"StartupFlags.allowReloadOverHTTP = {}", StartupFlags.allowReloadOverHTTP.get)
 
         for {
             value <- StartupFlags.allowReloadOverHTTP.future()
@@ -61,12 +62,12 @@ class StoreResponderADM extends Responder {
             }
 
             resetResponse <- (storeManager ? ResetTriplestoreContent(rdfDataObjects)).mapTo[ResetTriplestoreContentACK]
-            _ = log.debug(s"resetTriplestoreContent - triplestore reset done ${resetResponse.toString}")
+            _ = log.info(s"resetTriplestoreContent - triplestore reset done - {}", resetResponse.toString)
 
             loadOntologiesResponse <- (responderManager ? LoadOntologiesRequest(systemUser)).mapTo[LoadOntologiesResponse]
-            _ = log.debug(s"resetTriplestoreContent - load ontology done ${loadOntologiesResponse.toString}")
+            _ = log.info(s"resetTriplestoreContent - load ontology done - {}", loadOntologiesResponse.toString)
 
-            result = ResetTriplestoreContentResponseV1(message = "success")
+            result = ResetTriplestoreContentResponseADM(message = "success")
 
         } yield result
     }
