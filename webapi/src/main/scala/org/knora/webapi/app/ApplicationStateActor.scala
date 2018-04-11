b246c7d9ad48874a4bd6efd543cb090f6c5af401
Ivan Subotic <ibs@subotic.ch>
2018-04-11 12:24:21 +0200

    feature (webapi): OpenAPI / Swagger API documentation generation (#812)
    * feature (webapi): add OpenAPI / Swagger API document generation
    
    * doc (webapi): update admin docs
    
    * doc (webapi): update release notes

--- webapi/src/main/scala/org/knora/webapi/app/ApplicationStateActor.scala
+++ webapi/src/main/scala/org/knora/webapi/app/ApplicationStateActor.scala
@@ -7,7 +7,7 @@ import org.knora.webapi.{Settings, SettingsImpl}
 
 class ApplicationStateActor extends Actor with ActorLogging {
 
-    // the prometheus, zipkin, and jaeger flags can be set via application.conf and via command line parameter
+    // the prometheus, zipkin, jaeger, and printConfig flags can be set via application.conf and via command line parameter
     val settings: SettingsImpl = Settings(context.system)
 
     private var loadDemoDataState = false
@@ -15,6 +15,7 @@ class ApplicationStateActor extends Actor with ActorLogging {
     private var prometheusReporterState = false
     private var zipkinReporterState = false
     private var jaegerReporterState = false
+    private var printConfigState = false
 
     def receive: PartialFunction[Any, Unit] = {
         case Initialized() => sender ! InitializedResponse(true)
@@ -57,6 +58,14 @@ class ApplicationStateActor extends Actor with ActorLogging {
         case GetJaegerReporterState() => {
             log.debug("ApplicationStateActor - GetJaegerReporterState - value: {}", jaegerReporterState)
             sender ! (jaegerReporterState | settings.jaegerReporter)
+        }
+        case SetPrintConfigState(value) => {
+            log.debug("ApplicationStateActor - SetPrintConfigState - value: {}", value)
+            printConfigState = value
+        }
+        case GetPrintConfigState() => {
+            log.debug("ApplicationStateActor - GetPrintConfigState - value: {}", printConfigState)
+            sender ! (printConfigState | settings.printConfig)
         }
     }
 }
