38a50ac47a25e8fd0970068c175b70f095d0322c
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2018-07-06 17:47:31 +0200

    feature (webapi): Start implementing pre-update checks for value creation.

--- webapi/src/main/scala/org/knora/webapi/responders/v2/ValuesResponderV2.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/v2/ValuesResponderV2.scala
@@ -19,9 +19,17 @@
 
 package org.knora.webapi.responders.v2
 
+import akka.http.scaladsl.util.FastFuture
+import akka.pattern._
+import org.knora.webapi._
+import org.knora.webapi.messages.admin.responder.usersmessages.UserADM
+import org.knora.webapi.messages.v2.responder.resourcemessages.{ReadResourceV2, ReadResourcesSequenceV2}
+import org.knora.webapi.messages.v2.responder.searchmessages.GravsearchRequestV2
 import org.knora.webapi.messages.v2.responder.valuemessages._
-import org.knora.webapi.responders.Responder
+import org.knora.webapi.responders.{IriLocker, Responder}
 import org.knora.webapi.util.ActorUtil._
+import org.knora.webapi.util.SmartIri
+import org.knora.webapi.util.search.gravsearch.GravsearchParser
 
 import scala.concurrent.Future
 
@@ -30,7 +38,7 @@ import scala.concurrent.Future
   */
 class ValuesResponderV2 extends Responder {
     override def receive: Receive = {
-        case createValueRequest: CreateValueRequestV2 => future2Message(sender(), createValue(createValueRequest), log)
+        case createValueRequest: CreateValueRequestV2 => future2Message(sender(), createValueV2(createValueRequest), log)
         case other => handleUnexpectedMessage(sender(), other, log, this.getClass.getName)
     }
 
@@ -40,5 +48,83 @@ class ValuesResponderV2 extends Responder {
       * @param createValueRequest the request to create the value.
       * @return a [[CreateValueResponseV2]].
       */
-    def createValue(createValueRequest: CreateValueRequestV2): Future[CreateValueResponseV2] = ???
+    def createValueV2(createValueRequest: CreateValueRequestV2): Future[CreateValueResponseV2] = {
+        def makeTaskFuture(userIri: IRI): Future[CreateValueResponseV2] = {
+            for {
+                // Get ontology information about the property and about its cardinality in the resource class.
+
+                // Ensure that this is a value property.
+
+                // Get the resource's metadata and the values, if any, that the resource already has for the property.
+
+                resourceWithPropertyValues <- getResourceWithPropertyValues(
+                    resourceIri = createValueRequest.createValue.resourceIri,
+                    propertyIri = createValueRequest.createValue.propertyIri,
+                    requestingUser = createValueRequest.requestingUser
+                )
+
+                // Check that the resource class's cardinality for the property allows another value to be added
+                // for that property.
+
+                // Check that the new value would not duplicate an existing value.
+
+            } yield CreateValueResponseV2(valueIri = "")
+        }
+
+        for {
+            // Don't allow anonymous users to create values.
+            userIri <- Future {
+                if (createValueRequest.requestingUser.isAnonymousUser) {
+                    throw ForbiddenException("Anonymous users aren't allowed to create values")
+                } else {
+                    createValueRequest.requestingUser.id
+                }
+            }
+
+            // Do the remaining pre-update checks and the update while holding an update lock on the resource.
+            taskResult <- IriLocker.runWithIriLock(
+                createValueRequest.apiRequestID,
+                createValueRequest.createValue.resourceIri,
+                () => makeTaskFuture(userIri)
+            )
+        } yield taskResult
+    }
+
+    /**
+      * Returns a resource's metadata and its values for the specified property.
+      *
+      * @param resourceIri    the resource IRI.
+      * @param propertyIri    the property IRI.
+      * @param requestingUser the user making the request.
+      * @return a [[ReadResourceV2]] containing only the resource's metadata and its values for the specified property.
+      */
+    private def getResourceWithPropertyValues(resourceIri: IRI, propertyIri: SmartIri, requestingUser: UserADM): Future[ReadResourceV2] = {
+        // TODO: when text values in Gravsearch query results are shortened, make a way for this query to get the complete value.
+        val gravsearchQuery =
+            s"""
+               |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+               |
+               |CONSTRUCT {
+               |  ?resource knora-api:isMainResource true .
+               |  ?resource <$propertyIri> ?propertyValue .
+               |} WHERE {
+               |  BIND(<$resourceIri> AS ?resource)
+               |  ?resource a knora-api:Resource .
+               |  ?resource <$propertyIri> ?propertyValue .
+               |}
+            """.stripMargin
+
+        for {
+            parsedGravsearchQuery <- FastFuture.successful(GravsearchParser.parseQuery(gravsearchQuery))
+            searchResponse <- (responderManager ? GravsearchRequestV2(parsedGravsearchQuery, requestingUser)).mapTo[ReadResourcesSequenceV2]
+
+            _ = if (searchResponse.numberOfResources == 0) {
+                throw NotFoundException(s"Resource <$resourceIri> not found")
+            }
+
+            _ = if (searchResponse.numberOfResources > 1) {
+                throw AssertionException(s"More than one resource returned with IRI <$resourceIri>")
+            }
+        } yield searchResponse.resources.head
+    }
 }
