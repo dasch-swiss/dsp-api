df782c52eb0d0c1a966a7da4885e92770a93cebb
Ivan Subotic <ibs@subotic.ch>
2018-07-10 17:14:52 +0200

    feature (config): graphdb repository type (#921)
    * feature (config): use correct graphdb repository type
    
    * feature (webapi): change required repository type
    
    * build (travis): tweak graphdb memory settings
    
    * build (travis): change to openjdk8
    
    * build (travis): try graphdb 8.6.0-se

--- webapi/scripts/graphdb-se-docker-init-knora-prod.sh
+++ webapi/scripts/graphdb-se-docker-init-knora-prod.sh
@@ -16,20 +16,15 @@ cat graphdb-drop-knora-prod-repository.ttl | $CONSOLE
 
 printf "\n${GREEN}${DELIMITER}Creating repository${NO_COLOUR}\n\n"
 
-# in this docker version of the script, the path to the KnoraRules.pie is fixed
 sed -e 's@PIE_FILE@'"/graphdb/KnoraRules.pie"'@' graphdb-se-knora-prod-repository-config.ttl.tmpl > graphdb-se-knora-prod-repository-config.ttl
 
-curl -X POST -H "Content-Type:application/x-turtle" -T graphdb-se-knora-prod-repository-config.ttl "$GRAPHDB/repositories/SYSTEM/rdf-graphs/service?graph=http://www.knora.org/config"
+curl -X POST -H "Content-Type:text/turtle" -T graphdb-se-knora-prod-repository-config.ttl "$GRAPHDB/repositories/SYSTEM/rdf-graphs/service?graph=http://www.knora.org/config-prod"
 
-curl -X POST -H "Content-Type:application/x-turtle" -d "<http://www.knora.org/config> a <http://www.openrdf.org/config/repository#RepositoryContext>." $GRAPHDB/repositories/SYSTEM/statements
+curl -X POST -H "Content-Type:text/turtle" -d "<http://www.knora.org/config-prod> a <http://www.openrdf.org/config/repository#RepositoryContext>." $GRAPHDB/repositories/SYSTEM/statements
 
-printf "${GREEN}Repository created.\n\n${DELIMITER}Loading test data${NO_COLOUR}\n\n"
+printf "${GREEN}Repository created.\n\n${DELIMITER}Creating Lucene Index${NO_COLOUR}\n\n"
 
-./graphdb-knora-prod-data.expect $GRAPHDB
-
-printf "\n${GREEN}${DELIMITER}Creating Lucene index${NO_COLOUR}\n\n"
-
-STATUS=$(curl -s -w '%{http_code}' -S -X POST -H "Content-Type:text/turtle" --data-binary @./graphdb-se-knora-prod-index-config.ttl $GRAPHDB/repositories/knora-prod/statements)
+STATUS=$(curl -s -w '%{http_code}' -S -X POST --data-urlencode 'update@./graphdb-se-knora-index-create.rq' $GRAPHDB/repositories/knora-prod/statements)
 
 if [ "$STATUS" == "204" ]
 then
@@ -38,3 +33,12 @@ else
     printf "${RED}Building of Lucene index failed: ${STATUS}${NO_COLOUR}\n\n"
 fi
 
+printf "${GREEN}Loading Data${NO_COLOUR}\n\n"
+
+./graphdb-knora-prod-data.expect $GRAPHDB
+
+printf "${GREEN}Data Loaded.\n\n${DELIMITER}Updating Lucene Index${NO_COLOUR}\n\n"
+
+curl -X POST --data-urlencode 'update@./graphdb-se-knora-index-update.rq' $GRAPHDB/repositories/knora-prod/statements
+
+printf "${GREEN}Lucene Index Updated.${NO_COLOUR}"
