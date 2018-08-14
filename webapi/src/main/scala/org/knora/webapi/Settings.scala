225dee8ca007de19a77ee903c5f4c097718e4c77
Ivan Subotic <ibs@subotic.ch>
2018-08-14 12:36:20 +0200

    fix (webapi): merge conflict

--- webapi/src/main/scala/org/knora/webapi/Settings.scala
+++ webapi/src/main/scala/org/knora/webapi/Settings.scala
@@ -187,13 +187,7 @@ class SettingsImpl(config: Config) extends Extension {
     val zipkinReporter: Boolean = config.getBoolean("app.monitoring.zipkin-reporter")
     val jaegerReporter: Boolean = config.getBoolean("app.monitoring.jaeger-reporter")
     val dataDogReporter: Boolean = config.getBoolean("app.monitoring.datadog-reporter")
-
-    private def getFiniteDuration(path: String, underlying: Config): FiniteDuration = Duration(underlying.getString(path)) match {
-        case x: FiniteDuration ⇒ x
-        case _                 ⇒ throw new ConfigurationException(s"Config setting '$path' must be a finite duration")
-    }
-
-
+    
     private def getFiniteDuration(path: String, underlying: Config): FiniteDuration = Duration(underlying.getString(path)) match {
         case x: FiniteDuration ⇒ x
         case _                 ⇒ throw new ConfigurationException(s"Config setting '$path' must be a finite duration")
