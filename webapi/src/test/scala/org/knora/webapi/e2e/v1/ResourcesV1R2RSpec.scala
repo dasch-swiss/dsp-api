9d9ed3afeb2c2df5a0e839bf8c1354f85853c60e
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2018-08-21 21:07:09 +0200

    style (ResourcesV1R2RSpec): Remove extra comma.

--- webapi/src/test/scala/org/knora/webapi/e2e/v1/ResourcesV1R2RSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e/v1/ResourcesV1R2RSpec.scala
@@ -88,7 +88,7 @@ class ResourcesV1R2RSpec extends R2RSpec {
     override lazy val rdfDataObjects = List(
         RdfDataObject(path = "_test_data/all_data/anything-data.ttl", name = "http://www.knora.org/data/0001/anything"),
         RdfDataObject(path = "_test_data/demo_data/images-demo-data.ttl", name = "http://www.knora.org/data/00FF/images"),
-        RdfDataObject(path = "_test_data/all_data/incunabula-data.ttl", name = "http://www.knora.org/data/0803/incunabula"),
+        RdfDataObject(path = "_test_data/all_data/incunabula-data.ttl", name = "http://www.knora.org/data/0803/incunabula")
     )
 
     private val firstThingIri = new MutableTestIri
