2a9b94f5c3144307e5184ca21dec3e4cd1485c6b
Ivan Subotic <ibs@subotic.ch>
2018-11-07 17:16:42 +0100

    feature (webapi): add webapi version to response header (ongoing)

---
+++ webapi/src/main/scala/org/knora/webapi/http/ServerVersion.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2015 Lukas Rosenthaler, Benjamin Geer, Ivan Subotic,
+ * Tobias Schweizer, André Kilchenmann, and André Fatton.
+ * This file is part of Knora.
+ * Knora is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Knora is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.knora.webapi.http
+
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.headers
+import akka.http.scaladsl.model.headers.Server
+import org.knora.webapi.BuildInfo
+
+trait ServerVersion {
+
+    private val serverVersionHeader = List(
+
+        `Server`(BuildInfo.name + "/" + BuildInfo.version)
+        `Access-Control-Allow-Headers`("Authorization",
+            "Content-Type", "X-Requested-With")
+    )
+
+    def addServerVersionHeader(response: HttpResponse):HttpResponse =
+        response.withHeaders(corsResponseHeaders)
+
+}
