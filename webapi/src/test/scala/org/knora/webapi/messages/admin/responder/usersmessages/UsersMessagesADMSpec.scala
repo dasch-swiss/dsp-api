05091f32cebdcc7cbf852832137a2c37b51b99e5
Ivan Subotic <ibs@subotic.ch>
2018-11-20 23:20:03 +0100

    feature (webapi): add username field to UserADM (#1048)
    * feature (webapi): add username to UserADM
    
    * doc (webapi): update authentication chapter
    
    * test (webapi): fix test data
    
    * build (webapi): update version
    
    * build (salsah): update version
    
    * doc (webapi): update release notes
    
    * feature (webapi): update email regex
    
    * feature (StringFormatter): Make email address syntax slightly more restrictive.

--- webapi/src/test/scala/org/knora/webapi/messages/admin/responder/usersmessages/UsersMessagesADMSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/messages/admin/responder/usersmessages/UsersMessagesADMSpec.scala
@@ -19,8 +19,12 @@
 
 package org.knora.webapi.messages.admin.responder.usersmessages
 
+import java.util.UUID
+
+import akka.actor.Status.Failure
 import org.knora.webapi._
 import org.knora.webapi.messages.admin.responder.permissionsmessages.{PermissionDataType, PermissionsDataADM}
+import org.knora.webapi.util.StringFormatter
 import org.scalatest.{Matchers, WordSpecLike}
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder
 
@@ -30,6 +34,7 @@ import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder
 class UsersMessagesADMSpec extends WordSpecLike with Matchers {
 
     private val id = SharedTestDataADM.rootUser.id
+    private val username = SharedTestDataADM.rootUser.username
     private val email = SharedTestDataADM.rootUser.email
     private val password = SharedTestDataADM.rootUser.password
     private val token = SharedTestDataADM.rootUser.token
@@ -42,36 +47,12 @@ class UsersMessagesADMSpec extends WordSpecLike with Matchers {
     private val sessionId = SharedTestDataADM.rootUser.sessionId
     private val permissions = SharedTestDataADM.rootUser.permissions
 
-    "The UserADM case class " should {
+    private implicit val stringFormatter: StringFormatter = StringFormatter.getInstanceForConstantOntologies
+
+    "The UserADM case class" should {
         "return a RESTRICTED UserADM when requested " in {
-            val rootUser = UserADM(
-                id = id,
-                email = email,
-                password = password,
-                token = token,
-                givenName = givenName,
-                familyName = familyName,
-                status = status,
-                lang = lang,
-                groups = groups,
-                projects = projects,
-                sessionId = sessionId,
-                permissions = permissions
-            )
-            val rootUserRestricted = UserADM(
-                id = id,
-                email = email,
-                password = None,
-                token = None,
-                givenName = givenName,
-                familyName = familyName,
-                status = status,
-                lang = lang,
-                groups = groups,
-                projects = projects,
-                sessionId = sessionId,
-                permissions = permissions.ofType(PermissionDataType.RESTRICTED)
-            )
+            val rootUser = UserADM(id = id, username = username, email = email, password = password, token = token, givenName = givenName, familyName = familyName, status = status, lang = lang, groups = groups, projects = projects, sessionId = sessionId, permissions = permissions)
+            val rootUserRestricted = UserADM(id = id, username = username, email = email, password = None, token = None, givenName = givenName, familyName = familyName, status = status, lang = lang, groups = groups, projects = projects, sessionId = sessionId, permissions = permissions.ofType(PermissionDataType.RESTRICTED))
 
             assert(rootUser.ofType(UserInformationTypeADM.RESTRICTED) === rootUserRestricted)
         }
@@ -79,20 +60,7 @@ class UsersMessagesADMSpec extends WordSpecLike with Matchers {
             //hashedPassword =  encoder.encode(createRequest.password);
             val encoder = new SCryptPasswordEncoder
             val hp = encoder.encode("123456")
-            val up = UserADM(
-                id = "something",
-                email = "something",
-                password = Some(hp),
-                token = None,
-                givenName = "something",
-                familyName = "something",
-                status = status,
-                lang = lang,
-                groups = groups,
-                projects = projects,
-                sessionId = sessionId,
-                permissions = PermissionsDataADM()
-            )
+            val up = UserADM(id = "something", username = "something" , email = "something", password = Some(hp), token = None, givenName = "something", familyName = "something", status = status, lang = lang, groups = groups, projects = projects, sessionId = sessionId, permissions = PermissionsDataADM())
 
             // test SCrypt
             assert(encoder.matches("123456", encoder.encode("123456")))
@@ -103,6 +71,113 @@ class UsersMessagesADMSpec extends WordSpecLike with Matchers {
 
         "allow checking the password (2)" in {
             SharedTestDataADM.rootUser.passwordMatch("test") should equal(true)
+        }
+    }
+
+    "The CreateUserApiRequestADM case class" should {
+
+        "throw 'BadRequestException' if 'username'is missing" in {
+
+            assertThrows[BadRequestException](
+                CreateUserApiRequestADM(
+                    username = "",
+                    email = "ddd@example.com",
+                    givenName = "Donald",
+                    familyName = "Duck",
+                    password = "test",
+                    status = true,
+                    lang = "en",
+                    systemAdmin = false
+                )
+            )
+        }
+
+        "throw 'BadRequestException' if 'email' is missing" in {
+
+            assertThrows[BadRequestException](
+                CreateUserApiRequestADM(
+                    username = "ddd",
+                    email = "",
+                    givenName = "Donald",
+                    familyName = "Duck",
+                    password = "test",
+                    status = true,
+                    lang = "en",
+                    systemAdmin = false
+                )
+            )
+        }
+
+        "throw 'BadRequestException' if 'password' is missing" in {
+
+            assertThrows[BadRequestException](
+                CreateUserApiRequestADM(
+                    username = "donald.duck",
+                    email = "donald.duck@example.com",
+                    givenName = "Donald",
+                    familyName = "Duck",
+                    password = "",
+                    status = true,
+                    lang = "en",
+                    systemAdmin = false
+                )
+            )
+        }
+
+        "throw 'BadRequestException' if 'givenName' is missing" in {
+
+            assertThrows[BadRequestException](
+                CreateUserApiRequestADM(
+                    username = "donald.duck",
+                    email = "donald.duck@example.com",
+                    givenName = "",
+                    familyName = "Duck",
+                    password = "test",
+                    status = true,
+                    lang = "en",
+                    systemAdmin = false
+                )
+            )
+        }
+
+        "throw 'BadRequestException' if 'familyName' is missing" in {
+
+            assertThrows[BadRequestException](
+                CreateUserApiRequestADM(
+                    username = "donald.duck",
+                    email = "donald.duck@example.com",
+                    givenName = "Donald",
+                    familyName = "",
+                    password = "test",
+                    status = true,
+                    lang = "en",
+                    systemAdmin = false
+                )
+            )
+        }
+    }
+
+    "The UserIdentifierADM case class" should {
+
+        "return the identifier type" in {
+
+            // FIXME: not working!!!
+            val iriIdentifier = UserIdentifierADM("http://rdfh.ch/users/root")
+            iriIdentifier.hasType should be (UserIdentifierType.IRI)
+
+            val emailIdentifier = UserIdentifierADM("root@example.com")
+            emailIdentifier.hasType should be (UserIdentifierType.EMAIL)
+
+            val usernameIdentifier = UserIdentifierADM("root")
+            usernameIdentifier.hasType should be (UserIdentifierType.USERNAME)
+        }
+
+        "throw a BadRequestException for an empty identifier string" in {
+
+            assertThrows[BadRequestException](
+                UserIdentifierADM("")
+            )
+
         }
     }
 }
