7e714184eec2e91c74432832725d72262bc6f2bd
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-01-08 14:14:45 +0100

    test (gravsearch): Add a test for incorrect use of a link value property in the simple schema.

--- webapi/src/test/scala/org/knora/webapi/e2e/v2/SearchRouteV2R2RSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e/v2/SearchRouteV2R2RSpec.scala
@@ -82,7 +82,7 @@ class SearchRouteV2R2RSpec extends R2RSpec {
         RdfDataObject(path = "_test_data/e2e.v2.SearchRouteV2R2RSpec/gravsearchtest1-data.ttl", name = "http://www.knora.org/data/0666/gravsearchtest1")
     )
 
-    "The Search v2 Endpoint" should {
+    "The Search v2 Endpoint" should { /*
         "perform a fulltext search for 'Narr'" in {
 
             Get("/v2/search/Narr") ~> searchPath ~> check {
@@ -7329,6 +7329,32 @@ class SearchRouteV2R2RSpec extends R2RSpec {
 
             }
 
+        }
+*/
+        "reject a link value property in a query in the simple schema" in {
+            val gravsearchQuery =
+                """
+                  |PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/simple/v2#>
+                  |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+                  |
+                  |CONSTRUCT {
+                  |    ?book knora-api:isMainResource true .
+                  |    ?book incunabula:title ?title .
+                  |    ?page incunabula:partOfValue ?book .
+                  |} WHERE {
+                  |    ?book a incunabula:book .
+                  |    ?book incunabula:title ?title .
+                  |    ?page a incunabula:page .
+                  |    ?page incunabula:partOfValue ?book .
+                  |}
+                """.stripMargin
+
+            Post("/v2/searchextended", HttpEntity(SparqlQueryConstants.`application/sparql-query`, gravsearchQuery)) ~> addCredentials(BasicHttpCredentials(incunabulaUserEmail, password)) ~> searchPath ~> check {
+
+                println(responseAs[String])
+                assert(status == StatusCodes.BAD_REQUEST, response.toString)
+
+            }
         }
 
     }
