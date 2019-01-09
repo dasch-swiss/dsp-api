207a457bd83fa1bc20eadb88506b149550e962d9
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-01-09 20:06:48 +0100

    Fix property names for incoming links (#1144)
    * fix (api-v2): Fix property names for incoming links.
    
    * docs (release-notes): Update release notes.
    
    * test (search): Fix search test data.

--- webapi/src/main/scala/org/knora/webapi/messages/v2/responder/ontologymessages/KnoraApiV2WithValueObjectsTransformationRules.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/v2/responder/ontologymessages/KnoraApiV2WithValueObjectsTransformationRules.scala
@@ -307,8 +307,8 @@ object KnoraApiV2WithValueObjectsTransformationRules extends KnoraBaseTransforma
         objectType = Some(OntologyConstants.Xsd.String)
     )
 
-    private val HasIncomingLink: ReadPropertyInfoV2 = makeProperty(
-        propertyIri = OntologyConstants.KnoraApiV2WithValueObjects.HasIncomingLink,
+    private val HasIncomingLinkValue: ReadPropertyInfoV2 = makeProperty(
+        propertyIri = OntologyConstants.KnoraApiV2WithValueObjects.HasIncomingLinkValue,
         isResourceProp = true,
         propertyType = OntologyConstants.Owl.ObjectProperty,
         subjectType = Some(OntologyConstants.KnoraApiV2WithValueObjects.Resource),
@@ -319,8 +319,8 @@ object KnoraApiV2WithValueObjectsTransformationRules extends KnoraBaseTransforma
             makePredicate(
                 predicateIri = OntologyConstants.Rdfs.Label,
                 objectsWithLang = Map(
-                    LanguageCodes.DE -> "hat eingehende Verweise",
-                    LanguageCodes.EN -> "has incoming links"
+                    LanguageCodes.DE -> "hat eingehenden Verweis",
+                    LanguageCodes.EN -> "has incoming link"
                 )
             ),
             makePredicate(
@@ -1177,7 +1177,7 @@ object KnoraApiV2WithValueObjectsTransformationRules extends KnoraBaseTransforma
     )
 
     private val ResourceCardinalities = Map(
-        OntologyConstants.KnoraApiV2WithValueObjects.HasIncomingLink -> Cardinality.MayHaveMany
+        OntologyConstants.KnoraApiV2WithValueObjects.HasIncomingLinkValue -> Cardinality.MayHaveMany
     )
 
     private val DateBaseCardinalities = Map(
@@ -1439,7 +1439,7 @@ object KnoraApiV2WithValueObjectsTransformationRules extends KnoraBaseTransforma
         OntologyName,
         MappingHasName,
         ValueAsString,
-        HasIncomingLink,
+        HasIncomingLinkValue,
         SubjectType,
         ObjectType,
         TextValueHasStandoff,
