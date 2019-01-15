e8d3f18bcdade92b13dec256e55ca6df79348e47
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-01-15 13:57:41 +0100

    feature (ark): Add copyright notice.

--- webapi/src/main/python/knora-ark-url.py
+++ webapi/src/main/python/knora-ark-url.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
+# Copyright © 2015-2019 the contributors (see Contributors.md).
+#
+# This file is part of Knora.
+#
+# Knora is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Knora is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #################################################################################################
 # Command-line program for converting between Knora resource IRIs and ARK URLs.
 #################################################################################################
