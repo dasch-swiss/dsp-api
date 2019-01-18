f560806e3752623d82af2882f461ecc1d25bd5db
Ivan Subotic <ibs@subotic.ch>
2019-01-18 10:42:11 +0100

    test (webapi): change host dependent test data

--- webapi/src/test/scala/org/knora/webapi/SettingsSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/SettingsSpec.scala
@@ -47,24 +47,24 @@ class SettingsSpec extends CoreSpec("SettingsActorTestSystem", SettingsSpec.conf
             settings.externalKnoraApiBaseUrl should be ("http://0.0.0.0:3333")
 
             settings.internalSipiProtocol should be ("http")
-            settings.internalSipiHost should be ("localhost")
+            settings.internalSipiHost should be ("0.0.0.0")
             settings.internalSipiPort should be (1024)
-            settings.internalSipiBaseUrl should be ("http://localhost:1024")
+            settings.internalSipiBaseUrl should be ("http://0.0.0.0:1024")
 
             settings.externalSipiProtocol should be ("http")
-            settings.externalSipiHost should be ("localhost")
+            settings.externalSipiHost should be ("0.0.0.0")
             settings.externalSipiPort should be (1024)
-            settings.externalSipiBaseUrl should be ("http://localhost:1024")
+            settings.externalSipiBaseUrl should be ("http://0.0.0.0:1024")
 
             settings.sipiPrefix should be ("knora")
             settings.sipiFileServerPrefix should be ("server")
 
-            settings.externalSipiIIIFGetUrl should be ("http://localhost:1024/knora")
+            settings.externalSipiIIIFGetUrl should be ("http://0.0.0.0:1024/knora")
 
-            settings.internalSipiFileServerGetUrl should be ("http://localhost:1024/server/knora")
-            settings.externalSipiFileServerGetUrl should be ("http://localhost:1024/server/knora")
+            settings.internalSipiFileServerGetUrl should be ("http://0.0.0.0:1024/server/knora")
+            settings.externalSipiFileServerGetUrl should be ("http://0.0.0.0:1024/server/knora")
 
-            settings.internalSipiImageConversionUrlV1 should be ("http://localhost:1024")
+            settings.internalSipiImageConversionUrlV1 should be ("http://0.0.0.0:1024")
 
             settings.prometheusReporter should be (false)
             settings.zipkinReporter should be (false)
