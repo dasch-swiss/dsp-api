78c8181850c3d119cba728da60891fc287c4531c
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-03-28 10:08:27 +0100

    feat(upgrade): Upgrade repository (ongoing).

--- upgrade/1263-knora-admin/update-knora-admin.py
+++ upgrade/1263-knora-admin/update-knora-admin.py
@@ -28,8 +28,12 @@ import argparse
 import getpass
 import rdflib
 from rdflib.namespace import RDF
-import io
-from string import Template
+
+
+class UpdateException(Exception):
+    def __init__(self, message):
+        self.message = message
+
 
 # The property types used in knora-admin.
 property_types = {
@@ -38,62 +42,38 @@ property_types = {
     "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"
 }
 
-pred_obj_pairs = [
-    ("belongsToProject", "DefaultSharedOntologiesProject"),
-    ("belongsToProject", "SystemProject"),
-    ("currentproject", "DefaultSharedOntologiesProject"),
-    ("currentproject", "SystemProject"),
-    ("forGroup", "Creator"),
-    ("forGroup", "KnownUser"),
-    ("forGroup", "ProjectAdmin"),
-    ("forGroup", "ProjectMember"),
-    ("forGroup", "SystemAdmin"),
-    ("forGroup", "UnknownUser"),
-    ("forProject", "DefaultSharedOntologiesProject"),
-    ("forProject", "SystemProject"),
-    ("isInGroup", "Creator"),
-    ("isInGroup", "KnownUser"),
-    ("isInGroup", "ProjectAdmin"),
-    ("isInGroup", "ProjectMember"),
-    ("isInGroup", "SystemAdmin"),
-    ("isInGroup", "UnknownUser"),
-    ("isInProject", "DefaultSharedOntologiesProject"),
-    ("isInProject", "SystemProject"),
-    ("isInProjectAdminGroup", "DefaultSharedOntologiesProject"),
-    ("isInProjectAdminGroup", "SystemProject")
-]
+knora_base_context = "http://www.knora.org/ontology/knora-base"
+knora_base_namespace = knora_base_context + "#"
+knora_admin_context = "http://www.knora.org/ontology/knora-admin"
+knora_admin_namespace = knora_admin_context + "#"
+
+knora_admin_properties = []
+knora_admin_objects = []
 
 
-# Makes a request to GraphDB to update the repository.
-def do_request(graphdb_url, username, password):
+def split_namespace_and_local_name(entity):
+    entity_str = str(entity)
+    hash_pos = entity_str.rfind("#")
+
+    if hash_pos != -1:
+        return entity_str[:hash_pos], entity_str[hash_pos + 1:len(entity_str)]
+    else:
+        return entity_str, None
+
+
+# Read knora-base and knora-admin.
+def get_knora_admin_info():
     # Read knora-admin.
     knora_admin_graph = rdflib.Graph()
     knora_admin_graph.parse("../../knora-ontologies/knora-admin.ttl", format="turtle")
-    knora_admin_nt = knora_ontology_to_ntriples(knora_admin_graph)
-
-    # Read knora-base.
-    knora_base_graph = rdflib.Graph()
-    knora_base_graph.parse("../../knora-ontologies/knora-base.ttl", format="turtle")
-    knora_base_nt = knora_ontology_to_ntriples(knora_base_graph)
-
-    # From knora-admin, collect the property IRIs, and the IRIs of non-properties that can be used as objects in data.
-
-    knora_admin_properties = []
-    knora_admin_objects = []
 
     # Iterate over all the statements in knora-admin.
     for subject, predicate, obj in knora_admin_graph:
-        subject_str = str(subject)
-
         # Is this a statement about an ontology entity?
+        subj_namespace, subj_name = split_namespace_and_local_name(subject)
 
-        hash_pos = subject_str.rfind("#")
-
-        if hash_pos != -1:
-            # Yes. Get the local name of the subject.
-            subj_name = subject_str[hash_pos + 1:len(subject_str)]
-
-            # Is the predicate is rdf:type?
+        if subj_name is not None:
+            # Is the predicate rdf:type?
             if predicate == RDF.type:
                 # Yes. Is the object a property type?
                 if str(obj) in property_types:
@@ -104,56 +84,38 @@ def do_request(graphdb_url, username, password):
                     # Collect the subject as a non-property IRI that can be used as an object in data.
                     knora_admin_objects.append(subj_name)
 
-    # Use those IRIs to generate VALUES.
 
-    predicates = list(map(lambda knora_admin_prop:
-                          "(knora-base:{prop} knora-admin:{prop})".format(prop=knora_admin_prop),
-                          knora_admin_properties))
-    predicates_str = " ".join(predicates)
+def download_repository(graphdb_url, username, password):
+    contexts_response = requests.get(graphdb_url + "/contexts", params={"infer": "false"})
+    contexts_response_list = contexts_response.text.splitlines()
 
-    objects = list(map(lambda knora_admin_obj:
-                       "(knora-base:{obj} knora-admin:{obj})".format(obj=knora_admin_obj),
-                       knora_admin_objects))
-    objects_str = " ".join(objects)
+    if contexts_response_list[0] != "contextID":
+        raise UpdateException("Unexpected response from GraphDB: " + contexts_response.text)
 
-    preds_with_objs = list(map(lambda pred_and_obj:
-                               "(knora-base:{prop} knora-base:{obj} knora-admin:{prop} knora-admin:{obj})".format(
-                                   prop=pred_and_obj[0], obj=pred_and_obj[1]), pred_obj_pairs))
-    preds_with_objs_str = " ".join(preds_with_objs)
+    contexts = contexts_response_list.pop(0)
 
-    # Generate SPARQL using the main template file.
-
-    with open("sparql/update-knora-admin.rq.tmpl", 'r') as sparql_template_file:
-        sparql_template = Template(sparql_template_file.read())
-
-        template_dict = {
-            "knoraAdmin": knora_admin_nt,
-            "knoraBase": knora_base_nt,
-            "predicates": predicates_str,
-            "objects": objects_str,
-            "preds_with_objs": preds_with_objs_str
-        }
-
-        sparql = sparql_template.substitute(template_dict)
-
-        with open("/Users/benjamingeer/Desktop/sparql.rq", "w") as sparql_file:
-            sparql_file.write(sparql)
-
-        # Post the SPARQL to the triplestore.
-
-        data = {
-            "update": sparql
-        }
-
-        # r = requests.post(graphdb_url, data=data, auth=(username, password))
-        # r.raise_for_status()
+    for context in contexts:
+        print(context)
 
 
-# Reads an ontology from knora-ontologies and returns it in ntriples format.
-def knora_ontology_to_ntriples(graph):
-    nt_bytes = io.BytesIO()
-    graph.serialize(nt_bytes, format="ntriples")
-    return nt_bytes.getvalue().decode("UTF-8")
+def transform_entity(entity, local_name_list):
+    if entity.__class__.__name__ == "URIRef":
+        namespace, local_name = split_namespace_and_local_name(entity)
+
+        if namespace == knora_base_namespace and local_name in local_name_list:
+            return rdflib.URIRef(knora_admin_namespace + local_name)
+        else:
+            return entity
+    else:
+        return entity
+
+
+def update_repository(graphdb_url, username, password):
+    download_repository(
+        graphdb_url=graphdb_url,
+        username=args.username,
+        password=password
+    )
 
 
 # Command-line invocation.
@@ -180,13 +142,14 @@ def main():
     if not repository:
         repository = defaut_repository
 
-    graphdb_url = "http://{}:7200/repositories/{}/statements".format(graphdb_host, repository)
+    graphdb_url = "http://{}:7200/repositories/{}".format(graphdb_host, repository)
+
     password = args.password
 
     if not password:
         password = getpass.getpass()
 
-    do_request(
+    update_repository(
         graphdb_url=graphdb_url,
         username=args.username,
         password=password
