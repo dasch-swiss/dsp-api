e1671a0da6d1c0eb71f7ee88773b58c499ba078d
Tobias Schweizer <t.schweizer@unibas.ch>
2019-05-10 10:17:17 +0200

    test (gravsearch): add test for lits node in complex schema

--- webapi/src/test/scala/org/knora/webapi/e2e/v2/SearchRouteV2R2RSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e/v2/SearchRouteV2R2RSpec.scala
@@ -7356,6 +7356,38 @@ class SearchRouteV2R2RSpec extends R2RSpec {
             }
         }
 
+        "search for a list value that does not refer to a particular list node (submitting the complex schema)" in {
+            val gravsearchQuery =
+                """
+                  |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+                  |PREFIX anything: <http://0.0.0.0:3333/ontology/0001/anything/v2#>
+                  |
+                  |    CONSTRUCT {
+                  |        ?thing knora-api:isMainResource true .
+                  |
+                  |        ?thing anything:hasListItem ?listItem .
+                  |
+                  |    } WHERE {
+                  |        ?thing anything:hasListItem ?listItem .
+                  |
+                  |        FILTER NOT EXISTS {
+                  |
+                  |         ?listItem knora-api:listValueAsListNode <http://rdfh.ch/lists/0001/treeList02> .
+                  |
+                  |        }
+                  |
+                  |    }
+                """.stripMargin
+
+            Post("/v2/searchextended", HttpEntity(SparqlQueryConstants.`application/sparql-query`, gravsearchQuery)) ~> addCredentials(BasicHttpCredentials(incunabulaUserEmail, password)) ~> searchPath ~> check {
+
+                assert(status == StatusCodes.OK, response.toString)
+
+                val expectedAnswerJSONLD = readOrWriteTextFile(responseAs[String], new File("src/test/resources/test-data/searchR2RV2/thingNotReferringToSpecificListNode.jsonld"), writeTestDataFiles)
+                compareJSONLDForResourcesResponse(expectedJSONLD = expectedAnswerJSONLD, receivedJSONLD = responseAs[String])
+            }
+        }
+
         "search for a list value that refers to a particular list node that has subnodes (submitting the complex schema)" in {
             val gravsearchQuery =
                 """
