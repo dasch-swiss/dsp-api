c77fce780dd293bbc73329b319cf191c9be21271
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-10-02 16:05:18 +0200

    feat(update): Implement update selection algorithm.

--- webapi/src/main/scala/org/knora/webapi/update/UpdateRepository.scala
+++ webapi/src/main/scala/org/knora/webapi/update/UpdateRepository.scala
@@ -21,44 +21,217 @@ package org.knora.webapi.update
 
 import java.io._
 
-import org.eclipse.rdf4j.model.impl.LinkedHashModel
+import org.eclipse.rdf4j.model.impl.{LinkedHashModel, SimpleValueFactory}
+import org.eclipse.rdf4j.model.util.Models
+import org.eclipse.rdf4j.model.{Model, Statement}
 import org.eclipse.rdf4j.rio.helpers.StatementCollector
 import org.eclipse.rdf4j.rio.{RDFFormat, RDFParser, Rio}
+import org.knora.webapi.update.plugins._
+import org.knora.webapi.util.JavaUtil._
+import org.knora.webapi.{InconsistentTriplestoreDataException, OntologyConstants}
 import org.rogach.scallop._
+
+import scala.collection.JavaConverters._
 
 
 /**
-  * Updates a Knora repository to accommodate changes in Knora.
+  * Updates a dump of a Knora repository to accommodate changes in Knora.
   */
 object UpdateRepository extends App {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private val conf = new TransformDataConf(args)
-    private val inputFile = new File(conf.input())
-    private val outputFile = new File(conf.output())
+    /**
+      * A list of all plugins in chronological order.
+      */
+    val pluginsForVersions: Seq[PluginForKnoraBaseVersion] = Seq(
+        PluginForKnoraBaseVersion(versionNumber = 1, plugin = new UpdatePluginPR1307, prBasedVersionString = Some("PR 1307")),
+        PluginForKnoraBaseVersion(versionNumber = 2, plugin = new NoopPlugin, prBasedVersionString = Some("PR 1322")), // TODO
+        PluginForKnoraBaseVersion(versionNumber = 3, plugin = new NoopPlugin, prBasedVersionString = Some("PR 1367")), // TODO
+        PluginForKnoraBaseVersion(versionNumber = 4, plugin = new NoopPlugin, prBasedVersionString = Some("PR 1372")), // TODO
+        PluginForKnoraBaseVersion(versionNumber = 5, plugin = new NoopPlugin, prBasedVersionString = Some("PR 1440")),
+    )
 
-    private val trigParser: RDFParser = Rio.createParser(RDFFormat.TRIG)
-    private val fileReader = new BufferedReader(new FileReader(inputFile))
-    private val model = new LinkedHashModel()
-    trigParser.setRDFHandler(new StatementCollector(model))
+    /**
+      * The built-in named graphs that are always updated when there is a new version of knora-base.
+      */
+    val builtInNamedGraphs: Set[BuiltInNamedGraph] = Set(
+        BuiltInNamedGraph(
+            filename = "knora-admin.ttl",
+            iri = "http://www.knora.org/ontology/knora-admin"
+        ),
+        BuiltInNamedGraph(
+            filename = "knora-base.ttl",
+            iri = "http://www.knora.org/ontology/knora-base"
+        ),
+        BuiltInNamedGraph(
+            filename = "salsah-gui.ttl",
+            iri = "http://www.knora.org/ontology/salsah-gui"
+        ),
+        BuiltInNamedGraph(
+            filename = "standoff-onto.ttl",
+            iri = "http://www.knora.org/ontology/standoff"
+        ),
+        BuiltInNamedGraph(
+            filename = "standoff-data.ttl",
+            iri = "http://www.knora.org/data/standoff"
+        )
+    )
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+      * Represents an update plugin with its knora-base version number and version string.
+      *
+      * @param versionNumber        the knora-base version number that the plugin's transformation produces.
+      * @param plugin               the plugin.
+      * @param prBasedVersionString the plugin's PR-based version string (not used for new plugins).
+      */
+    case class PluginForKnoraBaseVersion(versionNumber: Int, plugin: UpdatePlugin, prBasedVersionString: Option[String] = None) {
+        lazy val versionString: String = {
+            prBasedVersionString match {
+                case Some(str) => str
+                case None => s"knora-base v$versionNumber"
+            }
+        }
+    }
+
+    /**
+      * Represents a Knora built-in named graph.
+      *
+      * @param filename the filename containing the named graph.
+      * @param iri      the IRI of the named graph.
+      */
+    case class BuiltInNamedGraph(filename: String, iri: String)
+
+    /**
+      * Constructs RDF4J values.
+      */
+    val valueFactory = SimpleValueFactory.getInstance
+
+    /**
+      * A map of version strings to plugins.
+      */
+    val pluginsForVersionsMap: Map[String, PluginForKnoraBaseVersion] = pluginsForVersions.map {
+        knoraBaseVersion => knoraBaseVersion.versionString -> knoraBaseVersion
+    }.toMap
+
+    // Parse the command-line arguments.
+    val conf = new TransformDataConf(args)
+    val inputFile = new File(conf.input())
+    val outputFile = new File(conf.output())
+
+    val updateStartTime = System.currentTimeMillis()
+
+    // Parse the input file.
     val parseStartTime = System.currentTimeMillis()
-    trigParser.parse(fileReader, "")
+    val model = readFileIntoModel(inputFile, RDFFormat.TRIG)
     val parseEndTime = System.currentTimeMillis()
-
     println(s"Parsed ${model.size} statements in ${parseEndTime - parseStartTime} ms")
 
-    private val fileWriter = new BufferedWriter(new FileWriter(outputFile))
-    Rio.write(model, fileWriter, RDFFormat.TRIG)
+    // Get the repository's version string, if any.
+    val maybeRepositoryVersionString: Option[String] = Models.getPropertyLiteral(
+        model,
+        valueFactory.createIRI(OntologyConstants.KnoraBase.KnoraBaseOntologyIri),
+        valueFactory.createIRI(OntologyConstants.KnoraBase.OntologyVersion)
+    ).toOption.map(_.stringValue)
 
-    println("Wrote output file")
+    // Is the repository up to date?
+    if (maybeRepositoryVersionString.contains(org.knora.webapi.KnoraBaseVersion)) {
+        // Yes. Nothing more to do.
+        println(s"Repository is up to date at version ${org.knora.webapi.KnoraBaseVersion}.")
+    } else {
+        // No. Construct the list of updates that it needs.
+        val pluginsForNeededUpdates: Seq[PluginForKnoraBaseVersion] = maybeRepositoryVersionString match {
+            case Some(repositoryVersion) =>
+                // The repository has a version string. Get the plugins for all subsequent versions.
+                val pluginForRepositoryVersion: PluginForKnoraBaseVersion = pluginsForVersionsMap.getOrElse(
+                    repositoryVersion,
+                    throw InconsistentTriplestoreDataException(s"No such repository version $repositoryVersion")
+                )
+
+                pluginsForVersions.filter(_.versionNumber > pluginForRepositoryVersion.versionNumber)
+
+            case None =>
+                // The repository has no version string. Include all updates.
+                pluginsForVersions
+        }
+
+        println(s"Needed updates: ${pluginsForNeededUpdates.map(_.versionString).mkString(", ")}")
+
+        // Run the update plugins.
+        for (pluginForNeededUpdate <- pluginsForNeededUpdates) {
+            println(s"Running update for ${pluginForNeededUpdate.versionString}...")
+            pluginForNeededUpdate.plugin.transform(model)
+        }
+
+        // Update the built-in named graphs.
+
+        println("Updating built-in named graphs...")
+
+        for (builtInNamedGraph <- builtInNamedGraphs) {
+            println(s"<${builtInNamedGraph.iri}>...")
+            val context = valueFactory.createIRI(builtInNamedGraph.iri)
+            model.removeAll(model.filter(null, null, null, context))
+
+            val namedGraphTurtleFile = new File(s"../knora-ontologies/${builtInNamedGraph.filename}")
+            val namedGraphModel: Model = readFileIntoModel(namedGraphTurtleFile, RDFFormat.TURTLE)
+
+            // Set the context on each statement.
+            for (statement: Statement <- namedGraphModel.asScala.toSet) {
+                namedGraphModel.remove(
+                    statement.getSubject,
+                    statement.getPredicate,
+                    statement.getObject,
+                    statement.getContext
+                )
+
+                namedGraphModel.add(
+                    statement.getSubject,
+                    statement.getPredicate,
+                    statement.getObject,
+                    context
+                )
+            }
+
+            model.addAll(namedGraphModel)
+        }
+
+        // Write the output file.
+        println("Writing output file...")
+        val fileWriter = new FileWriter(outputFile)
+        val bufferedWriter = new BufferedWriter(fileWriter)
+        Rio.write(model, fileWriter, RDFFormat.TRIG)
+        bufferedWriter.close()
+        fileWriter.close()
+
+        val updateEndTime = System.currentTimeMillis
+        println(s"Update completed in ${updateEndTime - updateStartTime} ms.")
+    }
+
+    /**
+      * Reads an RDF file into a [[Model]].
+      *
+      * @param file   the file.
+      * @param format the file format.
+      * @return a [[Model]] representing the contents of the file.
+      */
+    def readFileIntoModel(file: File, format: RDFFormat): Model = {
+        val trigParser: RDFParser = Rio.createParser(format)
+        val fileReader = new FileReader(file)
+        val bufferedReader = new BufferedReader(fileReader)
+        val model = new LinkedHashModel()
+        trigParser.setRDFHandler(new StatementCollector(model))
+        trigParser.parse(bufferedReader, "")
+        model
+    }
 
     /**
       * Parses command-line arguments.
       */
-    private class TransformDataConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+    class TransformDataConf(arguments: Seq[String]) extends ScallopConf(arguments) {
         banner(
             s"""
-               |Updates the structure of Knora repository data to accommodate changes in Knora.
+               |Updates a dump of a repository to accommodate changes in Knora.
                |
                |Usage: org.knora.webapi.util.UpdateRepository input output
             """.stripMargin)
