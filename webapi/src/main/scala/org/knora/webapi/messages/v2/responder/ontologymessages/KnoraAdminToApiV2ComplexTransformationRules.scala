8f348a21c161b515a478c621bba7d6a343ec5951
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-10-21 11:32:52 +0200

    fix(clientapi): Fix bugs.

--- webapi/src/main/scala/org/knora/webapi/messages/v2/responder/ontologymessages/KnoraAdminToApiV2ComplexTransformationRules.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/v2/responder/ontologymessages/KnoraAdminToApiV2ComplexTransformationRules.scala
@@ -478,19 +478,19 @@ object KnoraAdminToApiV2ComplexTransformationRules extends OntologyTransformatio
         )
     )
 
-    private val BasicUserInfo = makeClass(
-        classIri = OntologyConstants.KnoraAdminV2.BasicUserInfo,
+    private val UpdateUserRequest = makeClass(
+        classIri = OntologyConstants.KnoraAdminV2.UpdateUserRequest,
         predicates = Seq(
             makePredicate(
                 predicateIri = OntologyConstants.Rdfs.Label,
                 objectsWithLang = Map(
-                    LanguageCodes.EN -> "basic user info"
+                    LanguageCodes.EN -> "update user request"
                 )
             ),
             makePredicate(
                 predicateIri = OntologyConstants.Rdfs.Comment,
                 objectsWithLang = Map(
-                    LanguageCodes.EN -> "Basic information about a user."
+                    LanguageCodes.EN -> "A request to update a user."
                 )
             )
         ),
@@ -926,9 +926,61 @@ object KnoraAdminToApiV2ComplexTransformationRules extends OntologyTransformatio
         ),
         directCardinalities = Map(
             OntologyConstants.KnoraAdminV2.Name -> Cardinality.MustHaveOne,
-            OntologyConstants.KnoraAdminV2.Status -> Cardinality.MustHaveOne,
+            OntologyConstants.KnoraAdminV2.GroupDescription -> Cardinality.MayHaveOne,
             OntologyConstants.KnoraAdminV2.ProjectIri -> Cardinality.MustHaveOne,
+            OntologyConstants.KnoraAdminV2.Status -> Cardinality.MustHaveOne,
             OntologyConstants.KnoraAdminV2.SelfJoin -> Cardinality.MustHaveOne
+        )
+    )
+
+    private val UpdateGroupRequest = makeClass(
+        classIri = OntologyConstants.KnoraAdminV2.UpdateGroupRequest,
+        predicates = Seq(
+            makePredicate(
+                predicateIri = OntologyConstants.Rdfs.Label,
+                objectsWithLang = Map(
+                    LanguageCodes.EN -> "update group"
+                )
+            ),
+            makePredicate(
+                predicateIri = OntologyConstants.Rdfs.Comment,
+                objectsWithLang = Map(
+                    LanguageCodes.EN -> "A request to update a group."
+                )
+            )
+        ),
+        directCardinalities = Map(
+            OntologyConstants.KnoraAdminV2.Name -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.GroupDescription -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.Status -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.SelfJoin -> Cardinality.MayHaveOne
+        )
+    )
+
+    private val UpdateProjectRequest = makeClass(
+        classIri = OntologyConstants.KnoraAdminV2.UpdateProjectRequest,
+        predicates = Seq(
+            makePredicate(
+                predicateIri = OntologyConstants.Rdfs.Label,
+                objectsWithLang = Map(
+                    LanguageCodes.EN -> "update project"
+                )
+            ),
+            makePredicate(
+                predicateIri = OntologyConstants.Rdfs.Comment,
+                objectsWithLang = Map(
+                    LanguageCodes.EN -> "A request to update a project."
+                )
+            )
+        ),
+        directCardinalities = Map(
+            OntologyConstants.KnoraAdminV2.Shortname -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.Longname -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.ProjectDescription -> Cardinality.MayHaveMany,
+            OntologyConstants.KnoraAdminV2.KeywordsProperty -> Cardinality.MayHaveMany,
+            OntologyConstants.KnoraAdminV2.Logo -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.Status -> Cardinality.MayHaveOne,
+            OntologyConstants.KnoraAdminV2.SelfJoin -> Cardinality.MayHaveOne
         )
     )
 
@@ -1072,14 +1124,16 @@ object KnoraAdminToApiV2ComplexTransformationRules extends OntologyTransformatio
       * Classes that need to be added to the ontology after converting it to the target schema.
       */
     override val externalClassesToAdd: Map[SmartIri, ReadClassInfoV2] = Set(
-        BasicUserInfo,
+        UpdateUserRequest,
         UsersResponse,
         UserResponse,
         ProjectsResponse,
         ProjectResponse,
+        UpdateProjectRequest,
         GroupsResponse,
         GroupResponse,
         CreateGroupRequest,
+        UpdateGroupRequest,
         AdministrativePermissionsResponse,
         AdministrativePermissionResponse,
         AdministrativePermissionClass,
--- webapi/src/main/scala/org/knora/webapi/routing/admin/UsersRouteADM.scala
+++ webapi/src/main/scala/org/knora/webapi/routing/admin/UsersRouteADM.scala
@@ -85,7 +85,7 @@ class UsersRouteADM(routeData: KnoraRouteData) extends KnoraRoute(routeData) wit
     private val ProjectsResponse = classRef(OntologyConstants.KnoraAdminV2.ProjectsResponse.toSmartIri)
     private val GroupsResponse = classRef(OntologyConstants.KnoraAdminV2.GroupsResponse.toSmartIri)
     private val User = classRef(OntologyConstants.KnoraAdminV2.UserClass.toSmartIri)
-    private val BasicUserInfo = classRef(OntologyConstants.KnoraAdminV2.BasicUserInfo.toSmartIri)
+    private val UpdateUserRequest = classRef(OntologyConstants.KnoraAdminV2.UpdateUserRequest.toSmartIri)
     private val StoredUser = User.toStoredClassRef
 
     private val anythingUser1IriEnc = URLEncoder.encode(SharedTestDataADM.anythingUser1.id, "UTF-8")
@@ -334,7 +334,7 @@ class UsersRouteADM(routeData: KnoraRouteData) extends KnoraRoute(routeData) wit
     private val updateUserBasicInformationFunction: ClientFunction =
         "updateUserBasicInformation" description "Updates an existing user's basic information." params (
             "iri" description "The IRI of the user to be updated." paramType UriDatatype,
-            "userInfo" description "The user information to be updated." paramType BasicUserInfo
+            "userInfo" description "The user information to be updated." paramType UpdateUserRequest
             ) doThis {
             httpPut(
                 path = str("iri") / arg("iri") / str("BasicUserInformation"),
@@ -392,13 +392,13 @@ class UsersRouteADM(routeData: KnoraRouteData) extends KnoraRoute(routeData) wit
     private val updateUserPasswordFunction: ClientFunction =
         "updateUserPassword" description "Updates a user's password." params(
             "iri" description "The IRI of the user to be updated." paramType UriDatatype,
-            "oldPassword" description "The user's old password." paramType StringDatatype,
-            "newPassword" description "The user's new password." paramType StringDatatype
+            "requesterPassword" description "The requesting user's current password." paramType StringDatatype,
+            "newPassword" description "The specified user's new password." paramType StringDatatype
         ) doThis {
             httpPut(
                 path = str("iri") / arg("iri") / str("Password"),
                 body = Some(json(
-                    "requesterPassword" -> arg("oldPassword"),
+                    "requesterPassword" -> arg("requesterPassword"),
                     "newPassword" -> arg("newPassword")
                 ))
             )
