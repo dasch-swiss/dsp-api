27ebffb09f41d3ad6eadb71e34269c36a727a43f
Ivan Subotic <400790+subotic@users.noreply.github.com>
2019-11-20 16:44:16 +0100

    build: Update license

--- .github/scripts/decrypt_secrets.sh
+++ .github/scripts/decrypt_secrets.sh
@@ -2,4 +2,4 @@
 
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase=$SECRETS_PASSPHRASE --output $GITHUB_WORKSPACE/ci/secrets.tar $GITHUB_WORKSPACE/ci/secrets.tar.gpg
+gpg --quiet --batch --yes --decrypt --passphrase "$SECRETS_PASSPHRASE" --output "$GITHUB_WORKSPACE/ci/secrets.tar" "$GITHUB_WORKSPACE/ci/secrets.tar.gpg"
