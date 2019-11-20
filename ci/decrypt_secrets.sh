a798fe30b5ae7bc74e6b7a033dca22734ff931c4
Ivan Subotic <400790+subotic@users.noreply.github.com>
2019-11-20 15:16:11 +0100

    build: Update license

--- ci/decrypt_secrets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$license_encryption_key" \
---output $GITHUB_WORKSPACE/ci/secrets.tar $GITHUB_WORKSPACE/ci/secrets.tar.gpg
