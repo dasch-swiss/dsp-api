8eb21d9dceb2a29f9f18d66af54fb4260a511e8c
Ivan Subotic <400790+subotic@users.noreply.github.com>
2019-12-19 10:26:17 +0100

    build: add tests (ongoing)

--- bd.sh
+++ bd.sh
@@ -1,6 +1,6 @@
-buildozer 'remove deps @maven//:ch_qos_logback_logback_core' //webapi:library
-buildozer 'remove deps @maven//:ch_qos_logback_logback_classic' //webapi:library
-buildozer 'remove deps @maven//:com_typesafe_akka_akka_slf4j_2_12' //webapi:library
+buildozer 'remove deps @maven//:ch_qos_logback_logback_core' //webapi:main_library
+buildozer 'remove deps @maven//:ch_qos_logback_logback_classic' //webapi:main_library
+buildozer 'remove deps @maven//:com_typesafe_akka_akka_slf4j_2_12' //webapi:main_library
 buildozer 'remove deps @maven//:org_slf4j_slf4j_api' //webapi/src/it/scala/org/knora/webapi/e2e:VersionRouteITSpec
 buildozer 'remove deps @maven//:com_typesafe_akka_akka_testkit_2_12' //webapi/src/it/scala/org/knora/webapi/e2e:VersionRouteITSpec
 buildozer 'remove deps @maven//:com_typesafe_akka_akka_http_testkit_2_12' //webapi/src/it/scala/org/knora/webapi/e2e:VersionRouteITSpec
