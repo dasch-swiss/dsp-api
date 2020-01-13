b391feff6b0a658c14d57cea51282d134cfe03ab
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-01-13 10:13:58 +0100

    build (make): fix dockerhost on linux

--- vars.mk
+++ vars.mk
@@ -33,5 +33,5 @@ UNAME := $(shell uname)
 ifeq ($(UNAME),Darwin)
   DOCKERHOST :=  $(shell ifconfig en0 | grep inet | grep -v inet6 | cut -d ' ' -f2)
 else
-  DOCKERHOST := $(shell ip route get 8.8.8.8 | awk '{print $NF; exit}')
+  DOCKERHOST := $(shell ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
 endif
