3f8d406b0f44afc2d6928a44e103d613031c3f48
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-07-03 00:51:26 +0200

    fix: loading of data (DSP-445) (#1669)

--- webapi/scripts/fuseki-init-knora-test-unit.sh
+++ webapi/scripts/fuseki-init-knora-test-unit.sh
@@ -1,89 +1,10 @@
 #!/usr/bin/env bash
 
-#set -x
+# Including fuseki-funcions.sh implementing delete, create, and upload.
+source fuseki-functions.sh
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]; do
-  key="$1"
-
-  case $key in
-  -u | --username)
-    USERNAME="$2"
-    shift # past argument
-    shift # past value
-    ;;
-  -p | --password)
-    PASSWORD="$2"
-    shift # past argument
-    shift # past value
-    ;;
-  -h | --host)
-    HOST="$2"
-    shift # past argument
-    shift # past value
-    ;;
-  *) # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift              # past argument
-    ;;
-  esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
-
-FILE="$1"
-
+# Name of the repository
 REPOSITORY="knora-test-unit"
-
-if [[ -z "${HOST}" ]]; then
-  HOST="localhost:3030"
-fi
-
-if [[ -z "${USERNAME}" ]]; then
-  USERNAME="admin"
-fi
-
-if [[ -z "${PASSWORD}" ]]; then
-  PASSWORD="test"
-fi
-
-delete-repository() {
-  STATUS=$(curl -s -o /dev/null -w '%{http_code}' -u ${USERNAME}:${PASSWORD} -X DELETE http://${HOST}/\$/datasets/${REPOSITORY})
-
-  if [ "${STATUS}" -eq 200 ]; then
-    echo "==> delete repository done"
-    return 0
-  else
-    echo "==> delete repository failed"
-    return 1
-  fi
-}
-
-create-repository() {
-  STATUS=$(curl -s -o /dev/null -w '%{http_code}' -u ${USERNAME}:${PASSWORD} -F data=@./fuseki-knora-test-unit-repository-config.ttl http://${HOST}/\$/datasets)
-
-  if [ "${STATUS}" -eq 200 ]; then
-    echo "==> create repository done"
-    return 0
-  else
-    echo "==> create repository failed"
-    return 1
-  fi
-}
-
-upload-graph() {
-  STATUS=$(curl -s -o /dev/null -w '%{http_code}' -u ${USERNAME}:${PASSWORD} -H "Content-Type:text/turtle" -d @$1 -X PUT http://${HOST}/${REPOSITORY}\?graph\="$2")
-
-  if [ "${STATUS}" -eq 201 ]; then
-    echo "==> 201 Created: $1 -> $2"
-    return 0
-  elif [ "${STATUS}" -eq 200 ]; then
-    echo "==> 200 OK: $1 -> $2"
-    return 0
-  else
-    echo "==> failed with status code ${STATUS}: $1 -> $2"
-    return 1
-  fi
-}
 
 # delete-repository // delete dos not work correctly. need to delete database manually.
 create-repository
@@ -102,6 +23,7 @@ upload-graph ../_test_data/ontologies/images-onto.ttl http://www.knora.org/ontol
 upload-graph ../_test_data/demo_data/images-demo-data.ttl http://www.knora.org/data/00FF/images
 upload-graph ../_test_data/ontologies/anything-onto.ttl http://www.knora.org/ontology/0001/anything
 upload-graph ../_test_data/all_data/anything-data.ttl http://www.knora.org/data/0001/anything
+upload-graph ../_test_data/ontologies/minimal-onto.ttl http://www.knora.org/ontology/0001/minimal
 upload-graph ../_test_data/ontologies/something-onto.ttl http://www.knora.org/ontology/0001/something
 upload-graph ../_test_data/ontologies/beol-onto.ttl http://www.knora.org/ontology/0801/beol
 upload-graph ../_test_data/ontologies/biblio-onto.ttl http://www.knora.org/ontology/0801/biblio
