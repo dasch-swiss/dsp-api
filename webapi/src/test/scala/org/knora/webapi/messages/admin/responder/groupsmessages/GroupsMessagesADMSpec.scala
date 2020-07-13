ed7561ab1e7d03f2176eba30a45588c4d68f6312
Sepideh Alassi <sepideh.alassi@gmail.com>
2020-07-13 18:02:26 +0200

    refactor (tests) add a test file for groupMessageADMSpec, BUILD.bazel for it, Move validation of custom IRI to it

---
+++ webapi/src/test/scala/org/knora/webapi/messages/admin/responder/groupsmessages/GroupsMessagesADMSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2015-2019 the contributors (see Contributors.md).
+ *
+ * This file is part of Knora.
+ *
+ * Knora is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knora is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.knora.webapi.messages.admin.responder.groupsmessages
+
+import com.typesafe.config.ConfigFactory
+import org.knora.webapi.{SharedTestDataADM, _}
+
+object GroupsMessagesADMSpec {
+    val config = ConfigFactory.parseString(
+        """
+              akka.loglevel = "DEBUG"
+              akka.stdout-loglevel = "DEBUG"
+            """.stripMargin)
+}
+
+/**
+ * This spec is used to test 'ListAdminMessages'.
+ */
+class ProjectsMessagesADMSpec extends CoreSpec(GroupsMessagesADMSpec.config) {
+
+    "The CreateGroupsApiRequestADM case class" should {
+
+        "return 'BadRequest' if the supplied 'id' is not a valid IRI" in {
+
+            val caught = intercept[BadRequestException](
+                CreateGroupApiRequestADM(
+                  id = Some("invalid-group-IRI"),
+                  name = "NewGroupWithInvalidCustomIri",
+                  description = Some("A new group with an invalid custom Iri"),
+                  project = SharedTestDataADM.IMAGES_PROJECT_IRI,
+                  status = true,
+                  selfjoin = false
+                )
+            )
+            assert(caught.getMessage === "Invalid group IRI")
+        }
+    }
+}
