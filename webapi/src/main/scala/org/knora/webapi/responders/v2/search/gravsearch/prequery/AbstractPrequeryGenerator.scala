6dfd9fc9fc5635b7c406600a4713e7a15e821551
Sepideh Alassi <sepideh.alassi@gmail.com>
2020-07-16 11:27:28 +0200

    fix the pattern matching problem

--- webapi/src/main/scala/org/knora/webapi/responders/v2/search/gravsearch/prequery/AbstractPrequeryGenerator.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/v2/search/gravsearch/prequery/AbstractPrequeryGenerator.scala
@@ -280,7 +280,7 @@ abstract class AbstractPrequeryGenerator(constructClause: ConstructClause,
         }
 
         val maybeSubjectTypeIri: Option[SmartIri] = typeInspectionResult.getTypeOfEntity(statementPattern.subj) match {
-            case Some(nonPropertyTypeInfo: NonPropertyTypeInfo) => Some(nonPropertyTypeInfo.typeIri)
+            case Some(NonPropertyTypeInfo(subjectTypeIri, _, _)) => Some(subjectTypeIri)
             case _ => None
         }
 
@@ -1513,7 +1513,7 @@ abstract class AbstractPrequeryGenerator(constructClause: ConstructClause,
         val standoffTagVar = functionCallExpression.getArgAsQueryVar(pos = 1)
 
         typeInspectionResult.getTypeOfEntity(standoffTagVar) match {
-            case Some(nonPropertyTypeInfo: NonPropertyTypeInfo) if nonPropertyTypeInfo.typeIri.toString == OntologyConstants.KnoraApiV2Complex.StandoffTag => ()
+            case Some(NonPropertyTypeInfo(typeIri, _ , _)) if typeIri.toString == OntologyConstants.KnoraApiV2Complex.StandoffTag => ()
             case _ => throw GravsearchException(s"The second argument of ${functionIri.toSparql} must represent a knora-api:StandoffTag")
         }
 
