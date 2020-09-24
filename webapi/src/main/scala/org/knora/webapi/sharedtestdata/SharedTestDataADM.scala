7133831fa67d5c8c49d0710bfbd4f3cc70f65247
Sepideh Alassi <sepideh.alassi@gmail.com>
2020-09-24 15:01:59 +0200

    test (islamicCalendar) tests for creating property with islamic date value as single date and range

--- webapi/src/main/scala/org/knora/webapi/sharedtestdata/SharedTestDataADM.scala
+++ webapi/src/main/scala/org/knora/webapi/sharedtestdata/SharedTestDataADM.scala
@@ -1046,15 +1046,15 @@ object SharedTestDataADM {
     }
 
     def createDateValueWithDayPrecisionRequest(resourceIri: IRI,
-                                               dateValueHasCalendar: String,
-                                               dateValueHasStartYear: Int,
-                                               dateValueHasStartMonth: Int,
-                                               dateValueHasStartDay: Int,
-                                               dateValueHasStartEra: String,
-                                               dateValueHasEndYear: Int,
-                                               dateValueHasEndMonth: Int,
-                                               dateValueHasEndDay: Int,
-                                               dateValueHasEndEra: String): String = {
+        dateValueHasCalendar: String,
+        dateValueHasStartYear: Int,
+        dateValueHasStartMonth: Int,
+        dateValueHasStartDay: Int,
+        dateValueHasStartEra: String,
+        dateValueHasEndYear: Int,
+        dateValueHasEndMonth: Int,
+        dateValueHasEndDay: Int,
+        dateValueHasEndEra: String): String = {
         s"""{
            |  "@id" : "$resourceIri",
            |  "@type" : "anything:Thing",
@@ -1098,6 +1098,35 @@ object SharedTestDataADM {
            |    "knora-api:dateValueHasEndYear" : $dateValueHasEndYear,
            |    "knora-api:dateValueHasEndMonth" : $dateValueHasEndMonth,
            |    "knora-api:dateValueHasEndEra" : "$dateValueHasEndEra"
+           |  },
+           |  "@context" : {
+           |    "xsd" : "http://www.w3.org/2001/XMLSchema#",
+           |    "knora-api" : "http://api.knora.org/ontology/knora-api/v2#",
+           |    "anything" : "http://0.0.0.0:3333/ontology/0001/anything/v2#"
+           |  }
+           |}""".stripMargin
+    }
+
+    def createIslamicDateValueWithDayPrecisionRequest(resourceIri: IRI,
+                                               dateValueHasCalendar: String,
+                                               dateValueHasStartYear: Int,
+                                               dateValueHasStartMonth: Int,
+                                               dateValueHasStartDay: Int,
+                                               dateValueHasEndYear: Int,
+                                               dateValueHasEndMonth: Int,
+                                               dateValueHasEndDay: Int): String = {
+        s"""{
+           |  "@id" : "$resourceIri",
+           |  "@type" : "anything:Thing",
+           |  "anything:hasDate" : {
+           |    "@type" : "knora-api:DateValue",
+           |    "knora-api:dateValueHasCalendar" : "$dateValueHasCalendar",
+           |    "knora-api:dateValueHasStartYear" : $dateValueHasStartYear,
+           |    "knora-api:dateValueHasStartMonth" : $dateValueHasStartMonth,
+           |    "knora-api:dateValueHasStartDay" : $dateValueHasStartDay,
+           |    "knora-api:dateValueHasEndYear" : $dateValueHasEndYear,
+           |    "knora-api:dateValueHasEndMonth" : $dateValueHasEndMonth,
+           |    "knora-api:dateValueHasEndDay" : $dateValueHasEndDay,
            |  },
            |  "@context" : {
            |    "xsd" : "http://www.w3.org/2001/XMLSchema#",
