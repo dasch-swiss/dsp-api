5354bbb90b286072a98b16a28af32fb12ab075d1
Sepideh Alassi <sepideh.alassi@unibas.ch>
2020-10-28 16:08:46 +0100

    DSP-804: create a child node with a custom IRI (#1741)
    * feature (customNodeIri) create a child node with a custom IRI
    
    * refactor (customChildNodeIRI) fix typo
    
    * test (createChildNodeCustomIRI) test data
    
    * feat (customChildNodeIRI) add test data automatically to expected-client-test-data list

--- webapi/src/main/scala/org/knora/webapi/messages/admin/responder/listsmessages/ListsMessagesADM.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/admin/responder/listsmessages/ListsMessagesADM.scala
@@ -79,17 +79,22 @@ case class CreateListApiRequestADM(id: Option[IRI] = None,
  * is added can be either a root list node or a child list node. At least one label needs to be supplied. If other
  * child nodes exist, the newly created list node will be appended to the end.
  *
- * @param parentNodeIri
- * @param labels
- * @param comments
+ * @param id            the optional custom IRI of the list node.
+ * @param parentNodeIri the IRI of the parent node.
+ * @param projectIri    the IRI of the project.
+ * @param name          the optional name of the list node.
+ * @param labels        labels of the list node.
+ * @param comments      comments of the list node.
  */
-case class CreateChildNodeApiRequestADM(parentNodeIri: IRI,
+case class CreateChildNodeApiRequestADM(id: Option[IRI] = None,
+                                        parentNodeIri: IRI,
                                         projectIri: IRI,
                                         name: Option[String],
                                         labels: Seq[StringLiteralV2],
                                         comments: Seq[StringLiteralV2]) extends ListADMJsonProtocol {
 
     private val stringFormatter = StringFormatter.getInstanceForConstantOntologies
+    stringFormatter.validateOptionalListIri(id, throw BadRequestException(s"Invalid list node IRI"))
 
     if (parentNodeIri.isEmpty) {
         // println(this)
@@ -967,7 +972,7 @@ trait ListADMJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol with
 
 
     implicit val createListApiRequestADMFormat: RootJsonFormat[CreateListApiRequestADM] = jsonFormat(CreateListApiRequestADM, "id", "projectIri", "name", "labels", "comments")
-    implicit val createListNodeApiRequestADMFormat: RootJsonFormat[CreateChildNodeApiRequestADM] = jsonFormat(CreateChildNodeApiRequestADM, "parentNodeIri", "projectIri", "name", "labels", "comments")
+    implicit val createListNodeApiRequestADMFormat: RootJsonFormat[CreateChildNodeApiRequestADM] = jsonFormat(CreateChildNodeApiRequestADM, "id" , "parentNodeIri", "projectIri", "name", "labels", "comments")
     implicit val changeListInfoApiRequestADMFormat: RootJsonFormat[ChangeListInfoApiRequestADM] = jsonFormat(ChangeListInfoApiRequestADM, "listIri", "projectIri", "name", "labels", "comments")
     implicit val nodePathGetResponseADMFormat: RootJsonFormat[NodePathGetResponseADM] = jsonFormat(NodePathGetResponseADM, "elements")
     implicit val listsGetResponseADMFormat: RootJsonFormat[ListsGetResponseADM] = jsonFormat(ListsGetResponseADM, "lists")
