ca51770b361dadf88837a86fbd1301baf9333e38
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-11-02 10:21:09 +0100

    fix: server version header

--- tools/version_info/add_build_tag.sh
+++ tools/version_info/add_build_tag.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-tag=$(awk '/BUILD_SCM_TAG/ {print $2}' "$1")
-sed "s/{BUILD_TAG}/$tag/" "$2"
+# get the tag from volatile-status.txt
+EXTRACTED_TAG=$(awk '/BUILD_SCM_TAG/ {print $2}' "$1")
+
+# exchange the placeholder 'BUILD_TAG' with the 'EXTRACTED_TAG' extracted in the previous step
+sed "s/{BUILD_TAG}/$EXTRACTED_TAG/" "$2"
