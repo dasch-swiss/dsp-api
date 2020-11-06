533976c4be105f54376a7ad75809f0cee2f23a38
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2020-11-06 17:40:52 +0100

    feat: Start adding RDF API and Jena implementation.

---
+++ webapi/src/main/scala/org/knora/webapi/util/rdf/RdfModel.scala
@@ -0,0 +1,170 @@
+/*
+ * Copyright © 2015-2019 the contributors (see Contributors.md).
+ *
+ * This file is part of Knora.
+ *
+ * Knora is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knora is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.knora.webapi.util.rdf
+
+import org.knora.webapi.IRI
+
+/**
+ * Represents an RDF subject, predicate, or object.
+ */
+sealed trait RdfNode
+
+/**
+ * Represents an RDF node that can be used as a subject.
+ */
+sealed trait RdfResource extends RdfNode
+
+/**
+ * Represents a blank node.
+ */
+trait BlankNode extends RdfResource {
+    def id: String
+}
+
+/**
+ * Represents an IRI used as an RDF resource.
+ */
+trait IriNode extends RdfResource {
+    def iri: IRI
+}
+
+/**
+ * Represents an RDF literal.
+ */
+sealed trait RdfLiteral extends RdfNode
+
+/**
+ * Represents a literal value with a datatype.
+ */
+trait DatatypeLiteral extends RdfLiteral {
+    def value: String
+
+    def datatype: IRI
+}
+
+/**
+ * Represents a string value with a language tag.
+ */
+trait StringWithLanguage extends RdfLiteral {
+    def value: String
+
+    def language: String
+}
+
+/**
+ * Represents an RDF statement.
+ */
+trait Statement {
+    def subj: RdfResource
+
+    def pred: IriNode
+
+    def obj: RdfNode
+
+    def context: Option[IRI]
+}
+
+/**
+ * Represents an RDF model consisting of a default graph and/or one or more named graphs.
+ */
+trait RdfModel {
+    /**
+     * Constructs a blank node.
+     */
+    def makeBlankNode: BlankNode
+
+    /**
+     * Constructs a blank node with the specified ID.
+     *
+     * @param id the blank node ID.
+     */
+    def makeBlankNodeWithID(id: String): BlankNode
+
+    /**
+     * Constructs an IRI node.
+     *
+     * @param iri the IRI.
+     */
+    def makeIriNode(iri: IRI): IriNode
+
+    /**
+     * Constructs a literal value with a datatype.
+     *
+     * @param value    the lexical value of the literal.
+     * @param datatype the datatype IRI.
+     */
+    def makeDatatypeLiteral(value: String, datatype: IRI): DatatypeLiteral
+
+    /**
+     * Constructs a string with a language tag.
+     *
+     * @param value    the string.
+     * @param language the language tag.
+     */
+    def makeStringWithLanguage(value: String, language: String): StringWithLanguage
+
+    /**
+     * Constructs a statement.
+     *
+     * @param subj    the subject.
+     * @param pred    the predicate.
+     * @param obj     the object.
+     * @param context the IRI of the named graph, or `None` to use the default graph.
+     */
+    def makeStatement(subj: RdfResource, pred: IriNode, obj: RdfNode, context: Option[IRI] = None): Statement
+
+    /**
+     * Adds a statement to the model.
+     *
+     * @param statement the statement to be added.
+     */
+    def addStatement(statement: Statement): Unit
+
+    /**
+     * Constructs a statement and adds it to the model.
+     *
+     * @param subj    the subject.
+     * @param pred    the predicate.
+     * @param obj     the object.
+     * @param context the IRI of a named graph, or `None` to use the default graph.
+     */
+    def add(subj: RdfResource, pred: IriNode, obj: RdfNode, context: Option[IRI] = None): Unit
+
+    /**
+     * Removes statements from the model.
+     *
+     * @param subj    the subject, or `None` to match any subject.
+     * @param pred    the predicate, or `None` to match any predicate.
+     * @param obj     the object, or `None` to match any object.
+     * @param context the IRI of a named graph, or `None` to match any graph.
+     */
+    def remove(subj: Option[RdfResource], pred: Option[IriNode], obj: Option[RdfNode], context: Option[IRI] = None): Unit
+
+    /**
+     * Returns statements that match a pattern.
+     *
+     * @param subj    the subject, or `None` to match any subject.
+     * @param pred    the predicate, or `None` to match any predicate.
+     * @param obj     the object, or `None` to match any object.
+     * @param context the IRI of a named graph, or `None` to match any graph.
+     * @return the statements matching the pattern.
+     */
+    def find(subj: Option[RdfResource], pred: Option[IriNode], obj: Option[RdfNode], context: Option[IRI] = None): Set[Statement]
+}
---
+++ webapi/src/main/scala/org/knora/webapi/util/rdf/jenaimpl/JenaRdfModel.scala
@@ -0,0 +1,220 @@
+/*
+ * Copyright © 2015-2019 the contributors (see Contributors.md).
+ *
+ * This file is part of Knora.
+ *
+ * Knora is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Knora is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.knora.webapi.util.rdf.jenaimpl
+
+import org.apache.jena
+import org.knora.webapi.IRI
+import org.knora.webapi.exceptions.RdfProcessingException
+import org.knora.webapi.util.rdf._
+
+import scala.collection.JavaConverters._
+
+
+sealed trait JenaRdfNode {
+    def node: jena.graph.Node
+}
+
+case class JenaBlankNode(node: jena.graph.Node) extends JenaRdfNode with BlankNode {
+    override def id: String = node.getBlankNodeId.getLabelString
+}
+
+case class JenaIriNode(node: jena.graph.Node) extends JenaRdfNode with IriNode {
+    override def iri: IRI = node.getURI
+}
+
+case class JenaDatatypeLiteral(node: jena.graph.Node) extends JenaRdfNode with DatatypeLiteral {
+    override def value: String = node.getLiteralLexicalForm
+
+    override def datatype: IRI = node.getLiteralDatatypeURI
+}
+
+case class JenaStringWithLanguage(node: jena.graph.Node) extends JenaRdfNode with StringWithLanguage {
+    override def value: String = node.getLiteralLexicalForm
+
+    override def language: String = node.getLiteralLanguage
+}
+
+case class JenaStatement(quad: jena.sparql.core.Quad) extends Statement {
+    override def subj: RdfResource = {
+        val subj: jena.graph.Node = quad.getSubject
+
+        if (subj.isURI) {
+            JenaIriNode(quad.getSubject)
+        } else if (subj.isBlank) {
+            JenaBlankNode(quad.getSubject)
+        } else {
+            throw RdfProcessingException(s"Unexpected statement subject: $subj")
+        }
+    }
+
+    override def pred: IriNode = JenaIriNode(quad.getPredicate)
+
+    override def obj: RdfNode = {
+        val obj: jena.graph.Node = quad.getObject
+
+        if (obj.isURI) {
+            JenaIriNode(quad.getSubject)
+        } else if (obj.isBlank) {
+            JenaBlankNode(quad.getSubject)
+        } else if (obj.isLiteral) {
+            val literal: jena.graph.impl.LiteralLabel = obj.getLiteral
+
+            if (Option(literal.getDatatypeURI).isDefined) {
+                JenaDatatypeLiteral(obj)
+            } else {
+                JenaStringWithLanguage(obj)
+            }
+        } else {
+            throw RdfProcessingException(s"Unexpected statement object: $obj")
+        }
+    }
+
+    override def context: Option[IRI] = {
+        Option(quad.getGraph).map(_.getURI)
+    }
+}
+
+object JenaConversions {
+
+    implicit class ConvertibleJenaNode(val self: RdfNode) extends AnyVal {
+        def asJenaNode: jena.graph.Node = {
+            self match {
+                case jenaRdfNode: JenaRdfNode => jenaRdfNode.node
+                case other => throw RdfProcessingException(s"$other is not a Jena node")
+            }
+        }
+    }
+
+    implicit class ConvertibleJenaQuad(val self: Statement) extends AnyVal {
+        def asJenaQuad: jena.sparql.core.Quad = {
+            self match {
+                case jenaStatement: JenaStatement => jenaStatement.quad
+                case other => throw RdfProcessingException(s"$other is not a Jena statement")
+            }
+        }
+    }
+
+}
+
+/**
+ * An implementation of [[RdfModel]] that wraps a [[jena.query.Dataset]].
+ *
+ * @param dataset the underlying Jena dataset.
+ */
+class JenaRdfModel(dataset: jena.query.Dataset) extends RdfModel {
+
+    import JenaConversions._
+
+    private val datasetGraph: jena.sparql.core.DatasetGraph = dataset.asDatasetGraph
+    private val typeMapper = jena.datatypes.TypeMapper.getInstance
+
+    override def makeBlankNode: BlankNode = {
+        JenaBlankNode(jena.graph.NodeFactory.createBlankNode)
+    }
+
+    override def makeBlankNodeWithID(id: String): BlankNode = {
+        JenaBlankNode(jena.graph.NodeFactory.createBlankNode(id))
+    }
+
+    override def makeIriNode(iri: IRI): IriNode = {
+        JenaIriNode(jena.graph.NodeFactory.createURI(iri))
+    }
+
+    override def makeDatatypeLiteral(value: String, datatype: IRI): DatatypeLiteral = {
+        JenaDatatypeLiteral(jena.graph.NodeFactory.createLiteral(value, typeMapper.getTypeByName(datatype)))
+    }
+
+    override def makeStringWithLanguage(value: String, language: String): StringWithLanguage = {
+        JenaStringWithLanguage(jena.graph.NodeFactory.createLiteral(value, language))
+    }
+
+    override def makeStatement(subj: RdfResource, pred: IriNode, obj: RdfNode, context: Option[IRI]): Statement = {
+        JenaStatement(
+            new jena.sparql.core.Quad(
+                contextNodeOrDefaultGraph(context),
+                subj.asJenaNode,
+                pred.asJenaNode,
+                obj.asJenaNode
+            )
+        )
+    }
+
+    override def addStatement(statement: Statement): Unit = {
+        datasetGraph.add(statement.asJenaQuad)
+    }
+
+    /**
+     * Converts a named graph IRI to a [[jena.graph.Node]].
+     */
+    private def contextIriToNode(context: IRI): jena.graph.Node = {
+        jena.graph.NodeFactory.createURI(context)
+    }
+
+    /**
+     * Converts an optional named graph IRI to a [[jena.graph.Node]], converting
+     * `None` to the IRI of Jena's default graph.
+     */
+    private def contextNodeOrDefaultGraph(context: Option[IRI]): jena.graph.Node = {
+        context.map(contextIriToNode).getOrElse(jena.sparql.core.Quad.defaultGraphIRI)
+    }
+
+    /**
+     * Converts an optional named graph IRI to a [[jena.graph.Node]], converting
+     * `None` to a wildcard that will match any graph.
+     */
+    private def contextNodeOrWildcard(context: Option[IRI]): jena.graph.Node = {
+        context.map(contextIriToNode).getOrElse(jena.graph.Node.ANY)
+    }
+
+    /**
+     * Converts an optional [[RdfNode]] to a [[jena.graph.Node]], converting
+     * `None` to a wildcard that will match any node.
+     */
+    private def asJenaNodeOrWildcard(node: Option[RdfNode]): jena.graph.Node = {
+        node.map(_.asJenaNode).getOrElse(jena.graph.Node.ANY)
+    }
+
+    override def add(subj: RdfResource, pred: IriNode, obj: RdfNode, context: Option[IRI] = None): Unit = {
+        datasetGraph.add(
+            contextNodeOrDefaultGraph(context),
+            subj.asJenaNode,
+            pred.asJenaNode,
+            obj.asJenaNode
+        )
+    }
+
+    override def remove(subj: Option[RdfResource], pred: Option[IriNode], obj: Option[RdfNode], context: Option[IRI] = None): Unit = {
+        datasetGraph.delete(
+            contextNodeOrWildcard(context),
+            asJenaNodeOrWildcard(subj),
+            asJenaNodeOrWildcard(pred),
+            asJenaNodeOrWildcard(obj)
+        )
+    }
+
+    override def find(subj: Option[RdfResource], pred: Option[IriNode], obj: Option[RdfNode], context: Option[IRI] = None): Set[Statement] = {
+        datasetGraph.find(
+            contextNodeOrWildcard(context),
+            asJenaNodeOrWildcard(subj),
+            asJenaNodeOrWildcard(pred),
+            asJenaNodeOrWildcard(obj)
+        ).asScala.map(JenaStatement).toSet
+    }
+}
