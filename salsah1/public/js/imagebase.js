7c1460153816f88c6f4b57bf9951cd4e8d5ebd14
Ivan Subotic <400790+subotic@users.noreply.github.com>
2021-02-02 14:49:33 +0100

    chore: remove log output

--- salsah1/public/js/imagebase.js
+++ salsah1/public/js/imagebase.js
@@ -1493,7 +1493,6 @@ $(function() {
 					resource_label(data);
 					window_html.win('contentElement').css('overflow', 'auto'); // we have to set overlfow=auto to have a scrollbar, if the content does not fit....
 
-					console.log(data);
 					metadataAreaDomCreate(window_html.win('content'), data, {winid: window_html.win('getId')});
 
 					window_html.find('.resicon').dragndrop('makeDraggable', 'RESID', {resid: data.resdata.res_id});
