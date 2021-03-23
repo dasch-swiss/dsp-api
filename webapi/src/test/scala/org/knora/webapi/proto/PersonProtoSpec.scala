8e48b421e56be4035894f960e0d003615f27a132
Ivan Subotic <400790+subotic@users.noreply.github.com>
2021-03-23 21:14:45 +0100

    test(proto): add simple test to create person

---
+++ webapi/src/test/scala/org/knora/webapi/proto/PersonProtoSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2015-2021 the contributors (see Contributors.md).
+ *
+ *  This file is part of Knora.
+ *
+ *  Knora is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Knora is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public
+ *  License along with Knora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.knora.webapi.proto
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class PersonProtoSpec extends AnyWordSpecLike with Matchers {
+  "The generated proto" should {
+    "allow creating a person" in {
+      val testZip = Some(proto.zip_code.ZipCode(code = "4123"))
+
+      val testAddress = Some(
+        proto.address.Address(
+          city = "Allschwil",
+          zipCode = testZip
+        ))
+
+      val person = proto.person.Person(
+        name = "anyname",
+        id = 123456,
+        email = "any@example.org",
+        address = testAddress,
+        favoriteThing = "blue thing"
+      )
+
+      person.name should be("anyname")
+      person.id should be(123456)
+      person.email should be("any@example.org")
+      person.address should be(testAddress)
+      person.favoriteThing should be("blue thing")
+    }
+  }
+}
