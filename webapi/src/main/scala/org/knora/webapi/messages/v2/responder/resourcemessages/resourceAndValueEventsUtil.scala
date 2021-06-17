d5e70ba25986a2a3fef344112cd46f5b13855929
Sepideh Alassi <sepideh.alassi@unibas.ch>
2021-06-17 12:39:09 +0430

    feat(events): update resource last modification date event (#1877)
    * feat(events): event type for update resource metadata
    
    * feat(events): form resource metadata update event
    
    * feat (event): test for update resource metadata event
    
    * fix (api-v2): make sure lastModificationDate of an already updated resource is part of the resource metadata update request before attempting to update the resource.
    
    * refactor(events): some refactoring

--- webapi/src/main/scala/org/knora/webapi/messages/v2/responder/resourcemessages/resourceAndValueEventsUtil.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/v2/responder/resourcemessages/resourceAndValueEventsUtil.scala
@@ -7,6 +7,7 @@ object ResourceAndValueEventsUtil {
 
   val CREATE_RESOURCE_EVENT = "createResource"
   val DELETE_RESOURCE_EVENT = "deleteResource"
+  val UPDATE_RESOURCE_METADATA_EVENT = "updateResourceMetadata"
   val CREATE_VALUE_EVENT = "createValue"
   val UPDATE_VALUE_CONTENT_EVENT = "updateValueContent"
   val UPDATE_VALUE_PERMISSION_EVENT = "updateValuePermission"
