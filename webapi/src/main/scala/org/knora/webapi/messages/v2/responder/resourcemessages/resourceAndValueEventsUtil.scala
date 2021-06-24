f86de53ddcee6e4de6dfbe46fb33c6c9775ef872
Sepideh Alassi <sepideh.alassi@unibas.ch>
2021-06-24 15:09:42 +0430

    feat(resourceHistoryEvents): route for resource history events (DSP-1749) (#1882)
    * feat (resourceHistoryEvents): responder for getting history events of a single resource
    
    * feat (resourceHistoryEvents): add route
    
    * refactor (historyEvents): clean up
    
    * docs (resourceHistoryEvents): add documentation
    
    * refactor(resourceHistoryEvents): clean up

--- webapi/src/main/scala/org/knora/webapi/messages/v2/responder/resourcemessages/resourceAndValueEventsUtil.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/v2/responder/resourcemessages/resourceAndValueEventsUtil.scala
@@ -5,11 +5,11 @@ package org.knora.webapi.messages.v2.responder.resourcemessages
   */
 object ResourceAndValueEventsUtil {
 
-  val CREATE_RESOURCE_EVENT = "createResource"
-  val DELETE_RESOURCE_EVENT = "deleteResource"
-  val UPDATE_RESOURCE_METADATA_EVENT = "updateResourceMetadata"
-  val CREATE_VALUE_EVENT = "createValue"
-  val UPDATE_VALUE_CONTENT_EVENT = "updateValueContent"
-  val UPDATE_VALUE_PERMISSION_EVENT = "updateValuePermission"
-  val DELETE_VALUE_EVENT = "deleteValue"
+  val CREATE_RESOURCE_EVENT = "createdResource"
+  val DELETE_RESOURCE_EVENT = "deletedResource"
+  val UPDATE_RESOURCE_METADATA_EVENT = "updatedResourceMetadata"
+  val CREATE_VALUE_EVENT = "createdValue"
+  val UPDATE_VALUE_CONTENT_EVENT = "updatedValueContent"
+  val UPDATE_VALUE_PERMISSION_EVENT = "updatedValuePermission"
+  val DELETE_VALUE_EVENT = "deletedValue"
 }
