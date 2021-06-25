3782a0731f32f3e2de3d9687ff3affec471bdd9a
Sepideh Alassi <sepideh.alassi@gmail.com>
2021-06-25 15:44:22 +0200

    fix (customIRI): custom project IRI can contain UUID instead of project shortcode,

--- webapi/src/test/scala/org/knora/webapi/e2e/admin/ProjectsADME2ESpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e/admin/ProjectsADME2ESpec.scala
@@ -189,93 +189,92 @@ class ProjectsADME2ESpec
       }
     }
 
-    //TODO: return these tests after changing project IRI form
-//    "given a custom Iri" should {
-//      val customProjectIri: IRI = "http://rdfh.ch/projects/3333"
-//      "CREATE a new project with the provided custom Iri" in {
-//
-//        val createProjectWithCustomIRIRequest: String =
-//          s"""{
-//                       |    "id": "$customProjectIri",
-//                       |    "shortname": "newprojectWithIri",
-//                       |    "shortcode": "3333",
-//                       |    "longname": "new project with a custom IRI",
-//                       |    "description": [{"value": "a project created with a custom IRI", "language": "en"}],
-//                       |    "keywords": ["projectIRI"],
-//                       |    "logo": "/fu/bar/baz.jpg",
-//                       |    "status": true,
-//                       |    "selfjoin": false
-//                       |}""".stripMargin
-//
-//        clientTestDataCollector.addFile(
-//          TestDataFileContent(
-//            filePath = TestDataFilePath(
-//              directoryPath = clientTestDataPath,
-//              filename = "create-project-with-custom-Iri-request",
-//              fileExtension = "json"
-//            ),
-//            text = createProjectWithCustomIRIRequest
-//          )
-//        )
-//
-//        val request = Post(
-//          baseApiUrl + s"/admin/projects",
-//          HttpEntity(ContentTypes.`application/json`, createProjectWithCustomIRIRequest)) ~> addCredentials(
-//          BasicHttpCredentials(rootEmail, testPass))
-//        val response: HttpResponse = singleAwaitingRequest(request)
-//        response.status should be(StatusCodes.OK)
-//
-//        val result = AkkaHttpUtils.httpResponseToJson(response).fields("project").convertTo[ProjectADM]
-//
-//        //check that the custom IRI is correctly assigned
-//        result.id should be(customProjectIri)
-//
-//        //check the rest of project info
-//        result.shortcode should be("3333")
-//        result.shortname should be("newprojectWithIri")
-//        result.longname should be(Some("new project with a custom IRI"))
-//        result.keywords should be(Seq("projectIRI"))
-//        result.description should be(
-//          Seq(StringLiteralV2(value = "a project created with a custom IRI", language = Some("en"))))
-//
-//        clientTestDataCollector.addFile(
-//          TestDataFileContent(
-//            filePath = TestDataFilePath(
-//              directoryPath = clientTestDataPath,
-//              filename = "create-project-with-custom-Iri-response",
-//              fileExtension = "json"
-//            ),
-//            text = responseToString(response)
-//          )
-//        )
-//
-//      }
-//
-//      "return 'BadRequest' if the supplied project IRI is not unique" in {
-//        val params =
-//          s"""{
-//                       |    "id": "$customProjectIri",
-//                       |    "shortname": "newprojectWithDuplicateIri",
-//                       |    "shortcode": "2222",
-//                       |    "longname": "new project with a duplicate custom invalid IRI",
-//                       |    "description": [{"value": "a project created with a duplicate custom IRI", "language": "en"}],
-//                       |    "keywords": ["projectDuplicateIRI"],
-//                       |    "logo": "/fu/bar/baz.jpg",
-//                       |    "status": true,
-//                       |    "selfjoin": false
-//                       |}""".stripMargin
-//
-//        val request = Post(baseApiUrl + s"/admin/projects", HttpEntity(ContentTypes.`application/json`, params)) ~> addCredentials(
-//          BasicHttpCredentials(rootEmail, testPass))
-//        val response: HttpResponse = singleAwaitingRequest(request)
-//        response.status should be(StatusCodes.BadRequest)
-//
-//        val errorMessage: String = Await.result(Unmarshal(response.entity).to[String], 1.second)
-//        val invalidIri: Boolean =
-//          errorMessage.contains(s"IRI: '$customProjectIri' already exists, try another one.")
-//        invalidIri should be(true)
-//      }
-//    }
+    "given a custom Iri" should {
+      val customProjectIri: IRI = "http://rdfh.ch/projects/XGze0w7g6P3p3z6tj_SXPQ"
+      "CREATE a new project with the provided custom Iri" in {
+
+        val createProjectWithCustomIRIRequest: String =
+          s"""{
+                       |    "id": "$customProjectIri",
+                       |    "shortname": "newprojectWithIri",
+                       |    "shortcode": "3333",
+                       |    "longname": "new project with a custom IRI",
+                       |    "description": [{"value": "a project created with a custom IRI", "language": "en"}],
+                       |    "keywords": ["projectIRI"],
+                       |    "logo": "/fu/bar/baz.jpg",
+                       |    "status": true,
+                       |    "selfjoin": false
+                       |}""".stripMargin
+
+        clientTestDataCollector.addFile(
+          TestDataFileContent(
+            filePath = TestDataFilePath(
+              directoryPath = clientTestDataPath,
+              filename = "create-project-with-custom-Iri-request",
+              fileExtension = "json"
+            ),
+            text = createProjectWithCustomIRIRequest
+          )
+        )
+
+        val request = Post(
+          baseApiUrl + s"/admin/projects",
+          HttpEntity(ContentTypes.`application/json`, createProjectWithCustomIRIRequest)) ~> addCredentials(
+          BasicHttpCredentials(rootEmail, testPass))
+        val response: HttpResponse = singleAwaitingRequest(request)
+        response.status should be(StatusCodes.OK)
+
+        val result = AkkaHttpUtils.httpResponseToJson(response).fields("project").convertTo[ProjectADM]
+
+        //check that the custom IRI is correctly assigned
+        result.id should be(customProjectIri)
+
+        //check the rest of project info
+        result.shortcode should be("3333")
+        result.shortname should be("newprojectWithIri")
+        result.longname should be(Some("new project with a custom IRI"))
+        result.keywords should be(Seq("projectIRI"))
+        result.description should be(
+          Seq(StringLiteralV2(value = "a project created with a custom IRI", language = Some("en"))))
+
+        clientTestDataCollector.addFile(
+          TestDataFileContent(
+            filePath = TestDataFilePath(
+              directoryPath = clientTestDataPath,
+              filename = "create-project-with-custom-Iri-response",
+              fileExtension = "json"
+            ),
+            text = responseToString(response)
+          )
+        )
+
+      }
+
+      "return 'BadRequest' if the supplied project IRI is not unique" in {
+        val params =
+          s"""{
+                       |    "id": "$customProjectIri",
+                       |    "shortname": "newprojectWithDuplicateIri",
+                       |    "shortcode": "2222",
+                       |    "longname": "new project with a duplicate custom invalid IRI",
+                       |    "description": [{"value": "a project created with a duplicate custom IRI", "language": "en"}],
+                       |    "keywords": ["projectDuplicateIRI"],
+                       |    "logo": "/fu/bar/baz.jpg",
+                       |    "status": true,
+                       |    "selfjoin": false
+                       |}""".stripMargin
+
+        val request = Post(baseApiUrl + s"/admin/projects", HttpEntity(ContentTypes.`application/json`, params)) ~> addCredentials(
+          BasicHttpCredentials(rootEmail, testPass))
+        val response: HttpResponse = singleAwaitingRequest(request)
+        response.status should be(StatusCodes.BadRequest)
+
+        val errorMessage: String = Await.result(Unmarshal(response.entity).to[String], 1.second)
+        val invalidIri: Boolean =
+          errorMessage.contains(s"IRI: '$customProjectIri' already exists, try another one.")
+        invalidIri should be(true)
+      }
+    }
 
     "used to modify project information" should {
 
@@ -685,7 +684,7 @@ class ProjectsADME2ESpec
         assert(response.status === StatusCodes.OK)
 
         val keywords: Seq[String] = AkkaHttpUtils.httpResponseToJson(response).fields("keywords").convertTo[Seq[String]]
-        keywords.size should be(20)
+        keywords.size should be(21)
         clientTestDataCollector.addFile(
           TestDataFileContent(
             filePath = TestDataFilePath(
