ccf449c26c3b7f26af8e9c49f8d67b40c1916a2d
irinaschubert <irina.schubert@dasch.swiss>
2021-09-09 08:43:18 +0200

    Update CORSSupportE2ESpec.scala

--- webapi/src/test/scala/org/knora/webapi/e2e/CORSSupportE2ESpec.scala
+++ webapi/src/test/scala/org/knora/webapi/e2e/CORSSupportE2ESpec.scala
@@ -36,8 +36,8 @@ object CORSSupportE2ESpec {
 }
 
 /**
-  * End-to-end test specification for testing [[CORSSupport]].
-  */
+ * End-to-end test specification for testing [[CORSSupport]].
+ */
 class CORSSupportE2ESpec extends E2ESpec(CORSSupportE2ESpec.config) {
 
   implicit def default(implicit system: ActorSystem) = RouteTestTimeout(settings.defaultTimeout)
@@ -51,30 +51,31 @@ class CORSSupportE2ESpec extends E2ESpec(CORSSupportE2ESpec.config) {
   "A Route with enabled CORS support" should {
 
     "accept valid pre-flight requests" in {
-      val request = Options(baseApiUrl + s"/admin/projects") ~> Origin(exampleOrigin) ~> `Access-Control-Request-Method`(
-        GET)
+      val request =
+        Options(baseApiUrl + s"/admin/projects") ~> Origin(exampleOrigin) ~> `Access-Control-Request-Method`(GET)
       val response: HttpResponse = singleAwaitingRequest(request)
       response.status shouldBe StatusCodes.OK
       response.headers should contain allElementsOf Seq(
         `Access-Control-Allow-Origin`(exampleOrigin),
-        `Access-Control-Allow-Methods`(List(GET, PUT, POST, DELETE, HEAD, OPTIONS)),
+        `Access-Control-Allow-Methods`(List(GET, PUT, POST, DELETE, PATCH, HEAD, OPTIONS)),
         `Access-Control-Max-Age`(1800),
         `Access-Control-Allow-Credentials`(true)
       )
     }
 
-    "reject requests with invalid method" in {
-      val request = Options(baseApiUrl + s"/admin/projects") ~> Origin(exampleOrigin) ~> `Access-Control-Request-Method`(
-        PATCH)
-      val response: HttpResponse = singleAwaitingRequest(request)
-      responseToString(response) shouldEqual "CORS: invalid method 'PATCH'"
-      response.status shouldBe StatusCodes.BadRequest
-    }
+//    "reject requests with invalid method" in {
+//      val request = Options(baseApiUrl + s"/admin/projects") ~> Origin(exampleOrigin) ~> `Access-Control-Request-Method`(
+//        PATCH)
+//      val response: HttpResponse = singleAwaitingRequest(request)
+//      responseToString(response) shouldEqual "CORS: invalid method 'PATCH'"
+//      response.status shouldBe StatusCodes.BadRequest
+//    }
 
     "send `Access-Control-Allow-Origin` header when the Knora resource is found " in {
       val request = Get(
-        baseApiUrl + "/v1/resources/" + java.net.URLEncoder.encode("http://rdfh.ch/0001/55UrkgTKR2SEQgnsLWI9mg",
-                                                                   "utf-8")) ~> Origin(exampleOrigin)
+        baseApiUrl + "/v1/resources/" + java.net.URLEncoder
+          .encode("http://rdfh.ch/0001/55UrkgTKR2SEQgnsLWI9mg", "utf-8")
+      ) ~> Origin(exampleOrigin)
       val response = singleAwaitingRequest(request)
       response.status should equal(StatusCodes.OK)
       response.headers should contain allElementsOf Seq(
@@ -83,8 +84,9 @@ class CORSSupportE2ESpec extends E2ESpec(CORSSupportE2ESpec.config) {
     }
 
     "send `Access-Control-Allow-Origin` header when the Knora resource is NOT found " in {
-      val request = Get(baseApiUrl + "/v1/resources/" + java.net.URLEncoder.encode("http://rdfh.ch/0803/nonexistent",
-                                                                                   "utf-8")) ~> Origin(exampleOrigin)
+      val request = Get(
+        baseApiUrl + "/v1/resources/" + java.net.URLEncoder.encode("http://rdfh.ch/0803/nonexistent", "utf-8")
+      ) ~> Origin(exampleOrigin)
       val response = singleAwaitingRequest(request)
       response.status should equal(StatusCodes.NotFound)
       response.headers should contain allElementsOf Seq(
@@ -93,7 +95,7 @@ class CORSSupportE2ESpec extends E2ESpec(CORSSupportE2ESpec.config) {
     }
 
     "send `Access-Control-Allow-Origin` header when the api endpoint route is NOT found " in {
-      val request = Get(baseApiUrl + "/NotFound") ~> Origin(exampleOrigin)
+      val request  = Get(baseApiUrl + "/NotFound") ~> Origin(exampleOrigin)
       val response = singleAwaitingRequest(request)
       response.status should equal(StatusCodes.NotFound)
       response.headers should contain allElementsOf Seq(
