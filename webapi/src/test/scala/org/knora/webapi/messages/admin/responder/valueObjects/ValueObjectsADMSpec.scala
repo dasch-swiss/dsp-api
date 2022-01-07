d56dc38c2365b605405c85884d863f17f1e108be
Marcin Procyk <marcin.procyk@dasch.swiss>
2022-01-07 15:59:40 +0100

    refactor(userADM): bring back ValueObjectsADMSpec config

---
+++ webapi/src/test/scala/org/knora/webapi/messages/admin/responder/valueObjects/ValueObjectsADMSpec.scala
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2021 Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.messages.admin.responder.valueObjects
+import com.typesafe.config.{Config, ConfigFactory}
+
+object ValueObjectsADMSpec {
+  val config: Config = ConfigFactory.parseString("""
+          akka.loglevel = "DEBUG"
+          akka.stdout-loglevel = "DEBUG"
+        """.stripMargin)
+}
