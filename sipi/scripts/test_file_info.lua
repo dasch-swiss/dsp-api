bc8175947b3d37bf05a8b24cd807f9ba8c3a3a25
Marcin Procyk <marcin.procyk@dasch.swiss>
2022-01-25 16:16:18 +0100

    fix: license year range

--- sipi/scripts/test_file_info.lua
+++ sipi/scripts/test_file_info.lua
@@ -1,4 +1,4 @@
--- * Copyright © 2022 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+-- * Copyright © 2021 - 2022 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
 -- * SPDX-License-Identifier: Apache-2.0
 
 require "send_response"
