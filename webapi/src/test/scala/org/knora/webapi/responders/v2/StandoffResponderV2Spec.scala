73e38352ca452cebf142ab6b3e573ab61547b9db
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-02-21 12:09:51 +0100

    test: reasonably test creating a standoff mapping in a unit test

--- webapi/src/test/scala/org/knora/webapi/responders/v2/StandoffResponderV2Spec.scala
+++ webapi/src/test/scala/org/knora/webapi/responders/v2/StandoffResponderV2Spec.scala
@@ -5,42 +5,23 @@
 
 package org.knora.webapi.responders.v2
 
-import akka.actor.{ActorRef, Props}
+import akka.pattern.ask
 import akka.testkit.ImplicitSender
+import akka.util.Timeout
 import org.knora.webapi._
-import org.knora.webapi.app.ApplicationActor
 import org.knora.webapi.exceptions._
-import org.knora.webapi.messages.IriConversions._
-import org.knora.webapi.messages.admin.responder.usersmessages.UserADM
 import org.knora.webapi.messages.store.triplestoremessages._
-import org.knora.webapi.messages.util.rdf.SparqlSelectResult
-import org.knora.webapi.messages.util.search.gravsearch.GravsearchParser
-import org.knora.webapi.messages.util.{
-  CalendarNameGregorian,
-  DatePrecisionYear,
-  KnoraSystemInstances,
-  PermissionUtilADM
-}
-import org.knora.webapi.messages.v2.responder._
 import org.knora.webapi.messages.v2.responder.resourcemessages._
-import org.knora.webapi.messages.v2.responder.searchmessages.GravsearchRequestV2
 import org.knora.webapi.messages.v2.responder.standoffmessages._
-import org.knora.webapi.messages.v2.responder.valuemessages._
-import org.knora.webapi.messages.{OntologyConstants, SmartIri, StringFormatter}
-import org.knora.webapi.models.filemodels.{ChangeFileRequest, FileType}
+import org.knora.webapi.messages.StringFormatter
 import org.knora.webapi.models.standoffmodels.DefineStandoffMapping
-import org.knora.webapi.settings._
 import org.knora.webapi.sharedtestdata.SharedTestDataADM
-import org.knora.webapi.store.iiif.MockSipiConnector
-import org.knora.webapi.util.MutableTestIri
 
-import java.time.Instant
-import java.util.UUID
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 /**
- * Tests [[ValuesResponderV2]].
+ * Tests [[StandoffResponderV2]].
  */
 class StandoffResponderV2Spec extends CoreSpec() with ImplicitSender {
   private implicit val stringFormatter: StringFormatter = StringFormatter.getGeneralInstance
@@ -66,17 +47,22 @@ class StandoffResponderV2Spec extends CoreSpec() with ImplicitSender {
     response.resources.head
   }
 
-//  "Load test data" in {
-//    responderManager ! GetMappingRequestV2(
-//      mappingIri = "http://rdfh.ch/standoff/mappings/StandardMapping",
-//      featureFactoryConfig = defaultFeatureFactoryConfig,
-//      requestingUser = KnoraSystemInstances.Users.SystemUser
-//    )
-//
-//    expectMsgPF(timeout) { case mappingResponse: GetMappingResponseV2 =>
-//      standardMapping = Some(mappingResponse.mapping)
-//    }
-//  }
+  private def getMapping(iri: String): SparqlConstructResponse = {
+
+    val getMappingSparql = org.knora.webapi.messages.twirl.queries.sparql.v2.txt
+      .getMapping(
+        triplestore = settings.triplestoreType,
+        mappingIri = iri
+      )
+      .toString()
+
+    implicit val timeout: Timeout = Duration(10, SECONDS)
+    val resF: Future[SparqlConstructResponse] = (storeManager ? SparqlConstructRequest(
+      sparql = getMappingSparql,
+      featureFactoryConfig = defaultFeatureFactoryConfig
+    )).mapTo[SparqlConstructResponse]
+    Await.result(resF, 10.seconds)
+  }
 
   "The standoff responder" should {
     "create a standoff mapping" in {
@@ -142,53 +128,13 @@ class StandoffResponderV2Spec extends CoreSpec() with ImplicitSender {
         case res: CreateMappingResponseV2 => res
         case _                            => throw AssertionException("Could not create a mapping")
       }
-//      val mappingFromDB = getResource(response.mappingIri)
-//      println(mappingFromDB)
-      println()
 
-//      // Add the value.
-//
-//      val resourceIri: IRI = aThingIri
-//      val propertyIri: SmartIri = "http://0.0.0.0:3333/ontology/0001/anything/v2#hasInteger".toSmartIri
-//      val intValue = 4
-//      val maybeResourceLastModDate: Option[Instant] = getResourceLastModificationDate(resourceIri, anythingUser1)
-//
-//      responderManager ! CreateValueRequestV2(
-//        CreateValueV2(
-//          resourceIri = resourceIri,
-//          resourceClassIri = "http://0.0.0.0:3333/ontology/0001/anything/v2#Thing".toSmartIri,
-//          propertyIri = propertyIri,
-//          valueContent = IntegerValueContentV2(
-//            ontologySchema = ApiV2Complex,
-//            valueHasInteger = intValue
-//          )
-//        ),
-//        featureFactoryConfig = defaultFeatureFactoryConfig,
-//        requestingUser = anythingUser1,
-//        apiRequestID = UUID.randomUUID
-//      )
-//
-//      expectMsgPF(timeout) { case createValueResponse: CreateValueResponseV2 =>
-//        intValueIri.set(createValueResponse.valueIri)
-//        firstIntValueVersionIri.set(createValueResponse.valueIri)
-//        integerValueUUID = createValueResponse.valueUUID
-//      }
-//
-//      // Read the value back to check that it was added correctly.
-//
-//      val valueFromTriplestore = getValue(
-//        resourceIri = resourceIri,
-//        maybePreviousLastModDate = maybeResourceLastModDate,
-//        propertyIriForGravsearch = propertyIri,
-//        propertyIriInResult = propertyIri,
-//        expectedValueIri = intValueIri.get,
-//        requestingUser = anythingUser1
-//      )
-//
-//      valueFromTriplestore.valueContent match {
-//        case savedValue: IntegerValueContentV2 => savedValue.valueHasInteger should ===(intValue)
-//        case _                                 => throw AssertionException(s"Expected integer value, got $valueFromTriplestore")
-//      }
+      val expectedMappingIRI = f"${mapping.projectIRI}/mappings/$mappingName"
+      response.mappingIri should equal(expectedMappingIRI)
+      val mappingFromDB: SparqlConstructResponse = getMapping(response.mappingIri)
+      println(mappingFromDB)
+      mappingFromDB.statements should not be (Map.empty)
+      mappingFromDB.statements.get(expectedMappingIRI) should not be (Map.empty)
     }
 
   }
