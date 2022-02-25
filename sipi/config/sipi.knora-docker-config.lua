2a3df06ddb1a158ecf68549059973ff3a59d671c
Ivan Subotic <400790+subotic@users.noreply.github.com>
2022-02-25 18:05:30 +0100

    Update sipi/config/sipi.knora-docker-config.lua
    Co-authored-by: irinaschubert <irina.schubert@dasch.swiss>

--- sipi/config/sipi.knora-docker-config.lua
+++ sipi/config/sipi.knora-docker-config.lua
@@ -36,7 +36,7 @@ sipi = {
 
     --
     -- For scaling images, SIPI offers two methods. The value "high" offers best quality using expensive
-    -- algorithms (bilinear interpolation, if downscaling the image is first scaled up to an integer
+    -- algorithms: bilinear interpolation, if downscaling the image is first scaled up to an integer
     -- multiple of the requires size, and then downscaled using averaging. This results in the best
     -- image quality. "medium" uses bilinear interpolation but does not do upscaling before
     -- downscaling. Scaling quality is set to "low", then just a lookup table and nearest integer
