11053d9a150ead6475954f5508eb564d4a155565
Marcin Procyk <marcin.procyk@dasch.swiss>
2022-03-18 17:09:41 +0100

    update plugin

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/UpgradePluginPR2018.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/UpgradePluginPR2018.scala
@@ -5,10 +5,12 @@
 
 package org.knora.webapi.store.triplestore.upgrade.plugins
 
+import org.knora.webapi.exceptions.InconsistentRepositoryDataException
 import org.knora.webapi.feature.FeatureFactoryConfig
-import org.knora.webapi.messages.OntologyConstants
-import org.knora.webapi.messages.OntologyConstants.KnoraBase.LastModificationDate
+import org.knora.webapi.messages.OntologyConstants.KnoraAdmin.SystemProject
+import org.knora.webapi.messages.OntologyConstants.KnoraBase.{AttachedToProject, LastModificationDate}
 import org.knora.webapi.messages.OntologyConstants.Owl.Ontology
+import org.knora.webapi.messages.OntologyConstants.Xsd.DateTime
 import org.knora.webapi.messages.util.rdf._
 import org.knora.webapi.store.triplestore.upgrade.UpgradePlugin
 
@@ -19,18 +21,53 @@ import java.time.Instant
  */
 class UpgradePluginPR2018(featureFactoryConfig: FeatureFactoryConfig) extends UpgradePlugin {
   private val nodeFactory: RdfNodeFactory = RdfFeatureFactory.getRdfNodeFactory(featureFactoryConfig)
-
   private val newModificationDate = Instant.now.toString
-
   private val ontologyType: IriNode = nodeFactory.makeIriNode(Ontology)
 
   override def transform(model: RdfModel): Unit =
-    model.add(
-      subj = nodeFactory.makeIriNode("http://www.knora.org/ontology/7777/test"),
-      pred = nodeFactory.makeIriNode(LastModificationDate),
-      obj = nodeFactory.makeDatatypeLiteral(
-        value = newModificationDate,
-        datatype = OntologyConstants.Xsd.DateTime
+    for (ontology: IriNode <- getOntologiesToTransform(model)) {
+      model.add(
+        subj = ontology,
+        pred = nodeFactory.makeIriNode(LastModificationDate),
+        obj = nodeFactory.makeDatatypeLiteral(
+          value = newModificationDate,
+          datatype = DateTime
+        )
       )
-    )
+    }
+
+  private def getOntologiesToTransform(model: RdfModel): Iterator[IriNode] =
+    // find onotlogies
+    model
+      .find(
+        subj = None,
+        pred = None,
+        obj = Some(ontologyType)
+      )
+      .map(_.subj)
+      .filter { _: RdfResource =>
+        // that have no knora-base:lastModificationDate
+        model
+          .find(
+            subj = None,
+            pred = Some(nodeFactory.makeIriNode(LastModificationDate)),
+            obj = None
+          )
+          .isEmpty
+      }
+      .filter { _: RdfResource =>
+        // and are not attached to knora-admin:SystemProject
+        model
+          .find(
+            subj = None,
+            pred = Some(nodeFactory.makeIriNode(AttachedToProject)),
+            obj = Some(nodeFactory.makeIriNode(SystemProject))
+          )
+          .isEmpty
+      }
+      .map {
+        case iriNode: IriNode => iriNode
+        case other => throw InconsistentRepositoryDataException(s"Unexpected subject for $ontologyType: $other")
+      }
+
 }
