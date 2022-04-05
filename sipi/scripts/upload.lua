fe87c1c1518d4b8d1865950aa290a74e99950927
André Kilchenmann <github@milchkannen.ch>
2022-04-05 06:28:22 +0200

    refactor(sipi): fix typo in coment
    Co-authored-by: irinaschubert <irina.schubert@dasch.swiss>

--- sipi/scripts/upload.lua
+++ sipi/scripts/upload.lua
@@ -223,7 +223,7 @@ for file_index, file_params in pairs(server.uploads) do
     if media_type == VIDEO then
         
         local handle
-        -- get video file information with ffprobe: widht, height, duration and frame rate (fps)
+        -- get video file information with ffprobe: width, height, duration and frame rate (fps)
         handle = io.popen("ffprobe -v error -select_streams v:0 -show_entries stream=width,height,bit_rate,duration,nb_frames,r_frame_rate -print_format json -i " .. tmp_storage_file_path)
         local file_meta = handle:read("*a")
         handle:close()
