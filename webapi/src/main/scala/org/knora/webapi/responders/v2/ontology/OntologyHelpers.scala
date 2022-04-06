50e318628bf7b6e30e4dfd2472e69c3b8ccd65e2
irinaschubert <irina.schubert@dasch.swiss>
2022-04-06 13:26:57 +0200

    feat(ontology): Add support for additional ontologies (DEV-512) (#2029)
    * update java version for docker base image
    
    * allow external ontologies
    
    * add test
    
    * Update docstring
    
    * add test
    
    * add test
    
    * add test for resource creation
    
    * update test description
    
    * add missing new line
    
    * separate test into multiple parts
    
    * add type annotations

--- webapi/src/main/scala/org/knora/webapi/responders/v2/ontology/OntologyHelpers.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/v2/ontology/OntologyHelpers.scala
@@ -956,11 +956,15 @@ object OntologyHelpers {
       directCardinalities = internalClassDef.directCardinalities ++ linkValuePropCardinalitiesToAdd
     )
 
-    // Get the cardinalities that the class can inherit.
+    // Get the cardinalities that the class can inherit. If the ontology of the base class can't be found, it's assumed to be an external ontology (p.ex. foaf).
 
     val cardinalitiesAvailableToInherit: Map[SmartIri, KnoraCardinalityInfo] =
-      classDefWithAddedLinkValueProps.subClassOf.flatMap { baseClassIri =>
-        cacheData.ontologies(baseClassIri.getOntologyFromEntity).classes(baseClassIri).allCardinalities
+      classDefWithAddedLinkValueProps.subClassOf.flatMap { baseClassIri: SmartIri =>
+        val ontology = cacheData.ontologies.getOrElse(baseClassIri.getOntologyFromEntity, None)
+        ontology match {
+          case ontology: ReadOntologyV2 => ontology.classes(baseClassIri).allCardinalities
+          case _                        => None
+        }
       }.toMap
 
     // Check that the cardinalities directly defined on the class are compatible with any inheritable
