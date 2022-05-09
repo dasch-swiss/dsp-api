e688ccbf4971dfc9e3e4bc47f49e41731f9aebe0
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-05-09 18:55:24 +0200

    bump knora-base version

--- webapi/src/main/scala/org/knora/webapi/package.scala
+++ webapi/src/main/scala/org/knora/webapi/package.scala
@@ -11,7 +11,7 @@ package object webapi {
    * The version of `knora-base` and of the other built-in ontologies that this version of Knora requires.
    * Must be the same as the object of `knora-base:ontologyVersion` in the `knora-base` ontology being used.
    */
-  val KnoraBaseVersion: String = "knora-base v20"
+  val KnoraBaseVersion: String = "knora-base v21"
 
   /**
    * `IRI` is a synonym for `String`, used to improve code readability.
