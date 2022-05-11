0648eabb50c2ee79a032bcb06cbd72e6e821bda4
Ivan Subotic <400790+subotic@users.noreply.github.com>
2022-05-11 08:06:56 +0200

    ongoing refactoring of the triplestore manager

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/TriplestoreManager.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/TriplestoreManager.scala
@@ -5,25 +5,34 @@
 
 package org.knora.webapi.store.triplestore
 
-import akka.actor.Actor
-import akka.actor.ActorLogging
-import akka.actor.ActorRef
-import akka.actor.Props
-import akka.event.LoggingReceive
-import akka.routing.FromConfig
-import org.knora.webapi.core.ActorMaker
-import org.knora.webapi.feature.FeatureFactoryConfig
 import org.knora.webapi.messages.store.triplestoremessages.UpdateRepositoryRequest
-import org.knora.webapi.messages.util.FakeTriplestore
 import org.knora.webapi.settings._
-import org.knora.webapi.store.triplestore.http.HttpTriplestoreConnector
+import org.knora.webapi._
 import org.knora.webapi.store.triplestore.upgrade.RepositoryUpdater
-import org.knora.webapi.util.ActorUtil._
-
-import scala.concurrent.ExecutionContext
 
 import zio._
 import org.knora.webapi.store.triplestore.api.TriplestoreService
+import org.knora.webapi.exceptions.UnexpectedMessageException
+import org.knora.webapi.messages.store.triplestoremessages.SimulateTimeoutRequest
+import org.knora.webapi.messages.store.triplestoremessages.InsertGraphDataContentRequest
+import org.knora.webapi.messages.store.triplestoremessages.UploadRepositoryRequest
+import java.nio.file.Path
+import org.knora.webapi.messages.store.triplestoremessages.DownloadRepositoryRequest
+import org.knora.webapi.messages.store.triplestoremessages.CheckTriplestoreRequest
+import org.knora.webapi.messages.store.triplestoremessages.RdfDataObject
+import org.knora.webapi.messages.store.triplestoremessages.InsertRepositoryContent
+import org.knora.webapi.messages.store.triplestoremessages.DropAllTRepositoryContent
+import org.knora.webapi.messages.store.triplestoremessages.ResetRepositoryContent
+import org.knora.webapi.messages.store.triplestoremessages.SparqlAskRequest
+import org.knora.webapi.messages.store.triplestoremessages.SparqlUpdateRequest
+import org.knora.webapi.messages.store.triplestoremessages.NamedGraphDataRequest
+import org.knora.webapi.messages.util.rdf.QuadFormat
+import org.knora.webapi.messages.store.triplestoremessages.NamedGraphFileRequest
+import org.knora.webapi.messages.store.triplestoremessages.SparqlConstructFileRequest
+import org.knora.webapi.messages.store.triplestoremessages.SparqlExtendedConstructRequest
+import org.knora.webapi.messages.store.triplestoremessages.SparqlConstructRequest
+import org.knora.webapi.messages.store.triplestoremessages.SparqlSelectRequest
+import org.knora.webapi.messages.store.triplestoremessages.TriplestoreRequest
 
 /**
  * This service receives akka messages and translates them to calls to ZIO besed service implementations.
@@ -37,96 +46,46 @@ final case class TriplestoreManager(
   updater: RepositoryUpdater
 ) {
 
-  protected implicit val executionContext: ExecutionContext =
-    context.system.dispatchers.lookup(KnoraDispatchers.KnoraActorDispatcher)
-
-  private var storeActorRef: ActorRef = _
-
-  // TODO: run the fake triple store as an actor (the fake triple store will not be needed anymore, once the embedded triple store is implemented)
-  FakeTriplestore.init(settings.fakeTriplestoreDataDir)
-
-  if (settings.useFakeTriplestore) {
-    FakeTriplestore.load()
-    log.info("Loaded fake triplestore")
-  } else {
-    log.debug(s"Using triplestore: ${settings.triplestoreType}")
-  }
-
-  if (settings.prepareFakeTriplestore) {
-    FakeTriplestore.clear()
-    log.info("About to prepare fake triplestore")
-  }
-
-  log.debug(settings.triplestoreType)
-
-  // A RepositoryUpdater for processing requests to update the repository.
-  private val repositoryUpdater: RepositoryUpdater = new RepositoryUpdater(
-    system = context.system,
-    appActor = appActor,
-    settings = settings,
-    featureFactoryConfig = defaultFeatureFactoryConfig
-  )
-
-  override def preStart(): Unit = {
-    log.debug("TriplestoreManagerActor: start with preStart")
-
-    storeActorRef = makeActor(
-      FromConfig.props(Props[HttpTriplestoreConnector]()).withDispatcher(KnoraDispatchers.KnoraActorDispatcher),
-      name = HttpTriplestoreActorName
-    )
-
-    log.debug("TriplestoreManagerActor: finished with preStart")
-  }
-
   def receive(message: TriplestoreRequest) = message match {
-    case UpdateRepositoryRequest()           =>  repositoryUpdater.maybeUpdateRepository
-    case SparqlSelectRequest(sparql: String) => try2Message(sender(), sparqlHttpSelect(sparql), log)
+    case UpdateRepositoryRequest()           => updater.maybeUpdateRepository
+    case SparqlSelectRequest(sparql: String) => ts.sparqlHttpSelect(sparql)
     case sparqlConstructRequest: SparqlConstructRequest =>
-      try2Message(sender(), sparqlHttpConstruct(sparqlConstructRequest), log)
+      ts.sparqlHttpConstruct(sparqlConstructRequest)
     case sparqlExtendedConstructRequest: SparqlExtendedConstructRequest =>
-      try2Message(sender(), sparqlHttpExtendedConstruct(sparqlExtendedConstructRequest), log)
+      ts.sparqlHttpExtendedConstruct(sparqlExtendedConstructRequest)
     case SparqlConstructFileRequest(
           sparql: String,
           graphIri: IRI,
           outputFile: Path,
           outputFormat: QuadFormat,
-          featureFactoryConfig: FeatureFactoryConfig
+          _
         ) =>
-      try2Message(
-        sender(),
-        sparqlHttpConstructFile(sparql, graphIri, outputFile, outputFormat, featureFactoryConfig),
-        log
-      )
+      ts.sparqlHttpConstructFile(sparql, graphIri, outputFile, outputFormat)
     case NamedGraphFileRequest(
           graphIri: IRI,
           outputFile: Path,
           outputFormat: QuadFormat,
-          featureFactoryConfig: FeatureFactoryConfig
+          _
         ) =>
-      try2Message(sender(), sparqlHttpGraphFile(graphIri, outputFile, outputFormat, featureFactoryConfig), log)
-    case NamedGraphDataRequest(graphIri: IRI) => try2Message(sender(), sparqlHttpGraphData(graphIri), log)
-    case SparqlUpdateRequest(sparql: String)  => try2Message(sender(), sparqlHttpUpdate(sparql), log)
-    case SparqlAskRequest(sparql: String)     => try2Message(sender(), sparqlHttpAsk(sparql), log)
+      ts.sparqlHttpGraphFile(graphIri, outputFile, outputFormat)
+    case NamedGraphDataRequest(graphIri: IRI) => ts.sparqlHttpGraphData(graphIri)
+    case SparqlUpdateRequest(sparql: String)  => ts.sparqlHttpUpdate(sparql)
+    case SparqlAskRequest(sparql: String)     => ts.sparqlHttpAsk(sparql)
     case ResetRepositoryContent(rdfDataObjects: Seq[RdfDataObject], prependDefaults: Boolean) =>
-      try2Message(sender(), resetTripleStoreContent(rdfDataObjects, prependDefaults), log)
-    case DropAllTRepositoryContent() => try2Message(sender(), dropAllTriplestoreContent(), log)
+      ts.resetTripleStoreContent(rdfDataObjects, prependDefaults)
+    case DropAllTRepositoryContent() => ts.dropAllTriplestoreContent()
     case InsertRepositoryContent(rdfDataObjects: Seq[RdfDataObject]) =>
-      try2Message(sender(), insertDataIntoTriplestore(rdfDataObjects), log)
-    case HelloTriplestore(msg: String) if msg == settings.triplestoreType =>
-      sender() ! HelloTriplestore(settings.triplestoreType)
-    case CheckTriplestoreRequest() => try2Message(sender(), checkFusekiTriplestore(), log)
-    case SearchIndexUpdateRequest(subjectIri: Option[String]) =>
-      try2Message(sender(), Success(SparqlUpdateResponse()), log)
-    case DownloadRepositoryRequest(outputFile: Path, featureFactoryConfig: FeatureFactoryConfig) =>
-      try2Message(sender(), downloadRepository(outputFile, featureFactoryConfig), log)
-    case UploadRepositoryRequest(inputFile: Path) => try2Message(sender(), uploadRepository(inputFile), log)
+      ts.insertDataIntoTriplestore(rdfDataObjects)
+    // case HelloTriplestore(msg: String) if msg == settings.triplestoreType => sender() ! HelloTriplestore(settings.triplestoreType)
+    case CheckTriplestoreRequest() => ts.checkTriplestore()
+    // case SearchIndexUpdateRequest(subjectIri: Option[String]) => try2Message(sender(), Success(SparqlUpdateResponse()), log)
+    case DownloadRepositoryRequest(outputFile: Path, _) =>
+      ts.downloadRepository(outputFile)
+    case UploadRepositoryRequest(inputFile: Path) => ts.uploadRepository(inputFile)
     case InsertGraphDataContentRequest(graphContent: String, graphName: String) =>
-      try2Message(sender(), insertDataGraphRequest(graphContent, graphName), log)
-    case SimulateTimeoutRequest() => try2Message(sender(), doSimulateTimeout(), log)
-    case other =>
-      sender() ! Status.Failure(
-        UnexpectedMessageException(s"Unexpected message $other of type ${other.getClass.getCanonicalName}")
-      )
+      ts.insertDataGraphRequest(graphContent, graphName)
+    case SimulateTimeoutRequest() => ts.doSimulateTimeout()
+    case other => ZIO.die(UnexpectedMessageException(s"Unexpected message $other of type ${other.getClass.getCanonicalName}"))
   }
 
 }
