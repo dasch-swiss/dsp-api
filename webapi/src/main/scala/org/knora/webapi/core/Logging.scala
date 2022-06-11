0ba692ac0dad6f60c39db3927a1c0e7623135b26
Ivan Subotic <400790+subotic@users.noreply.github.com>
2022-06-12 01:29:32 +0200

    change logging format

--- webapi/src/main/scala/org/knora/webapi/core/Logging.scala
+++ webapi/src/main/scala/org/knora/webapi/core/Logging.scala
@@ -11,6 +11,11 @@ object Logging {
 
   val textFormat: LogFormat =
     timestamp.fixed(32).color(LogColor.BLUE) |-| level.highlight.fixed(14) |-| line.highlight
+  
+  val colored: LogFormat =
+    label("timestamp", timestamp.fixed(32)).color(LogColor.BLUE) |-|
+      label("level", level).highlight |-|
+      label("message", quoted(line)).highlight
 
   val fromDebug: ZLayer[Any, Nothing, Unit] = {
     console(
@@ -22,7 +27,7 @@ object Logging {
   val fromInfo: ZLayer[Any, Nothing, Unit] = {
     console(
       logLevel = LogLevel.Info,
-      format = textFormat
+      format = colored
     )
   }
 
