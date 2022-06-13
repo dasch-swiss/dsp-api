9a0d6918e17e803c7e0dd2dc9442e684af519ba4
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-06-13 19:39:32 +0200

    reasonably implement project domain

--- dsp-project/core/src/main/scala/dsp/project/domain/ProjectDomain.scala
+++ dsp-project/core/src/main/scala/dsp/project/domain/ProjectDomain.scala
@@ -7,137 +7,90 @@ package dsp.project.domain
 
 import zio.prelude.Validation
 
-// import java.util.UUID
+import java.util.UUID
+import dsp.valueobjects.Project._
+import dsp.valueobjects.Iri._
 
-// TODO: all the following needs to be adjusted
+/**
+ * Stores the project ID, i.e. UUID, IRI and shortcode of the project
+ *
+ * @param uuid      the UUID of the project
+ * @param iri       the IRI of the project
+ * @param shortcode the shortcode of the project
+ */
+abstract case class ProjectId private (
+  uuid: UUID,
+  iri: ProjectIri,
+  shortcode: Shortcode
+)
 
-// // move this to shared value objects project once we have it
-// sealed trait Iri
-// object Iri {
+/**
+ * Companion object for ProjectId. Contains factory methods for creating ProjectId instances.
+ */
+object ProjectId {
 
-//   /**
-//    * UserIri value object.
-//    */
-//   sealed abstract case class UserIri private (value: String) extends Iri
-//   object UserIri { self =>
+  // TODO: add docs for shortcode
 
-//     def make(value: String): UserIri = new UserIri(value) {}
-//   }
-//   // ...
+  /**
+   * Generates a ProjectId instance with a new (random) UUID and an IRI which is created from a prefix and the UUID.
+   *
+   * @return a new ProjectId instance
+   */
+  def fromIri(iri: ProjectIri, shortcode: Shortcode): ProjectId = {
+    val uuid: UUID = UUID.fromString(iri.value.split("/").last)
+    new ProjectId(uuid = uuid, iri = iri, shortcode = shortcode) {}
+  }
 
-// }
+  /**
+   * Generates a ProjectId instance with a new (random) UUID and an IRI which is created from a prefix and the UUID.
+   *
+   * @return a new ProjectId instance
+   */
+  def fromUuid(uuid: UUID, shortcode: Shortcode): ProjectId = {
+    val iri: ProjectIri = ProjectIri.make(s"http://rdfh.ch/projects/${uuid}").fold(e => throw e.head, v => v)
+    new ProjectId(uuid = uuid, iri = iri, shortcode = shortcode) {}
+  }
 
-// /**
-//  * Stores the user ID, i.e. UUID and IRI of the user
-//  *
-//  * @param uuid the UUID of the user
-//  * @param iri the IRI of the user
-//  */
-// abstract case class UserId private (
-//   uuid: UUID,
-//   iri: Iri.UserIri
-// )
+  /**
+   * Generates a ProjectId instance with a new (random) UUID and an IRI which is created from a prefix and the UUID.
+   *
+   * @return a new ProjectId instance
+   */
+  def make(shortcode: Shortcode): ProjectId = {
+    val uuid: UUID      = UUID.randomUUID()
+    val iri: ProjectIri = ProjectIri.make(s"http://rdfh.ch/projects/${uuid}").fold(e => throw e.head, v => v)
+    new ProjectId(uuid = uuid, iri = iri, shortcode = shortcode) {}
+  }
+}
 
-// /**
-//  * Companion object for UserId. Contains factory methods for creating UserId instances.
-//  */
-// object UserId {
+/**
+ * Represents the project domain object.
+ *
+ * @param id          the ID of the project
+ * @param name        the name of the project
+ * @param description the description of the project
+ */
+sealed abstract case class Project private (
+  id: ProjectId,
+  name: String,
+  description: String
+) extends Ordered[Project] { self =>
 
-//   /**
-//    * Generates a UserId instance from a given string (either UUID or IRI).
-//    *
-//    * @param value the string to parse (either UUID or IRI)
-//    * @return a new UserId instance
-//    */
-//   // TODO not sure if we need this
-//   // def fromString(value: String): UserId = {
-//   //   val uuidPattern = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$".r
-//   //   val iriPattern  = "^http*".r
+  /**
+   * Allows to sort collections of [[User]]s. Sorting is done by the IRI.
+   */
+  def compare(that: Project): Int = self.id.iri.toString().compareTo(that.id.iri.toString())
 
-//   //   value match {
-//   //     case uuidPattern(value) => new UserId(UUID.fromString(value), Iri.UserIri.make(value)) {}
-//   //     case iriPattern(value) =>
-//   //       new UserId(UUID.fromString(value.substring(value.lastIndexOf("/") + 1)), Iri.UserIri.make(value)) {}
-//   //     //case _                  => ???
-//   //   }
-//   // }
-
-//   /**
-//    * Generates a UserId instance with a new (random) UUID and an IRI which is created from a prefix and the UUID.
-//    *
-//    * @return a new UserId instance
-//    */
-//   def fromIri(iri: Iri.UserIri): UserId = {
-//     val uuid: UUID = UUID.fromString(iri.value.split("/").last)
-//     new UserId(uuid, iri) {}
-//   }
-
-//   /**
-//    * Generates a UserId instance with a new (random) UUID and an IRI which is created from a prefix and the UUID.
-//    *
-//    * @return a new UserId instance
-//    */
-//   def fromUuid(uuid: UUID): UserId = {
-//     val iri: Iri.UserIri = Iri.UserIri.make("http://rdfh.ch/users/" + uuid.toString)
-//     new UserId(uuid, iri) {}
-//   }
-
-//   /**
-//    * Generates a UserId instance with a new (random) UUID and an IRI which is created from a prefix and the UUID.
-//    *
-//    * @return a new UserId instance
-//    */
-//   // TODO should this return a Validation[Throwable, UserId]
-//   def make(): UserId = {
-//     val uuid: UUID       = UUID.randomUUID()
-//     val iri: Iri.UserIri = Iri.UserIri.make("http://rdfh.ch/users/" + uuid.toString)
-//     new UserId(uuid, iri) {}
-//   }
-// }
-
-// /**
-//  * Represents the user domain object.
-//  *
-//  * @param id          the ID of the user
-//  * @param givenName   the given name of the user
-//  * @param familyName  the family name of the user
-//  * @param username    the username of the user
-//  * @param email       the email of the user
-//  * @param password    the password of the user
-//  * @param language    the user's preferred language
-//  * @param role        the user's role
-//  */
-// sealed abstract case class User private (
-//   id: UserId,
-//   givenName: GivenName,
-//   familyName: FamilyName,
-//   username: Username,
-//   email: Email,
-//   password: Option[Password],
-//   language: LanguageCode
-//   //role: Role
-// ) extends Ordered[User] { self =>
-
-//   /**
-//    * Allows to sort collections of [[User]]s. Sorting is done by the IRI.
-//    */
-//   def compare(that: User): Int = self.id.iri.toString().compareTo(that.id.iri.toString())
-
+  // TODO: add this kind of things
 //   def updateUsername(value: Username): User =
 //     new User(self.id, self.givenName, self.familyName, value, self.email, self.password, self.language) {}
-// }
-// object User {
-//   def make(
-//     givenName: GivenName,
-//     familyName: FamilyName,
-//     username: Username,
-//     email: Email,
-//     password: Password,
-//     language: LanguageCode
-//     //role: Role
-//   ): User = {
-//     val id = UserId.make()
-//     new User(id, givenName, familyName, username, email, Some(password), language) {}
-//   }
+}
+object Project {
+  def make(
+    id: ProjectId,
+    name: String,
+    description: String
+  ): Project =
+    new Project(id = id, name = name, description = description) {}
 
-// }
+}
