ad86d69f323e42979ba417a85bbbefe30fa2ca91
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-07-26 14:10:10 +0200

    add tests for SchemaCommands

---
+++ dsp-schema/core/src/test/scala/dsp/schema/domain/SchemaCommandsSpec.scala
@@ -0,0 +1,51 @@
+package dsp.schema.domain
+
+import zio._
+import zio.test._
+import zio.test.Assertion._
+import zio.prelude.Validation
+import java.time.Instant
+import dsp.valueobjects.LangString
+import dsp.valueobjects.LanguageCode
+import dsp.valueobjects.Schema
+import dsp.constants.SalsahGui
+
+/**
+ * This spec is used to test [[dsp.schema.domain.SchemaCommands]].
+ */
+object SchemaCommandsSpec extends ZIOSpecDefault {
+
+  def spec = (createPropertyCommandTest)
+
+  private val createPropertyCommandTest = suite("CreatePropertyCommand")(
+    test("create a createPropertyCommand") {
+      val ontologyIri          = SmartIri("Ontology IRI")
+      val lastModificationDate = Instant.now()
+      val propertyIri          = SmartIri("")
+      val subjectType          = None
+      val objectType           = SmartIri("Object Type")
+      val superProperties      = List(SmartIri("Super Property IRI"))
+      (for {
+        label             <- LangString.make(LanguageCode.en, "some label")
+        commentLangString <- LangString.make(LanguageCode.en, "some comment")
+        comment            = Some(commentLangString)
+        guiAttribute      <- Schema.GuiAttribute.make("hlist=http://rdfh.ch/lists/082F/PbRLUy66TsK10qNP1mBwzA")
+        guiElement        <- Schema.GuiElement.make(SalsahGui.List)
+        guiObject         <- Schema.GuiObject.make(guiAttributes = List(guiAttribute), guiElement = Some(guiElement))
+        command = CreatePropertyCommand.make(
+                    ontologyIri = ontologyIri,
+                    lastModificationDate = lastModificationDate,
+                    propertyIri = propertyIri,
+                    subjectType = subjectType,
+                    objectType = objectType,
+                    label = label,
+                    comment = comment,
+                    superProperties = superProperties,
+                    guiObject = guiObject
+                  )
+        // commant <- CreatePropertyCommand.make()
+      } yield assert(command.toEither)(isRight)).toZIO
+    }
+  )
+
+}
