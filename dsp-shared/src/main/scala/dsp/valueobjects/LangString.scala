236c10e6a4b67eb4d816977c273f105fc7be3810
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-07-26 13:16:55 +0200

    improve LangString value object

--- dsp-shared/src/main/scala/dsp/valueobjects/LangString.scala
+++ dsp-shared/src/main/scala/dsp/valueobjects/LangString.scala
@@ -3,11 +3,20 @@ package dsp.valueobjects
 import dsp.errors.ValidationException
 import zio.prelude.Validation
 
-// TODO: docstrings
-
+/**
+ * LangString value object
+ */
 sealed abstract case class LangString private (language: LanguageCode, value: String)
 
 object LangString {
+
+  /**
+   * Creates a [[zio.prelude.Validation]] that either fails with a ValidationException or succeeds with a LangString value object.
+   *
+   * @param language a [[LanguageCode]] value object representing the language of the LangString.
+   * @param value    the string value of the LangString.
+   * @return a Validation of a LangString value object.
+   */
   def make(language: LanguageCode, value: String): Validation[ValidationException, LangString] =
     if (value.isBlank()) {
       Validation.fail(ValidationException(LangStringErrorMessages.LangStringValueEmpty))
@@ -15,12 +24,27 @@ object LangString {
       Validation.succeed(new LangString(language, value) {})
     }
 
+  /**
+   * Creates a [[zio.prelude.Validation]] that either fails with a ValidationException or succeeds with a LangString value object.
+   *
+   * @param language a two-digit language code string representing the language of the LangString.
+   * @param value    the string value of the LangString.
+   * @return a Validation of a LangString value object.
+   */
   def makeFromStrings(language: String, value: String): Validation[ValidationException, LangString] =
     for {
       languageCode <- LanguageCode.make(language)
       langString   <- LangString.make(languageCode, value)
     } yield langString
 
+  /**
+   * Unsafely creates a LangString value object.
+   * Warning: skipps all validation. Should not be used unless there is no possibility for the data to be invalid.
+   *
+   * @param languagea [[LanguageCode]] value object representing the language of the LangString.
+   * @param value     the string value of the LangString.
+   * @return a LanguageCode value object
+   */
   def unsafeMake(language: LanguageCode, value: String): LangString =
     new LangString(language, value) {}
 }
