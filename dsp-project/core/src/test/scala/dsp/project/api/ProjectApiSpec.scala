687d456e3ba2ba0b1510332870da2005605cefd7
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-08-04 10:17:28 +0200

    delete test for interface

--- dsp-project/core/src/test/scala/dsp/project/api/ProjectApiSpec.scala
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright © 2021 - 2022 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-package dsp.project.api
-
-// TODO-BL: I don't think we should have to (or even can) test interfaces, so this should be removed
