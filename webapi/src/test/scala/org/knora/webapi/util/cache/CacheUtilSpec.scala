94976168581f2e898877400285c2653c68adacb8
irinaschubert <irina.schubert@dasch.swiss>
2022-09-14 16:49:00 +0200

    refactor CacheUtilSpec to remove settings

--- webapi/src/test/scala/org/knora/webapi/util/cache/CacheUtilSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/util/cache/CacheUtilSpec.scala
@@ -5,58 +5,40 @@
 
 package org.knora.webapi.util.cache
 
-import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import com.typesafe.scalalogging.LazyLogging
-import org.scalatest.BeforeAndAfterAll
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpecLike
 
-import org.knora.webapi.messages.StringFormatter
+import org.knora.webapi.CoreSpec
 import org.knora.webapi.routing.Authenticator
-import org.knora.webapi.settings.KnoraSettings
-import org.knora.webapi.settings.KnoraSettingsImpl
 import org.knora.webapi.sharedtestdata.SharedTestDataV1
 
-class CacheUtilSpec
-    extends TestKit(ActorSystem("CacheUtilSpec"))
-    with AnyWordSpecLike
-    with Matchers
-    with BeforeAndAfterAll
-    with LazyLogging
-    with Authenticator {
-
-  StringFormatter.initForTest()
-  val settings: KnoraSettingsImpl = KnoraSettings(system)
+class CacheUtilSpec extends CoreSpec {
 
   private val cacheName = Authenticator.AUTHENTICATION_INVALIDATION_CACHE_NAME
   private val sessionId = System.currentTimeMillis().toString
 
-  final override def beforeAll(): Unit = {
-    CacheUtil.removeAllCaches()
-    CacheUtil.createCaches(settings.caches)
-    // CacheUtil.createCaches(appConfig.cacheConfigs)
-  }
-
-  final override def afterAll(): Unit = {
-    CacheUtil.removeAllCaches()
-    TestKit.shutdownActorSystem(system)
-  }
-
   "Caching" should {
 
     "allow to set and get the value " in {
+      CacheUtil.removeAllCaches()
+      CacheUtil.createCaches(appConfig.cacheConfigs)
       CacheUtil.put(cacheName, sessionId, SharedTestDataV1.rootUser)
       CacheUtil.get(cacheName, sessionId) should be(Some(SharedTestDataV1.rootUser))
+      TestKit.shutdownActorSystem(system)
     }
 
     "return none if key is not found " in {
+      CacheUtil.removeAllCaches()
+      CacheUtil.createCaches(appConfig.cacheConfigs)
       CacheUtil.get(cacheName, 213.toString) should be(None)
+      TestKit.shutdownActorSystem(system)
     }
 
     "allow to delete a set value " in {
+      CacheUtil.removeAllCaches()
+      CacheUtil.createCaches(appConfig.cacheConfigs)
       CacheUtil.remove(cacheName, sessionId)
       CacheUtil.get(cacheName, sessionId) should be(None)
+      TestKit.shutdownActorSystem(system)
     }
   }
 }
