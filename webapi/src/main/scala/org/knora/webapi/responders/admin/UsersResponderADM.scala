1be262db6df435c22b7155f9dd2ee5075e287c16
irinaschubert <irina.schubert@dasch.swiss>
2022-10-03 15:32:55 +0200

    fix bug

--- webapi/src/main/scala/org/knora/webapi/responders/admin/UsersResponderADM.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/admin/UsersResponderADM.scala
@@ -314,11 +314,18 @@ class UsersResponderADM(responderData: ResponderData) extends Responder(responde
           getUserFromCacheOrTriplestore(identifier)
         }
 
+      // check if the requesting user is project admin of one of the projects the user belongs to
+      isRequestingUserProjectAdmin = maybeUserADM match {
+                                       case Some(user) =>
+                                         user.projects.map(p => requestingUser.permissions.isProjectAdmin(p.id))
+                                       case None => Seq(false)
+                                     }
+
       // return the correct amount of information depending on either the request or user permission
       finalResponse: Option[UserADM] =
         if (
           requestingUser.permissions.isSystemAdmin || requestingUser
-            .isSelf(identifier) || requestingUser.isSystemUser
+            .isSelf(identifier) || requestingUser.isSystemUser || isRequestingUserProjectAdmin.contains(true)
         ) {
           // return everything or what was requested
           maybeUserADM.map(user => user.ofType(userInformationType))
