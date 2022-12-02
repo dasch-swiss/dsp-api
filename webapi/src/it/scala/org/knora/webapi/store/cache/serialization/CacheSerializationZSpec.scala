c9d36fa8393c9dffb2210546b5084ba48b3cf718
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2022-12-02 11:54:33 +0100

    revert changes in CacheSerializationZSpec

--- webapi/src/it/scala/org/knora/webapi/store/cache/serialization/CacheSerializationZSpec.scala
+++ webapi/src/it/scala/org/knora/webapi/store/cache/serialization/CacheSerializationZSpec.scala
@@ -1,11 +1,16 @@
+/*
+ * Copyright © 2021 - 2022 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.store.cache.serialization
 
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectADM
 import org.knora.webapi.messages.admin.responder.usersmessages.UserADM
 import org.knora.webapi.sharedtestdata.SharedTestDataADM
-import zio.test.Assertion.equalTo
+import zio.test.Assertion._
 import zio.test.TestAspect.ignore
-import zio.test.ZIOSpecDefault
+import zio.test._
 
 /**
  * This spec is used to test [[CacheSerialization]].
