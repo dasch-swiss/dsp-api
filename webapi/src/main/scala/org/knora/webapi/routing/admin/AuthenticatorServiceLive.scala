2935a6c674d3847371ff1206b4e54b6a5461e41f
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2022-12-27 12:23:21 +0100

    clean up code: make credential extraction lazy, reuse Authenticator.calculateCookieName

--- webapi/src/main/scala/org/knora/webapi/routing/admin/AuthenticatorServiceLive.scala
+++ webapi/src/main/scala/org/knora/webapi/routing/admin/AuthenticatorServiceLive.scala
@@ -1,53 +1,43 @@
 package org.knora.webapi.routing.admin
 
-import org.apache.commons.codec.binary.Base32
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
 import zhttp.http._
 import zio.Task
 import zio.ZIO
 
+import scala.concurrent.ExecutionContext
+
 import org.knora.webapi.config.AppConfig
-import org.knora.webapi.core.AppRouter
 import org.knora.webapi.messages.StringFormatter
 import org.knora.webapi.messages.admin.responder.usersmessages._
 import org.knora.webapi.messages.v2.routing.authenticationmessages.KnoraCredentialsV2
 import org.knora.webapi.messages.v2.routing.authenticationmessages.KnoraCredentialsV2.KnoraJWTTokenCredentialsV2
 import org.knora.webapi.messages.v2.routing.authenticationmessages.KnoraCredentialsV2.KnoraPasswordCredentialsV2
 import org.knora.webapi.messages.v2.routing.authenticationmessages.KnoraCredentialsV2.KnoraSessionCredentialsV2
+import org.knora.webapi.responders.ActorDeps
 import org.knora.webapi.routing.Authenticator
 
-case class AuthenticatorServiceLive(appConfig: AppConfig, stringFormatter: StringFormatter, appRouter: AppRouter)
+case class AuthenticatorServiceLive(actorDeps: ActorDeps, appConfig: AppConfig, stringFormatter: StringFormatter)
     extends AuthenticatorService {
-  implicit val sf: StringFormatter = stringFormatter
-  implicit val system              = appRouter.system
-  implicit val ref                 = appRouter.ref
-  implicit val ec                  = appRouter.system.dispatcher
+  private implicit val sf: StringFormatter  = stringFormatter
+  private implicit val system: ActorSystem  = actorDeps.system
+  private implicit val appActor: ActorRef   = actorDeps.appActor
+  private implicit val ec: ExecutionContext = actorDeps.executionContext
 
-  def credsToUser(creds: Option[KnoraCredentialsV2]): Task[UserADM] =
-    ZIO.fromFuture(_ =>
-      Authenticator.getUserADMThroughCredentialsV2(
-        creds,
-        appConfig
-      )
-    )
   override def getUser(request: Request): Task[UserADM] =
-    ZIO.succeed(extractCredentials(request)).flatMap(credentials => credsToUser(creds = credentials))
-
-  val authCookieName: String = {
-    //
-    val base32 = new Base32('9'.toByte)
-    "KnoraAuthentication" + base32.encodeAsString(appConfig.knoraApi.externalKnoraApiHostPort.getBytes())
-  }
+    ZIO
+      .succeed(extractCredentials(request))
+      .flatMap(credentials => ZIO.fromFuture(_ => Authenticator.getUserADMThroughCredentialsV2(credentials, appConfig)))
 
   private def extractCredentials(request: Request): Option[KnoraCredentialsV2] = {
-    // Session token from cookie header
-    val maybeSessionCreds = request.cookieValue(authCookieName).map(c => KnoraSessionCredentialsV2(c.toString))
-
-    // Authorization header
-    val maybeBasicAuthCreds = request.basicAuthorizationCredentials.map(credentials =>
-      KnoraPasswordCredentialsV2(UserIdentifierADM(maybeUsername = Some(credentials.uname)), credentials.upassword)
+    val basicAuth = request.basicAuthorizationCredentials.map(c =>
+      KnoraPasswordCredentialsV2(UserIdentifierADM(Some(c.uname)), c.upassword)
     )
-    val maybeBearerTokenCreds = request.bearerToken.map(KnoraJWTTokenCredentialsV2(_))
-
-    maybeBasicAuthCreds.orElse(maybeBearerTokenCreds.orElse(maybeSessionCreds))
+    lazy val bearerToken   = request.bearerToken.map(KnoraJWTTokenCredentialsV2)
+    lazy val sessionCookie = request.cookieValue(authCookieName).map(c => KnoraSessionCredentialsV2(c.toString))
+    basicAuth.orElse(bearerToken.orElse(sessionCookie))
   }
+
+  private val authCookieName = Authenticator.calculateCookieName(appConfig)
 }
