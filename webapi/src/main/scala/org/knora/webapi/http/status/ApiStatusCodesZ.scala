20c47ce93bb4f845cf1f3da78c64856db032ed94
Christian Kleinbölting <seakayone@users.noreply.github.com>
2023-01-02 16:30:05 +0100

    Update webapi/src/main/scala/org/knora/webapi/http/status/ApiStatusCodesZ.scala
    Co-authored-by: Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>

--- webapi/src/main/scala/org/knora/webapi/http/status/ApiStatusCodesZ.scala
+++ webapi/src/main/scala/org/knora/webapi/http/status/ApiStatusCodesZ.scala
@@ -12,7 +12,7 @@ import org.knora.webapi.store.triplestore.errors.TriplestoreTimeoutException
 
 /**
  * The possible values for the HTTP status code that is returned as part of each Knora API v2 response.
- * migrated from [[org.knora.webapi.http.status.ApiStatusCodes]]
+ * Migrated from [[org.knora.webapi.http.status.ApiStatusCodes]]
  */
 object ApiStatusCodesZ {
 
