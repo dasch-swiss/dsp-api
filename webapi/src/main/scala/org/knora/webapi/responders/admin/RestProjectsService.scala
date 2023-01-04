ec226925a7a577f03cd43fd01a8adbd90ac42857
irinaschubert <irina.schubert@dasch.swiss>
2023-01-04 11:20:20 +0100

    remove unused method

--- webapi/src/main/scala/org/knora/webapi/responders/admin/RestProjectsService.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/admin/RestProjectsService.scala
@@ -3,29 +3,12 @@ import zio.Task
 import zio.URLayer
 import zio.ZLayer
 
-import org.knora.webapi.IRI
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectGetRequestADM
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectGetResponseADM
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectIdentifierADM
 import org.knora.webapi.responders.ActorToZioBridge
 
 final case class RestProjectsService(bridge: ActorToZioBridge) {
-
-  /**
-   * Finds the project by an [[IRI]] and returns the information as a [[ProjectGetResponseADM]].
-   *
-   * @param projectIri           an [[IRI]] identifying the project
-   * @return
-   *     '''success''': information about the project as a [[ProjectGetResponseADM]]
-   *
-   *     '''failure''': [[dsp.errors.NotFoundException]] when no project for the given IRI can be found
-   *                    [[dsp.errors.ValidationException]] if the given `projectIri` is invalid
-   */
-  def getSingleProjectADMRequest(projectIri: IRI): Task[ProjectGetResponseADM] =
-    ProjectIdentifierADM.IriIdentifier
-      .fromString(projectIri)
-      .toZIO
-      .flatMap(getSingleProjectADMRequest(_))
 
   /**
    * Finds the project by its [[ProjectIdentifierADM]] and returns the information as a [[ProjectGetResponseADM]].
