065b1bf8c905e17646ae14244456200078677a4a
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-01-04 22:14:52 +0100

    cleanup, remove unused code

--- webapi/src/main/scala/org/knora/webapi/slice/ontology/domain/model/Cardinality.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/ontology/domain/model/Cardinality.scala
@@ -4,7 +4,7 @@
  */
 
 package org.knora.webapi.slice.ontology.domain.model
-import dsp.schema.domain
+import dsp.schema.domain.{Cardinality => OldCardinality}
 import dsp.schema.domain.Cardinality.MayHaveMany
 import dsp.schema.domain.Cardinality.MayHaveOne
 import dsp.schema.domain.Cardinality.MustHaveOne
@@ -16,10 +16,7 @@ sealed trait Cardinality {
   def min: Int
   def max: Option[Int]
 
-  val oldCardinality: dsp.schema.domain.Cardinality
-
-  def isStricterThan(other: KnoraCardinalityInfo): Boolean =
-    Cardinality.get(other).isStricterThan(this)
+  val oldCardinality: OldCardinality
 
   def isStricterThan(other: Cardinality): Boolean = (other.min, other.max) match {
     case (otherMin, _) if otherMin < this.min => true
@@ -35,30 +32,32 @@ sealed trait Cardinality {
 
 object Cardinality {
   case object AtLeastOne extends Cardinality {
-    override val min: Int                           = 1
-    override val max: Option[Int]                   = None
-    override val oldCardinality: domain.Cardinality = MustHaveSome
+    override val min: Int                       = 1
+    override val max: Option[Int]               = None
+    override val oldCardinality: OldCardinality = MustHaveSome
   }
 
   case object ExactlyOne extends Cardinality {
-    override val min: Int                           = 1
-    override val max: Option[Int]                   = Some(1)
-    override val oldCardinality: domain.Cardinality = MustHaveOne
+    override val min: Int                       = 1
+    override val max: Option[Int]               = Some(1)
+    override val oldCardinality: OldCardinality = MustHaveOne
   }
 
   case object Unbounded extends Cardinality {
-    override val min: Int                           = 0
-    override val max: Option[Int]                   = None
-    override val oldCardinality: domain.Cardinality = MayHaveMany
+    override val min: Int                       = 0
+    override val max: Option[Int]               = None
+    override val oldCardinality: OldCardinality = MayHaveMany
   }
 
   case object ZeroOrOne extends Cardinality {
-    override val min: Int                           = 0
-    override val max: Option[Int]                   = Some(1)
-    override val oldCardinality: domain.Cardinality = MayHaveOne
+    override val min: Int                       = 0
+    override val max: Option[Int]               = Some(1)
+    override val oldCardinality: OldCardinality = MayHaveOne
   }
 
   val allCardinalities: Array[Cardinality] = Array(AtLeastOne, ExactlyOne, Unbounded, ZeroOrOne)
-  def get(cardinalityInfo: KnoraCardinalityInfo): Cardinality =
-    allCardinalities.find(_.oldCardinality == cardinalityInfo.cardinality).getOrElse(throw new IllegalStateException)
+
+  def get(cardinalityInfo: KnoraCardinalityInfo): Cardinality = get(cardinalityInfo.cardinality)
+  def get(cardinality: OldCardinality): Cardinality =
+    allCardinalities.find(_.oldCardinality == cardinality).getOrElse(throw new IllegalStateException)
 }
