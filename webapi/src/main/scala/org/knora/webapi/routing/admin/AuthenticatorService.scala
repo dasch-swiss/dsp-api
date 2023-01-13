88e067bbff6d0f43f586d6f26ffc1de513bc493f
irinaschubert <irina.schubert@dasch.swiss>
2023-01-13 10:28:04 +0100

    test: Add tests for ZIO HTTP project routes (#2377)
    Co-authored-by: Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
    Co-authored-by: Marcin Procyk <29251288+mpro7@users.noreply.github.com>

--- webapi/src/main/scala/org/knora/webapi/routing/admin/AuthenticatorService.scala
+++ webapi/src/main/scala/org/knora/webapi/routing/admin/AuthenticatorService.scala
@@ -1,15 +1,29 @@
+/*
+ * Copyright © 2021 - 2022 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.routing.admin
 
 import zhttp.http._
-import zio.Task
-import zio.ZLayer
+import zio._
 
 import org.knora.webapi.messages.admin.responder.usersmessages._
+import org.knora.webapi.messages.util.KnoraSystemInstances
 
 trait AuthenticatorService {
   def getUser(request: Request): Task[UserADM]
 }
 
 object AuthenticatorService {
-  val layer = ZLayer.fromFunction(AuthenticatorServiceLive(_, _, _))
+  val layer = ZLayer.fromFunction(AuthenticatorServiceLive.apply _)
+
+  def mock(user: Option[UserADM] = None) = ZLayer(
+    ZIO.succeed(
+      new AuthenticatorService() {
+        override def getUser(request: Request): Task[UserADM] =
+          ZIO.attempt(user.getOrElse(KnoraSystemInstances.Users.AnonymousUser))
+      }
+    )
+  )
 }
---
+++ webapi/src/test/scala/org/knora/webapi/responders/admin/ProjectsServiceLiveSpec.scala
@@ -0,0 +1,172 @@
+/*
+ * Copyright © 2021 - 2022 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.responders.admin
+
+import zio._
+import zio.mock._
+import zio.prelude.Validation
+import zio.test._
+
+import dsp.valueobjects.Iri._
+import dsp.valueobjects.Project.ShortCode
+import dsp.valueobjects.Project._
+import dsp.valueobjects.V2
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectCreatePayloadADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectCreateRequestADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectGetRequestADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectGetResponseADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectIdentifierADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectOperationResponseADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectsGetRequestADM
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectsGetResponseADM
+import org.knora.webapi.messages.store.triplestoremessages.StringLiteralV2
+import org.knora.webapi.messages.util.KnoraSystemInstances
+import org.knora.webapi.responders.ActorToZioBridge
+import org.knora.webapi.responders.ActorToZioBridgeMock
+import org.knora.webapi.responders.admin.ProjectsService
+
+object ProjectsServiceLiveSpec extends ZIOSpecDefault {
+
+  private val expectNoInteraction = ActorToZioBridgeMock.empty
+
+  val layers = ZLayer.makeSome[ActorToZioBridge, ProjectsService](ProjectsService.live)
+
+  /**
+   * Creates a [[ProjectADM]] with empty content or optionally with a given ID.
+   */
+  val projectADM =
+    ProjectADM(
+      id = "",
+      shortname = "",
+      shortcode = "",
+      longname = None,
+      description = Seq(StringLiteralV2("")),
+      keywords = Seq.empty,
+      logo = None,
+      ontologies = Seq.empty,
+      status = true,
+      selfjoin = false
+    )
+
+  val getAllProjectsSpec = test("get all projects") {
+    val expectedResponse = ProjectsGetResponseADM(Seq(projectADM))
+    val projectsService =
+      ZIO
+        .serviceWithZIO[ProjectsService](_.getProjectsADMRequest())
+        .provideSome[ActorToZioBridge](layers)
+    val actorToZioBridge = ActorToZioBridgeMock.AskAppActor
+      .of[ProjectsGetResponseADM]
+      .apply(assertion = Assertion.equalTo(ProjectsGetRequestADM()), result = Expectation.value(expectedResponse))
+      .toLayer
+    for {
+      _ <- projectsService.provide(actorToZioBridge)
+    } yield assertTrue(true)
+  }
+
+  val getProjectByIdSpec = suite("get project by identifier")(
+    test("get project by shortcode") {
+      val identifier = ProjectIdentifierADM.ShortcodeIdentifier
+        .fromString("0001")
+        .getOrElse(throw new Exception("invalid shortcode"))
+      val projectsService =
+        ZIO
+          .serviceWithZIO[ProjectsService](_.getSingleProjectADMRequest(identifier))
+          .provideSome[ActorToZioBridge](layers)
+      val actorToZioBridge = ActorToZioBridgeMock.AskAppActor
+        .of[ProjectGetResponseADM]
+        .apply(
+          assertion = Assertion.equalTo(ProjectGetRequestADM(identifier)),
+          result = Expectation.value(ProjectGetResponseADM(projectADM))
+        )
+        .toLayer
+      for {
+        _ <- projectsService.provide(actorToZioBridge)
+      } yield assertTrue(true)
+    },
+    test("get project by shortname") {
+      val identifier = ProjectIdentifierADM.ShortnameIdentifier
+        .fromString("someProject")
+        .getOrElse(throw new Exception("invalid shortname"))
+      val projectsService =
+        ZIO
+          .serviceWithZIO[ProjectsService](_.getSingleProjectADMRequest(identifier))
+          .provideSome[ActorToZioBridge](layers)
+      val actorToZioBridge = ActorToZioBridgeMock.AskAppActor
+        .of[ProjectGetResponseADM]
+        .apply(
+          assertion = Assertion.equalTo(ProjectGetRequestADM(identifier)),
+          result = Expectation.value(ProjectGetResponseADM(projectADM))
+        )
+        .toLayer
+      for {
+        _ <- projectsService.provide(actorToZioBridge)
+      } yield assertTrue(true)
+    },
+    test("get project by IRI") {
+      val identifier = ProjectIdentifierADM.IriIdentifier
+        .fromString("http://rdfh.ch/projects/0001")
+        .getOrElse(throw new Exception("invalid IRI"))
+      val projectsService =
+        ZIO
+          .serviceWithZIO[ProjectsService](_.getSingleProjectADMRequest(identifier))
+          .provideSome[ActorToZioBridge](layers)
+      val actorToZioBridge = ActorToZioBridgeMock.AskAppActor
+        .of[ProjectGetResponseADM]
+        .apply(
+          assertion = Assertion.equalTo(ProjectGetRequestADM(identifier)),
+          result = Expectation.value(ProjectGetResponseADM(projectADM))
+        )
+        .toLayer
+      for {
+        _ <- projectsService.provide(actorToZioBridge)
+      } yield assertTrue(true)
+    }
+  )
+
+  val createProjectSpec = test("create a project") {
+    val payload: ProjectCreatePayloadADM =
+      Validation
+        .validateWith(
+          ProjectIri.make(None),
+          ShortName.make("newproject"),
+          ShortCode.make("3333"),
+          Name.make(Some("project longname")),
+          ProjectDescription.make(Seq(V2.StringLiteralV2("project description", Some("en")))),
+          Keywords.make(Seq("test project")),
+          Logo.make(None),
+          ProjectStatus.make(true),
+          ProjectSelfJoin.make(false)
+        )(ProjectCreatePayloadADM.apply)
+        .getOrElse(throw new Exception("Invalid Payload"))
+    val requestingUser = KnoraSystemInstances.Users.SystemUser
+    val projectsService =
+      ZIO
+        .serviceWithZIO[ProjectsService](_.createProjectADMRequest(payload, requestingUser))
+        .provideSome[ActorToZioBridge](layers)
+    for {
+      uuid   <- ZIO.random.flatMap(_.nextUUID)
+      _      <- TestRandom.feedUUIDs(uuid)
+      request = ProjectCreateRequestADM(payload, requestingUser, uuid)
+      actorToZioBridge =
+        ActorToZioBridgeMock.AskAppActor
+          .of[ProjectOperationResponseADM]
+          .apply(
+            assertion = Assertion.equalTo(request),
+            result = Expectation.value(ProjectOperationResponseADM(projectADM))
+          )
+          .toLayer
+      _ <- projectsService.provide(actorToZioBridge)
+    } yield assertTrue(true)
+  }
+
+  override def spec: Spec[TestEnvironment with Scope, Any] =
+    suite("RestProjectsService")(
+      getAllProjectsSpec,
+      getProjectByIdSpec,
+      createProjectSpec
+    )
+}
