d501f59b59c62bee5016f0da7e1136e76b163959
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-01-31 12:03:33 +0100

    chore: update Scala to 2.13.10 (#2415)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     "daschswiss/apache-jena-fuseki:2.0.11" // should be the same version as in docker-compose.yml, also make sure to use the same version when deploying it (i.e. version in ops-deploy)!
   val sipiImage = "daschswiss/sipi:3.6.3" // base image the knora-sipi image is created from
 
-  val ScalaVersion = "2.13.8"
+  val ScalaVersion = "2.13.10"
 
   val AkkaActorVersion = "2.6.20"
   val AkkaHttpVersion  = "10.2.10"
