52be34710c92daaee5c21c383c4a8220c9ea6dfb
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-02-06 15:31:28 +0100

    add TriplestoreServiceFakeSpec test for querying subclass relations

--- webapi/src/test/scala/org/knora/webapi/store/triplestore/api/TriplestoreServiceFakeSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/store/triplestore/api/TriplestoreServiceFakeSpec.scala
@@ -7,6 +7,7 @@ package org.knora.webapi.store.triplestore.api
 import zio.test._
 
 import org.knora.webapi.messages.util.rdf._
+import org.knora.webapi.slice.resourceinfo.domain.IriTestConstants.Biblio
 import org.knora.webapi.store.triplestore.TestDatasetBuilder.datasetLayerFromTurtle
 
 object TriplestoreServiceFakeSpec extends ZIOSpecDefault {
@@ -20,81 +21,140 @@ object TriplestoreServiceFakeSpec extends ZIOSpecDefault {
       |
       |""".stripMargin
 
-  val spec = suite("TriplestoreServiceFake")(
-    suite("sparqlHttpAsk")(
-      test("should return true if thing exists") {
-        val query = """
-                      |PREFIX rdf:         <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                      |PREFIX anything:    <http://www.knora.org/ontology/0001/anything#>
-                      |
-                      |ASK WHERE {
-                      | <http://rdfh.ch/0001/knownThing> a anything:Thing .
-                      |}
-                      |""".stripMargin
-        for {
-          result <- TriplestoreService.sparqlHttpAsk(query)
-        } yield assertTrue(result.result)
-      },
-      test("should return false if thing dose not exist") {
-        val query = """
-                      |PREFIX rdf:         <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                      |PREFIX anything:    <http://www.knora.org/ontology/0001/anything#>
-                      |
-                      |ASK WHERE {
-                      | <http://rdfh.ch/0001/nonexisting> a anything:Thing .
-                      |}
-                      |""".stripMargin
-        for {
-          result <- TriplestoreService.sparqlHttpAsk(query)
-        } yield assertTrue(!result.result)
-      }
-    ),
-    suite("sparqlHttpSelect")(
-      test("not find non-existing thing") {
-        val query = """
-                      |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                      |
-                      |SELECT ?p ?o
-                      |WHERE {
-                      |  <http://rdfh.ch/0001/nonexisting> ?p ?o.
-                      |}
-                      |""".stripMargin
-        for {
-          result <- TriplestoreService.sparqlHttpSelect(query)
-        } yield assertTrue(
-          result == SparqlSelectResult(
-            SparqlSelectResultHeader(List("p", "o")),
-            SparqlSelectResultBody(List())
-          )
-        )
-      },
-      test("find an existing thing") {
-        val query = """
-                      |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-                      |
-                      |SELECT ?p ?o
-                      |WHERE {
-                      |  <http://rdfh.ch/0001/knownThing> ?p ?o.
-                      |}
-                      |""".stripMargin
-        for {
-          result <- TriplestoreService.sparqlHttpSelect(query)
-        } yield assertTrue(
-          result == SparqlSelectResult(
-            SparqlSelectResultHeader(List("p", "o")),
-            SparqlSelectResultBody(
-              List(
-                VariableResultsRow(
-                  Map(
-                    "p" -> "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-                    "o" -> "http://www.knora.org/ontology/0001/anything#Thing"
+  val spec: Spec[Any, Throwable] =
+    suite("foo")(
+      suite("TriplestoreServiceFake")(
+        suite("sparqlHttpAsk")(
+          test("should return true if thing exists") {
+            val query = """
+                          |PREFIX rdf:         <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                          |PREFIX anything:    <http://www.knora.org/ontology/0001/anything#>
+                          |
+                          |ASK WHERE {
+                          | <http://rdfh.ch/0001/knownThing> a anything:Thing .
+                          |}
+                          |""".stripMargin
+            for {
+              result <- TriplestoreService.sparqlHttpAsk(query)
+            } yield assertTrue(result.result)
+          },
+          test("should return false if thing does not exist") {
+            val query = """
+                          |PREFIX rdf:         <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                          |PREFIX anything:    <http://www.knora.org/ontology/0001/anything#>
+                          |
+                          |ASK WHERE {
+                          | <http://rdfh.ch/0001/nonexisting> a anything:Thing .
+                          |}
+                          |""".stripMargin
+            for {
+              result <- TriplestoreService.sparqlHttpAsk(query)
+            } yield assertTrue(!result.result)
+          }
+        ),
+        suite("sparqlHttpSelect")(
+          test("not find non-existing thing") {
+            val query = """
+                          |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                          |
+                          |SELECT ?p ?o
+                          |WHERE {
+                          |  <http://rdfh.ch/0001/nonexisting> ?p ?o.
+                          |}
+                          |""".stripMargin
+            for {
+              result <- TriplestoreService.sparqlHttpSelect(query)
+            } yield assertTrue(
+              result == SparqlSelectResult(
+                SparqlSelectResultHeader(List("p", "o")),
+                SparqlSelectResultBody(List())
+              )
+            )
+          },
+          test("find an existing thing") {
+            val query = """
+                          |PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                          |
+                          |SELECT ?p ?o
+                          |WHERE {
+                          |  <http://rdfh.ch/0001/knownThing> ?p ?o.
+                          |}
+                          |""".stripMargin
+            for {
+              result <- TriplestoreService.sparqlHttpSelect(query)
+            } yield assertTrue(
+              result == SparqlSelectResult(
+                SparqlSelectResultHeader(List("p", "o")),
+                SparqlSelectResultBody(
+                  List(
+                    VariableResultsRow(
+                      Map(
+                        "p" -> "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+                        "o" -> "http://www.knora.org/ontology/0001/anything#Thing"
+                      )
+                    )
                   )
                 )
               )
             )
-          )
+          }
         )
-      }
+      ).provide(TriplestoreServiceFake.layer, datasetLayerFromTurtle(testDataSet)),
+      suite("subclass relation querying")(
+        test("should find direct subclass (rdfs:subClassOf)") {
+          val query = s"""
+                         |PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                         |PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
+                         |PREFIX owl:    <http://www.w3.org/2002/07/owl#>
+                         |
+                         |SELECT ?entity
+                         |WHERE {
+                         |  ?entity    rdf:type          ?type .
+                         |  ?type      rdfs:subClassOf  <${Biblio.Class.Publication.value}> .
+                         |}
+                         |""".stripMargin
+          for {
+            result <- TriplestoreService.sparqlHttpSelect(query)
+          } yield assertTrue(result.results.bindings.size == 1)
+        },
+        test("should find all subclasses (rdfs:subClassOf*)") {
+          val query = s"""
+                         |PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                         |PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>
+                         |PREFIX owl:    <http://www.w3.org/2002/07/owl#>
+                         |
+                         |SELECT ?entity
+                         |WHERE {
+                         |  ?entity    rdf:type          ?type .
+                         |  ?type      rdfs:subClassOf*  <${Biblio.Class.Publication.value}> .
+                         |}
+                         |""".stripMargin
+          for {
+            result <- TriplestoreService.sparqlHttpSelect(query)
+          } yield assertTrue(result.results.bindings.size == 2)
+        }
+      ).provide(
+        TriplestoreServiceFake.layer,
+        datasetLayerFromTurtle(
+          s"""
+             |@prefix rdf:        <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+             |@prefix rdfs:       <http://www.w3.org/2000/01/rdf-schema#> .
+             |@prefix owl:        <http://www.w3.org/2002/07/owl#> .
+             |@prefix biblio:     <${Biblio.Ontology.value}> .
+             |
+             |<${Biblio.Class.Publication.value}>     rdf:type          owl:Class .
+             |
+             |<${Biblio.Class.Article.value}>         rdf:type          owl:Class ;
+             |                                        rdfs:subClassOf   <${Biblio.Class.Publication.value}> .
+             |
+             |<${Biblio.Class.JournalArticle.value}>  rdf:type          owl:Class ;
+             |                                        rdfs:subClassOf   <${Biblio.Class.Article.value}> .
+             |
+             |<http://anArticle>                      a                 <${Biblio.Class.Article.value}> .
+             |<http://aJournalArticle>                a                 <${Biblio.Class.JournalArticle.value}> .
+             |
+             |""".stripMargin
+        )
+      )
     )
-  ).provide(TriplestoreServiceFake.layer, datasetLayerFromTurtle(testDataSet))
 }
