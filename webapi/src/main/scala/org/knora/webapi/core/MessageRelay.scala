854d442a63722c1c1372b7fdd6908dab6466fb5a
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-02-14 15:58:02 +0100

    remove unused code

--- webapi/src/main/scala/org/knora/webapi/core/MessageRelay.scala
+++ webapi/src/main/scala/org/knora/webapi/core/MessageRelay.scala
@@ -1,6 +1,5 @@
 package org.knora.webapi.core
 import zio.Ref
-import zio.Runtime
 import zio.Task
 import zio.UIO
 import zio.ULayer
@@ -8,7 +7,6 @@ import zio.ZIO
 import zio.ZLayer
 
 import org.knora.webapi.messages.ResponderRequest
-import org.knora.webapi.routing.UnsafeZioRun
 
 trait MessageRelay {
   def ask(message: ResponderRequest): Task[Any]
@@ -34,7 +32,5 @@ case class MessageRelayLive(handlersRef: Ref[List[MessageHandler]]) extends Mess
 }
 
 object MessageRelayLive {
-  private val make: UIO[MessageRelay]               = Ref.make(List.empty[MessageHandler]).map(MessageRelayLive(_))
-  def empty(implicit r: Runtime[Any]): MessageRelay = UnsafeZioRun.runOrThrow(make)
   val layer: ULayer[MessageRelay]                   = ZLayer.fromZIO(Ref.make(List.empty[MessageHandler]).map(MessageRelayLive(_)))
 }
