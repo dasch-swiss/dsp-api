3a5b4f9331b8eed82ff8e89ea860e0b4e9ce101d
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-02-15 15:56:49 +0100

    add metrics to zio http server

--- webapi/src/main/scala/org/knora/webapi/core/HttpServerZ.scala
+++ webapi/src/main/scala/org/knora/webapi/core/HttpServerZ.scala
@@ -13,6 +13,7 @@ import zio.http.model.Method
 import org.knora.webapi.config.AppConfig
 import org.knora.webapi.routing.admin.ProjectsRouteZ
 import org.knora.webapi.slice.resourceinfo.api.ResourceInfoRoute
+import scala.annotation.tailrec
 
 object HttpServerZ {
 
@@ -35,14 +36,42 @@ object HttpServerZ {
     riRoute       <- ZIO.service[ResourceInfoRoute].map(_.route)
   } yield projectsRoute ++ riRoute
 
+  private def pathLabelMapper(): PartialFunction[Request, String] = { case r: Request =>
+    val slugs = Set(
+      "iri",
+      "shortcode",
+      "shortname"
+    )
+    @tailrec
+    def step(seg: List[Path.Segment], acc: Path): Path =
+      seg match {
+        case head :: slug :: next if slugs.contains(head.text) => step(next, acc / head.text / s":${head.text}")
+        case head :: next                                      => step(next, acc / head.text)
+        case Nil                                               => acc
+      }
+    // TODO: have to check if the assumption holds that a path with max. 4 segments doesn't contain any slugs
+    if (r.path.segments.size <= 4) r.path.toString()
+    else step(r.path.segments.toList, Path.empty).toString()
+  }
+
+  private def metricsMiddleware() =
+    Middleware.metrics(
+      pathLabelMapper = pathLabelMapper(),
+      concurrentRequestsName = "zio_http_concurrent_requests_total",
+      totalRequestsName = "zio_http_requests_total",
+      requestDurationName = "zio_http_request_duration_seconds"
+    )
+
   val layer: ZLayer[ResourceInfoRoute with ProjectsRouteZ with AppConfig, Nothing, Unit] = ZLayer {
     for {
-      port        <- ZIO.service[AppConfig].map(_.knoraApi.externalZioPort)
-      routes      <- apiRoutes
-      cors        <- corsMiddleware
-      serverConfig = ZLayer.succeed(ServerConfig.default.port(port))
-      _           <- Server.serve(routes @@ cors).provide(Server.live, serverConfig).forkDaemon
-      _           <- ZIO.logInfo(">>> Acquire ZIO HTTP Server <<<")
+      port                <- ZIO.service[AppConfig].map(_.knoraApi.externalZioPort)
+      routes              <- apiRoutes
+      cors                <- corsMiddleware
+      metrics              = metricsMiddleware()
+      routesWithMiddleware = routes @@ cors @@ metrics
+      serverConfig         = ZLayer.succeed(ServerConfig.default.port(port))
+      _                   <- Server.serve(routesWithMiddleware).provide(Server.live, serverConfig).forkDaemon
+      _                   <- ZIO.logInfo(">>> Acquire ZIO HTTP Server <<<")
     } yield ()
   }
 }
