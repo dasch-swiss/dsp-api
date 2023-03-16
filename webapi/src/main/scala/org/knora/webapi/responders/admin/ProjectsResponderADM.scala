181fdf008ca6e3e930047efe5b28eabc50411ecc
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-03-16 18:59:39 +0100

    Integrate findAll

--- webapi/src/main/scala/org/knora/webapi/responders/admin/ProjectsResponderADM.scala
+++ webapi/src/main/scala/org/knora/webapi/responders/admin/ProjectsResponderADM.scala
@@ -24,7 +24,6 @@ import org.knora.webapi.core.MessageHandler
 import org.knora.webapi.core.MessageRelay
 import org.knora.webapi.instrumentation.InstrumentationSupport
 import org.knora.webapi.messages.IriConversions._
-import org.knora.webapi.messages.OntologyConstants.KnoraAdmin._
 import org.knora.webapi.messages._
 import org.knora.webapi.messages.admin.responder.permissionsmessages._
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectIdentifierADM._
@@ -39,9 +38,6 @@ import org.knora.webapi.messages.store.cacheservicemessages.CacheServicePutProje
 import org.knora.webapi.messages.store.triplestoremessages._
 import org.knora.webapi.messages.util.KnoraSystemInstances
 import org.knora.webapi.messages.util.rdf._
-import org.knora.webapi.messages.v2.responder.ontologymessages.OntologyMetadataGetByProjectRequestV2
-import org.knora.webapi.messages.v2.responder.ontologymessages.OntologyMetadataV2
-import org.knora.webapi.messages.v2.responder.ontologymessages.ReadOntologyMetadataV2
 import org.knora.webapi.responders.IriLocker
 import org.knora.webapi.responders.IriService
 import org.knora.webapi.responders.Responder
@@ -236,61 +232,6 @@ final case class ProjectsResponderADMLive(
   }
 
   /**
-   * Gets all the projects and returns them as a sequence containing [[ProjectADM]].
-   *
-   * @return all the projects as a sequence containing [[ProjectADM]].
-   */
-  private def projectsGetADM(): Task[Seq[ProjectADM]] = {
-    val query = twirl.queries.sparql.admin.txt
-      .getProjects(
-        maybeIri = None,
-        maybeShortname = None,
-        maybeShortcode = None
-      )
-    for {
-      projectsResponse      <- triplestoreService.sparqlHttpExtendedConstruct(query.toString())
-      projectIris            = projectsResponse.statements.keySet.map(_.toString)
-      ontologiesForProjects <- getOntologiesForProjects(projectIris)
-      projects =
-        projectsResponse.statements.toList.map {
-          case (projectIriSubject: SubjectV2, propsMap: Map[SmartIri, Seq[LiteralV2]]) =>
-            val projectOntologies =
-              ontologiesForProjects.getOrElse(projectIriSubject.toString, Seq.empty[IRI])
-            convertStatementsToProjectADM(
-              statements = (projectIriSubject, propsMap),
-              ontologies = projectOntologies
-            )
-        }
-
-    } yield projects.sorted
-  }
-
-  /**
-   * Given a set of project IRIs, gets the ontologies that belong to each project.
-   *
-   * @param projectIris    a set of project IRIs. If empty, returns the ontologies for all projects.
-   * @return a map of project IRIs to sequences of ontology IRIs.
-   */
-  private def getOntologiesForProjects(projectIris: Set[IRI]): Task[Map[IRI, Seq[IRI]]] = {
-    def getIriPair(ontology: OntologyMetadataV2) =
-      ontology.projectIri.fold(
-        throw InconsistentRepositoryDataException(s"Ontology ${ontology.ontologyIri} has no project")
-      )(project => (project.toString, ontology.ontologyIri.toString))
-
-    val request = OntologyMetadataGetByProjectRequestV2(
-      projectIris = projectIris.map(_.toSmartIri),
-      requestingUser = KnoraSystemInstances.Users.SystemUser
-    )
-
-    for {
-      ontologyMetadataResponse <- messageRelay.ask[ReadOntologyMetadataV2](request)
-      ontologies                = ontologyMetadataResponse.ontologies.toList
-      iriPairs                  = ontologies.map(getIriPair)
-      projectToOntologyMap      = iriPairs.groupMap { case (project, _) => project } { case (_, onto) => onto }
-    } yield projectToOntologyMap
-  }
-
-  /**
    * Gets all the projects and returns them as a [[ProjectADM]].
    *
    * @return all the projects as a [[ProjectADM]].
@@ -473,7 +414,7 @@ final case class ProjectsResponderADMLive(
    */
   override def projectsKeywordsGetRequestADM(): Task[ProjectsKeywordsGetResponseADM] =
     for {
-      projects <- projectsGetADM()
+      projects <- projectRepo.findAll()
 
       keywords: Seq[String] = projects.flatMap(_.keywords).distinct.sorted
 
@@ -1156,46 +1097,6 @@ final case class ProjectsResponderADMLive(
       logger.debug("getProjectFromCacheOrTriplestore - caching disabled. getting from triplestore.")
       projectRepo.findByProjectIdentifier(identifier)
     }
-
-  /**
-   * Helper method that turns SPARQL result rows into a [[ProjectADM]].
-   *
-   * @param statements results from the SPARQL query representing information about the project.
-   * @param ontologies the ontologies in the project.
-   * @return a [[ProjectADM]] representing information about project.
-   */
-  @throws[InconsistentRepositoryDataException]("If the statements do not contain expected keys.")
-  private def convertStatementsToProjectADM(
-    statements: (SubjectV2, Map[SmartIri, Seq[LiteralV2]]),
-    ontologies: Seq[IRI]
-  ): ProjectADM = {
-    val projectIri: IRI                              = statements._1.toString
-    val propertiesMap: Map[SmartIri, Seq[LiteralV2]] = statements._2
-
-    def getOption[A <: LiteralV2](key: IRI): Option[Seq[A]] =
-      propertiesMap.get(key.toSmartIri).map(_.map(_.asInstanceOf[A]))
-
-    def getOrThrow[A <: LiteralV2](
-      key: IRI
-    ): Seq[A] =
-      getOption[A](key).getOrElse(
-        throw InconsistentRepositoryDataException(s"Project: $projectIri has no $key defined.")
-      )
-
-    val shortname = getOrThrow[StringLiteralV2](ProjectShortname).head.value
-    val shortcode = getOrThrow[StringLiteralV2](ProjectShortcode).head.value
-    val longname  = getOption[StringLiteralV2](ProjectLongname).map(_.head.value)
-    val description = getOrThrow[StringLiteralV2](ProjectDescription)
-      .map(desc => V2.StringLiteralV2(desc.value, desc.language))
-    val keywords = getOption[StringLiteralV2](ProjectKeyword).getOrElse(Seq.empty).map(_.value).sorted
-    val logo     = getOption[StringLiteralV2](ProjectLogo).map(_.head.value)
-    val status   = getOrThrow[BooleanLiteralV2](Status).head.value
-    val selfjoin = getOrThrow[BooleanLiteralV2](HasSelfJoinEnabled).head.value
-
-    val project =
-      ProjectADM(projectIri, shortname, shortcode, longname, description, keywords, logo, ontologies, status, selfjoin)
-    project.unescape
-  }
 
   /**
    * Helper method for checking if a project identified by shortname exists.
