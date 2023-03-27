e730ba464e0530674c0b11dd5b11679d43fd0797
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-03-27 16:25:27 +0200

    test: run integration tests against akka and zio routes (DEV-1585) (#2545)

--- webapi/src/it/scala/org/knora/webapi/core/LayersTest.scala
+++ webapi/src/it/scala/org/knora/webapi/core/LayersTest.scala
@@ -1,39 +1,11 @@
 package org.knora.webapi.core
 
-import org.knora.webapi.config.AppConfig
-import org.knora.webapi.config.AppConfigForTestContainers
-import org.knora.webapi.messages.StringFormatter
-import org.knora.webapi.responders.IriService
-import org.knora.webapi.responders.admin.GroupsResponderADM
-import org.knora.webapi.responders.admin.GroupsResponderADMLive
-import org.knora.webapi.responders.admin.ProjectsResponderADM
-import org.knora.webapi.responders.admin.ProjectsResponderADMLive
-import org.knora.webapi.responders.v1.UsersResponderV1
-import org.knora.webapi.responders.v1.UsersResponderV1Live
-import org.knora.webapi.routing.ApiRoutes
-import org.knora.webapi.slice.ontology.api.service.RestCardinalityServiceLive
-import org.knora.webapi.slice.ontology.domain.service.CardinalityService
-import org.knora.webapi.slice.ontology.repo.service.OntologyCache
-import org.knora.webapi.slice.ontology.repo.service.OntologyRepoLive
-import org.knora.webapi.slice.ontology.repo.service.PredicateRepositoryLive
-import org.knora.webapi.slice.resourceinfo.api.RestResourceInfoService
-import org.knora.webapi.slice.resourceinfo.domain.IriConverter
-import org.knora.webapi.slice.resourceinfo.domain.ResourceInfoRepo
-import org.knora.webapi.store.cache.api.CacheService
-import org.knora.webapi.store.cache.impl.CacheServiceInMemImpl
-import org.knora.webapi.store.iiif.api.IIIFService
-import org.knora.webapi.store.iiif.impl.IIIFServiceMockImpl
-import org.knora.webapi.store.iiif.impl.IIIFServiceSipiImpl
-import org.knora.webapi.store.triplestore.api.TriplestoreService
-import org.knora.webapi.store.triplestore.impl.TriplestoreServiceLive
-import org.knora.webapi.store.triplestore.upgrade.RepositoryUpdater
-import org.knora.webapi.testcontainers.FusekiTestContainer
-import org.knora.webapi.testcontainers.SipiTestContainer
-import org.knora.webapi.testservices.TestClientService
 import zio._
 
-import org.knora.webapi.messages.util.standoff.StandoffTagUtilV2
-import org.knora.webapi.messages.util.standoff.StandoffTagUtilV2Live
+import org.knora.webapi.config.AppConfig
+import org.knora.webapi.config.AppConfigForTestContainers
+import org.knora.webapi.http.middleware.AuthenticationMiddleware
+import org.knora.webapi.messages.StringFormatter
 import org.knora.webapi.messages.util.ConstructResponseUtilV2
 import org.knora.webapi.messages.util.ConstructResponseUtilV2Live
 import org.knora.webapi.messages.util.PermissionUtilADM
@@ -41,65 +13,98 @@ import org.knora.webapi.messages.util.PermissionUtilADMLive
 import org.knora.webapi.messages.util.ValueUtilV1
 import org.knora.webapi.messages.util.ValueUtilV1Live
 import org.knora.webapi.messages.util.search.QueryTraverser
-import org.knora.webapi.messages.util.search.gravsearch.types.GravsearchTypeInspectionUtil
 import org.knora.webapi.messages.util.search.SparqlTransformerLive
+import org.knora.webapi.messages.util.search.gravsearch.types.GravsearchTypeInspectionUtil
+import org.knora.webapi.messages.util.standoff.StandoffTagUtilV2
+import org.knora.webapi.messages.util.standoff.StandoffTagUtilV2Live
+import org.knora.webapi.responders.IriService
+import org.knora.webapi.responders.admin.GroupsResponderADM
+import org.knora.webapi.responders.admin.GroupsResponderADMLive
 import org.knora.webapi.responders.admin.ListsResponderADM
 import org.knora.webapi.responders.admin.ListsResponderADMLive
-import org.knora.webapi.responders.admin.UsersResponderADM
-import org.knora.webapi.responders.admin.UsersResponderADMLive
-import org.knora.webapi.slice.ontology.api.service.RestCardinalityService
-import org.knora.webapi.responders.admin.SipiResponderADM
-import org.knora.webapi.responders.admin.SipiResponderADMLive
 import org.knora.webapi.responders.admin.PermissionsResponderADM
 import org.knora.webapi.responders.admin.PermissionsResponderADMLive
-import org.knora.webapi.responders.v1.CkanResponderV1
-import org.knora.webapi.responders.v1.CkanResponderV1Live
-import org.knora.webapi.responders.v2.ListsResponderV2
-import org.knora.webapi.responders.v2.ListsResponderV2Live
-import org.knora.webapi.responders.v1.ListsResponderV1
-import org.knora.webapi.responders.v1.ListsResponderV1Live
-import org.knora.webapi.responders.v1.ProjectsResponderV1
-import org.knora.webapi.responders.v1.ProjectsResponderV1Live
-import org.knora.webapi.responders.v1.OntologyResponderV1
-import org.knora.webapi.responders.v1.OntologyResponderV1Live
-import org.knora.webapi.responders.v1.SearchResponderV1
-import org.knora.webapi.responders.v1.SearchResponderV1Live
+import org.knora.webapi.responders.admin.ProjectsResponderADM
+import org.knora.webapi.responders.admin.ProjectsResponderADMLive
+import org.knora.webapi.responders.admin.ProjectsServiceLive
+import org.knora.webapi.responders.admin.SipiResponderADM
+import org.knora.webapi.responders.admin.SipiResponderADMLive
 import org.knora.webapi.responders.admin.StoresResponderADM
 import org.knora.webapi.responders.admin.StoresResponderADMLive
-import org.knora.webapi.responders.v1.StandoffResponderV1
-import org.knora.webapi.responders.v1.StandoffResponderV1Live
-import org.knora.webapi.responders.v1.ValuesResponderV1
-import org.knora.webapi.responders.v1.ValuesResponderV1Live
+import org.knora.webapi.responders.admin.UsersResponderADM
+import org.knora.webapi.responders.admin.UsersResponderADMLive
+import org.knora.webapi.responders.v1.CkanResponderV1
+import org.knora.webapi.responders.v1.CkanResponderV1Live
+import org.knora.webapi.responders.v1.ListsResponderV1
+import org.knora.webapi.responders.v1.ListsResponderV1Live
+import org.knora.webapi.responders.v1.OntologyResponderV1
+import org.knora.webapi.responders.v1.OntologyResponderV1Live
+import org.knora.webapi.responders.v1.ProjectsResponderV1
+import org.knora.webapi.responders.v1.ProjectsResponderV1Live
 import org.knora.webapi.responders.v1.ResourcesResponderV1
 import org.knora.webapi.responders.v1.ResourcesResponderV1Live
-import org.knora.webapi.responders.v2.ResourceUtilV2
-import org.knora.webapi.responders.v2.ResourceUtilV2Live
-import org.knora.webapi.responders.v2.StandoffResponderV2
-import org.knora.webapi.responders.v2.StandoffResponderV2Live
-import org.knora.webapi.store.cache.CacheServiceRequestMessageHandler
-import org.knora.webapi.store.cache.CacheServiceRequestMessageHandlerLive
-import org.knora.webapi.store.iiif.IIIFRequestMessageHandler
-import org.knora.webapi.store.iiif.IIIFRequestMessageHandlerLive
-import org.knora.webapi.store.triplestore.TriplestoreRequestMessageHandler
-import org.knora.webapi.store.triplestore.TriplestoreRequestMessageHandlerLive
-import org.knora.webapi.responders.v2.ontology.CardinalityHandler
-import org.knora.webapi.responders.v2.ontology.CardinalityHandlerLive
-import org.knora.webapi.responders.v2.ontology.OntologyHelpers
-import org.knora.webapi.responders.v2.ontology.OntologyHelpersLive
+import org.knora.webapi.responders.v1.SearchResponderV1
+import org.knora.webapi.responders.v1.SearchResponderV1Live
+import org.knora.webapi.responders.v1.StandoffResponderV1
+import org.knora.webapi.responders.v1.StandoffResponderV1Live
+import org.knora.webapi.responders.v1.UsersResponderV1
+import org.knora.webapi.responders.v1.UsersResponderV1Live
+import org.knora.webapi.responders.v1.ValuesResponderV1
+import org.knora.webapi.responders.v1.ValuesResponderV1Live
+import org.knora.webapi.responders.v2.ListsResponderV2
+import org.knora.webapi.responders.v2.ListsResponderV2Live
 import org.knora.webapi.responders.v2.OntologyResponderV2
 import org.knora.webapi.responders.v2.OntologyResponderV2Live
+import org.knora.webapi.responders.v2.ResourceUtilV2
+import org.knora.webapi.responders.v2.ResourceUtilV2Live
 import org.knora.webapi.responders.v2.ResourcesResponderV2
 import org.knora.webapi.responders.v2.ResourcesResponderV2Live
 import org.knora.webapi.responders.v2.SearchResponderV2
 import org.knora.webapi.responders.v2.SearchResponderV2Live
-import org.knora.webapi.slice.ontology.domain.service.OntologyRepo
-import org.knora.webapi.slice.ontology.repo.service.OntologyCacheLive
+import org.knora.webapi.responders.v2.StandoffResponderV2
+import org.knora.webapi.responders.v2.StandoffResponderV2Live
 import org.knora.webapi.responders.v2.ValuesResponderV2
 import org.knora.webapi.responders.v2.ValuesResponderV2Live
+import org.knora.webapi.responders.v2.ontology.CardinalityHandler
+import org.knora.webapi.responders.v2.ontology.CardinalityHandlerLive
+import org.knora.webapi.responders.v2.ontology.OntologyHelpers
+import org.knora.webapi.responders.v2.ontology.OntologyHelpersLive
+import org.knora.webapi.routing.ApiRoutes
 import org.knora.webapi.routing.Authenticator
 import org.knora.webapi.routing.AuthenticatorLive
 import org.knora.webapi.routing.JwtService
 import org.knora.webapi.routing.JwtServiceLive
+import org.knora.webapi.routing.admin.AuthenticatorService
+import org.knora.webapi.routing.admin.ProjectsRouteZ
+import org.knora.webapi.slice.ontology.api.service.RestCardinalityService
+import org.knora.webapi.slice.ontology.api.service.RestCardinalityServiceLive
+import org.knora.webapi.slice.ontology.domain.service.CardinalityService
+import org.knora.webapi.slice.ontology.domain.service.OntologyRepo
+import org.knora.webapi.slice.ontology.repo.service.OntologyCache
+import org.knora.webapi.slice.ontology.repo.service.OntologyCacheLive
+import org.knora.webapi.slice.ontology.repo.service.OntologyRepoLive
+import org.knora.webapi.slice.ontology.repo.service.PredicateRepositoryLive
+import org.knora.webapi.slice.resourceinfo.api.RestResourceInfoService
+import org.knora.webapi.slice.resourceinfo.domain.IriConverter
+import org.knora.webapi.slice.resourceinfo.domain.ResourceInfoRepo
+import org.knora.webapi.store.cache.CacheServiceRequestMessageHandler
+import org.knora.webapi.store.cache.CacheServiceRequestMessageHandlerLive
+import org.knora.webapi.store.cache.api.CacheService
+import org.knora.webapi.store.cache.impl.CacheServiceInMemImpl
+import org.knora.webapi.store.iiif.IIIFRequestMessageHandler
+import org.knora.webapi.store.iiif.IIIFRequestMessageHandlerLive
+import org.knora.webapi.store.iiif.api.IIIFService
+import org.knora.webapi.store.iiif.impl.IIIFServiceMockImpl
+import org.knora.webapi.store.iiif.impl.IIIFServiceSipiImpl
+import org.knora.webapi.store.triplestore.TriplestoreRequestMessageHandler
+import org.knora.webapi.store.triplestore.TriplestoreRequestMessageHandlerLive
+import org.knora.webapi.store.triplestore.api.TriplestoreService
+import org.knora.webapi.store.triplestore.impl.TriplestoreServiceLive
+import org.knora.webapi.store.triplestore.upgrade.RepositoryUpdater
+import org.knora.webapi.testcontainers.FusekiTestContainer
+import org.knora.webapi.testcontainers.SipiTestContainer
+import org.knora.webapi.testservices.TestClientService
+import org.knora.webapi.slice.resourceinfo.api.ResourceInfoRoute
 object LayersTest {
 
   /**
@@ -168,6 +173,8 @@ object LayersTest {
     ZLayer.makeSome[CommonR0, CommonR](
       ApiRoutes.layer,
       AppRouter.layer,
+      AuthenticationMiddleware.layer,
+      AuthenticatorService.layer,
       AuthenticatorLive.layer,
       CacheServiceInMemImpl.layer,
       CacheServiceRequestMessageHandlerLive.layer,
@@ -177,6 +184,7 @@ object LayersTest {
       ConstructResponseUtilV2Live.layer,
       GroupsResponderADMLive.layer,
       HttpServer.layer,
+      HttpServerZ.layer,
       IIIFRequestMessageHandlerLive.layer,
       IriConverter.layer,
       IriService.layer,
@@ -194,8 +202,11 @@ object LayersTest {
       PredicateRepositoryLive.layer,
       ProjectsResponderADMLive.layer,
       ProjectsResponderV1Live.layer,
+      ProjectsRouteZ.layer,
+      ProjectsServiceLive.layer,
       RepositoryUpdater.layer,
       ResourceInfoRepo.layer,
+      ResourceInfoRoute.layer,
       ResourceUtilV2Live.layer,
       ResourcesResponderV1Live.layer,
       ResourcesResponderV2Live.layer,
