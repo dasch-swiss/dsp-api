7e23c38da1ed897f80a4f1997176b43370ff9a23
SamuelBoerlin <11892708+SamuelBoerlin@users.noreply.github.com>
2023-03-28 13:55:48 +0200

    Update webapi/scripts/healthcheck.sh
    Co-authored-by: irinaschubert <irina.schubert@dasch.swiss>

--- webapi/scripts/healthcheck.sh
+++ webapi/scripts/healthcheck.sh
@@ -7,7 +7,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Check severity reported by health route
+# Check severity reported by health route, exit with 1 if severity is other than "non fatal"
 severity=$(echo "$json" | jq -r .severity)
 if [ "$severity" != "non fatal" ]; then
   echo "Health route reports other than a non fatal severity"
