41379b5a46ddf9cbba6279b410bdda3c5ef86428
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-03-29 09:30:06 +0200

    add copyright headers

--- webapi/src/main/scala/org/knora/webapi/slice/admin/domain/model/KnoraProject.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/domain/model/KnoraProject.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.slice.admin.domain.model
 import dsp.valueobjects.V2.StringLiteralV2
 import org.knora.webapi.slice.resourceinfo.domain.InternalIri
