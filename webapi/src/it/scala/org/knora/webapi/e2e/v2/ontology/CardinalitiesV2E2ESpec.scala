760846f30b0cef5d6e38a9186898af7eecd17acb
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-04-06 10:18:46 +0200

    add more tests (red)

--- webapi/src/it/scala/org/knora/webapi/e2e/v2/ontology/CardinalitiesV2E2ESpec.scala
+++ webapi/src/it/scala/org/knora/webapi/e2e/v2/ontology/CardinalitiesV2E2ESpec.scala
@@ -259,7 +259,7 @@ class CardinalitiesV2E2ESpec extends E2ESpec {
       HttpEntity(RdfMediaTypes.`application/ld+json`, payload)
     ) ~> addCredentials(rootCredentials)
     val response = singleAwaitingRequest(request)
-    assert(response.status == StatusCodes.OK, responseToString(response))
+    response.status
   }
 
   private def getLastModificationDate(response: HttpResponse): String =
@@ -272,7 +272,7 @@ class CardinalitiesV2E2ESpec extends E2ESpec {
   private val rootCredentials = BasicHttpCredentials(SharedTestDataADM.rootUser.email, SharedTestDataADM.testPass)
 
   "Ontologies endpoint" should {
-    "be able to create resource instances with all properties when adding cardinalities on super class first" in {
+    "be able to create resource instances with all properties, when adding cardinalities on super class first" in {
 
       val projectIri = createProject("test1", "4441")
 
@@ -328,24 +328,26 @@ class CardinalitiesV2E2ESpec extends E2ESpec {
         )
       }
 
-      createValue(
+      val superStatus = createValue(
         projectIri = projectIri,
         ontologyIri = ontologyIri,
         ontologyName = ontologyName,
         className = superClassName,
         propertyNames = List(superClassProperty1, superClassProperty2)
       )
+      assert(superStatus == StatusCodes.OK)
 
-      createValue(
+      val subStatus = createValue(
         projectIri = projectIri,
         ontologyIri = ontologyIri,
         ontologyName = ontologyName,
         className = subClassName,
         propertyNames = List(superClassProperty1, superClassProperty2, subClassProperty1, subClassProperty2)
       )
+      assert(subStatus == StatusCodes.OK)
     }
 
-    "be able to create resource instances with all properties when adding cardinalities on sub class first" in {
+    "be able to create resource instances with all properties, when adding cardinalities on sub class first" in {
 
       val projectIri = createProject("test2", "4442")
 
@@ -401,21 +403,157 @@ class CardinalitiesV2E2ESpec extends E2ESpec {
         )
       }
 
-      createValue(
+      val superStatus = createValue(
         projectIri = projectIri,
         ontologyIri = ontologyIri,
         ontologyName = ontologyName,
         className = superClassName,
         propertyNames = List(superClassProperty1, superClassProperty2)
       )
+      assert(superStatus == StatusCodes.OK)
 
-      createValue(
+      val subStatus = createValue(
         projectIri = projectIri,
         ontologyIri = ontologyIri,
         ontologyName = ontologyName,
         className = subClassName,
         propertyNames = List(superClassProperty1, superClassProperty2, subClassProperty1, subClassProperty2)
       )
+      assert(subStatus == StatusCodes.OK)
+    }
+
+    "not be able to create subclass instances with missing properties defined on superclass, when adding cardinalities on super class first" in {
+
+      val projectIri = createProject("test3", "4443")
+
+      val (ontologyIri, lmd)   = createOntology(projectIri)
+      var lastModificationDate = lmd
+
+      val superClassName = "SuperClass"
+      val ontologyName   = "inherit"
+      lastModificationDate = createClass(
+        ontologyIri = ontologyIri,
+        ontologyName = ontologyName,
+        className = superClassName,
+        superClass = None,
+        lastModificationDate = lastModificationDate
+      )
+
+      val subClassName = "SubClass"
+      lastModificationDate = createClass(
+        ontologyIri = ontologyIri,
+        ontologyName = ontologyName,
+        className = subClassName,
+        superClass = Some(superClassName),
+        lastModificationDate = lastModificationDate
+      )
+
+      val superClassProperty1 = "superClassProperty1"
+      val superClassProperty2 = "superClassProperty2"
+      val subClassProperty1   = "subClassProperty1"
+      val subClassProperty2   = "subClassProperty2"
+      for (prop <- List(superClassProperty1, superClassProperty2, subClassProperty1, subClassProperty2)) {
+        lastModificationDate = createProperty(
+          ontologyIri = ontologyIri,
+          ontologyName = ontologyName,
+          propertyName = prop,
+          lastModificationDate = lastModificationDate
+        )
+      }
+
+      // first adding the the cardinalities to the *super*, then to the *sub* class
+      val clsAndProps = List(
+        (superClassName, superClassProperty1),
+        (superClassName, superClassProperty2),
+        (subClassName, subClassProperty1),
+        (subClassName, subClassProperty2)
+      )
+      for ((cls, prop) <- clsAndProps) {
+        lastModificationDate = addCardinalityToClass(
+          ontologyIri = ontologyIri,
+          ontologyName = ontologyName,
+          className = cls,
+          propertyName = prop,
+          lastModificationDate = lastModificationDate
+        )
+      }
+
+      val status = createValue(
+        projectIri = projectIri,
+        ontologyIri = ontologyIri,
+        ontologyName = ontologyName,
+        className = subClassName,
+        // missing mandatory props defined on super class
+        propertyNames = List(subClassProperty1, subClassProperty2)
+      )
+      assert(status == StatusCodes.BadRequest)
+    }
+
+    "not be able to create subclass instances with missing properties defined on superclass, when adding cardinalities on sub class first" in {
+
+      val projectIri = createProject("test4", "4444")
+
+      val (ontologyIri, lmd)   = createOntology(projectIri)
+      var lastModificationDate = lmd
+
+      val superClassName = "SuperClass"
+      val ontologyName   = "inherit"
+      lastModificationDate = createClass(
+        ontologyIri = ontologyIri,
+        ontologyName = ontologyName,
+        className = superClassName,
+        superClass = None,
+        lastModificationDate = lastModificationDate
+      )
+
+      val subClassName = "SubClass"
+      lastModificationDate = createClass(
+        ontologyIri = ontologyIri,
+        ontologyName = ontologyName,
+        className = subClassName,
+        superClass = Some(superClassName),
+        lastModificationDate = lastModificationDate
+      )
+
+      val superClassProperty1 = "superClassProperty1"
+      val superClassProperty2 = "superClassProperty2"
+      val subClassProperty1   = "subClassProperty1"
+      val subClassProperty2   = "subClassProperty2"
+      for (prop <- List(superClassProperty1, superClassProperty2, subClassProperty1, subClassProperty2)) {
+        lastModificationDate = createProperty(
+          ontologyIri = ontologyIri,
+          ontologyName = ontologyName,
+          propertyName = prop,
+          lastModificationDate = lastModificationDate
+        )
+      }
+
+      // first adding the the cardinalities to the *sub*, then to the *super* class
+      val clsAndProps = List(
+        (subClassName, subClassProperty1),
+        (subClassName, subClassProperty2),
+        (superClassName, superClassProperty1),
+        (superClassName, superClassProperty2)
+      )
+      for ((cls, prop) <- clsAndProps) {
+        lastModificationDate = addCardinalityToClass(
+          ontologyIri = ontologyIri,
+          ontologyName = ontologyName,
+          className = cls,
+          propertyName = prop,
+          lastModificationDate = lastModificationDate
+        )
+      }
+
+      val status = createValue(
+        projectIri = projectIri,
+        ontologyIri = ontologyIri,
+        ontologyName = ontologyName,
+        className = subClassName,
+        // missing mandatory props defined on super class
+        propertyNames = List(subClassProperty1, subClassProperty2)
+      )
+      assert(status == StatusCodes.BadRequest)
     }
 
   }
