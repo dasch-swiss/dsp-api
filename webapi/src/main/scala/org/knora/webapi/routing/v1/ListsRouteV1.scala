b690dc2923874b08a4926e28599b2f80d2b9c020
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-04-11 15:07:06 +0200

    ziofy

--- webapi/src/main/scala/org/knora/webapi/routing/v1/ListsRouteV1.scala
+++ webapi/src/main/scala/org/knora/webapi/routing/v1/ListsRouteV1.scala
@@ -6,6 +6,7 @@
 package org.knora.webapi.routing.v1
 
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.RequestContext
 import akka.http.scaladsl.server.Route
 import zio._
 
@@ -13,63 +14,66 @@ import dsp.errors.BadRequestException
 import org.knora.webapi.core.MessageRelay
 import org.knora.webapi.messages.StringFormatter
 import org.knora.webapi.messages.v1.responder.listmessages._
+import org.knora.webapi.messages.v1.responder.usermessages.UserProfileV1
 import org.knora.webapi.routing.Authenticator
-import org.knora.webapi.routing.KnoraRoute
-import org.knora.webapi.routing.KnoraRouteData
 import org.knora.webapi.routing.RouteUtilV1
 
 /**
  * Provides API routes that deal with lists.
  */
-final case class ListsRouteV1(
-  private val routeData: KnoraRouteData,
-  override protected implicit val runtime: Runtime[Authenticator with MessageRelay]
-) extends KnoraRoute(routeData, runtime) {
+final case class ListsRouteV1()(implicit
+  val runtime: Runtime[Authenticator with StringFormatter with MessageRelay]
+) {
 
-  /**
-   * Returns the route.
-   */
-  override def makeRoute: Route = {
+  def makeRoute: Route = {
 
     val stringFormatter = StringFormatter.getGeneralInstance
 
     path("v1" / "hlists" / Segment) { iri =>
       get { requestContext =>
-        val requestMessageFuture = for {
-          userProfile <- getUserADM(requestContext).map(_.asUserProfileV1)
-          listIri = stringFormatter.validateAndEscapeIri(
-                      iri,
-                      throw BadRequestException(s"Invalid param list IRI: $iri")
-                    )
-
-          requestMessage = requestContext.request.uri.query().get("reqtype") match {
-                             case Some("node")  => NodePathGetRequestV1(listIri, userProfile)
-                             case Some(reqtype) => throw BadRequestException(s"Invalid reqtype: $reqtype")
-                             case None          => HListGetRequestV1(listIri, userProfile)
-                           }
-        } yield requestMessage
-
-        RouteUtilV1.runJsonRouteF(requestMessageFuture, requestContext)
+        val requestTask = for {
+          user <- RouteUtilV1.getUserProfileV1(requestContext)
+          task <- makeGetHierarchicalListMessage(iri, user, requestContext)
+        } yield task
+        RouteUtilV1.runJsonRouteZ(requestTask, requestContext)
       }
     } ~
       path("v1" / "selections" / Segment) { iri =>
         get { requestContext =>
           val requestMessageFuture = for {
-            userProfile <- getUserADM(requestContext).map(_.asUserProfileV1)
-            selIri = stringFormatter.validateAndEscapeIri(
-                       iri,
-                       throw BadRequestException(s"Invalid param list IRI: $iri")
-                     )
-
-            requestMessage = requestContext.request.uri.query().get("reqtype") match {
-                               case Some("node")  => NodePathGetRequestV1(selIri, userProfile)
-                               case Some(reqtype) => throw BadRequestException(s"Invalid reqtype: $reqtype")
-                               case None          => SelectionGetRequestV1(selIri, userProfile)
-                             }
-          } yield requestMessage
-
-          RouteUtilV1.runJsonRouteF(requestMessageFuture, requestContext)
+            user <- RouteUtilV1.getUserProfileV1(requestContext)
+            task <- makeGetFlatListMessage(iri, user, requestContext)
+          } yield task
+          RouteUtilV1.runJsonRouteZ(requestMessageFuture, requestContext)
         }
       }
   }
+
+  private def makeGetHierarchicalListMessage(
+    iri: String,
+    userProfile: UserProfileV1,
+    requestContext: RequestContext
+  ): ZIO[StringFormatter with MessageRelay, Throwable, ListsResponderRequestV1] =
+    for {
+      listIri <- RouteUtilV1.validateAndEscapeIri(iri, s"Invalid param list IRI: $iri")
+      requestMessage <- requestContext.request.uri.query().get("reqtype") match {
+                          case Some("node")  => ZIO.succeed(NodePathGetRequestV1(listIri, userProfile))
+                          case Some(reqtype) => ZIO.fail(BadRequestException(s"Invalid reqtype: $reqtype"))
+                          case None          => ZIO.succeed(HListGetRequestV1(listIri, userProfile))
+                        }
+    } yield requestMessage
+
+  private def makeGetFlatListMessage(
+    iri: String,
+    userProfile: UserProfileV1,
+    requestContext: RequestContext
+  ): ZIO[StringFormatter with MessageRelay, Throwable, ListsResponderRequestV1] =
+    for {
+      selIri <- RouteUtilV1.validateAndEscapeIri(iri, s"Invalid param list IRI: $iri")
+      requestMessage <- requestContext.request.uri.query().get("reqtype") match {
+                          case Some("node")  => ZIO.succeed(NodePathGetRequestV1(selIri, userProfile))
+                          case Some(reqtype) => ZIO.fail(BadRequestException(s"Invalid reqtype: $reqtype"))
+                          case None          => ZIO.succeed(HListGetRequestV1(selIri, userProfile))
+                        }
+    } yield requestMessage
 }
