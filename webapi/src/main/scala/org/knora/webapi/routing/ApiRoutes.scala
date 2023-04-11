c581b7d27b14d32dcdd456f6d1589c9eba5f853e
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-04-11 11:54:29 +0200

    refactor: ziofy StandoffRouteV1 (DEV-2031)

--- webapi/src/main/scala/org/knora/webapi/routing/ApiRoutes.scala
+++ webapi/src/main/scala/org/knora/webapi/routing/ApiRoutes.scala
@@ -5,6 +5,7 @@
 
 package org.knora.webapi.routing
 
+import akka.actor
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives
@@ -89,7 +90,9 @@ private final case class ApiRoutesImpl(
 ) extends ApiRoutes
     with AroundDirectives {
 
-  val routes =
+  private implicit val system: actor.ActorSystem = routeData.system
+
+  val routes: Route =
     logDuration {
       ServerVersion.addServerHeader {
         DSPApiDirectives.handleErrors(routeData.system, appConfig) {
@@ -100,7 +103,7 @@ private final case class ApiRoutesImpl(
                 RejectingRoute(routeData, runtime).makeRoute ~
                 ResourcesRouteV1(routeData, runtime).makeRoute ~
                 ValuesRouteV1().makeRoute ~
-                StandoffRouteV1(routeData, runtime).makeRoute ~
+                StandoffRouteV1().makeRoute ~
                 ListsRouteV1(routeData, runtime).makeRoute ~
                 ResourceTypesRouteV1(routeData, runtime).makeRoute ~
                 SearchRouteV1(routeData, runtime).makeRoute ~
