8366733b692cc65b82919cc69c384a06792f660f
irinaschubert <irina.schubert@dasch.swiss>
2023-04-27 16:14:19 +0200

    handle video preview

--- sipi/scripts/sipi.init.lua
+++ sipi/scripts/sipi.init.lua
@@ -156,34 +156,53 @@ end
 --
 -- Returns:
 --    permission:
---       'allow' : currently all files are allowed
+--       'allow' : the view is allowed with the given IIIF parameters
+--       'deny' : no access!
 --    filepath: server-path where the master file is located
 -------------------------------------------------------------------------------
 
 function file_pre_flight(identifier, cookie)
     log("file_pre_flight called in sipi.init.lua", server.loglevel.LOG_DEBUG)
-    log("file_pre_flight - param idnetifier: " .. identifier, server.loglevel.LOG_DEBUG)
+    log("file_pre_flight - param identifier: " .. identifier, server.loglevel.LOG_DEBUG)
 
-    filepath = identifier
-
-    -- get the segments of the file path (should be `/sipi/images/:shortcode/:filename`)
+    -- get the segments of the identifier (should be `/sipi/images/:shortcode/:filename`)
     segments = {}
-
     for w in string.gmatch(identifier, "[^\\/]+") do
         table.insert(segments, w)
     end
 
-    if #segments ~= 4 then
-        log("file_pre_flight - segments should be exactly 4 elements. Got: [" .. table.concat(segments, ",") .. "]",
-            server.loglevel.LOG_ERR)
-        return 'allow', filepath -- normally this shouldn't be "allow"
-    end
-
+    -- get the shortcode
     shortcode = segments[3]
     log("file_pre_flight - shortcode: " .. shortcode, server.loglevel.LOG_DEBUG)
-    file_name = segments[4]
-    log("file_pre_flight - image name: " .. file_name, server.loglevel.LOG_DEBUG)
 
+    -- get the file name
+    if #segments == 4 then
+        file_name = segments[4]
+        file_name_preview  = ''
+    -- in case of a preview file of a video, get the file path of the video file to check permissions on the video
+    elseif #segments == 5 then
+        log("file_pre_flight - found 5 segments, it's assumed to the preview file for a video", server.loglevel.LOG_ERR)
+        file_name = segments[4] .. '.mp4'
+        file_name_preview = segments[4] .. '/' .. segments[5]
+    else
+        log("file_pre_flight - wrong number of segments. Got: [" .. table.concat(segments, ",") .. "]", server.loglevel.LOG_ERR)
+        return "deny"
+    end
+
+    
+    log("file_pre_flight - file name: " .. file_name, server.loglevel.LOG_DEBUG)
+
+    local first_character_of_identifier = file_name:sub(1, 1)
+    local second_character_of_identifier = file_name:sub(2, 2)
+
+    filepath = config.imgroot .. '/' .. shortcode .. '/' .. first_character_of_identifier .. '/' .. second_character_of_identifier .. '/' .. file_name
+    filepath_preview = config.imgroot .. '/' .. shortcode .. '/' .. first_character_of_identifier .. '/' .. second_character_of_identifier .. '/' .. file_name_preview
+
+    if shortcode == "082A" then -- SVA / 082A allows file access even when permissions are set to restricted
+        return "allow", filepath
+    end
+
+    log("file_pre_flight - filepath: " .. filepath, server.loglevel.LOG_DEBUG)
 
     dsp_cookie_header = nil
 
@@ -207,14 +226,11 @@ function file_pre_flight(identifier, cookie)
     if webapi_hostname == nil then
         webapi_hostname = config.knora_path
     end
-    log("file_pre_flight - webapi_hostname: " .. webapi_hostname,
-        server.loglevel.LOG_DEBUG)
 
     local webapi_port = os.getenv("SIPI_WEBAPI_PORT")
     if webapi_port == nil then
         webapi_port = config.knora_port
     end
-    log("file_pre_flight - webapi_port: " .. webapi_port, server.loglevel.LOG_DEBUG)
 
     api_url = 'http://' .. webapi_hostname .. ':' .. webapi_port .. '/admin/files/' .. shortcode .. '/' .. file_name
 
@@ -226,14 +242,14 @@ function file_pre_flight(identifier, cookie)
     if not success then
         log("file_pre_flight - Server.http() failed: " .. result,
             server.loglevel.LOG_ERR)
-        return 'allow', filepath -- normally this shouldn't be "allow"
+        return 'deny'
     end
 
     if result.status_code ~= 200 then
         log("file_pre_flight - DSP-API returned HTTP status code " ..
         result.status_code, server.loglevel.LOG_ERR)
         log("result body: " .. result.body, server.loglevel.LOG_ERR)
-        return 'allow', filepath -- normally this shouldn't be "allow"
+        return 'deny'
     end
 
     log("file_pre_flight - response body: " .. tostring(result.body),
@@ -242,7 +258,7 @@ function file_pre_flight(identifier, cookie)
     success, response_json = server.json_to_table(result.body)
     if not success then
         log("file_pre_flight - Server.http() failed: " .. response_json, server.loglevel.LOG_WARNING)
-        return 'allow', filepath -- normally this shouldn't be "allow"
+        return 'deny'
     end
 
     log("file_pre_flight - permission code: " .. response_json.permissionCode,
@@ -250,18 +266,22 @@ function file_pre_flight(identifier, cookie)
 
     if response_json.permissionCode == 0 then
         -- no view permission on file
-        log("file_pre_flight - API reported 'deny', returning 'allow' instead", server.loglevel.LOG_WARNING)
-        return 'allow', filepath -- normally this shouldn't be "allow"
+        log("file_pre_flight - permission code 0 (no view), access denied", server.loglevel.LOG_WARNING)
+        return 'deny'
     elseif response_json.permissionCode == 1 then
         -- restricted view permission on file
-        log("file_pre_flight - API reported 'restricted', returning 'allow' instead", server.loglevel.LOG_WARNING)
-        return 'allow', filepath -- normally this shouldn't be "allow"
+        log("file_pre_flight - permission code 1 (restricted view), access denied", server.loglevel.LOG_WARNING)
+        return 'deny'
     elseif response_json.permissionCode >= 2 then
         -- full view permissions on file
         log("file_pre_flight - access granted", server.loglevel.LOG_DEBUG)
-        return 'allow', filepath
+        if #segments == 5 then
+            return 'allow', filepath_preview
+        else
+            return 'allow', filepath
+        end
     else
         -- invalid permission code
-        return 'allow', filepath -- normally this shouldn't be "allow"
+        return 'deny'
     end
 end
