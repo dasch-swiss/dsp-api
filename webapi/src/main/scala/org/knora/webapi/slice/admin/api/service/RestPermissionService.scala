1041abe3d33fbd4acf3248b484a3d7f0bbe59831
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-05-02 15:24:26 +0200

    scaladoc

--- webapi/src/main/scala/org/knora/webapi/slice/admin/api/service/RestPermissionService.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/api/service/RestPermissionService.scala
@@ -10,12 +10,40 @@ import org.knora.webapi.slice.admin.api.service.RestPermissionService.isSystemAd
 import org.knora.webapi.slice.admin.api.service.RestPermissionService.isSystemOrProjectAdmin
 import org.knora.webapi.slice.admin.domain.model.KnoraProject
 
+/**
+ * Provides methods for checking permissions.
+ * This service is used by the REST API services.
+ * All `ensure…` methods fail with a [[ForbiddenException]] if the user is not active or the respective check fails.
+ * @see [[RestPermissionServiceLive]].
+ */
 @accessible
 trait RestPermissionService {
+
+  /**
+   * Checks if the user is a system administrator.
+   * Checks if the user is active.
+   * @param user The [[UserADM]] to check.
+   * @return [[Unit]] if the user is active and a system administrator.
+   *         Fails with a [[ForbiddenException]] otherwise.
+   */
   def ensureSystemAdmin(user: UserADM): IO[ForbiddenException, Unit]
+
+  /**
+   * Checks if the user is a system or project administrator.
+   * Checks if the user is active.
+   * @param user The [[UserADM]] to check.
+   * @param project The [[KnoraProject]] to check.
+   * @return [[Unit]] if the user is active and is a system or project administrator.
+   *         Fails with a [[ForbiddenException]] otherwise.
+   */
   def ensureSystemOrProjectAdmin(user: UserADM, project: KnoraProject): IO[ForbiddenException, Unit]
 }
 
+/**
+ * Provides helper methods for checking permissions.
+ * All functions are pure.
+ * Functions do not check if the user is active.
+ */
 object RestPermissionService {
   def isActive(userADM: UserADM): Boolean                           = userADM.status
   def isSystemAdmin(user: UserADM): Boolean                         = user.permissions.isSystemAdmin
@@ -35,11 +63,9 @@ final case class RestPermissionServiceLive() extends RestPermissionService {
   private def failUnless(condition: Boolean, errorMsg: String): ZIO[Any, ForbiddenException, Unit] =
     ZIO.fail(ForbiddenException(errorMsg)).unless(condition).unit
 
-  private def ensureIsActive(userADM: UserADM) =
-    failUnless(
-      isActive(userADM),
-      s"The account with username '${userADM.username}' is not active."
-    )
+  private def ensureIsActive(userADM: UserADM): IO[ForbiddenException, Unit] =
+    failUnless(isActive(userADM), s"The account with username '${userADM.username}' is not active.")
+
   override def ensureSystemOrProjectAdmin(user: UserADM, project: KnoraProject): IO[ForbiddenException, Unit] =
     ensureIsActive(user) *>
       failUnless(
