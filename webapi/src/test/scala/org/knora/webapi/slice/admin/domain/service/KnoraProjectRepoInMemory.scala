e68daa62bcf9dcae893c9a186d1740d793d262c6
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-05-02 19:53:02 +0200

    Add AbstractInMemoryCrudRepository

--- webapi/src/test/scala/org/knora/webapi/slice/admin/domain/service/KnoraProjectRepoInMemory.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/admin/domain/service/KnoraProjectRepoInMemory.scala
@@ -2,24 +2,18 @@ package org.knora.webapi.slice.admin.domain.service
 
 import zio.Ref
 import zio.Task
-import zio.UIO
 import zio.ULayer
 import zio.ZLayer
 
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectIdentifierADM
 import org.knora.webapi.slice.admin.domain.model.KnoraProject
-import org.knora.webapi.slice.common.service.CrudRepository
+import org.knora.webapi.slice.common.repo.AbstractInMemoryCrudRepository
 import org.knora.webapi.slice.resourceinfo.domain.InternalIri
-final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]])
-    extends KnoraProjectRepo
-    with CrudRepository[KnoraProject, InternalIri] {
 
-  /**
-   * Retrieves an entity by its id.
-   *
-   * @param id The identifier of type [[ProjectIdentifierADM]].
-   * @return the entity with the given id or [[None]] if none found.
-   */
+final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]])
+    extends AbstractInMemoryCrudRepository[KnoraProject, InternalIri](projects, _.id)
+    with KnoraProjectRepo {
+
   override def findById(id: ProjectIdentifierADM): Task[Option[KnoraProject]] = projects.get.map(
     _.find(
       id match {
@@ -29,44 +23,6 @@ final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]])
       }
     )
   )
-
-  /**
-   * Retrieves an entity by its id.
-   *
-   * @param id The identifier of type [[InternalIri]].
-   * @return the entity with the given id or [[None]] if none found.
-   */
-  override def findById(id: InternalIri): Task[Option[KnoraProject]] = projects.get.map(_.find(_.id == id))
-
-  /**
-   * Returns all instances of the type.
-   *
-   * @return all instances of the type.
-   */
-  override def findAll(): Task[List[KnoraProject]] = projects.get
-
-  /**
-   * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the entity instance completely.
-   *
-   * @param entity The entity to be saved.
-   * @return the saved entity.
-   */
-  override def save(entity: KnoraProject): Task[KnoraProject] = projects.update(entity :: _).as(entity)
-
-  /**
-   * Deletes a given entity.
-   *
-   * @param entity The entity to be deleted
-   */
-  override def delete(entity: KnoraProject): Task[Unit] = deleteById(entity.id)
-
-  /**
-   * Deletes the entity with the given id.
-   * If the entity is not found in the persistence store it is silently ignored.
-   *
-   * @param id The identifier to the entity to be deleted
-   */
-  override def deleteById(id: InternalIri): Task[Unit] = projects.update(_.filterNot(_.id == id)).unit
 }
 
 object KnoraProjectRepoInMemory {
---
+++ webapi/src/test/scala/org/knora/webapi/slice/common/repo/AbstractInMemoryCrudRepository.scala
@@ -0,0 +1,48 @@
+package org.knora.webapi.slice.common.repo
+
+import zio.Ref
+import zio.Task
+
+import org.knora.webapi.slice.common.service.CrudRepository
+
+abstract class AbstractInMemoryCrudRepository[Entity, Id](entities: Ref[List[Entity]], getId: Entity => Id)
+    extends CrudRepository[Entity, Id] {
+
+  /**
+   * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the entity instance completely.
+   *
+   * @param entity The entity to be saved.
+   * @return the saved entity.
+   */
+  override def save(entity: Entity): Task[Entity] = entities.update(entity :: _).as(entity)
+
+  /**
+   * Deletes a given entity.
+   *
+   * @param entity The entity to be deleted
+   */
+  override def delete(entity: Entity): Task[Unit] = deleteById(getId(entity))
+
+  /**
+   * Deletes the entity with the given id.
+   * If the entity is not found in the persistence store it is silently ignored.
+   *
+   * @param id The identifier to the entity to be deleted
+   */
+  override def deleteById(id: Id): Task[Unit] = entities.update(_.filterNot(getId(_) == id))
+
+  /**
+   * Retrieves an entity by its id.
+   *
+   * @param id The identifier of type [[Id]].
+   * @return the entity with the given id or [[None]] if none found.
+   */
+  override def findById(id: Id): Task[Option[Entity]] = entities.get.map(_.find(getId(_) == id))
+
+  /**
+   * Returns all instances of the type.
+   *
+   * @return all instances of the type.
+   */
+  override def findAll(): Task[List[Entity]] = entities.get
+}
