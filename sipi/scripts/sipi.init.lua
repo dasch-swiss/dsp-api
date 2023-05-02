8f78c0958b85ecfbdba8b578057a158c5a3a7d09
irinaschubert <irina.schubert@dasch.swiss>
2023-05-02 16:56:36 +0200

    fix preview issue when permission is set to restricted

--- sipi/scripts/sipi.init.lua
+++ sipi/scripts/sipi.init.lua
@@ -5,6 +5,94 @@ require "get_knora_session"
 require "log_util"
 
 -------------------------------------------------------------------------------
+-- This function returns the filepath of the temporary location the file is 
+-- stored in. The path is created from the first four characters of the filename.
+-------------------------------------------------------------------------------
+function get_tmp_filepath(shortcode, filename)
+    local first_character_of_filename = filename:sub(1, 1)
+    local second_character_of_filename = filename:sub(2, 2)
+    local third_character_of_filename = filename:sub(3, 3)
+    local fourth_character_of_filename = filename:sub(4, 4)
+
+    local first_subfolder = first_character_of_filename .. second_character_of_filename
+    local second_subfolder = third_character_of_filename .. fourth_character_of_filename
+
+    local filepath = ''
+    if config.prefix_as_path then
+        filepath = config.imgroot .. '/' .. shortcode .. '/' .. first_subfolder .. '/' .. second_subfolder .. '/' .. filename
+    else
+        filepath = config.imgroot .. '/' .. first_subfolder .. '/' .. second_subfolder .. '/' .. filename
+    end
+    return filepath
+end
+
+-------------------------------------------------------------------------------
+-- This function returns the segments from the identifier
+-- The identifier is expected to look like: /sipi/images/:shortcode/:filename
+-------------------------------------------------------------------------------
+function get_segments_from_identifier(identifier)
+    segments = {}
+    for w in string.gmatch(identifier, "[^\\/]+") do
+        table.insert(segments, w)
+    end
+    return segments
+end
+
+-------------------------------------------------------------------------------
+-- This function checks the cookie and returns the cookie header
+-------------------------------------------------------------------------------
+function get_cookie_header(cookie)
+    -- tries to extract the DSP session name and id from the cookie:
+    -- gets the digits between "sid=" and the closing ";" (only given in case of several key value pairs)
+    -- returns nil if it cannot find it
+    session = get_session_id(cookie)
+
+    if session == nil or session["name"] == nil or session["id"] == nil then
+        -- no session could be extracted
+        log("check_cookie - cookie key is invalid: " .. cookie, server.loglevel.LOG_ERR)
+    else
+        dsp_cookie_header = { Cookie = session["name"] .. "=" .. session["id"] }
+        log("check_cookie - dsp_cookie_header: " ..
+        dsp_cookie_header["Cookie"], server.loglevel.LOG_DEBUG)
+    end
+
+    return dsp_cookie_header
+end
+
+-------------------------------------------------------------------------------
+-- This function gets the hostname of the DSP-API
+-- either from the environment variable or from config
+-------------------------------------------------------------------------------
+function get_api_hostname()
+    hostname = os.getenv("SIPI_WEBAPI_HOSTNAME")
+    if hostname == nil then
+        hostname = config.knora_path
+    end
+
+    return hostname
+end
+
+-------------------------------------------------------------------------------
+-- This function gets the port of the DSP-API 
+-- either from the environment variable or from config
+-------------------------------------------------------------------------------
+function get_api_port()
+    port = os.getenv("SIPI_WEBAPI_PORT")
+    if port == nil then
+        port = config.knora_port
+    end
+
+    return port
+end
+
+
+-------------------------------------------------------------------------------
+-- This function returns the API URL from the given parameters
+-------------------------------------------------------------------------------
+function get_api_url(webapi_hostname, webapi_port, prefix, identifier)
+    return 'http://' .. webapi_hostname .. ':' .. webapi_port .. '/admin/files/' .. prefix .. '/' .. identifier
+end
+-------------------------------------------------------------------------------
 -- This function is being called from sipi before the file is served.
 -- DSP-API is called to ask for the user's permissions on the file
 -- Parameters:
@@ -22,20 +110,9 @@ require "log_util"
 -------------------------------------------------------------------------------
 function pre_flight(prefix, identifier, cookie)
     log("pre_flight called in sipi.init.lua", server.loglevel.LOG_DEBUG)
+    log("pre_flight - param identifier: " .. identifier, server.loglevel.LOG_DEBUG)
 
-    local first_character_of_identifier = identifier:sub(1, 1)
-    local second_character_of_identifier = identifier:sub(2, 2)
-    local third_character_of_identifier = identifier:sub(3, 3)
-    local fourth_character_of_identifier = identifier:sub(4, 4)
-
-    local first_subfolder = first_character_of_identifier .. second_character_of_identifier
-    local second_subfolder = third_character_of_identifier .. fourth_character_of_identifier
-
-    if config.prefix_as_path then
-        filepath = config.imgroot .. '/' .. prefix .. '/' .. first_subfolder .. '/' .. second_subfolder .. '/' .. identifier
-    else
-        filepath = config.imgroot .. '/' .. first_subfolder .. '/' .. second_subfolder .. '/' .. identifier
-    end
+    filepath = get_tmp_filepath(prefix, identifier)
 
     log("pre_flight - filepath: " .. filepath, server.loglevel.LOG_DEBUG)
 
@@ -47,71 +124,42 @@ function pre_flight(prefix, identifier, cookie)
     dsp_cookie_header = nil
 
     if cookie ~= '' then
-        -- tries to extract the DSP session name and id from the cookie:
-        -- gets the digits between "sid=" and the closing ";" (only given in case of several key value pairs)
-        -- returns nil if it cannot find it
-        session = get_session_id(cookie)
-
-        if session == nil or session["name"] == nil or session["id"] == nil then
-            -- no session could be extracted
-            log("cookie key is invalid: " .. cookie, server.loglevel.LOG_ERR)
-        else
-            dsp_cookie_header = { Cookie = session["name"] .. "=" .. session["id"] }
-            log("pre_flight - dsp_cookie_header: " ..
-            dsp_cookie_header["Cookie"], server.loglevel.LOG_DEBUG)
-        end
+        dsp_cookie_header = get_cookie_header(cookie)
     end
 
-    --
-    -- Allows to set SIPI_WEBAPI_HOSTNAME environment variable and use its value.
-    --
-    local webapi_hostname = os.getenv("SIPI_WEBAPI_HOSTNAME")
-    if webapi_hostname == nil then
-        webapi_hostname = config.knora_path
-    end
+    local webapi_hostname = get_api_hostname()
+    local webapi_port = get_api_port()
 
-    --
-    -- Allows to set SIPI_WEBAPI_PORT environment variable and use its value.
-    --
-    local webapi_port = os.getenv("SIPI_WEBAPI_PORT")
-    if webapi_port == nil then
-        webapi_port = config.knora_port
-    end
-
-    api_url = 'http://' .. webapi_hostname .. ':' .. webapi_port .. '/admin/files/' .. prefix .. '/' .. identifier
-
+    api_url = get_api_url(webapi_hostname, webapi_port, prefix, identifier)
     log("pre_flight - api_url: " .. api_url, server.loglevel.LOG_DEBUG)
 
+    -- request the image over HTTP
     success, result = server.http("GET", api_url, dsp_cookie_header, 5000)
 
-    -- check HTTP request was successful
     if not success then
-        log("pre_flight - Server.http() failed: " .. result,
-            server.loglevel.LOG_ERR)
+        log("pre_flight - Server.http() failed: " .. result, server.loglevel.LOG_ERR)
         return 'deny'
     end
 
     if result.status_code ~= 200 then
-        log("pre_flight - DSP-API returned HTTP status code " ..
-        result.status_code, server.loglevel.LOG_ERR)
+        log("pre_flight - DSP-API returned HTTP status code " .. result.status_code, server.loglevel.LOG_ERR)
         log("result body: " .. result.body, server.loglevel.LOG_ERR)
         return 'deny'
     end
 
-    log("pre_flight - response body: " .. tostring(result.body),
-        server.loglevel.LOG_DEBUG)
+    log("pre_flight - response body: " .. tostring(result.body), server.loglevel.LOG_DEBUG)
 
     success, response_json = server.json_to_table(result.body)
     if not success then
-        log("Server.http() failed: " .. response_json, server.loglevel.LOG_ERR)
+        log("pre_flight - Server.http() failed: " .. response_json, server.loglevel.LOG_ERR)
         return 'deny'
     end
 
-    log("pre_flight - permission code: " .. response_json.permissionCode,
-        server.loglevel.LOG_DEBUG)
+    log("pre_flight - permission code: " .. response_json.permissionCode, server.loglevel.LOG_DEBUG)
 
     if response_json.permissionCode == 0 then
         -- no view permission on file
+        log("pre_flight - permission code 0 (no view), access denied", server.loglevel.LOG_WARNING)
         return 'deny'
     elseif response_json.permissionCode == 1 then
         -- restricted view permission on file
@@ -121,21 +169,17 @@ function pre_flight(prefix, identifier, cookie)
         local restrictedViewSize
 
         if response_json.restrictedViewSettings ~= nil then
-            log("pre_flight - restricted view settings - watermark: " ..
-            tostring(response_json.restrictedViewSettings.watermark), server.loglevel.LOG_DEBUG)
+            log("pre_flight - restricted view settings - watermark: " .. tostring(response_json.restrictedViewSettings.watermark), server.loglevel.LOG_DEBUG)
 
             if response_json.restrictedViewSettings.size ~= nil then
-                log("pre_flight - restricted view settings - size: " ..
-                tostring(response_json.restrictedViewSettings.size), server.loglevel.LOG_DEBUG)
+                log("pre_flight - restricted view settings - size: " .. tostring(response_json.restrictedViewSettings.size), server.loglevel.LOG_DEBUG)
                 restrictedViewSize = response_json.restrictedViewSettings.size
             else
-                log("pre_flight - using default restricted view size",
-                    server.loglevel.LOG_DEBUG)
+                log("pre_flight - using default restricted view size", server.loglevel.LOG_DEBUG)
                 restrictedViewSize = config.thumb_size
             end
         else
-            log("pre_flight - using default restricted view size",
-                server.loglevel.LOG_DEBUG)
+            log("pre_flight - using default restricted view size", server.loglevel.LOG_DEBUG)
             restrictedViewSize = config.thumb_size
         end
 
@@ -170,117 +214,87 @@ function file_pre_flight(identifier, cookie)
     log("file_pre_flight called in sipi.init.lua", server.loglevel.LOG_DEBUG)
     log("file_pre_flight - param identifier: " .. identifier, server.loglevel.LOG_DEBUG)
 
-    -- get the segments of the identifier (should be `/sipi/images/:shortcode/:filename`)
-    segments = {}
-    for w in string.gmatch(identifier, "[^\\/]+") do
-        table.insert(segments, w)
-    end
-
+    local segments = get_segments_from_identifier(identifier)
+    
     -- get the shortcode
-    shortcode = segments[3]
+    local shortcode = segments[3]
     log("file_pre_flight - shortcode: " .. shortcode, server.loglevel.LOG_DEBUG)
 
     if shortcode == "082A" then -- SVA / 082A allows file access even when permissions are set to restricted!
-        return "allow", filepath
+        return "allow", identifier
     end
 
     -- get the file name
-    file_name_preview  = ''
+    local file_name = ''
+    local file_name_preview  = ''
     if #segments == 4 then
         file_name = segments[4]
+        log("file_pre_flight - file name: " .. file_name, server.loglevel.LOG_DEBUG)
     -- in case of a preview file of a video, get the file path of the video file to check permissions on the video
     elseif #segments == 5 then
         log("file_pre_flight - found 5 segments, it's assumed to the preview file for a video", server.loglevel.LOG_ERR)
         file_name = segments[4] .. '.mp4'
         file_name_preview = segments[4] .. '/' .. segments[5]
+        log("file_pre_flight - file name: " .. file_name, server.loglevel.LOG_DEBUG)
+        log("file_pre_flight - file name preview: " .. file_name_preview, server.loglevel.LOG_DEBUG)
     else
         log("file_pre_flight - wrong number of segments. Got: [" .. table.concat(segments, ",") .. "]", server.loglevel.LOG_ERR)
         return "deny"
     end
 
-    log("file_pre_flight - file name: " .. file_name, server.loglevel.LOG_DEBUG)
-
-    local first_character_of_identifier = file_name:sub(1, 1)
-    local second_character_of_identifier = file_name:sub(2, 2)
-    local third_character_of_identifier = file_name:sub(3, 3)
-    local fourth_character_of_identifier = file_name:sub(4, 4)
-
-    local first_subfolder = first_character_of_identifier .. second_character_of_identifier
-    local second_subfolder = third_character_of_identifier .. fourth_character_of_identifier
-
-    filepath = config.imgroot .. '/' .. shortcode .. '/' .. first_subfolder .. '/' .. second_subfolder .. '/' .. file_name
-    filepath_preview = config.imgroot .. '/' .. shortcode .. '/' .. first_subfolder .. '/' .. second_subfolder .. '/' .. file_name_preview
+    local filepath = get_tmp_filepath(shortcode, file_name)
+    local filepath_preview = get_tmp_filepath(shortcode, file_name_preview)
 
     log("file_pre_flight - filepath: " .. filepath, server.loglevel.LOG_DEBUG)
 
-    dsp_cookie_header = nil
+    local dsp_cookie_header = nil
 
     if cookie ~= '' then
-        -- tries to extract the DSP session name and id from the cookie:
-        -- gets the digits between "sid=" and the closing ";" (only given in case of several key value pairs)
-        -- returns nil if it cannot find it
-        session = get_session_id(cookie)
-
-        if session == nil or session["name"] == nil or session["id"] == nil then
-            -- no session could be extracted
-            log("file_pre_flight - cookie key is invalid: " .. cookie, server.loglevel.LOG_ERR)
-        else
-            dsp_cookie_header = { Cookie = session["name"] .. "=" .. session["id"] }
-            log("file_pre_flight - dsp_cookie_header: " ..
-            dsp_cookie_header["Cookie"], server.loglevel.LOG_DEBUG)
-        end
+        local dsp_cookie_header = get_cookie_header(cookie)
     end
 
-    local webapi_hostname = os.getenv("SIPI_WEBAPI_HOSTNAME")
-    if webapi_hostname == nil then
-        webapi_hostname = config.knora_path
-    end
+    local webapi_hostname = get_api_hostname()
+    local webapi_port = get_api_port()
 
-    local webapi_port = os.getenv("SIPI_WEBAPI_PORT")
-    if webapi_port == nil then
-        webapi_port = config.knora_port
-    end
-
-    api_url = 'http://' .. webapi_hostname .. ':' .. webapi_port .. '/admin/files/' .. shortcode .. '/' .. file_name
-
+    local api_url = get_api_url(webapi_hostname, webapi_port, shortcode, file_name)
     log("file_pre_flight - api_url: " .. api_url, server.loglevel.LOG_DEBUG)
 
-    success, result = server.http("GET", api_url, dsp_cookie_header, 5000)
+    -- request the image over HTTP
+    local success, result = server.http("GET", api_url, dsp_cookie_header, 5000)
 
-    -- check HTTP request was successful
     if not success then
-        log("file_pre_flight - Server.http() failed: " .. result,
-            server.loglevel.LOG_ERR)
+        log("file_pre_flight - Server.http() failed: " .. result, server.loglevel.LOG_ERR)
         return 'deny'
     end
 
     if result.status_code ~= 200 then
-        log("file_pre_flight - DSP-API returned HTTP status code " ..
-        result.status_code, server.loglevel.LOG_ERR)
+        log("file_pre_flight - DSP-API returned HTTP status code " .. result.status_code, server.loglevel.LOG_ERR)
         log("result body: " .. result.body, server.loglevel.LOG_ERR)
         return 'deny'
     end
 
-    log("file_pre_flight - response body: " .. tostring(result.body),
-        server.loglevel.LOG_DEBUG)
+    log("file_pre_flight - response body: " .. tostring(result.body), server.loglevel.LOG_DEBUG)
 
-    success, response_json = server.json_to_table(result.body)
+    local success, response_json = server.json_to_table(result.body)
     if not success then
         log("file_pre_flight - Server.http() failed: " .. response_json, server.loglevel.LOG_WARNING)
         return 'deny'
     end
 
-    log("file_pre_flight - permission code: " .. response_json.permissionCode,
-        server.loglevel.LOG_DEBUG)
+    log("file_pre_flight - permission code: " .. response_json.permissionCode, server.loglevel.LOG_DEBUG)
 
     if response_json.permissionCode == 0 then
         -- no view permission on file
         log("file_pre_flight - permission code 0 (no view), access denied", server.loglevel.LOG_WARNING)
         return 'deny'
     elseif response_json.permissionCode == 1 then
-        -- restricted view permission on file (this doesn't have a meaning at the moment for files other than images)
+        -- restricted view permission on file means full access !! Because, at the moment, this doesn't have a meaning for files other than images.
         log("file_pre_flight - permission code 1 (restricted view), access granted", server.loglevel.LOG_DEBUG)
-        return 'allow', filepath
+        if #segments == 5 then
+            return 'allow', filepath_preview
+        else
+            return 'allow', filepath
+        end
     elseif response_json.permissionCode >= 2 then
         -- full view permissions on file
         log("file_pre_flight - access granted", server.loglevel.LOG_DEBUG)
