f80a0ab82d8bab6b176ee2c00fbcd196372b0507
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-05-02 17:45:11 +0200

    add save and delete to in memory implementation for KnoraProjectRepoInMemory

--- webapi/src/test/scala/org/knora/webapi/slice/admin/domain/service/KnoraProjectRepoInMemory.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/admin/domain/service/KnoraProjectRepoInMemory.scala
@@ -2,13 +2,17 @@ package org.knora.webapi.slice.admin.domain.service
 
 import zio.Ref
 import zio.Task
+import zio.UIO
 import zio.ULayer
 import zio.ZLayer
 
 import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectIdentifierADM
 import org.knora.webapi.slice.admin.domain.model.KnoraProject
+import org.knora.webapi.slice.common.service.CrudRepository
 import org.knora.webapi.slice.resourceinfo.domain.InternalIri
-final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]]) extends KnoraProjectRepo {
+final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]])
+    extends KnoraProjectRepo
+    with CrudRepository[KnoraProject, InternalIri] {
 
   /**
    * Retrieves an entity by its id.
@@ -40,6 +44,29 @@ final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]]) ext
    * @return all instances of the type.
    */
   override def findAll(): Task[List[KnoraProject]] = projects.get
+
+  /**
+   * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the entity instance completely.
+   *
+   * @param entity The entity to be saved.
+   * @return the saved entity.
+   */
+  override def save(entity: KnoraProject): Task[KnoraProject] = projects.update(entity :: _).as(entity)
+
+  /**
+   * Deletes a given entity.
+   *
+   * @param entity The entity to be deleted
+   */
+  override def delete(entity: KnoraProject): Task[Unit] = deleteById(entity.id)
+
+  /**
+   * Deletes the entity with the given id.
+   * If the entity is not found in the persistence store it is silently ignored.
+   *
+   * @param id The identifier to the entity to be deleted
+   */
+  override def deleteById(id: InternalIri): Task[Unit] = projects.update(_.filterNot(_.id == id)).unit
 }
 
 object KnoraProjectRepoInMemory {
