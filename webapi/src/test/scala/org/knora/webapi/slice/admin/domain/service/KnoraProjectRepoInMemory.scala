2d337f1416d7e6a75147a6111f61ea14c9a50704
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-05-02 11:57:07 +0200

    refactor: Separate rest and permission logic from ProjectsResponderADM
    Introduces:
    * KnoraProjectRepoInMemory an inmemory implementation for tests
    * ProjectExportService the service which will be responsible for importing and exporting project data
    * RestPermissionService a service containing the logic for checking persmissions and turning them into a authorization exception
    
    Moves:
    * function projectDataNamedGraphV2 function from StringFormatter to ProjectADMService companion object

---
+++ webapi/src/test/scala/org/knora/webapi/slice/admin/domain/service/KnoraProjectRepoInMemory.scala
@@ -0,0 +1,41 @@
+package org.knora.webapi.slice.admin.domain.service
+
+import zio.Ref
+import zio.Task
+import zio.ULayer
+import zio.ZLayer
+
+import org.knora.webapi.messages.admin.responder.projectsmessages.ProjectIdentifierADM
+import org.knora.webapi.slice.admin.domain.model.KnoraProject
+import org.knora.webapi.slice.resourceinfo.domain.InternalIri
+final case class KnoraProjectRepoInMemory(projects: Ref[List[KnoraProject]]) extends KnoraProjectRepo {
+  override def findById(id: ProjectIdentifierADM): Task[Option[KnoraProject]] = projects.get.map(
+    _.find(
+      id match {
+        case ProjectIdentifierADM.ShortcodeIdentifier(shortcode) => _.shortcode == shortcode.value
+        case ProjectIdentifierADM.ShortnameIdentifier(shortname) => _.shortname == shortname.value
+        case ProjectIdentifierADM.IriIdentifier(iri)             => _.id.value == iri.value
+      }
+    )
+  )
+
+  /**
+   * Retrieves an entity by its id.
+   *
+   * @param id The identifier of type [[Id]].
+   * @return the entity with the given id or [[None]] if none found.
+   */
+  override def findById(id: InternalIri): Task[Option[KnoraProject]] = projects.get.map(_.find(_.id == id))
+
+  /**
+   * Returns all instances of the type.
+   *
+   * @return all instances of the type.
+   */
+  override def findAll(): Task[List[KnoraProject]] = projects.get
+}
+
+object KnoraProjectRepoInMemory {
+  val layer: ULayer[KnoraProjectRepoInMemory] =
+    ZLayer.fromZIO(Ref.make(List.empty[KnoraProject]).map(KnoraProjectRepoInMemory(_)))
+}
