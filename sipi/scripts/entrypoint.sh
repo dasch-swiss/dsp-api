c74e0e2672a1f67223c6e35b5feb7dc604c0dd77
Samuel Börlin <11892708+SamuelBoerlin@users.noreply.github.com>
2023-05-09 15:51:29 +0200

    feat(sipi): add clean temp dir cronjob (DEV-2090)

---
+++ sipi/scripts/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o pipefail
+
+if [ -n "$CLEAN_TMP_DIR_CRON_SCHEDULE" ]; then
+  parts=$(echo "$CLEAN_TMP_DIR_CRON_SCHEDULE" | wc -w)
+  command="/bin/bash /sipi/scripts/clean_temp_dir.sh"
+
+  # Validate and install clean temp dir crontab
+  [ $parts -eq 5 ] && (crontab -l 2>/dev/null; echo "$CLEAN_TMP_DIR_CRON_SCHEDULE $command") | crontab - 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "Invalid clean temp dir cron schedule: $CLEAN_TMP_DIR_CRON_SCHEDULE" >&2
+    exit 1
+  fi
+
+  # Start cron process in background
+  cron &
+fi
+
+# Start SIPI
+cd /sipi && ./sipi "$@"
