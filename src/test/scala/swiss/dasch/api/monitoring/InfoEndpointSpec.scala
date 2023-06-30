de34a35475790078a727eac05a0d06ded65a2eff
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-06-30 17:16:07 +0200

    Add copyright header

--- src/test/scala/swiss/dasch/api/monitoring/InfoEndpointSpec.scala
+++ src/test/scala/swiss/dasch/api/monitoring/InfoEndpointSpec.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package swiss.dasch.api.monitoring
 
 import swiss.dasch.api.monitoring.InfoEndpoint.InfoEndpointResponse
