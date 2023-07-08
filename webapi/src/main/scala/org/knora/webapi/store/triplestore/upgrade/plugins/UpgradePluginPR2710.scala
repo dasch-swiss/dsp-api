220430fc97a55903e1307d4c6e7ebbb5c07beb85
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-07-08 21:29:27 +0200

    tone down logging again

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/UpgradePluginPR2710.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/UpgradePluginPR2710.scala
@@ -98,36 +98,36 @@ class UpgradePluginPR2710(log: Logger) extends UpgradePlugin {
     }
 
   override def transform(model: RdfModel): Unit = {
-    log.info("Transformation started.")
+    log.info("Textvalue transformation started.")
 
     val ontologyToProjectMap: Map[IRI, IRI] = collectOntologyToProjectMap(model)
-    log.info(s"Found ${ontologyToProjectMap.size} ontologies for ${ontologyToProjectMap.values.toSet.size} projects.")
+    log.debug(s"Found ${ontologyToProjectMap.size} ontologies for ${ontologyToProjectMap.values.toSet.size} projects.")
 
     val textValuePropsPerOntology: Map[IRI, Seq[IRI]] = collectTextValuePropIris(model)
-    log.info(s"Found ${textValuePropsPerOntology.values.flatten.size} TextValue properties.")
+    log.debug(s"Found ${textValuePropsPerOntology.values.flatten.size} TextValue properties.")
 
     val textValueProps: Set[TextValueProp] = makeTextValueProps(model, textValuePropsPerOntology, ontologyToProjectMap)
-    log.info(s"Created ${textValueProps.size} TextValueProperty objects.")
+    log.debug(s"Created ${textValueProps.size} TextValueProperty objects.")
 
     val dataVerifiedTextValueProps: Set[TextValueProp] = verifyTextValuePropsAgainstData(model, textValueProps)
-    log.info(s"Verified ${dataVerifiedTextValueProps.size} TextValueProperty objects against usage in data.")
+    log.debug(s"Verified ${dataVerifiedTextValueProps.size} TextValueProperty objects against usage in data.")
 
     val verifiedTextValueProps: Set[TextValueProp] =
       verifyTextValuePropsWithInheritance(model, dataVerifiedTextValueProps)
-    log.info(
+    log.debug(
       s"Verified ${verifiedTextValueProps.size} TextValueProperty objects against the ontology for inheritance implications."
     )
 
     val dataAdjustments = collectDataAdjustments(model, verifiedTextValueProps)
-    log.info(s"Found ${dataAdjustments.size} values in data to adjust.")
+    log.debug(s"Found ${dataAdjustments.size} values in data to adjust.")
 
     val dataStatementsToRemove = dataAdjustments.flatMap(_.statementsToRemove)
-    log.info(s"Found ${dataStatementsToRemove.size} statements in data to remove.")
+    log.debug(s"Found ${dataStatementsToRemove.size} statements in data to remove.")
     val dataStatementsToInsert = dataAdjustments.flatMap(_.statementsToInsert)
-    log.info(s"Found ${dataStatementsToInsert.size} statements in data to insert.")
+    log.debug(s"Found ${dataStatementsToInsert.size} statements in data to insert.")
 
     val ontologyStatementsToRemove = verifiedTextValueProps.flatMap(_.statementsToRemove)
-    log.info(s"Found ${ontologyStatementsToRemove.size} statements in ontologies to remove.")
+    log.debug(s"Found ${ontologyStatementsToRemove.size} statements in ontologies to remove.")
     val ontologyStatementsToInsert = verifiedTextValueProps.map(tvp =>
       nodeFactory.makeStatement(
         tvp.objectClassConstraint.subj,
@@ -136,7 +136,7 @@ class UpgradePluginPR2710(log: Logger) extends UpgradePlugin {
         tvp.objectClassConstraint.context
       )
     )
-    log.info(s"Found ${ontologyStatementsToInsert.size} statements in ontologies to insert.")
+    log.debug(s"Found ${ontologyStatementsToInsert.size} statements in ontologies to insert.")
 
     val statementsToRemove = dataStatementsToRemove ++ ontologyStatementsToRemove
     log.info(s"Removing ${statementsToRemove.size} statements.")
