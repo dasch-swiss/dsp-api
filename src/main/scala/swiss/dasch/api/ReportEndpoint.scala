4e8b90e442924e6faad633e4da7a1d3adf49a5ec
Christian Kleinbölting <seakayone@users.noreply.github.com>
2023-07-13 16:12:15 +0200

    Add summary to checksum report (#45)

--- src/main/scala/swiss/dasch/api/ReportEndpoint.scala
+++ src/main/scala/swiss/dasch/api/ReportEndpoint.scala
@@ -34,20 +34,52 @@ object ReportEndpoint {
       SingleFileCheckResultResponse(result.file.filename.toString, result.checksumMatches)
   }
 
-  final case class AssetCheckResultResponse(assetId: String, results: List[SingleFileCheckResultResponse])
+  final case class AssetCheckResultEntry(
+      assetId: String,
+      originalFilename: String,
+      results: List[SingleFileCheckResultResponse],
+    )
+  private object AssetCheckResultEntry         {
+    implicit val encoder: JsonEncoder[AssetCheckResultEntry]                              = DeriveJsonEncoder.gen[AssetCheckResultEntry]
+    implicit val schema: Schema[AssetCheckResultEntry]                                    = DeriveSchema.gen[AssetCheckResultEntry]
+    def make(assetInfo: AssetInfo, results: Chunk[ChecksumResult]): AssetCheckResultEntry =
+      AssetCheckResultEntry(
+        assetInfo.asset.id.toString,
+        assetInfo.originalFilename.toString,
+        results.map(SingleFileCheckResultResponse.make).toList,
+      )
+  }
+  final case class AssetCheckResultSummary(
+      numberOfAssets: Int,
+      numberOfFiles: Int,
+      numberOfChecksumMatches: Int,
+    )
+  private object AssetCheckResultSummary       {
+    implicit val encoder: JsonEncoder[AssetCheckResultSummary] = DeriveJsonEncoder.gen[AssetCheckResultSummary]
+    implicit val schema: Schema[AssetCheckResultSummary]       = DeriveSchema.gen[AssetCheckResultSummary]
+  }
+  final case class AssetCheckResultResponse(summary: AssetCheckResultSummary, results: List[AssetCheckResultEntry])
+
   private object AssetCheckResultResponse {
     implicit val encoder: JsonEncoder[AssetCheckResultResponse] = DeriveJsonEncoder.gen[AssetCheckResultResponse]
     implicit val schema: Schema[AssetCheckResultResponse]       = DeriveSchema.gen[AssetCheckResultResponse]
 
-    def make(report: Report): List[AssetCheckResultResponse]                               =
-      report.results.map { case (info, results) => AssetCheckResultResponse.to(info.asset, results) }.toList
-    private def to(asset: Asset, results: Chunk[ChecksumResult]): AssetCheckResultResponse =
-      AssetCheckResultResponse(asset.id.toString, results.map(SingleFileCheckResultResponse.make).toList)
+    def make(report: Report): AssetCheckResultResponse = {
+      val reportResults = report.results
+      val results       = reportResults.map { case (info, checksum) => AssetCheckResultEntry.make(info, checksum) }.toList
+      val summary       = AssetCheckResultSummary(
+        reportResults.keys.size,
+        reportResults.values.map(_.size).sum,
+        reportResults.values.map(_.count(_.checksumMatches)).sum,
+      )
+      AssetCheckResultResponse(summary, results)
+    }
+
   }
 
   private val endpoint = Endpoint
     .get(projects / shortcodePathVar / "checksumreport")
-    .out[List[AssetCheckResultResponse]]
+    .out[AssetCheckResultResponse]
     .outErrors(
       HttpCodec.error[ProjectNotFound](Status.NotFound),
       HttpCodec.error[IllegalArguments](Status.BadRequest),
