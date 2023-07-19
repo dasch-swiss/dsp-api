a3b384e0917f1deb97db6508d352af80a5907781
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-07-19 14:13:01 +0200

    adjust paths

--- webapi/src/it/scala/org/knora/webapi/e2e/v1/SearchV1R2RSpec.scala
+++ webapi/src/it/scala/org/knora/webapi/e2e/v1/SearchV1R2RSpec.scala
@@ -29,7 +29,7 @@ class SearchV1R2RSpec extends R2RSpec {
 
   override lazy val rdfDataObjects = List(
     RdfDataObject(path = "test_data/all_data/anything-data.ttl", name = "http://www.knora.org/data/0001/anything"),
-    RdfDataObject(path = "test_data/demo_data/images-demo-data.ttl", name = "http://www.knora.org/data/00FF/images"),
+    RdfDataObject(path = "test_data/all_data/images-demo-data.ttl", name = "http://www.knora.org/data/00FF/images"),
     RdfDataObject(path = "test_data/all_data/incunabula-data.ttl", name = "http://www.knora.org/data/0803/incunabula")
   )
 
