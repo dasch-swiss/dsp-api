11d8ecfa574d1830720e0a8346421a16618ee551
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-10-31 16:47:52 +0100

    Revert "add benchmark for fuseki, add threshold for health"
    This reverts commit 99591d9e0ae15d818e42612dbda96e2b74d7379e.

--- k6/health.js
+++ k6/health.js
@@ -1,23 +1,10 @@
 import http from "k6/http";
-import { check, sleep } from "k6";
-
+import { sleep } from "k6";
 export const options = {
-  stages: [
-    { duration: "10s", target: 2 },
-    { duration: "10s", target: 100 },
-  ],
-  thresholds: {
-    http_req_duration: ["p(90)<130", "p(95)<150"],
-  },
+  vus: 10,
+  duration: "30s",
 };
-
 export default function () {
-  const res = http.get("https://api.dev.dasch.swiss/health", {
-    tags: { what: "health" },
-  });
-  check(res, {
-    "status was 200": (r) => r.status == 200,
-    "duration was <=": (r) => r.timings.duration <= 10,
-  });
+  http.get("http://0.0.0.0:3333/health");
   sleep(1);
 }
