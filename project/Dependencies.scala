1058ee6777e4def029d5790559735a6afb47460b
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-11-08 15:34:13 +0100

    add graph-core to ignore list

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -129,7 +129,7 @@ object Dependencies {
     "com.softwaremill.sttp.tapir" %% "tapir-json-zio"          % tapirVersion,
     "com.softwaremill.sttp.tapir" %% "tapir-json-spray"        % tapirVersion,
     "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion,
-    "com.softwaremill.sttp.tapir" %% "tapir-refined"           % tapirVersion
+    "com.softwaremill.sttp.tapir" %% "tapir-refined"           % "1.2.13"
   )
   val metrics = Seq(
     "dev.zio"                     %% "zio-metrics-connectors"            % ZioMetricsConnectorsVersion,
