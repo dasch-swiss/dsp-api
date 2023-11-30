c31b3eb1adf9a768858c6c0136c876c529719367
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-11-30 11:11:04 +0100

    add description to in

--- webapi/src/main/scala/org/knora/webapi/slice/search/search/api/SearchEndpoints.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/search/search/api/SearchEndpoints.scala
@@ -5,28 +5,20 @@
 
 package org.knora.webapi.slice.search.search.api
 
-import sttp.model.HeaderNames
-import sttp.model.MediaType
-import sttp.tapir.Codec.PlainCodec
-import sttp.tapir.*
-import zio.Task
-import zio.ZIO
-import zio.ZLayer
-
 import dsp.errors.BadRequestException
 import org.knora.webapi.*
 import org.knora.webapi.config.AppConfig
 import org.knora.webapi.messages.admin.responder.usersmessages.UserADM
-import org.knora.webapi.messages.util.rdf.JsonLD
-import org.knora.webapi.messages.util.rdf.RdfFormat
+import org.knora.webapi.messages.util.rdf.{JsonLD, RdfFormat}
 import org.knora.webapi.messages.v2.responder.KnoraResponseV2
 import org.knora.webapi.responders.v2.SearchResponderV2
-import org.knora.webapi.slice.common.api.BaseEndpoints
-import org.knora.webapi.slice.common.api.SecuredEndpointAndZioHandler
-import org.knora.webapi.slice.search.search.api.ApiV2.Headers
-import org.knora.webapi.slice.search.search.api.ApiV2.QueryParams
-import org.knora.webapi.slice.search.search.api.ApiV2.defaultApiV2Schema
+import org.knora.webapi.slice.common.api.{BaseEndpoints, SecuredEndpointAndZioHandler}
+import org.knora.webapi.slice.search.search.api.ApiV2.{Headers, QueryParams, defaultApiV2Schema}
 import org.knora.webapi.slice.search.search.api.ApiV2Codecs.apiV2SchemaRendering
+import sttp.model.{HeaderNames, MediaType}
+import sttp.tapir.*
+import sttp.tapir.Codec.PlainCodec
+import zio.{Task, ZIO, ZLayer}
 
 final case class SearchEndpoints(baseEndpoints: BaseEndpoints) {
 
@@ -35,7 +27,11 @@ final case class SearchEndpoints(baseEndpoints: BaseEndpoints) {
 
   val postGravsearch = baseEndpoints.securedEndpoint.post
     .in(searchBase / "gravsearch")
-    .in(stringBody)
+    .in(
+      stringBody.description(
+        "The Gravsearch query. See https://docs.dasch.swiss/latest/DSP-API/03-endpoints/api-v2/query-language/"
+      )
+    )
     .in(apiV2SchemaRendering)
     .out(stringBody)
     .out(header[MediaType](HeaderNames.ContentType))
