4c980bc520ba0611333145cc317f3447a21b08ac
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-12-19 15:31:55 +0100

    streamline tests

--- webapi/src/test/scala/org/knora/webapi/messages/v2/responder/valuemessages/ValuesV2Spec.scala
+++ webapi/src/test/scala/org/knora/webapi/messages/v2/responder/valuemessages/ValuesV2Spec.scala
@@ -5,26 +5,18 @@
 
 package org.knora.webapi.messages.v2.responder.valuemessages
 
-import zio.Scope
-import zio.Task
-import zio.ZIO
-import zio.ZLayer
+import zio.*
 import zio.nio.file.Path
-import zio.test.Spec
-import zio.test.TestEnvironment
-import zio.test.ZIOSpecDefault
+import zio.test.Assertion.*
 import zio.test.*
 
 import dsp.errors.AssertionException
 import org.knora.webapi.messages.admin.responder.usersmessages.UserADM
-import org.knora.webapi.messages.store.sipimessages.DeleteTemporaryFileRequest
-import org.knora.webapi.messages.store.sipimessages.IIIFServiceStatusResponse
-import org.knora.webapi.messages.store.sipimessages.MoveTemporaryFileToPermanentStorageRequest
-import org.knora.webapi.messages.store.sipimessages.SipiGetTextFileRequest
-import org.knora.webapi.messages.store.sipimessages.SipiGetTextFileResponse
+import org.knora.webapi.messages.store.sipimessages.*
 import org.knora.webapi.messages.util.rdf.JsonLDUtil
 import org.knora.webapi.messages.v2.responder.SuccessResponseV2
 import org.knora.webapi.messages.v2.responder.resourcemessages.CreateResourceRequestV2.AssetIngestState
+import org.knora.webapi.slice.admin.domain.model.KnoraProject
 import org.knora.webapi.slice.admin.domain.service.Asset
 import org.knora.webapi.store.iiif.api.FileMetadataSipiResponse
 import org.knora.webapi.store.iiif.api.SipiService
@@ -35,56 +27,63 @@ object ValuesV2Spec extends ZIOSpecDefault {
       |  "http://api.knora.org/ontology/knora-api/v2#fileValueHasFilename" : "filename"
       |  }""".stripMargin
 
-  private val response: FileMetadataSipiResponse =
-    FileMetadataSipiResponse(None, None, "", None, None, None, None, None)
-
   override def spec: Spec[TestEnvironment with Scope, Any] =
     suite("ValuesV2")(
       test("Expect file to be not ingested and in `tmp` folder") {
         for {
-          info <- ValueContentV2
+          ingested <- ValueContentV2
+                        .getFileInfo("0001", AssetIngestState.AssetIngested, JsonLDUtil.parseJsonLD(json).body)
+                        .exit
+          temp <- ValueContentV2
                     .getFileInfo("0001", AssetIngestState.AssetInTemp, JsonLDUtil.parseJsonLD(json).body)
-                    .provide(ZLayer.succeed(new SipiService {
-                      override def getFileMetadata(filePath: String): Task[FileMetadataSipiResponse] =
-                        if (filePath.startsWith("/tmp")) ZIO.succeed(response)
-                        else ZIO.fail(AssertionException(filePath))
-
-                      override def moveTemporaryFileToPermanentStorage(
-                        moveTemporaryFileToPermanentStorageRequestV2: MoveTemporaryFileToPermanentStorageRequest
-                      ): Task[SuccessResponseV2] = ???
-                      override def deleteTemporaryFile(
-                        deleteTemporaryFileRequestV2: DeleteTemporaryFileRequest
-                      ): Task[SuccessResponseV2] = ???
-                      override def getTextFileRequest(
-                        textFileRequest: SipiGetTextFileRequest
-                      ): Task[SipiGetTextFileResponse] = ???
-                      override def getStatus(): Task[IIIFServiceStatusResponse]                                    = ???
-                      override def downloadAsset(asset: Asset, targetDir: Path, user: UserADM): Task[Option[Path]] = ???
-                    }))
-        } yield assertTrue(info.metadata == response)
-      },
+                    .exit
+        } yield assert(ingested)(fails(isSubtype[AssertionException](anything))) && assert(temp)(succeeds(anything))
+      }.provide(sipiServiceAllowMetadataInTemp),
       test("Expect file to be already ingested and in project folder") {
         for {
-          info <- ValueContentV2
-                    .getFileInfo("0001", AssetIngestState.AssetIngested, JsonLDUtil.parseJsonLD(json).body)
-                    .provide(ZLayer.succeed(new SipiService {
-                      override def getFileMetadata(filePath: String): Task[FileMetadataSipiResponse] =
-                        if (filePath.startsWith("/0001")) ZIO.succeed(response)
-                        else ZIO.fail(AssertionException(filePath))
-
-                      override def moveTemporaryFileToPermanentStorage(
-                        moveTemporaryFileToPermanentStorageRequestV2: MoveTemporaryFileToPermanentStorageRequest
-                      ): Task[SuccessResponseV2] = ???
-                      override def deleteTemporaryFile(
-                        deleteTemporaryFileRequestV2: DeleteTemporaryFileRequest
-                      ): Task[SuccessResponseV2] = ???
-                      override def getTextFileRequest(
-                        textFileRequest: SipiGetTextFileRequest
-                      ): Task[SipiGetTextFileResponse] = ???
-                      override def getStatus(): Task[IIIFServiceStatusResponse]                                    = ???
-                      override def downloadAsset(asset: Asset, targetDir: Path, user: UserADM): Task[Option[Path]] = ???
-                    }))
-        } yield assertTrue(info.metadata == response)
-      }
+          ingested <- ValueContentV2
+                        .getFileInfo("0001", AssetIngestState.AssetIngested, JsonLDUtil.parseJsonLD(json).body)
+                        .exit
+          temp <- ValueContentV2
+                    .getFileInfo("0001", AssetIngestState.AssetInTemp, JsonLDUtil.parseJsonLD(json).body)
+                    .exit
+        } yield assert(ingested)(succeeds(anything)) && assert(temp)(fails(isSubtype[AssertionException](anything)))
+      }.provide(sipiServiceAllowMetadataInProjectFolder)
     )
+
+  private val response: FileMetadataSipiResponse =
+    FileMetadataSipiResponse(None, None, "", None, None, None, None, None)
+
+  private def sipiServiceAllowMetadataInTemp: ULayer[SipiService] = ZLayer.succeed(new SipiService {
+    override def getFileMetadataFromTemp(filename: String): Task[FileMetadataSipiResponse] =
+      ZIO.succeed(response)
+    override def getFileMetadata(filename: String, shortcode: KnoraProject.Shortcode): Task[FileMetadataSipiResponse] =
+      ZIO.fail(AssertionException(filename))
+    def moveTemporaryFileToPermanentStorage(
+      moveTemporaryFileToPermanentStorageRequestV2: MoveTemporaryFileToPermanentStorageRequest
+    ): Task[SuccessResponseV2] = ???
+    def deleteTemporaryFile(
+      deleteTemporaryFileRequestV2: DeleteTemporaryFileRequest
+    ): Task[SuccessResponseV2] = ???
+    def getTextFileRequest(textFileRequest: SipiGetTextFileRequest): Task[SipiGetTextFileResponse] = ???
+    def getStatus(): Task[IIIFServiceStatusResponse]                                               = ???
+    def downloadAsset(asset: Asset, targetDir: Path, user: UserADM): Task[Option[Path]]            = ???
+  })
+
+  private def sipiServiceAllowMetadataInProjectFolder: ULayer[SipiService] = ZLayer.succeed(new SipiService {
+    override def getFileMetadata(filename: String, shortcode: KnoraProject.Shortcode): Task[FileMetadataSipiResponse] =
+      ZIO.succeed(response)
+    override def getFileMetadataFromTemp(filename: String): Task[FileMetadataSipiResponse] =
+      ZIO.fail(AssertionException(filename))
+    def moveTemporaryFileToPermanentStorage(
+      moveTemporaryFileToPermanentStorageRequestV2: MoveTemporaryFileToPermanentStorageRequest
+    ): Task[SuccessResponseV2] = ???
+    def deleteTemporaryFile(
+      deleteTemporaryFileRequestV2: DeleteTemporaryFileRequest
+    ): Task[SuccessResponseV2] = ???
+    def getTextFileRequest(textFileRequest: SipiGetTextFileRequest): Task[SipiGetTextFileResponse] = ???
+    def getStatus(): Task[IIIFServiceStatusResponse]                                               = ???
+    def downloadAsset(asset: Asset, targetDir: Path, user: UserADM): Task[Option[Path]]            = ???
+  })
+
 }
