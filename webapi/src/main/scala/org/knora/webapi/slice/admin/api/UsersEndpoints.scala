60f675c35137fdf31be6e975ccd77c5436c0e7c5
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-12-20 12:06:04 +0100

    start adding user endpoints

---
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/api/UsersEndpoints.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.slice.admin.api
+
+import sttp.tapir.*
+import sttp.tapir.generic.auto.*
+import sttp.tapir.json.spray.jsonBody as sprayJsonBody
+import zio.*
+
+import org.knora.webapi.messages.admin.responder.usersmessages.UsersGetResponseADM
+import org.knora.webapi.slice.common.api.BaseEndpoints
+
+final case class UsersEndpoints(baseEndpoints: BaseEndpoints) {
+
+  import org.knora.webapi.messages.admin.responder.usersmessages.UsersADMJsonProtocol.*
+
+  private val projectsBase = "admin" / "users"
+
+  private val tags = List("Users", "Admin API")
+
+  val getUsers = baseEndpoints.securedEndpoint.get
+    .in(projectsBase)
+    .out(sprayJsonBody[UsersGetResponseADM])
+    .description("Returns all users.")
+    .tags(tags)
+
+}
+
+object UsersEndpoints {
+  val layer = ZLayer.derive[UsersEndpoints]
+}
