6a6739ada06aef5de889c2aa0ccf444b4042ab7d
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-21 16:12:02 +0100

    feat: Add def getAssetInfo(shortcode: KnoraProject.Shortcode, assetId: AssetId): Task[AssetInfoResponse] to DspIngestClient

--- webapi/src/main/scala/org/knora/webapi/slice/admin/domain/service/DspIngestClient.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/domain/service/DspIngestClient.scala
@@ -25,17 +25,22 @@ import zio.http.Headers
 import zio.http.MediaType
 import zio.http.Request
 import zio.http.URL
+import zio.json.DecoderOps
+import zio.json.DeriveJsonDecoder
+import zio.json.JsonDecoder
 import zio.macros.accessible
 import zio.nio.file.Files
 import zio.nio.file.Path
 import zio.stream.ZSink
 
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.DurationInt
 
 import org.knora.webapi.config.DspIngestConfig
 import org.knora.webapi.routing.Jwt
 import org.knora.webapi.routing.JwtService
+import org.knora.webapi.slice.admin.api.model.MaintenanceRequests.AssetId
 import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortcode
 
 @accessible
@@ -43,6 +48,25 @@ trait DspIngestClient {
   def exportProject(shortcode: Shortcode): ZIO[Scope, Throwable, Path]
 
   def importProject(shortcode: Shortcode, fileToImport: Path): Task[Path]
+
+  def getAssetInfo(shortcode: Shortcode, assetId: AssetId): Task[AssetInfoResponse]
+}
+
+final case class AssetInfoResponse(
+  internalFilename: String,
+  originalInternalFilename: String,
+  originalFilename: String,
+  checksumOriginal: String,
+  checksumDerivative: String,
+  width: Option[Int] = None,
+  height: Option[Int] = None,
+  duration: Option[Double] = None,
+  fps: Option[Double] = None,
+  internalMimeType: Option[String] = None,
+  originalMimeType: Option[String] = None
+)
+object AssetInfoResponse {
+  implicit val decoder: JsonDecoder[AssetInfoResponse] = DeriveJsonDecoder.gen[AssetInfoResponse]
 }
 
 final case class DspIngestClientLive(
@@ -67,7 +91,16 @@ final case class DspIngestClientLive(
 
   private val authenticatedRequest = getJwtString.map(basicRequest.auth.bearer(_))
 
-  def exportProject(shortcode: Shortcode): ZIO[Scope, Throwable, Path] =
+  override def getAssetInfo(shortcode: Shortcode, assetId: AssetId): Task[AssetInfoResponse] =
+    for {
+      request  <- authenticatedRequest.map(_.get(uri"${projectsPath(shortcode)}/assets/$assetId"))
+      response <- ZIO.blocking(request.send(backend = sttpBackend))
+      result <- ZIO
+                  .fromEither(response.body.flatMap(str => str.fromJson[AssetInfoResponse]))
+                  .mapError(err => new IOException(s"Error parsing response: $err"))
+    } yield result
+
+  override def exportProject(shortcode: Shortcode): ZIO[Scope, Throwable, Path] =
     for {
       tempDir   <- Files.createTempDirectoryScoped(Some("export"), List.empty)
       exportFile = tempDir / "export.zip"
@@ -76,11 +109,11 @@ final case class DspIngestClientLive(
                      .readTimeout(30.minutes)
                      .response(asStreamAlways(ZioStreams)(_.run(ZSink.fromFile(exportFile.toFile))))
                  }
-      response <- request.send(backend = sttpBackend)
+      response <- ZIO.blocking(request.send(backend = sttpBackend))
       _        <- ZIO.logInfo(s"Response from ingest :${response.code}")
     } yield exportFile
 
-  def importProject(shortcode: Shortcode, fileToImport: Path): Task[Path] = ZIO.scoped {
+  override def importProject(shortcode: Shortcode, fileToImport: Path): Task[Path] = ZIO.scoped {
     for {
       importUrl <- ZIO.fromEither(URL.decode(s"${projectsPath(shortcode)}/import"))
       token     <- jwtService.createJwtForDspIngest()
