056ed4cd35ea4f72cb02d84aab9cfd602d8e637c
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-28 00:25:46 +0100

    try fix ci

--- integration/src/test/scala/org/knora/webapi/testcontainers/SharedVolumes.scala
+++ integration/src/test/scala/org/knora/webapi/testcontainers/SharedVolumes.scala
@@ -1,16 +1,13 @@
 package org.knora.webapi.testcontainers
 
-import zio.ULayer
-import zio.ZIO
-import zio.ZLayer
-import zio.nio.file.Files
-import zio.nio.file.Path
-
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.nio.file.StandardCopyOption
-
 import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortcode
+import zio.nio.file.{Files, Path}
+import zio.{ULayer, ZIO, ZLayer}
+
+import java.io.{FileNotFoundException, IOException}
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.PosixFilePermissions
+import java.nio.file.attribute.PosixFilePermissions.asFileAttribute
 
 object SharedVolumes {
 
@@ -19,19 +16,20 @@ object SharedVolumes {
     def copyFileToAssetFolder(shortcode: Shortcode, source: Path): ZIO[Images, IOException, Unit] =
       ZIO.fail(new FileNotFoundException(s"File not found $source")).whenZIO(Files.notExists(source)).logError *>
         ZIO.serviceWithZIO[SharedVolumes.Images] { imagesVolume =>
-          val filename = source.filename.toString()
-          val seg01    = filename.substring(0, 2).toLowerCase()
-          val seg02    = filename.substring(2, 4).toLowerCase()
-          val target   = Path(s"${imagesVolume.hostPath}/${shortcode.value}/$seg01/$seg02/$filename")
-          Files.createDirectories(target.parent.head) *>
-            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING)
+          val filename  = source.filename.toString()
+          val seg01     = filename.substring(0, 2).toLowerCase()
+          val seg02     = filename.substring(2, 4).toLowerCase()
+          val targetDir = Path(s"${imagesVolume.hostPath}/${shortcode.value}/$seg01/$seg02")
+          Files.createDirectories(targetDir, asFileAttribute(PosixFilePermissions.fromString("rwxrwxrwx"))).logError *>
+            Files.copy(source, targetDir / filename, StandardCopyOption.REPLACE_EXISTING)
         }
 
     val layer: ULayer[Images] =
       ZLayer
         .scoped(for {
-          tmpPath <- Files.createTempDirectoryScoped(None, List.empty)
-          absDir  <- tmpPath.toAbsolutePath.map(_.toString())
+          tmpPath <-
+            Files.createTempDirectoryScoped(None, Seq(asFileAttribute(PosixFilePermissions.fromString("rwxrwxrwx"))))
+          absDir <- tmpPath.toAbsolutePath.map(_.toString())
         } yield Images(absDir))
         .orDie
   }
