9234c87bbd56174abd11bc612b3887728ea63bdd
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-27 18:23:00 +0100

    rm unused code

--- integration/src/test/scala/org/knora/webapi/testcontainers/DspIngestTestContainer.scala
+++ integration/src/test/scala/org/knora/webapi/testcontainers/DspIngestTestContainer.scala
@@ -2,32 +2,13 @@ package org.knora.webapi.testcontainers
 
 import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.GenericContainer
-import org.testcontainers.utility.MountableFile
-import zio.Task
 import zio.URLayer
 import zio.ZIO
 import zio.ZLayer
-import zio.nio.file.Path
 
-import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortcode
 import org.knora.webapi.testcontainers.TestContainerOps.StartableOps
 
-final class DspIngestTestContainer extends GenericContainer[DspIngestTestContainer](s"daschswiss/dsp-ingest:latest") {
-
-  def copyFileFromClassPathToAssetDirInContainer(source: Path, shortcode: Shortcode): Task[Unit] = {
-    val filename = source.filename.toString
-    val seg01    = filename.substring(0, 2).toLowerCase()
-    val seg02    = filename.substring(2, 4).toLowerCase()
-    val target   = Path(s"${DspIngestTestContainer.assetDir}/$shortcode/$seg01/$seg02/$filename")
-    copyFileFromClassPathToContainer(source, target)
-  }
-
-  def copyFileFromClassPathToContainer(source: Path, target: Path): Task[Unit] = {
-    val mountableFile = MountableFile.forClasspathResource(source.toString(), 777)
-    ZIO.attemptBlockingIO(copyFileToContainer(mountableFile, target.toFile.toString)) <*
-      ZIO.logInfo(s"Copied $source to $target")
-  }
-}
+final class DspIngestTestContainer extends GenericContainer[DspIngestTestContainer](s"daschswiss/dsp-ingest:latest") {}
 
 object DspIngestTestContainer {
 
