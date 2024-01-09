4eedccb870928cbc06f098cb9921a8754ea73e37
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-01-09 13:47:15 +0100

    Use production value for auth cookie name

--- webapi/src/main/scala/org/knora/webapi/slice/common/api/DocsGenerator.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/common/api/DocsGenerator.scala
@@ -1,34 +1,28 @@
+/*
+ * Copyright © 2021 - 2024 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.slice.common.api
 
 import org.apache.pekko.http.scaladsl.model.HttpResponse
 import org.apache.pekko.http.scaladsl.server.RequestContext
-import sttp.apispec.openapi.Server
-import sttp.apispec.openapi.circe.yaml.RichOpenAPI
-import sttp.tapir.AnyEndpoint
-import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
-import zio.Chunk
-import zio.Task
-import zio.ZIO
-import zio.ZIOAppArgs
-import zio.ZIOAppDefault
-import zio.ZLayer
-import zio.nio.file.Files
-import zio.nio.file.Path
-
 import org.knora.webapi.http.version.BuildInfo
 import org.knora.webapi.messages.admin.responder.usersmessages.UserIdentifierADM
 import org.knora.webapi.messages.v2.routing.authenticationmessages.KnoraCredentialsV2
 import org.knora.webapi.routing.Authenticator
-import org.knora.webapi.slice.admin.api.AdminApiEndpoints
-import org.knora.webapi.slice.admin.api.MaintenanceEndpoints
-import org.knora.webapi.slice.admin.api.PermissionsEndpoints
-import org.knora.webapi.slice.admin.api.ProjectsEndpoints
-import org.knora.webapi.slice.admin.api.UsersEndpoints
+import org.knora.webapi.slice.admin.api.{AdminApiEndpoints, MaintenanceEndpoints, PermissionsEndpoints, ProjectsEndpoints, UsersEndpoints}
 import org.knora.webapi.slice.admin.domain.model.User
+import sttp.apispec.openapi.Server
+import sttp.apispec.openapi.circe.yaml.RichOpenAPI
+import sttp.tapir.AnyEndpoint
+import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
+import zio.{Chunk, Task, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.nio.file.{Files, Path}
 
 final case class DocsNoopAuthenticator() extends Authenticator {
   override def getUserADM(requestContext: RequestContext): Task[User]                                    = ???
-  override def calculateCookieName(): String                                                             = "foo"
+  override def calculateCookieName(): String                                                             = "KnoraAuthenticationMFYGSLTEMFZWG2BOON3WS43THI2DIMY9"
   override def getUserADMThroughCredentialsV2(credentials: Option[KnoraCredentialsV2]): Task[User]       = ???
   override def doLogoutV2(requestContext: RequestContext): Task[HttpResponse]                            = ???
   override def doLoginV2(credentials: KnoraCredentialsV2.KnoraPasswordCredentialsV2): Task[HttpResponse] = ???
