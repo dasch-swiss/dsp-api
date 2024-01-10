360d9f8283f2ea666ce3e6095bbc3e19778dae62
Marcin Procyk <marcin.procyk@dasch.swiss>
2024-01-10 09:44:05 +0100

    add endpoints

--- webapi/src/main/scala/org/knora/webapi/slice/admin/api/GroupsEndpoints.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/api/GroupsEndpoints.scala
@@ -9,9 +9,8 @@ import sttp.tapir.*
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.spray.jsonBody as sprayJsonBody
 import zio.*
-
-import org.knora.webapi.messages.admin.responder.groupsmessages.GroupsGetResponseADM
-import org.knora.webapi.messages.admin.responder.usersmessages.UsersADMJsonProtocol.groupsGetResponseADMFormat
+import org.knora.webapi.messages.admin.responder.groupsmessages.*
+import org.knora.webapi.messages.admin.responder.usersmessages.UsersADMJsonProtocol.*
 import org.knora.webapi.slice.common.api.BaseEndpoints
 
 final case class GroupsEndpoints(baseEndpoints: BaseEndpoints) {
@@ -24,7 +23,19 @@ final case class GroupsEndpoints(baseEndpoints: BaseEndpoints) {
     .description("Returns all groups.")
     .tags(tags)
 
-  val endpoints: Seq[AnyEndpoint] = Seq(getGroups)
+  val getGroup = baseEndpoints.publicEndpoint.get
+    .in(base)
+    .out(sprayJsonBody[GroupGetResponseADM])
+    .description("Returns a single group identified by IRI.")
+    .tags(tags)
+
+  val getGroupMembers = baseEndpoints.publicEndpoint.get
+    .in(base)
+    .out(sprayJsonBody[GroupMembersGetResponseADM])
+    .description("Returns all members of a single group.")
+    .tags(tags)
+
+  val endpoints: Seq[AnyEndpoint] = Seq(getGroups, getGroup, getGroupMembers)
 }
 
 object GroupsEndpoints {
