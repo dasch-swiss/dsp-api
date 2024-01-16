597e6fe0a87f45a6c616293fbef69cde712dc094
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2024-01-16 19:54:05 +0100

    continue on rdf model

--- webapi/src/main/scala/org/knora/webapi/messages/util/rdf/NewRdfModel.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/util/rdf/NewRdfModel.scala
@@ -1,103 +1,201 @@
 package org.knora.webapi.messages.util.rdf
 
-import dsp.valueobjects.V2
-import org.apache.jena.rdf.model.{Literal, Model, Resource}
-import org.knora.webapi.messages.util.rdf.Errors.{ConversionError, LiteralNotPresent, NotALiteral, RdfError}
+import org.apache.jena.rdf.model.Literal
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.Property
+import org.apache.jena.rdf.model.Resource
 import zio.*
 
 import scala.jdk.CollectionConverters.*
 
+import org.knora.webapi.messages.util.rdf.Errors.ConversionError
+import org.knora.webapi.messages.util.rdf.Errors.LiteralNotPresent
+import org.knora.webapi.messages.util.rdf.Errors.NotALiteral
+import org.knora.webapi.messages.util.rdf.Errors.ObjectNotAResource
+import org.knora.webapi.messages.util.rdf.Errors.ObjectNotPresent
+import org.knora.webapi.messages.util.rdf.Errors.RdfError
+import org.knora.webapi.messages.util.rdf.Errors.ResourceNotPresent
+import org.knora.webapi.slice.resourceinfo.domain.InternalIri
+
 object Errors {
   sealed trait RdfError
 
-  case class LiteralNotPresent(key: String) extends RdfError
-  case class NotALiteral(key: String)       extends RdfError
-  case class ConversionError(msg: String)   extends RdfError
+  final case class ResourceNotPresent(key: String) extends RdfError
+  final case class LiteralNotPresent(key: String)  extends RdfError
+  final case class ObjectNotPresent(key: String)   extends RdfError
+  final case class NotALiteral(key: String)        extends RdfError
+  final case class ObjectNotAResource(key: String) extends RdfError
+  final case class ConversionError(msg: String)    extends RdfError
 }
 
-final case class NewRdfResource(private val res: Resource, private val model: Model) {
+final case class LangString(value: String, lang: Option[String])
 
-  def getStringLiteralByProperty(propertyIri: String): Task[String] = ZIO.attempt {
-    val property = model.createProperty(propertyIri)
-    res.getProperty(property).getLiteral.getString
-  }
+final case class NewRdfResource(private val res: Resource) {
+
+  private val model = res.getModel
+
+  private def property(iri: String): UIO[Property] = ZIO.succeed(model.createProperty(iri))
+
+  private def getObjectUri(propertyIri: String): IO[Option[ObjectNotAResource], String] =
+    for {
+      prop <- property(propertyIri)
+      stmt <- ZIO.fromOption(Option(res.getProperty(prop)))
+      obj  <- ZIO.attempt(stmt.getResource).orElseFail(Some(ObjectNotAResource(propertyIri)))
+      uri  <- ZIO.fromOption(Option(obj.getURI))
+    } yield uri
+
+  private def getObjectUris(propertyIri: String): IO[ObjectNotAResource, Chunk[String]] =
+    for {
+      prop       <- property(propertyIri)
+      stmtIter    = res.listProperties(prop)
+      stmts       = Chunk.fromIterable(stmtIter.toList.asScala.toList)
+      objects    <- ZIO.foreach(stmts)(stmt => ZIO.attempt(stmt.getResource).orElseFail(ObjectNotAResource(propertyIri)))
+      uriOptions <- ZIO.foreach(objects)(obj => ZIO.fromOption(Option(obj.getURI)).unsome)
+      uris        = uriOptions.flatten
+    } yield uris
 
   private def getLiteral(propertyIri: String): IO[Option[NotALiteral], Literal] =
-    ZIO
-      .succeed(model.createProperty(propertyIri))
-      .flatMap(prop => ZIO.fromOption(Option(res.getProperty(prop))))
-      .flatMap(stmt => ZIO.attempt(stmt.getLiteral).orElseFail(NotALiteral(s"$propertyIri")).mapError(Some(_)))
+    for {
+      prop <- property(propertyIri)
+      stmt <- ZIO.fromOption(Option(res.getProperty(prop)))
+      lit  <- ZIO.attempt(stmt.getLiteral).orElseFail(Some(NotALiteral(propertyIri)))
+    } yield lit
 
-  private def getTypedLiteral[A](literal: Literal, f: Literal => A)(implicit
+  private def getLiterals(propertyIri: String): IO[NotALiteral, Chunk[Literal]] =
+    for {
+      prop     <- property(propertyIri)
+      stmtIter  = res.listProperties(prop)
+      stmts     = Chunk.fromIterable(stmtIter.toList.asScala.toList)
+      literals <- ZIO.attempt(stmts.map(_.getLiteral)).mapBoth(_ => NotALiteral(propertyIri), Chunk.fromIterable)
+    } yield literals
+
+  private def toTypedLiteral[A](literal: Literal, f: Literal => A)(implicit
     tag: Tag[A]
   ): IO[ConversionError, A] =
     ZIO
       .attempt(f(literal))
       .orElseFail(ConversionError(s"$literal is not an ${tag.getClass.getSimpleName}"))
 
-  private def getTypedLiteral[A, B](
+  private def literalAsDomainObject[A, B](
     propertyIri: String,
     f: Literal => A,
     mapper: A => Either[String, B]
-  )(implicit tag: Tag[A]): IO[RdfError, Option[B]] =
-    getLiteral(propertyIri)
-      .flatMap(lit => getTypedLiteral(lit, f).mapError(Some(_)))
-      .flatMap(a => ZIO.fromEither(mapper(a)).mapError(ConversionError.apply).mapError(Some(_)))
-      .unsome
+  )(implicit tag: Tag[A]): IO[Option[RdfError], B] =
+    for {
+      literal      <- getLiteral(propertyIri)
+      typedLiteral <- toTypedLiteral(literal, f).mapError(Some(_))
+      domainObject <- ZIO.fromEither(mapper(typedLiteral)).mapError(err => Some(ConversionError(err)))
+    } yield domainObject
 
-  def getStringLiteralPropertySingleOption[A](
+  private def literalsAsDomainObjects[A, B](
+    propertyIri: String,
+    f: Literal => A,
+    mapper: A => Either[String, B]
+  )(implicit tag: Tag[A]): IO[RdfError, Chunk[B]] =
+    for {
+      literals      <- getLiterals(propertyIri)
+      typedLiterals <- ZIO.foreach(literals)(toTypedLiteral(_, f))
+      domainObjects <- ZIO.foreach(typedLiterals)(a => ZIO.fromEither(mapper(a)).mapError(ConversionError))
+    } yield Chunk.fromIterable(domainObjects)
+
+  def getStringLiteral[A](propertyIri: String)(implicit mapper: String => Either[String, A]): IO[RdfError, Option[A]] =
+    literalAsDomainObject(propertyIri, _.getString, mapper).unsome
+
+  def getStringLiteralOrFail[A](propertyIri: String)(implicit mapper: String => Either[String, A]): IO[RdfError, A] =
+    getStringLiteral(propertyIri).someOrFail(LiteralNotPresent(propertyIri))
+
+  def getStringLiterals[A](propertyIri: String)(implicit mapper: String => Either[String, A]): IO[RdfError, Chunk[A]] =
+    literalsAsDomainObjects(propertyIri, _.getString, mapper)
+
+  def getStringLiteralsOrFail[A](
     propertyIri: String
-  )(implicit mapper: String => Either[String, A]): IO[RdfError, Option[A]] =
-    getTypedLiteral(propertyIri, _.getString, mapper)
+  )(implicit mapper: String => Either[String, A]): IO[RdfError, NonEmptyChunk[A]] =
+    for {
+      chunk         <- getStringLiterals(propertyIri)
+      nonEmptyChunk <- ZIO.fromOption(NonEmptyChunk.fromChunk(chunk)).orElseFail(LiteralNotPresent(propertyIri))
+    } yield nonEmptyChunk
 
-  def getStringLiteralPropertyOrFail[A](propertyIri: String)(implicit
-    mapper: String => Either[String, A]
-  ): IO[RdfError, A] = getStringLiteralPropertySingleOption(propertyIri).someOrFail(LiteralNotPresent(propertyIri))
+  def getLangStringLiteral[A](
+    propertyIri: String
+  )(implicit mapper: LangString => Either[String, A]): IO[RdfError, Option[A]] =
+    literalAsDomainObject(
+      propertyIri,
+      stmt => LangString(stmt.getString, Option(stmt.getLanguage)),
+      mapper
+    ).unsome
 
-  def getBooleanLiteralPropertySingleOption[A](propertyIri: String)(implicit
+  def getLangStringLiteralOrFail[A](
+    propertyIri: String
+  )(implicit mapper: LangString => Either[String, A]): IO[RdfError, A] =
+    getLangStringLiteral(propertyIri).someOrFail(LiteralNotPresent(propertyIri))
+
+  def getLangStringLiterals[A](
+    propertyIri: String
+  )(implicit mapper: LangString => Either[String, A]): IO[RdfError, Chunk[A]] =
+    literalsAsDomainObjects(
+      propertyIri,
+      stmt => LangString(stmt.getString, Option(stmt.getLanguage)),
+      mapper
+    )
+
+  def getLangStringLiteralsOrFail[A](
+    propertyIri: String
+  )(implicit mapper: LangString => Either[String, A]): IO[RdfError, NonEmptyChunk[A]] =
+    for {
+      chunk         <- getLangStringLiterals(propertyIri)
+      nonEmptyChunk <- ZIO.fromOption(NonEmptyChunk.fromChunk(chunk)).orElseFail(LiteralNotPresent(propertyIri))
+    } yield nonEmptyChunk
+
+  def getBooleanLiteral[A](propertyIri: String)(implicit
     mapper: Boolean => Either[String, A]
   ): IO[RdfError, Option[A]] =
-    getTypedLiteral(propertyIri, _.getBoolean, mapper)
+    literalAsDomainObject(propertyIri, _.getBoolean, mapper).unsome
 
-  def getBooleanLiteralPropertyOrFail[A](propertyIri: String)(implicit
+  def getBooleanLiteralOrFail[A](propertyIri: String)(implicit
     mapper: Boolean => Either[String, A]
-  ): IO[RdfError, A] = getBooleanLiteralPropertySingleOption(propertyIri).someOrFail(LiteralNotPresent(propertyIri))
+  ): IO[RdfError, A] =
+    getBooleanLiteral(propertyIri).someOrFail(LiteralNotPresent(propertyIri))
 
-  def getStringLiteralsByProperty(propertyIri: String): Task[List[String]] = ZIO.attempt {
-    val property = model.createProperty(propertyIri)
-    res.listProperties(property).toList.asScala.toList.map(_.getLiteral.getString)
-  }
+  def getBooleanLiterals[A](propertyIri: String)(implicit
+    mapper: Boolean => Either[String, A]
+  ): IO[RdfError, Chunk[A]] =
+    literalsAsDomainObjects(propertyIri, _.getBoolean, mapper)
 
-  def getLangStringLiteralsByProperty(propertyIri: String): Task[List[V2.StringLiteralV2]] = ZIO.attempt {
-    val property = model.createProperty(propertyIri)
-    res.listProperties(property).toList.asScala.toList.map { stmt =>
-      val lang       = stmt.getLiteral.getLanguage
-      val langOption = Option.when(lang.nonEmpty)(lang)
-      val value      = stmt.getLiteral.getString
-      V2.StringLiteralV2(value, langOption)
-    }
-  }
+  def getBooleanLiteralsOrFail[A](propertyIri: String)(implicit
+    mapper: Boolean => Either[String, A]
+  ): IO[RdfError, NonEmptyChunk[A]] =
+    for {
+      chunk         <- getBooleanLiterals(propertyIri)
+      nonEmptyChunk <- ZIO.fromOption(NonEmptyChunk.fromChunk(chunk)).orElseFail(LiteralNotPresent(propertyIri))
+    } yield nonEmptyChunk
 
-  def getBooleanLiteralByProperty(propertyIri: String): Task[Boolean] = ZIO.attempt {
-    val property = model.createProperty(propertyIri)
-    res.getProperty(property).getLiteral.getBoolean
-  }
+  def getObjectIri(propertyIri: String): IO[RdfError, Option[InternalIri]] =
+    for {
+      prop <- property(propertyIri)
+      iri  <- getObjectUri(propertyIri).map(InternalIri).unsome
+    } yield iri
 
-  def getObjectIriByProperty(propertyIri: String): Task[String] = ZIO.attempt {
-    val property = model.createProperty(propertyIri)
-    res.getProperty(property).getResource.getURI
-  }
+  def getObjectIriOrFail(propertyIri: String): IO[RdfError, InternalIri] =
+    getObjectIri(propertyIri).someOrFail(ObjectNotPresent(propertyIri))
 
-  def getObjectIrisByProperty(propertyIri: String): Task[List[String]] = ZIO.attempt {
-    val property = model.createProperty(propertyIri)
-    res.listProperties(property).toList.asScala.toList.map(_.getResource.getURI)
-  }
+  def getObjectIris(propertyIri: String): IO[RdfError, Chunk[InternalIri]] =
+    for {
+      prop <- property(propertyIri)
+      iris <- getObjectUris(propertyIri).map(_.map(InternalIri))
+    } yield iris
+
+  def getObjectIrisOrFail(propertyIri: String): IO[RdfError, NonEmptyChunk[InternalIri]] =
+    for {
+      chunk         <- getObjectIris(propertyIri)
+      nonEmptyChunk <- ZIO.fromOption(NonEmptyChunk.fromChunk(chunk)).orElseFail(ObjectNotPresent(propertyIri))
+    } yield nonEmptyChunk
 
 }
 
 final case class NewRdfModel(private val model: Model) {
-  def getResource(subjectIri: String): Task[NewRdfResource] = ZIO.attempt {
-    val resource = model.getResource(subjectIri)
-    NewRdfResource(resource, model)
-  }
+  def getResource(subjectIri: String): IO[RdfError, NewRdfResource] = ZIO.attempt {
+    val resource = model.createResource(subjectIri)
+    NewRdfResource(resource)
+    // TODO: this will probably not fail even if the resource is not present (should check if it is empty)
+  }.orElseFail(ResourceNotPresent(subjectIri))
 }
