b99094c272b86d4012dca0fd00cb6c1f7334f1f8
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-01-17 19:07:22 +0100

    cleanup

--- webapi/src/main/scala/org/knora/webapi/slice/admin/api/Codecs.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/api/Codecs.scala
@@ -1,22 +1,18 @@
 package org.knora.webapi.slice.admin.api
 
-import sttp.tapir.Codec
-import sttp.tapir.CodecFormat
-import zio.json.JsonCodec
-
 import org.knora.webapi.slice.admin.api.model.MaintenanceRequests.AssetId
-import org.knora.webapi.slice.admin.domain.model.KnoraProject.ProjectIri
-import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortcode
-import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortname
+import org.knora.webapi.slice.admin.domain.model.KnoraProject.{ProjectIri, Shortcode, Shortname}
 import org.knora.webapi.slice.common.Value.StringValue
 import org.knora.webapi.slice.common.domain.SparqlEncodedString
+import sttp.tapir.{Codec, CodecFormat}
+import zio.json.JsonCodec
 
 object Codecs {
   object TapirCodec {
 
     private type StringCodec[A] = Codec[String, A, CodecFormat.TextPlain]
     private def stringCodec[A <: StringValue](from: String => Either[String, A]): StringCodec[A] =
-      Codec.string.mapEither(from)(_.value)
+      stringCodec(from, _.value)
     private def stringCodec[A](from: String => Either[String, A], to: A => String): StringCodec[A] =
       Codec.string.mapEither(from)(to)
 
@@ -31,7 +27,7 @@ object Codecs {
 
     private type StringCodec[A] = JsonCodec[A]
     private def stringCodec[A <: StringValue](from: String => Either[String, A]): StringCodec[A] =
-      JsonCodec[String].transformOrFail(from, _.value)
+      stringCodec(from, _.value)
     private def stringCodec[A](from: String => Either[String, A], to: A => String): StringCodec[A] =
       JsonCodec[String].transformOrFail(from, to)
 
