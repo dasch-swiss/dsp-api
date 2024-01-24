8cc78241789fd258585a3aef7e083f3299dc0ef5
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-01-24 21:25:30 +0100

    fixup!

--- webapi/src/main/scala/org/knora/webapi/slice/admin/domain/model/RestrictedView.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/domain/model/RestrictedView.scala
@@ -5,10 +5,10 @@
 
 package org.knora.webapi.slice.admin.domain.model
 
-import scala.util.matching.Regex
-
 import org.knora.webapi.slice.common.StringValueCompanion
 import org.knora.webapi.slice.common.Value.StringValue
+
+import scala.util.matching.Regex
 
 final case class RestrictedView(size: RestrictedViewSize, watermark: Boolean)
 
@@ -16,18 +16,18 @@ case class RestrictedViewSize private (value: String) extends AnyVal with String
 
 object RestrictedViewSize extends StringValueCompanion[RestrictedViewSize] {
 
-  val default: RestrictedViewSize = RestrictedViewSize.unsafeFrom("!512,512")
-
   // matches strings "pct:n" with n between 1 and 100
-  private val percentage: Regex = "pct:(?:100|[1-9][0-9]?)$".r
+  private val percentagePattern: Regex = "pct:(?:100|[1-9][0-9]?)$".r
 
   // matches strings "!x,x" where x is a positive integer and represents the dimensions of the restricted view
   private val dimensionsPattern: Regex = "!(\\d+),(\\1)$".r
 
+  val default: RestrictedViewSize = RestrictedViewSize.unsafeFrom("!512,512")
+
   def from(value: String): Either[String, RestrictedViewSize] =
     value match {
       case _ if value.isEmpty                    => Left("RestrictedViewSize cannot be empty.")
-      case _ if percentage.matches(value)        => Right(RestrictedViewSize(value))
+      case _ if percentagePattern.matches(value) => Right(RestrictedViewSize(value))
       case _ if dimensionsPattern.matches(value) => Right(RestrictedViewSize(value))
       case _                                     => Left(s"Invalid RestrictedViewSize: $value")
     }
