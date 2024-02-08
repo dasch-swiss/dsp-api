16d41c288be41227567974d629e7c162b0ceb4ff
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2024-02-08 11:31:40 +0100

    chore: Remove `knora-ontologies` symlink

--- webapi/scripts/fuseki-init-knora-test.sh
+++ webapi/scripts/fuseki-init-knora-test.sh
@@ -8,11 +8,11 @@ REPOSITORY="knora-test"
 
 # delete-repository // delete dos not work correctly. need to delete database manually.
 create-repository
-upload-graph ../../knora-ontologies/knora-admin.ttl http://www.knora.org/ontology/knora-admin
-upload-graph ../../knora-ontologies/knora-base.ttl http://www.knora.org/ontology/knora-base
-upload-graph ../../knora-ontologies/standoff-onto.ttl http://www.knora.org/ontology/standoff
-upload-graph ../../knora-ontologies/standoff-data.ttl http://www.knora.org/data/standoff
-upload-graph ../../knora-ontologies/salsah-gui.ttl http://www.knora.org/ontology/salsah-gui
+upload-graph ../../webapi/src/main/resources/knora-ontologies/knora-admin.ttl http://www.knora.org/ontology/knora-admin
+upload-graph ../../webapi/src/main/resources/knora-ontologies/knora-base.ttl http://www.knora.org/ontology/knora-base
+upload-graph ../../webapi/src/main/resources/knora-ontologies/standoff-onto.ttl http://www.knora.org/ontology/standoff
+upload-graph ../../webapi/src/main/resources/knora-ontologies/standoff-data.ttl http://www.knora.org/data/standoff
+upload-graph ../../webapi/src/main/resources/knora-ontologies/salsah-gui.ttl http://www.knora.org/ontology/salsah-gui
 upload-graph ../../test_data/project_data/admin-data.ttl http://www.knora.org/data/admin
 upload-graph ../../test_data/project_data/permissions-data.ttl http://www.knora.org/data/permissions
 upload-graph ../../test_data/project_data/system-data.ttl http://www.knora.org/data/0000/SystemProject
