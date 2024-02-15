54c769ed80b0a621feae0af8e291e47cdb2b57c5
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2024-02-15 19:54:41 +0100

    make named graph adjustable

--- webapi/src/test/scala/org/knora/webapi/store/triplestore/TestDatasetBuilder.scala
+++ webapi/src/test/scala/org/knora/webapi/store/triplestore/TestDatasetBuilder.scala
@@ -21,10 +21,10 @@ object TestDatasetBuilder {
 
   private def readToModel(turtle: String)(model: Model): Model = model.read(new StringReader(turtle), null, "TTL")
 
-  private def transactionalWrite(change: Model => Model)(ds: Dataset): Task[Dataset] = ZIO.attempt {
+  private def transactionalWrite(change: Model => Model, graph: String)(ds: Dataset): Task[Dataset] = ZIO.attempt {
     ds.begin(ReadWrite.WRITE)
     try {
-      change apply ds.getNamedModel("http://www.example.org/graph")
+      change apply ds.getNamedModel(graph)
       ds.commit()
     } finally {
       ds.end()
@@ -32,12 +32,8 @@ object TestDatasetBuilder {
     ds
   }
 
-  private def datasetFromTurtle(turtle: String): Task[Dataset] =
-    for {
-      dsEmpty <- createEmptyDataset
-      ds      <- transactionalWrite(readToModel(turtle))(dsEmpty)
-    } yield ds
-//    createEmptyDataset.flatMap(transactionalWrite(readToModel(turtle)))
+  private def datasetFromTurtle(turtle: String, graph: String = "http://www.example.org/graph"): Task[Dataset] =
+    createEmptyDataset.flatMap(transactionalWrite(readToModel(turtle), graph))
 
   def datasetFromTriG(trig: String): Task[Dataset] =
     for {
