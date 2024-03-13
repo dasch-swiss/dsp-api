5a96d95396c72a24619decfb4323ee77ae59ea92
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-03-13 12:05:38 +0100

    header & fmt

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/AbstractSparqlUpdatePlugin.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/AbstractSparqlUpdatePlugin.scala
@@ -1,8 +1,14 @@
+/*
+ * Copyright © 2021 - 2024 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.store.triplestore.upgrade.plugins
 
 import org.apache.jena.update.UpdateExecutionFactory
 import org.apache.jena.update.UpdateFactory
 import org.eclipse.rdf4j.sparqlbuilder.core.query.ModifyQuery
+
 import org.knora.webapi.messages.util.rdf.JenaModel
 import org.knora.webapi.messages.util.rdf.RdfModel
 import org.knora.webapi.store.triplestore.upgrade.UpgradePlugin
@@ -13,8 +19,8 @@ abstract class AbstractSparqlUpdatePlugin extends UpgradePlugin {
 
   override def transform(model: RdfModel): Unit = {
     val dataset = model.asInstanceOf[JenaModel].getDataset
-    val update = UpdateFactory.create(getQuery.getQueryString)
-    val qExec  = UpdateExecutionFactory.create(update, dataset)
+    val update  = UpdateFactory.create(getQuery.getQueryString)
+    val qExec   = UpdateExecutionFactory.create(update, dataset)
     qExec.execute()
   }
 }
