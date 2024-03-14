e563ab95b6d9c052e39b544e020d65c7ecb29476
Christian Kleinbölting <seakayone@users.noreply.github.com>
2024-03-14 11:46:05 +0100

    fix: Remove invalid watermark triples (DEV-3418) (#3116)

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/RepositoryUpdatePlan.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/RepositoryUpdatePlan.scala
@@ -8,6 +8,7 @@ package org.knora.webapi.store.triplestore.upgrade
 import com.typesafe.scalalogging.Logger
 
 import org.knora.webapi.messages.store.triplestoremessages.RdfDataObject
+import org.knora.webapi.store.triplestore.upgrade.plugins.UpgradePluginPR3110
 import org.knora.webapi.store.triplestore.upgrade.plugins.*
 
 /**
@@ -64,6 +65,7 @@ object RepositoryUpdatePlan {
       PluginForKnoraBaseVersion(versionNumber = 27, plugin = new MigrateOnlyBuiltInGraphs), // PR 3026
       PluginForKnoraBaseVersion(versionNumber = 28, plugin = new MigrateOnlyBuiltInGraphs), // PR 3038
       PluginForKnoraBaseVersion(versionNumber = 29, plugin = new UpgradePluginPR3110()),
+      PluginForKnoraBaseVersion(versionNumber = 30, plugin = new UpgradePluginPR3111()),
       // KEEP IT ON THE BOTTOM
       // From "versionNumber = 6" don't use prBasedVersionString!
     )
