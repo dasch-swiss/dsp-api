465959c4c7da93fabc91defa986cdf7a81591104
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-03-18 13:03:59 +0100

    fmt

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/UpgradePluginPR3112.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/upgrade/plugins/UpgradePluginPR3112.scala
@@ -6,8 +6,8 @@
 package org.knora.webapi.store.triplestore.upgrade.plugins
 
 import org.eclipse.rdf4j.sparqlbuilder.core.SparqlBuilder.`var` as variable
-import org.eclipse.rdf4j.sparqlbuilder.core.query.ModifyQuery
 import org.eclipse.rdf4j.sparqlbuilder.core.query.*
+import org.eclipse.rdf4j.sparqlbuilder.core.query.ModifyQuery
 import org.eclipse.rdf4j.sparqlbuilder.rdf.Rdf
 
 import org.knora.webapi.slice.admin.AdminConstants
