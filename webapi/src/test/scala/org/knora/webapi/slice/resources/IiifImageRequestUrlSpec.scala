f5e852050a40f5f3553c05e5239d0072d0c37f3c
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-03-26 11:23:01 +0100

    add test case for image information request url

--- webapi/src/test/scala/org/knora/webapi/slice/resources/IiifImageRequestUrlSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/resources/IiifImageRequestUrlSpec.scala
@@ -12,15 +12,28 @@ import java.net.URI
 
 object IiifImageRequestUrlSpec extends ZIOSpecDefault {
 
-  val spec: Spec[TestEnvironment with Scope, Nothing] = suite("IiifImageRequestUrl")(test("should") {
-    val validUrls =
-      Seq(
-        "https://iiif.ub.unibe.ch/image/v2.1/632664f2-20cb-43e4-8584-2fa3988c63a2/full/max/0/default.jpg",
-        "https://iiif.dasch.swiss/0811/5Jd909CLmCJ-BUUL1DDOXGJ.jp2/full/3360,2123/0/default.jpg",
-      )
-    check(Gen.fromIterable(validUrls)) { url =>
-      val actual = IiifImageRequestUrl.from(url)
-      assertTrue(actual.isRight, actual == Right(IiifImageRequestUrl(URI.create(url).toURL)))
-    }
-  })
+  val spec: Spec[TestEnvironment with Scope, Nothing] = suite("IiifImageRequestUrl")(
+    test("should accept a IIIF image request url") {
+      val validUrls =
+        Seq(
+          "https://iiif.ub.unibe.ch/image/v2.1/632664f2-20cb-43e4-8584-2fa3988c63a2/full/max/0/default.jpg",
+          "https://iiif.dasch.swiss/0811/5Jd909CLmCJ-BUUL1DDOXGJ.jp2/full/3360,2123/0/default.jpg",
+        )
+      check(Gen.fromIterable(validUrls)) { url =>
+        val actual = IiifImageRequestUrl.from(url)
+        assertTrue(actual.isRight, actual == Right(IiifImageRequestUrl(URI.create(url).toURL)))
+      }
+    },
+    test("should reject a IIIF image information request url") {
+      val invalidUrls =
+        Seq(
+          "https://iiif.ub.unibe.ch/image/v2.1/632664f2-20cb-43e4-8584-2fa3988c63a2/info.json",
+          "https://iiif.dasch.swiss/0811/5Jd909CLmCJ-BUUL1DDOXGJ.jp2/info.json",
+        )
+      check(Gen.fromIterable(invalidUrls)) { url =>
+        val actual = IiifImageRequestUrl.from(url)
+        assertTrue(actual.isLeft)
+      }
+    },
+  )
 }
