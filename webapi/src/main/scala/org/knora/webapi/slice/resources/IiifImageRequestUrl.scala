f12beabd83ce8acdbaf0d9924820c357b0741602
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-03-26 12:03:38 +0100

    headers fmt

--- webapi/src/main/scala/org/knora/webapi/slice/resources/IiifImageRequestUrl.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/resources/IiifImageRequestUrl.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2024 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.slice.resources
 
 import java.net.URI
@@ -11,12 +16,13 @@ final case class IiifImageRequestUrl(value: URL) extends AnyVal with Value[URL]
 
 object IiifImageRequestUrl extends WithFrom[String, IiifImageRequestUrl] {
 
-  private val iiifImageUrlRegex = """^(https?://[^/]+/[^/]+/[^/]+/[^/]+/[^/]+/[^/]+(?:/.*)?)$""".r
+  private val iiifImageUrlRegex1 = """^(https?://[^/]+/[^/]+/[^/]+/[^/]+/[^/]+/[^/]+(?:/.*)?)$""".r
+//  private val iiifImageUrlRegex2 = """^(https?://[^/]+/[^/]+/[^/]+/[^/]+/[^/]+/[^/]+(?:/.*)?)$""".r
   def from(value: String): Either[String, IiifImageRequestUrl] =
     Try(URI.create(value).toURL).toEither.left.map(_ => s"Invalid URL: $value").flatMap { url =>
       value match {
-        case iiifImageUrlRegex(_) => Right(IiifImageRequestUrl(url))
-        case _                    => Left(s"Invalid IIIF image URL: $value")
+        case iiifImageUrlRegex1(_) => Right(IiifImageRequestUrl(url))
+        case _                     => Left(s"Invalid IIIF image URL: $value")
       }
     }
 }
--- webapi/src/test/scala/org/knora/webapi/slice/resources/IiifImageRequestUrlSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/resources/IiifImageRequestUrlSpec.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2024 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.slice.resources
 
 import zio.Scope
@@ -13,11 +18,30 @@ import java.net.URI
 object IiifImageRequestUrlSpec extends ZIOSpecDefault {
 
   val spec: Spec[TestEnvironment with Scope, Nothing] = suite("IiifImageRequestUrl")(
-    test("should accept a IIIF image request url") {
+    test("should accept a IIIF image request url version 1") {
+      // https://iiif.io/api/image/1.0/#21-image-request-url-syntax
+      // http[s]://server/[prefix/]identifier/region/size/rotation/quality[.format]
       val validUrls =
         Seq(
-          "https://iiif.ub.unibe.ch/image/v2.1/632664f2-20cb-43e4-8584-2fa3988c63a2/full/max/0/default.jpg",
-          "https://iiif.dasch.swiss/0811/5Jd909CLmCJ-BUUL1DDOXGJ.jp2/full/3360,2123/0/default.jpg",
+          // region https://iiif.io/api/image/1.0/#41-region
+          "http://www.example.org/image-service/abcd1234/80,15,60,75/full/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/pct:10,10,80,70/full/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full/full/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/100,/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/,100/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/pct:50/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/150,75/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/!150,75/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full90/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full180/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full270/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full22.5/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full/600,/0/native.jpg",
+          "http://www.example.org/image-service/abcd1234/full/600,/0/color.jpg",
+          "http://www.example.org/image-service/abcd1234/full/600,/0/grey.jpg",
+          "http://www.example.org/image-service/abcd1234/full/600,/0/bitonal.jpg",
         )
       check(Gen.fromIterable(validUrls)) { url =>
         val actual = IiifImageRequestUrl.from(url)
@@ -29,6 +53,18 @@ object IiifImageRequestUrlSpec extends ZIOSpecDefault {
         Seq(
           "https://iiif.ub.unibe.ch/image/v2.1/632664f2-20cb-43e4-8584-2fa3988c63a2/info.json",
           "https://iiif.dasch.swiss/0811/5Jd909CLmCJ-BUUL1DDOXGJ.jp2/info.json",
+        )
+      check(Gen.fromIterable(invalidUrls)) { url =>
+        val actual = IiifImageRequestUrl.from(url)
+        assertTrue(actual.isLeft)
+      }
+    },
+    test("should reject an invalid IIIF image request url") {
+      val invalidUrls =
+        Seq(
+          // invalid rotation (mustbeint)
+          "https://iiif.ub.unibe.ch/image/v2.1/632664f2-20cb-43e4-8584-2fa3988c63a2/full/max/mustbeint/default.jpg",
+          "https://iiif.dasch.swiss/0811/5Jd909CLmCJ-BUUL1DDOXGJ.jp2/full/3360,2123/mustbeint/default.jpg",
         )
       check(Gen.fromIterable(invalidUrls)) { url =>
         val actual = IiifImageRequestUrl.from(url)
