54e0165f4330d4dfcd9114250b710d50c26e6c7c
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2024-03-27 22:25:32 +0100

    add segment e2e test

---
+++ integration/src/test/scala/org/knora/webapi/e2ez/SegmentE2EZSpec.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2021 - 2024 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.e2ez
+
+import zio.Clock
+import zio.test._
+
+import org.knora.webapi.E2EZSpec
+import org.knora.webapi.messages.store.triplestoremessages.RdfDataObject
+
+object SegmentE2EZSpec extends E2EZSpec {
+
+  override def rdfDataObjects: List[RdfDataObject] = List(
+    RdfDataObject(
+      path = "test_data/project_data/anything-data.ttl",
+      name = "http://www.knora.org/data/0001/anything",
+    ),
+  )
+
+  private val withoutSubclasses =
+    suite("Segments using knora-base classes directly")(
+      test("Create an instance of `knora-base:VideoSegment`") {
+        val createPayload =
+          """|{
+             |  "@type": "knora-api:Resource",
+             |  "knora-api:hasComment": {
+             |    "@type": "knora-api:TextValue",
+             |    "knora-api:valueAsString": "This is a test video segment."
+             |  },
+             |  "knora-api:hasSegmentBounds": {
+             |    "@type": "knora-api:IntervalValue",
+             |    "knora-api:intervalValueHasStart" : {
+             |      "@type" : "xsd:decimal",
+             |      "@value" : "1.0"
+             |    },
+             |    "knora-api:intervalValueHasEnd" : {
+             |      "@type" : "xsd:decimal",
+             |      "@value" : "3.0"
+             |    }
+             |  },
+             |  "knora-api:isVideoSegmentOfValue": {
+             |    "@type": "knora-api:LinkValue",
+             |    "knora-api:linkValueHasTargetIri": {
+             |      "@id": "http://rdfh.ch/0001/zUelKon-SdmuL9iiHMgnGw"
+             |    }
+             |  },
+             |  "rdfs:label": "Test Video Segment",
+             |  "knora-api:attachedToProject": {
+             |    "@id": "http://rdfh.ch/projects/0001"
+             |  },
+             |  "@context": {
+             |    "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+             |    "knora-api" : "http://api.knora.org/ontology/knora-api/v2#",
+             |    "rdfs" : "http://www.w3.org/2000/01/rdf-schema#",
+             |    "xsd" : "http://www.w3.org/2001/XMLSchema#",
+             |    "anything" : "http://0.0.0.0:3333/ontology/0001/anything/v2#"
+             |  }
+             |}
+             |""".stripMargin
+        for {
+          _        <- Clock.currentDateTime.debug
+          token    <- getToken("root@example.com", "test")
+          response <- sendPostRequestStringOrFail("/v2/resources", createPayload, Some(token))
+        } yield assertTrue(response.contains("knora-base:Segment"))
+      },
+    )
+
+  private val withSubclasses =
+    suite("Segments using subcalsses of knora-base classes")(
+    )
+
+  override def e2eSpec: Spec[env, Any] =
+    suite("SegmentE2EZSpec")(
+      withoutSubclasses,
+      withSubclasses,
+    )
+}
