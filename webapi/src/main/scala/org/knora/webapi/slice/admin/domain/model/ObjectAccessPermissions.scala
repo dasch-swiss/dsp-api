02c1747bca463fa833b6494e6d5cbc1bc7602127
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-03-28 08:42:22 +0100

    Remove unused code

--- webapi/src/main/scala/org/knora/webapi/slice/admin/domain/model/ObjectAccessPermissions.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/domain/model/ObjectAccessPermissions.scala
@@ -5,46 +5,36 @@
 
 package org.knora.webapi.slice.admin.domain.model
 
-import org.knora.webapi.IRI
-import org.knora.webapi.messages.admin.responder.permissionsmessages.PermissionADM
-
 sealed trait ObjectAccessPermission extends Ordered[ObjectAccessPermission] {
   self =>
-  def name: String
   def token: String
   def code: Int
-  final def toPermissionADM(groupIri: IRI): PermissionADM       = PermissionADM.from(self, groupIri)
   final override def compare(that: ObjectAccessPermission): Int = self.code - that.code
   final override def toString: String                           = token
 }
 
 object ObjectAccessPermission {
   case object RestrictedView extends ObjectAccessPermission {
-    override val name: String  = "restricted view permission"
     override val token: String = "RV"
     override val code: Int     = 1
   }
 
   case object View extends ObjectAccessPermission {
-    override val name: String  = "view permission"
     override val token: String = "V"
     override val code: Int     = 2
   }
 
   case object Modify extends ObjectAccessPermission {
-    override val name: String  = "modify permission"
     override val token: String = "M"
     override val code: Int     = 6
   }
 
   case object Delete extends ObjectAccessPermission {
-    override val name: String  = "delete permission"
     override val token: String = "D"
     override val code: Int     = 7
   }
 
   case object ChangeRights extends ObjectAccessPermission {
-    override val name: String  = "change rights permission"
     override val token: String = "CR"
     override val code: Int     = 8
   }
