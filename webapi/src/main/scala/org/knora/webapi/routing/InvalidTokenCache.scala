f1d28aee403980b7f886da86aad5919ed7a4238d
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-04-21 13:07:33 +0200

    fixup

--- webapi/src/main/scala/org/knora/webapi/routing/InvalidTokenCache.scala
+++ webapi/src/main/scala/org/knora/webapi/routing/InvalidTokenCache.scala
@@ -11,15 +11,13 @@ import zio.ZLayer
 import org.knora.webapi.slice.infrastructure.CacheManager
 import org.knora.webapi.slice.infrastructure.EhCache
 
-final case class InvalidTokenCache(cache: EhCache[String, Boolean]) {
-  def put(token: String): Unit         = cache.put(token, true)
+final case class InvalidTokenCache(cache: EhCache[String, String]) {
+  def put(token: String): Unit         = cache.put(token, "")
   def contains(token: String): Boolean = cache.get(token).isDefined
 }
 
 object InvalidTokenCache {
   val layer = ZLayer.fromZIO(
-    ZIO.serviceWithZIO[CacheManager](
-      _.createCache[String, Boolean]("authenticationInvalidationCache").map(InvalidTokenCache.apply),
-    ),
+    ZIO.serviceWithZIO[CacheManager](_.createCache[String, String]("invalidTokenCache").map(InvalidTokenCache.apply)),
   )
 }
