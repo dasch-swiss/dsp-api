6cef0dcae43ec3a5ee1f2b52188a8f662b794b85
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-10-23 16:26:48 +0200

    Introduce JenaModelOps

--- webapi/src/main/scala/org/knora/webapi/slice/common/JenaModelOps.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/common/JenaModelOps.scala
@@ -5,32 +5,55 @@
 
 package org.knora.webapi.slice.common
 
+import com.apicatalog.jsonld.document.JsonDocument
+import com.apicatalog.jsonld.JsonLd
+import com.apicatalog.jsonld.JsonLdOptions
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.riot.Lang
-import zio.IO
-import zio.ZIO
+import org.apache.jena.riot.RDFDataMgr
+import org.apache.jena.riot.RDFFormat
+
+import java.nio.charset.StandardCharsets
+import org.knora.webapi.slice.common.JenaModelOps.Errors.ParseError
+import zio.*
+import zio.json
+import zio.json.ast.Json
 
 import java.io.ByteArrayInputStream
-import java.io.IOException
-import java.nio.charset.StandardCharsets
+import java.io.StringWriter
+import zio.json.DecoderOps
+import zio.json.ast.JsonCursor.field
+import zio.json.ast.JsonCursor.isObject
 
-import org.knora.webapi.slice.common.JenaModelOps.Errors.ParseError
-
-object JenaModelOps {
+object JenaModelOps { self =>
 
   enum Errors {
     case ParseError(msg: String) extends Errors
   }
   object Errors {
-    def parseError(ex: IOException): ParseError = ParseError(ex.getMessage)
+    def parseError(ex: Throwable): ParseError = ParseError(ex.getMessage)
   }
 
-  def fromJsonLd(str: String): IO[ParseError, Model] = from(str, Lang.JSONLD)
+  def fromJsonLd(str: String): ZIO[Scope, ParseError, Model] = from(str, Lang.JSONLD)
 
-  def from(str: String, lang: Lang): IO[ParseError, Model] = ZIO.attemptBlockingIO {
-    val model = ModelFactory.createDefaultModel()
-    model.read(new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)), lang.getName)
-    model
-  }.mapError(Errors.parseError)
+  private val createModel =
+    ZIO.acquireRelease(ZIO.succeed(ModelFactory.createDefaultModel()))(m => ZIO.succeed(m.close()))
+
+  def from(str: String, lang: Lang): ZIO[Scope, Errors.ParseError, Model] =
+    for {
+      m <- createModel
+      _ <- ZIO
+             .attempt(RDFDataMgr.read(m, ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)), lang))
+             .mapError(Errors.parseError)
+    } yield m
+
+  extension (model: Model) {
+    def toJsonLd: UIO[String] = ZIO.scoped {
+      for {
+        w <- ZIO.fromAutoCloseable(ZIO.succeed(StringWriter()))
+        _ <- ZIO.succeed(RDFDataMgr.write(w, model, RDFFormat.JSONLD))
+      } yield w.toString
+    }
+  }
 }
---
+++ webapi/src/test/scala/org/knora/webapi/slice/common/JenaModelOpsSpec.scala
@@ -0,0 +1,63 @@
+package org.knora.webapi.slice.common
+
+import org.knora.webapi.slice.common.JenaModelOps.toJsonLd
+import zio.test.*
+import zio.*
+import zio.json.ast.Json
+import zio.json.DecoderOps
+import zio.json.EncoderOps
+
+object JenaModelOpsSpec extends ZIOSpecDefault {
+
+  private val jsonLd = """{
+                     "@id" : "http://rdfh.ch/0001/a-thing",
+                     "@type" : "anything:Thing",
+                     "anything:hasInteger" : {
+                       "@type" : "knora-api:IntValue",
+                       "knora-api:intValueAsInt" : 4
+                     },
+                     "@context" : {
+                       "knora-api" : "http://api.knora.org/ontology/knora-api/v2#",
+                       "anything" : "http://0.0.0.0:3333/ontology/0001/anything/v2#"
+                     }
+                   }
+                   """.stripMargin
+  private val jsonLd2 = """{
+                     "@id" : "http://rdfh.ch/0001/a-thing",
+                     "@type" : "anything:Thing",
+                     "anything:hasInteger" : {
+                       "@type" : "knora-api:IntValue",
+                       "knora-api:intValueAsInt" : {
+                          "@type" : "xsd",
+                          "@value" : "4"
+                        }
+                     },
+                     "@context" : {
+                       "knora-api" : "http://api.knora.org/ontology/knora-api/v2#",
+                       "anything" : "http://0.0.0.0:3333/ontology/0001/anything/v2#",
+                       "xsd" : "http://www.w3.org/2001/XMLSchema#integer"
+                     }
+                   }
+                   """.stripMargin
+  val spec = suite("JenaModelOps")(
+    suite("fromJsonLd")(
+      test("should parse the json ld") {
+        JenaModelOps.fromJsonLd(jsonLd).flatMap { model =>
+          assertTrue(model.size() == 4)
+        }
+      },
+      test("should produce isomorphic models") {
+        for {
+          model1 <- JenaModelOps.fromJsonLd(jsonLd)
+          model2 <- JenaModelOps.fromJsonLd(jsonLd2)
+        } yield assertTrue(model1.isIsomorphicWith(model2))
+      },
+      test("get id"){
+        for {
+          model<- JenaModelOps.fromJsonLd(jsonLd)
+          id <- model.getï
+        }
+      }
+    ),
+  )
+}
