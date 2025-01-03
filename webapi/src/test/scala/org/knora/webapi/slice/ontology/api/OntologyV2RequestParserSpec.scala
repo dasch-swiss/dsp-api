6e42f05eb3039fd9a4eb17063e4c53007e028f71
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-01-03 10:12:03 +0100

    add test

--- webapi/src/test/scala/org/knora/webapi/slice/ontology/api/OntologyV2RequestParserSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/ontology/api/OntologyV2RequestParserSpec.scala
@@ -23,7 +23,7 @@ object OntologyV2RequestParserSpec extends ZIOSpecDefault {
   private val user   = TestDataFactory.User.rootUser
 
   private val changeOntologyMetadataRequestV2Suite =
-    suite("ChangeOntologyMetadataRequestV2") {
+    suite("ChangeOntologyMetadataRequestV2")(
       test("should parse correct jsonLd") {
         val instant = Instant.parse("2017-12-19T15:23:42.166Z")
         val jsonLd: String =
@@ -69,8 +69,46 @@ object OntologyV2RequestParserSpec extends ZIOSpecDefault {
             ),
           )
         }
-      }
-    }
+      },
+      test("should parse correct jsonLd without optional fields") {
+        val instant = Instant.parse("2017-12-19T15:23:42.166Z")
+        val jsonLd: String =
+          """
+            |{
+            |  "@id" : "http://0.0.0.0:3333/ontology/0001/anything/v2",
+            |  "@type" : "owl:Ontology",
+            |  "knora-api:lastModificationDate" : {
+            |    "@type" : "xsd:dateTimeStamp",
+            |    "@value" : "2017-12-19T15:23:42.166Z"
+            |  },
+            |  "@context" : {
+            |    "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            |    "knora-api" : "http://api.knora.org/ontology/knora-api/v2#",
+            |    "owl" : "http://www.w3.org/2002/07/owl#",
+            |    "rdfs" : "http://www.w3.org/2000/01/rdf-schema#",
+            |    "xsd" : "http://www.w3.org/2001/XMLSchema#",
+            |    "anything" : "http://0.0.0.0:3333/ontology/0001/anything/v2#"
+            |  }
+            |}
+            |""".stripMargin
+
+        check(JsonLdTransformations.allGen) { t =>
+          for {
+            uuid <- Random.nextUUID
+            req  <- parser(_.changeOntologyMetadataRequestV2(t(jsonLd), uuid, user))
+          } yield assertTrue(
+            req == ChangeOntologyMetadataRequestV2(
+              sf.toSmartIri("http://0.0.0.0:3333/ontology/0001/anything/v2"),
+              None,
+              None,
+              instant,
+              uuid,
+              user,
+            ),
+          )
+        }
+      },
+    )
 
   val spec = suite("OntologyV2RequestParser")(changeOntologyMetadataRequestV2Suite)
     .provide(OntologyV2RequestParser.layer, IriConverter.layer, StringFormatter.test)
