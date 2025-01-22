b58e89be85f91fafebecc800711ba96f7e263c4c
Christian Kleinbölting <seakayone@users.noreply.github.com>
2025-01-22 17:29:38 +0100

    refactor: Make Project.shortname typed (#3469)

--- webapi/src/main/scala/org/knora/webapi/slice/admin/api/model/ProjectsMessagesADM.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/api/model/ProjectsMessagesADM.scala
@@ -11,6 +11,7 @@ import zio.json.JsonCodec
 import org.knora.webapi.IRI
 import org.knora.webapi.messages.admin.responder.AdminKnoraResponseADM
 import org.knora.webapi.messages.store.triplestoremessages.StringLiteralV2
+import org.knora.webapi.slice.admin.api.Codecs.ZioJsonCodec.shortname
 import org.knora.webapi.slice.admin.domain.model.KnoraProject.*
 import org.knora.webapi.slice.admin.domain.model.RestrictedView
 import org.knora.webapi.slice.admin.domain.model.User
@@ -31,7 +32,7 @@ import org.knora.webapi.slice.admin.domain.model.User
  */
 case class Project(
   id: IRI,
-  shortname: String,
+  shortname: Shortname,
   shortcode: String,
   longname: Option[String],
   description: Seq[StringLiteralV2],
@@ -44,7 +45,6 @@ case class Project(
 
   def projectIri: ProjectIri = ProjectIri.unsafeFrom(id)
 
-  def getShortname: Shortname = Shortname.unsafeFrom(shortname)
   def getShortcode: Shortcode = Shortcode.unsafeFrom(shortcode)
 
   /**
