81886367764905e38293ec5d4ca51dc24f095946
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-02-19 13:07:56 +0100

    add FileValueV2 Validation

--- webapi/src/main/scala/org/knora/webapi/slice/admin/domain/service/LegalInfoService.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/domain/service/LegalInfoService.scala
@@ -5,12 +5,20 @@
 
 package org.knora.webapi.slice.admin.domain.service
 import zio.*
+import zio.prelude.Validation
 
+import org.knora.webapi.messages.v2.responder.valuemessages.FileValueV2
+import org.knora.webapi.slice.admin.domain.model.CopyrightHolder
+import org.knora.webapi.slice.admin.domain.model.KnoraProject
 import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortcode
 import org.knora.webapi.slice.admin.domain.model.License
+import org.knora.webapi.slice.admin.domain.model.LicenseIri
 import org.knora.webapi.slice.admin.repo.LicenseRepo
 
-case class LegalInfoService(private val licenses: LicenseRepo) {
+case class LegalInfoService(
+  private val licenses: LicenseRepo,
+  private val projects: KnoraProjectService,
+) {
 
   /**
    * Currently, the project is not used in the implementation and all allowed licenses are returned.
@@ -20,6 +28,47 @@ case class LegalInfoService(private val licenses: LicenseRepo) {
    */
   def findLicenses(id: Shortcode): UIO[Chunk[License]] =
     licenses.findAll().orDie
+
+  def validateLegalInfo(fileValue: FileValueV2, id: Shortcode): IO[String, FileValueV2] =
+    for {
+      licenseValid         <- licenseValidation(fileValue.licenseIri, id)
+      copyrightHolderValid <- copyrightHolderValidation(fileValue.copyrightHolder, id)
+      _ <- Validation
+             .validate(licenseValid, copyrightHolderValid)
+             .toZIOParallelErrors
+             .mapError(_.mkString(", "))
+    } yield fileValue
+
+  private def licenseValidation(
+    licenseIri: Option[LicenseIri],
+    shortcode: Shortcode,
+  ): UIO[Validation[String, Unit]] =
+    licenseIri match
+      case None => ZIO.succeed(Validation.unit)
+      case Some(iri) =>
+        findLicenses(shortcode).map { licenses =>
+          if (licenses.map(_.id).contains(iri)) { Validation.unit }
+          else { Validation.fail(s"License $iri is not allowed in project $shortcode") }
+        }
+
+  private def copyrightHolderValidation(
+    copyrightHolder: Option[CopyrightHolder],
+    shortcode: Shortcode,
+  ): UIO[Validation[String, Unit]] =
+    copyrightHolder match
+      case None => ZIO.succeed(Validation.unit)
+      case Some(holder) =>
+        projects
+          .findByShortcode(shortcode)
+          .orDie
+          .map {
+            case None => Validation.fail(s"Project $shortcode not found")
+            case Some(project) =>
+              val holders = project.allowedCopyrightHolders
+              if (holders.contains(holder)) { Validation.unit }
+              else { Validation.fail(s"Copyright holder $holder is not allowed in project $shortcode") }
+          }
+
 }
 
 object LegalInfoService {
