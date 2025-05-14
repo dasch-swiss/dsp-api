597a6e83b3f1d86f51525a276837eb825dbcc982
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-05-14 11:03:28 +0200

    rm unused code

--- webapi/src/main/scala/org/knora/webapi/RdfMediaTypes.scala
+++ webapi/src/main/scala/org/knora/webapi/RdfMediaTypes.scala
@@ -7,7 +7,6 @@ package org.knora.webapi
 
 import org.apache.pekko
 
-import pekko.http.scaladsl.model.ContentType
 import pekko.http.scaladsl.model.HttpCharsets
 import pekko.http.scaladsl.model.MediaType
 import pekko.http.scaladsl.model.MediaTypes
@@ -38,57 +37,4 @@ object RdfMediaTypes {
     subType = "rdf+xml",
     fileExtensions = List("rdf"),
   )
-
-  val `application/trig`: MediaType.WithFixedCharset = MediaType.customWithFixedCharset(
-    mainType = "application",
-    subType = "trig",
-    charset = HttpCharsets.`UTF-8`,
-    fileExtensions = List("trig"),
-  )
-
-  val `application/n-quads`: MediaType.WithFixedCharset = MediaType.customWithFixedCharset(
-    mainType = "application",
-    subType = "n-quads",
-    charset = HttpCharsets.`UTF-8`,
-    fileExtensions = List("nq"),
-  )
-
-  /**
-   * A map of MIME types (strings) to supported RDF media types.
-   */
-  val registry: Map[String, MediaType.NonBinary] = Set(
-    `application/json`,
-    `application/ld+json`,
-    `text/turtle`,
-    `application/trig`,
-    `application/rdf+xml`,
-    `application/n-quads`,
-  ).map { mediaType =>
-    mediaType.toString -> mediaType
-  }.toMap
-
-  /**
-   * Ensures that a media specifies the UTF-8 charset if necessary.
-   *
-   * @param mediaType a non-binary media type.
-   * @return the same media type, specifying the UTF-8 charset if necessary.
-   */
-  def toUTF8ContentType(mediaType: MediaType.NonBinary): ContentType.NonBinary =
-    mediaType match {
-      case withFixedCharset: MediaType.WithFixedCharset => withFixedCharset.toContentType
-      case withOpenCharset: MediaType.WithOpenCharset   => withOpenCharset.toContentType(HttpCharsets.`UTF-8`)
-    }
-
-  /**
-   * Converts less specific media types to more specific ones if necessary (e.g. specifying
-   * JSON-LD instead of JSON).
-   *
-   * @param mediaType a non-binary media type.
-   * @return the most specific similar media type that the Knora API server supports.
-   */
-  def toMostSpecificMediaType(mediaType: MediaType.NonBinary): MediaType.NonBinary =
-    mediaType match {
-      case `application/json` => `application/ld+json`
-      case other              => other
-    }
 }
