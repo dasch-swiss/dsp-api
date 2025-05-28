e930f0ac652f7ebb75fc534ad8a919d9e41b20ab
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-05-28 10:46:21 +0200

    Improve setup

--- integration/src/test/scala/org/knora/webapi/core/LayersTestLive.scala
+++ integration/src/test/scala/org/knora/webapi/core/LayersTestLive.scala
@@ -34,7 +34,7 @@ import org.knora.webapi.slice.common.api.*
 import org.knora.webapi.slice.common.repo.service.PredicateObjectMapper
 import org.knora.webapi.slice.infrastructure.CacheManager
 import org.knora.webapi.slice.infrastructure.InfrastructureModule
-import org.knora.webapi.slice.infrastructure.OpenTelemetryTracerLive
+import org.knora.webapi.slice.infrastructure.OpenTelemetry
 import org.knora.webapi.slice.infrastructure.api.ManagementEndpoints
 import org.knora.webapi.slice.infrastructure.api.ManagementRoutes
 import org.knora.webapi.slice.lists.api.ListsApiModule
@@ -114,7 +114,7 @@ object LayersTestLive { self =>
       MessageRelayLive.layer,
       OntologyApiModule.layer,
       OntologyResponderV2.layer,
-      OpenTelemetryTracerLive.layer,
+      OpenTelemetry.test,
       PermissionUtilADMLive.layer,
       PermissionsResponder.layer,
       PredicateObjectMapper.layer,
