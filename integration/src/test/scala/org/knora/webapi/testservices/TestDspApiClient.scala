21492c448757acc4d9c278884189212075c8a69b
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-06-10 12:39:36 +0200

    add TestDspApiClient

--- integration/src/test/scala/org/knora/webapi/testservices/TestDspApiClient.scala
+++ integration/src/test/scala/org/knora/webapi/testservices/TestDspApiClient.scala
@@ -1,5 +1,6 @@
 package org.knora.webapi.testservices
 
+import dsp.errors.AssertionException
 import sttp.capabilities.zio.ZioStreams
 import sttp.client4.*
 import sttp.client4.ziojson.*
@@ -7,6 +8,8 @@ import sttp.model.*
 import zio.*
 import zio.json.JsonDecoder
 import org.knora.webapi.config.KnoraApi
+import org.knora.webapi.messages.util.rdf.JsonLDDocument
+import org.knora.webapi.messages.util.rdf.JsonLDUtil
 import org.knora.webapi.slice.admin.domain.model.User
 import org.knora.webapi.slice.infrastructure.JwtService
 import org.knora.webapi.slice.security.ScopeResolver
@@ -40,6 +43,17 @@ final case class TestDspApiClient(
   def getAsString(wholePath: String): Task[Response[Either[String, String]]] =
     getStringRequest(wholePath).send(backend)
 
+  def getAsJsonLd(wholePath: String, user: User): ZIO[Any, Throwable, Response[Either[String, JsonLDDocument]]] =
+    getAsString(wholePath, user).flatMap(parseJsonLd)
+
+  private def parseJsonLd(response: Response[Either[String, String]]) =
+    response.body match
+      case Left(error) => ZIO.succeed(response.copy(body = Left(error)))
+      case Right(jsonString) =>
+        ZIO.attempt(JsonLDUtil.parseJsonLD(jsonString)).map(jsonLd => response.copy(body = Right(jsonLd)))
+
+  def getAsJsonLd(wholePath: String): ZIO[Any, Throwable, Response[Either[String, JsonLDDocument]]] =
+    getAsString(wholePath).flatMap(parseJsonLd)
 }
 
 object TestDspApiClient {
@@ -61,5 +75,24 @@ object TestDspApiClient {
   def getAsString(wholePath: String): ZIO[TestDspApiClient, Throwable, Response[Either[String, String]]] =
     ZIO.serviceWithZIO[TestDspApiClient](_.getAsString(wholePath))
 
+  def getAsJsonLd(
+    wholePath: String,
+    user: User,
+  ): ZIO[TestDspApiClient, Throwable, Response[Either[String, JsonLDDocument]]] =
+    ZIO.serviceWithZIO[TestDspApiClient](_.getAsJsonLd(wholePath, user))
+
+  def getAsJsonLd(wholePath: String): ZIO[TestDspApiClient, Throwable, Response[Either[String, JsonLDDocument]]] =
+    ZIO.serviceWithZIO[TestDspApiClient](_.getAsJsonLd(wholePath))
+
   val layer = ZLayer.derive[TestDspApiClient]
+}
+
+object ResponseOps {
+  extension [A](response: Response[Either[String, A]]) {
+    def `200`: IO[AssertionException, A] =
+      (if response.code == StatusCode.Ok
+       then ZIO.fromEither(response.body)
+       else ZIO.fail(s"Expected 200 OK, got ${response.code} with body: ${response.body}"))
+        .mapError(AssertionException(_))
+  }
 }
