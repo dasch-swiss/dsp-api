26671d41fd00d141f3fd5d53c63431797073edb4
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-06-10 14:41:28 +0200

    revert /Users/christian/git/dasch/dsp-api/integration/src/test/scala/org/knora/webapi/E2EZSpec.scala

--- integration/src/test/scala/org/knora/webapi/E2EZSpec.scala
+++ integration/src/test/scala/org/knora/webapi/E2EZSpec.scala
@@ -39,7 +39,18 @@ abstract class E2EZSpec extends ZIOSpecDefault with TestStartupUtils {
   ).provideShared(testLayers, Client.default, Scope.default)
     @@ TestAspect.withLiveEnvironment
 
-  private def sendGetRequest(url: String, token: Option[String] = None): URIO[env, Response] =
+  def sendGetRequestAsRoot(url: String): URIO[env, Response] =
+    getRootToken.mapError(Exception(_)).orDie.flatMap(token => sendGetRequest(url, Some(token)))
+
+  def sendGetRequestAsRootDecode[A: JsonDecoder](url: String): ZIO[env, String, A] =
+    sendGetRequestAsRoot(url)
+      .flatMap(response => response.body.asString.mapBoth(_.getMessage, (response.status, _)))
+      .filterOrElseWith { case (status, _) => status.isSuccess } { case (status, body) =>
+        ZIO.fail(s"Failed request: Status $status, $body")
+      }
+      .flatMap { case (_, body) => ZIO.fromEither(body.fromJson[A]) }
+
+  def sendGetRequest(url: String, token: Option[String] = None): URIO[env, Response] =
     for {
       client   <- ZIO.service[Client]
       urlStr    = s"http://localhost:3333$url"
