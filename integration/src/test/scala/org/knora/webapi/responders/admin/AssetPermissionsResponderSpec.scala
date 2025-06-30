fba32450559160ee2165a19af00647c0c1b6a53f
Christian Kleinbölting <seakayone@users.noreply.github.com>
2025-06-30 20:08:53 +0200

    test: Migrate AssetPermissionsResponderSpec from scalaTest to zio-test (#3666)
    <!-- Important! Please follow the guidelines for naming Pull Requests:
    https://docs.dasch.swiss/latest/developers/contribution/ -->
    
    ### Description
    
    <!-- Please add a short description of the changes -->
    
    <!-- * **What kind of change does this PR introduce?** (Bug fix,
    feature, docs update, ...) -->
    
    <!-- * **What is the current behavior?** (You can also link to an open
    issue here) -->
    
    <!-- * **What is the new behavior (if this is a feature change)?** -->
    
    <!-- * **Does this PR introduce a breaking change?**
    (What changes might users need to make in their application due to this
    PR?) -->
    
    <!-- * **Other information**: -->

--- integration/src/test/scala/org/knora/webapi/responders/admin/AssetPermissionsResponderSpec.scala
+++ integration/src/test/scala/org/knora/webapi/responders/admin/AssetPermissionsResponderSpec.scala
@@ -5,63 +5,42 @@
 
 package org.knora.webapi.responders.admin
 
-import org.apache.pekko.testkit.*
 import zio.ZIO
+import zio.test.*
 
 import org.knora.webapi.*
 import org.knora.webapi.messages.store.triplestoremessages.RdfDataObject
-import org.knora.webapi.routing.UnsafeZioRun
-import org.knora.webapi.sharedtestdata.SharedTestDataADM
+import org.knora.webapi.sharedtestdata.SharedTestDataADM.*
 import org.knora.webapi.slice.admin.api.model.PermissionCodeAndProjectRestrictedViewSettings
 import org.knora.webapi.slice.admin.api.model.ProjectRestrictedViewSettingsADM
-import org.knora.webapi.slice.admin.domain.model.KnoraProject.Shortcode
-import org.knora.webapi.slice.admin.domain.model.User
 
-/**
- * Tests [[AssetPermissionsResponder]].
- */
-class AssetPermissionsResponderSpec extends E2ESpec with ImplicitSender {
+object AssetPermissionsResponderSpec extends E2EZSpec {
 
-  private def getFileInfoForSipiADM(shortcode: Shortcode, filename: String, user: User) =
-    ZIO.serviceWithZIO[AssetPermissionsResponder](
-      _.getPermissionCodeAndProjectRestrictedViewSettings(shortcode, filename, user),
-    )
+  private val assetPermissionResponder = ZIO.serviceWithZIO[AssetPermissionsResponder]
+  private val asset: String            = "incunabula_0000003328.jp2"
 
-  override lazy val rdfDataObjects = List(
-    RdfDataObject(
-      path = "test_data/project_data/incunabula-data.ttl",
-      name = "http://www.knora.org/data/0803/incunabula",
-    ),
+  override lazy val rdfDataObjects: List[RdfDataObject] = List(incunabulaRdfData)
+
+  override val e2eSpec = suite("The AssetPermissionsResponder")(
+    test("return details of a full quality file value") {
+      assetPermissionResponder(
+        _.getPermissionCodeAndProjectRestrictedViewSettings(incunabulaProject.shortcode, asset, incunabulaMemberUser),
+      ).map { actual =>
+        assertTrue(actual == PermissionCodeAndProjectRestrictedViewSettings(permissionCode = 6, None))
+      }
+    },
+    test("return details of a restricted view file value") {
+      // http://localhost:3333/v1/files/http%3A%2F%2Frdfh.ch%2F8a0b1e75%2Freps%2F7e4ba672
+      assetPermissionResponder(
+        _.getPermissionCodeAndProjectRestrictedViewSettings(incunabulaProject.shortcode, asset, anonymousUser),
+      ).map(actual =>
+        assertTrue(
+          actual == PermissionCodeAndProjectRestrictedViewSettings(
+            permissionCode = 1,
+            Some(ProjectRestrictedViewSettingsADM(size = Some("!512,512"), watermark = false)),
+          ),
+        ),
+      )
+    },
   )
-
-  "The Sipi responder" should {
-    "return details of a full quality file value" in {
-      // http://localhost:3333/v1/files/http%3A%2F%2Frdfh.ch%2F8a0b1e75%2Freps%2F7e4ba672
-      val actual = UnsafeZioRun.runOrThrow(
-        getFileInfoForSipiADM(
-          Shortcode.unsafeFrom("0803"),
-          "incunabula_0000003328.jp2",
-          SharedTestDataADM.incunabulaMemberUser,
-        ),
-      )
-
-      actual shouldEqual PermissionCodeAndProjectRestrictedViewSettings(permissionCode = 6, None)
-    }
-
-    "return details of a restricted view file value" in {
-      // http://localhost:3333/v1/files/http%3A%2F%2Frdfh.ch%2F8a0b1e75%2Freps%2F7e4ba672
-      val actual = UnsafeZioRun.runOrThrow(
-        getFileInfoForSipiADM(
-          Shortcode.unsafeFrom("0803"),
-          "incunabula_0000003328.jp2",
-          SharedTestDataADM.anonymousUser,
-        ),
-      )
-
-      actual shouldEqual PermissionCodeAndProjectRestrictedViewSettings(
-        permissionCode = 1,
-        Some(ProjectRestrictedViewSettingsADM(size = Some("!512,512"), watermark = false)),
-      )
-    }
-  }
 }
