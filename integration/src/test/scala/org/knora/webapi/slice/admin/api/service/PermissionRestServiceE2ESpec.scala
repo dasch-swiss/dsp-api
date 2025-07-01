d13d1b010b1286e017899600e9a17b868e4b6a43
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-07-01 10:42:03 +0200

    use E2EZSpec.failsWithMessageContaining in PermissionRestServiceE2ESpec

--- integration/src/test/scala/org/knora/webapi/slice/admin/api/service/PermissionRestServiceE2ESpec.scala
+++ integration/src/test/scala/org/knora/webapi/slice/admin/api/service/PermissionRestServiceE2ESpec.scala
@@ -8,7 +8,6 @@ package org.knora.webapi.slice.admin.api.service
 import zio.Exit
 import zio.ZIO
 import zio.test.*
-import zio.test.Assertion.*
 
 import scala.reflect.ClassTag
 
@@ -26,9 +25,6 @@ import org.knora.webapi.slice.admin.domain.service.KnoraGroupRepo
 object PermissionRestServiceE2ESpec extends E2EZSpec {
   private val permissionRestService = ZIO.serviceWithZIO[PermissionRestService]
 
-  private def failsWithA[A <: Throwable](msg: String)(implicit tag: ClassTag[A]): Assertion[Exit[Any, Any]] =
-    fails(isSubtype[A](hasMessage(containsString(msg))))
-
   val e2eSpec = suite("PermissionResponder")(
     suite("Administrative Permission Create Requests")(
       test("return 'BadRequest' if the supplied project IRI for AdministrativePermissionCreateRequestADM is not valid") {
@@ -41,7 +37,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException]("Project IRI is invalid.")))
+        ).exit.map(assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException]("Project IRI is invalid.")))
       },
       test("return 'BadRequest' if the supplied group IRI for AdministrativePermissionCreateRequestADM is not valid") {
         val groupIri = "invalid-group-iri"
@@ -54,7 +50,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException](s"Invalid group IRI $groupIri")))
+        ).exit.map(assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException](s"Invalid group IRI $groupIri")))
       },
       test(
         "return 'BadRequest' if the supplied permission IRI for AdministrativePermissionCreateRequestADM is not valid",
@@ -70,7 +66,11 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException](s"Invalid permission IRI: $permissionIri.")))
+        ).exit.map(
+          assert(_)(
+            E2EZSpec.failsWithMessageContaining[BadRequestException](s"Invalid permission IRI: $permissionIri."),
+          ),
+        )
       },
       test("return 'BadRequest' if the no permissions supplied for AdministrativePermissionCreateRequestADM") {
         val invalidName    = "Delete"
@@ -86,7 +86,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[BadRequestException](
+            E2EZSpec.failsWithMessageContaining[BadRequestException](
               s"Invalid value for name parameter of hasPermissions: $invalidName, it should be one of " +
                 s"${Permission.Administrative.allTokens.mkString(", ")}",
             ),
@@ -105,7 +105,8 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException]("Permissions needs to be supplied.")))
+        ).exit
+          .map(assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException]("Permissions needs to be supplied.")))
       },
       test(
         "return 'ForbiddenException' if the user requesting AdministrativePermissionCreateRequestADM is not system or project admin",
@@ -121,7 +122,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[ForbiddenException](
+            E2EZSpec.failsWithMessageContaining[ForbiddenException](
               "You are logged in with username 'images-reviewer-user', " +
                 "but only a system administrator or project administrator has permissions for this operation.",
             ),
@@ -144,7 +145,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException]("Project IRI is invalid.")))
+        ).exit.map(assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException]("Project IRI is invalid.")))
       },
       test(
         "return 'BadRequest' if the supplied group IRI for DefaultObjectAccessPermissionCreateRequestADM is not valid",
@@ -161,7 +162,8 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException](s"Group IRI is invalid: $groupIri")))
+        ).exit
+          .map(assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException](s"Group IRI is invalid: $groupIri")))
       },
       test(
         "return 'BadRequest' if the supplied custom permission IRI for DefaultObjectAccessPermissionCreateRequestADM is not valid",
@@ -179,7 +181,9 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException](s"Couldn't parse IRI: $permissionIri")))
+        ).exit.map(
+          assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException](s"Couldn't parse IRI: $permissionIri")),
+        )
       },
       test("return 'BadRequest' if the no permissions supplied for DefaultObjectAccessPermissionCreateRequestADM") {
         permissionRestService(
@@ -191,7 +195,8 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
             ),
             SharedTestDataADM.imagesUser01,
           ),
-        ).exit.map(assert(_)(failsWithA[BadRequestException]("Permissions needs to be supplied.")))
+        ).exit
+          .map(assert(_)(E2EZSpec.failsWithMessageContaining[BadRequestException]("Permissions needs to be supplied.")))
       },
       test(
         "not create a DefaultObjectAccessPermission for project and property if hasPermissions set contained permission with invalid name",
@@ -208,7 +213,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           .exit
           .map(
             assert(_)(
-              failsWithA[BadRequestException](
+              E2EZSpec.failsWithMessageContaining[BadRequestException](
                 "Invalid value for name parameter of hasPermissions: invalid, it should be one of " +
                   s"${Permission.ObjectAccess.allTokens.mkString(", ")}",
               ),
@@ -231,7 +236,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           .exit
           .map(
             assert(_)(
-              failsWithA[BadRequestException](
+              E2EZSpec.failsWithMessageContaining[BadRequestException](
                 s"Invalid value for permissionCode parameter of hasPermissions: $invalidCode, it should be one of " +
                   s"${Permission.ObjectAccess.allCodes.mkString(", ")}",
               ),
@@ -253,7 +258,9 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           .exit
           .map(
             assert(_)(
-              failsWithA[BadRequestException](s"Given permission code 2 and permission name CR are not consistent."),
+              E2EZSpec.failsWithMessageContaining[BadRequestException](
+                s"Given permission code 2 and permission name CR are not consistent.",
+              ),
             ),
           )
       },
@@ -272,7 +279,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           .exit
           .map(
             assert(_)(
-              failsWithA[BadRequestException](
+              E2EZSpec.failsWithMessageContaining[BadRequestException](
                 s"One of permission code or permission name must be provided for a default object access permission.",
               ),
             ),
@@ -293,7 +300,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           .exit
           .map(
             assert(_)(
-              failsWithA[BadRequestException](
+              E2EZSpec.failsWithMessageContaining[BadRequestException](
                 s"additionalInformation of a default object access permission type cannot be empty.",
               ),
             ),
@@ -314,7 +321,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[ForbiddenException](
+            E2EZSpec.failsWithMessageContaining[ForbiddenException](
               "You are logged in with username 'anything.user02', but only a system administrator or project administrator has permissions for this operation.",
             ),
           ),
@@ -337,7 +344,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[BadRequestException](
+            E2EZSpec.failsWithMessageContaining[BadRequestException](
               "DOAP restrictions must be either for a group, a resource class, a property, " +
                 "or a combination of a resource class and a property. ",
             ),
@@ -361,7 +368,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[BadRequestException](
+            E2EZSpec.failsWithMessageContaining[BadRequestException](
               "DOAP restrictions must be either for a group, a resource class, a property, " +
                 "or a combination of a resource class and a property. ",
             ),
@@ -384,7 +391,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[BadRequestException](
+            E2EZSpec.failsWithMessageContaining[BadRequestException](
               "<http://rdfh.ch/0001/5zCt1EMJKezFUOW_RCB0Gw/values/tdWAtnWK2qUC6tr4uQLAHA> is not a Knora property IRI",
             ),
           ),
@@ -405,7 +412,7 @@ object PermissionRestServiceE2ESpec extends E2EZSpec {
           ),
         ).exit.map(
           assert(_)(
-            failsWithA[BadRequestException](
+            E2EZSpec.failsWithMessageContaining[BadRequestException](
               "DOAP restrictions must be either for a group, a resource class, a property, " +
                 "or a combination of a resource class and a property. ",
             ),
