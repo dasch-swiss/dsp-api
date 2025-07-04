fe8500acdb84b79aa30f78f4a834bb131ae5df7c
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-07-04 11:16:27 +0200

    test: Migrate SparqlTransformerSpec to zio-test
    * Split test into :
     ** default zio test SparqlTransformerSpec
     ** E2EZSPec OntologyInferenecerE2ESpec

---
+++ integration/src/test/scala/org/knora/webapi/messages/util/search/gravsearch/transformers/OntologyInferencerE2ESpec.scala
@@ -0,0 +1,126 @@
+/*
+ * Copyright © 2021 - 2025 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.messages.util.search.gravsearch.transformers
+
+import zio.*
+import zio.test.*
+
+import org.knora.webapi.E2EZSpec
+import org.knora.webapi.messages.IriConversions.ConvertibleIri
+import org.knora.webapi.messages.OntologyConstants
+import org.knora.webapi.messages.StringFormatter
+import org.knora.webapi.messages.util.search.IriRef
+import org.knora.webapi.messages.util.search.QueryVariable
+import org.knora.webapi.messages.util.search.StatementPattern
+import org.knora.webapi.messages.util.search.ValuesPattern
+
+object OntologyInferencerE2ESpec extends E2EZSpec {
+
+  private implicit val sf: StringFormatter = StringFormatter.getInitializedTestInstance
+  private val ontologyInferencer           = ZIO.serviceWithZIO[OntologyInferencer]
+
+  private val thingIRI         = "http://www.knora.org/ontology/0001/anything#Thing".toSmartIri
+  private val blueThingIRI     = "http://www.knora.org/ontology/0001/anything#BlueThing".toSmartIri
+  private val hasOtherThingIRI = "http://www.knora.org/ontology/0001/anything#hasOtherThing".toSmartIri
+  private val hasTextIRI       = "http://www.knora.org/ontology/0001/anything#hasText".toSmartIri
+
+  override val e2eSpec = suite("OntologyInferencer")(
+    suite("transformStatementInWhere")(
+      test("not simulate any RDF inference for a class, if there are no known subclasses") {
+        val typeStatement = StatementPattern(
+          subj = QueryVariable("foo"),
+          pred = IriRef(OntologyConstants.Rdf.Type.toSmartIri),
+          obj = IriRef(blueThingIRI),
+        )
+        val expectedStatements = Seq(
+          StatementPattern(
+            subj = QueryVariable("foo"),
+            pred = IriRef(OntologyConstants.Rdf.Type.toSmartIri),
+            obj = IriRef(blueThingIRI),
+          ),
+        )
+        ontologyInferencer(_.transformStatementInWhere(statementPattern = typeStatement, simulateInference = true))
+          .map(expandedStatements => assertTrue(expandedStatements == expectedStatements))
+      },
+      test("not simulate any RDF inference for a property, if there are no known subproperties") {
+        val hasValueStatement =
+          StatementPattern(
+            subj = QueryVariable("foo"),
+            pred = IriRef(hasTextIRI),
+            obj = QueryVariable("text"),
+          )
+        val expectedStatements: Seq[StatementPattern] = Seq(
+          StatementPattern(
+            subj = QueryVariable(variableName = "foo"),
+            pred = IriRef(
+              iri = hasTextIRI,
+              propertyPathOperator = None,
+            ),
+            obj = QueryVariable(variableName = "text"),
+          ),
+        )
+        ontologyInferencer(_.transformStatementInWhere(statementPattern = hasValueStatement, simulateInference = true))
+          .map(expandedStatements => assertTrue(expandedStatements == expectedStatements))
+      },
+      test("create a values pattern to simulate RDF inference for a class, if there are known subclasses") {
+        val typeStatement = StatementPattern(
+          subj = QueryVariable("foo"),
+          pred = IriRef(OntologyConstants.Rdf.Type.toSmartIri),
+          obj = IriRef(thingIRI),
+        )
+        val expected = List(
+          ValuesPattern(
+            QueryVariable("resTypes5432"),
+            Set(
+              IriRef("http://www.knora.org/ontology/0001/something#Something".toSmartIri),
+              IriRef("http://www.knora.org/ontology/0001/anything#BlueThing".toSmartIri),
+              IriRef("http://www.knora.org/ontology/0001/anything#Thing".toSmartIri),
+              IriRef("http://www.knora.org/ontology/0001/anything#ThingWithSeqnum".toSmartIri),
+            ),
+          ),
+          StatementPattern(
+            QueryVariable("foo"),
+            IriRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type".toSmartIri),
+            QueryVariable("resTypes5432"),
+          ),
+        )
+        ontologyInferencer(
+          _.transformStatementInWhere(
+            statementPattern = typeStatement,
+            simulateInference = true,
+            queryVariableSuffix = Some("5432"),
+          ),
+        ).map(expandedStatements => assertTrue(expandedStatements == expected))
+      },
+      test("create a values pattern to simulate RDF inference for a property, if there are known subproperties") {
+        val hasValueStatement =
+          StatementPattern(
+            subj = QueryVariable("foo"),
+            pred = IriRef(hasOtherThingIRI),
+            obj = QueryVariable("text"),
+          )
+        val expected = List(
+          ValuesPattern(
+            QueryVariable("subProp5432"),
+            Set(
+              IriRef("http://www.knora.org/ontology/0001/something#hasOtherSomething".toSmartIri),
+              IriRef("http://www.knora.org/ontology/0001/anything#hasOtherThing".toSmartIri),
+              IriRef("http://www.knora.org/ontology/0001/anything#hasBlueThing".toSmartIri),
+            ),
+          ),
+          StatementPattern(QueryVariable("foo"), QueryVariable("subProp5432"), QueryVariable("text")),
+        )
+        ontologyInferencer(
+          _.transformStatementInWhere(
+            statementPattern = hasValueStatement,
+            simulateInference = true,
+            queryVariableSuffix = Some("5432"),
+          ),
+        ).map(expandedStatements => assertTrue(expandedStatements == expected))
+      },
+    ),
+  )
+}
---
+++ webapi/src/test/scala/org/knora/webapi/messages/util/search/gravsearch/transformers/SparqlTransformerSpec.scala
@@ -0,0 +1,151 @@
+/*
+ * Copyright © 2021 - 2025 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.messages.util.search.gravsearch.transformers
+
+import zio.*
+import zio.test.*
+
+import org.knora.webapi.messages.IriConversions.*
+import org.knora.webapi.messages.OntologyConstants
+import org.knora.webapi.messages.StringFormatter
+import org.knora.webapi.messages.util.search.*
+
+object SparqlTransformerSpec extends ZIOSpecDefault {
+
+  private implicit val sf: StringFormatter = StringFormatter.getInitializedTestInstance
+
+  private val thingIRI         = "http://www.knora.org/ontology/0001/anything#Thing".toSmartIri
+  private val hasOtherThingIRI = "http://www.knora.org/ontology/0001/anything#hasOtherThing".toSmartIri
+  private val hasTextIRI       = "http://www.knora.org/ontology/0001/anything#hasText".toSmartIri
+
+  override val spec = suite("SparqlTransformer")(
+    test("create a syntactically valid base name from a given variable") {
+      val baseName = SparqlTransformer.escapeEntityForVariable(QueryVariable("book"))
+      assertTrue(baseName == "book")
+    },
+    test("create a syntactically valid base name from a given data IRI") {
+      val baseName = SparqlTransformer.escapeEntityForVariable(IriRef("http://rdfh.ch/users/91e19f1e01".toSmartIri))
+      assertTrue(baseName == "httprdfhchusers91e19f1e01")
+    },
+    test("create a syntactically valid base name from a given ontology IRI") {
+      val baseName = SparqlTransformer.escapeEntityForVariable(
+        IriRef("http://www.knora.org/ontology/0803/incunabula#book".toSmartIri),
+      )
+      assertTrue(baseName == "httpwwwknoraorgontology0803incunabulabook")
+    },
+    test("create a syntactically valid base name from a given string literal") {
+      val baseName =
+        SparqlTransformer.escapeEntityForVariable(XsdLiteral("dumm", OntologyConstants.Xsd.String.toSmartIri))
+      assertTrue(baseName == "dumm")
+    },
+    test("create a unique variable name based on an entity and a property") {
+      val generatedQueryVar =
+        SparqlTransformer.createUniqueVariableNameFromEntityAndProperty(
+          QueryVariable("linkingProp1"),
+          OntologyConstants.KnoraBase.HasLinkToValue,
+        )
+      assertTrue(generatedQueryVar == QueryVariable("linkingProp1__hasLinkToValue"))
+    },
+    test("optimise knora-base:isDeleted") {
+      val typeStatement = StatementPattern(
+        subj = QueryVariable("foo"),
+        pred = IriRef(OntologyConstants.Rdf.Type.toSmartIri),
+        obj = IriRef(thingIRI),
+      )
+      val isDeletedStatement = StatementPattern(
+        subj = QueryVariable("foo"),
+        pred = IriRef(OntologyConstants.KnoraBase.IsDeleted.toSmartIri),
+        obj = XsdLiteral(value = "false", datatype = OntologyConstants.Xsd.Boolean.toSmartIri),
+      )
+      val linkStatement = StatementPattern(
+        subj = QueryVariable("foo"),
+        pred = IriRef(hasOtherThingIRI),
+        obj = IriRef("http://rdfh.ch/0001/a-thing".toSmartIri),
+      )
+      val patterns: Seq[StatementPattern] = Seq(
+        typeStatement,
+        isDeletedStatement,
+        linkStatement,
+      )
+      val optimisedPatterns = SparqlTransformer.optimiseIsDeletedWithFilter(patterns)
+      val expectedPatterns = Seq(
+        typeStatement,
+        linkStatement,
+        FilterNotExistsPattern(
+          Seq(
+            StatementPattern(
+              subj = QueryVariable("foo"),
+              pred = IriRef(OntologyConstants.KnoraBase.IsDeleted.toSmartIri),
+              obj = XsdLiteral(value = "true", datatype = OntologyConstants.Xsd.Boolean.toSmartIri),
+            ),
+          ),
+        ),
+      )
+      assertTrue(optimisedPatterns == expectedPatterns)
+    },
+    test("move a BIND pattern to the beginning of a block") {
+      val typeStatement = StatementPattern(
+        subj = QueryVariable("foo"),
+        pred = IriRef(OntologyConstants.Rdf.Type.toSmartIri),
+        obj = IriRef(thingIRI),
+      )
+      val hasValueStatement =
+        StatementPattern(
+          subj = QueryVariable("foo"),
+          pred = IriRef(hasTextIRI),
+          obj = QueryVariable("text"),
+        )
+      val bindPattern =
+        BindPattern(variable = QueryVariable("foo"), expression = IriRef("http://rdfh.ch/0001/a-thing".toSmartIri))
+      val patterns: Seq[QueryPattern] = Seq(
+        typeStatement,
+        hasValueStatement,
+        bindPattern,
+      )
+      val optimisedPatterns = SparqlTransformer.moveBindToBeginning(patterns)
+      val expectedPatterns: Seq[QueryPattern] = Seq(
+        bindPattern,
+        typeStatement,
+        hasValueStatement,
+      )
+      assertTrue(optimisedPatterns == expectedPatterns)
+    },
+    test("move a Lucene query pattern to the beginning of a block") {
+      val hasValueStatement =
+        StatementPattern(
+          subj = QueryVariable("foo"),
+          pred = IriRef(hasTextIRI),
+          obj = QueryVariable("text"),
+        )
+      val valueHasStringStatement =
+        StatementPattern(
+          subj = QueryVariable("text"),
+          pred = IriRef(OntologyConstants.KnoraBase.ValueHasString.toSmartIri),
+          QueryVariable("text__valueHasString"),
+        )
+      val luceneQueryPattern = StatementPattern(
+        subj = QueryVariable("text"),
+        pred = IriRef(OntologyConstants.Fuseki.luceneQueryPredicate.toSmartIri),
+        obj = XsdLiteral(
+          value = "Zeitglöcklein",
+          datatype = OntologyConstants.Xsd.String.toSmartIri,
+        ),
+      )
+      val patterns: Seq[QueryPattern] = Seq(
+        hasValueStatement,
+        valueHasStringStatement,
+        luceneQueryPattern,
+      )
+      val optimisedPatterns = SparqlTransformer.moveLuceneToBeginning(patterns)
+      val expectedPatterns: Seq[QueryPattern] = Seq(
+        luceneQueryPattern,
+        hasValueStatement,
+        valueHasStringStatement,
+      )
+      assertTrue(optimisedPatterns == expectedPatterns)
+    },
+  )
+}
