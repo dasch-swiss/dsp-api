798917b0e74bb8e96f196593f33eb6cf818995c9
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-08-20 16:28:02 +0200

    move test to SearchEndpointsPostGravsearchE2ESpec

--- modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsE2ESpec.scala
+++ modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsE2ESpec.scala
@@ -195,49 +195,6 @@ class SearchEndpointsE2ESpec extends E2ESpec {
       checkCountResponse(actual, 18)
     }
 
-    "perform a Gravsearch query for books that have not been published on the first of March 1497 (Julian Calendar)" in {
-      val gravsearchQuery =
-        """PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/simple/v2#>
-          |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
-          |
-          |    CONSTRUCT {
-          |        ?book knora-api:isMainResource true .
-          |
-          |        ?book incunabula:title ?title .
-          |
-          |        ?book incunabula:pubdate ?pubdate .
-          |    } WHERE {
-          |
-          |        ?book a incunabula:book .
-          |        ?book a knora-api:Resource .
-          |
-          |        ?book incunabula:title ?title .
-          |        incunabula:title knora-api:objectType xsd:string .
-          |
-          |        ?title a xsd:string .
-          |
-          |        ?book incunabula:pubdate ?pubdate .
-          |        incunabula:pubdate knora-api:objectType knora-api:Date .
-          |
-          |        ?pubdate a knora-api:Date .
-          |
-          |        FILTER(?pubdate != "JULIAN:1497-03-01"^^knora-api:Date)
-          |
-          |    } ORDER BY ?pubdate
-                """.stripMargin
-
-      val actual = getResponseAsString(
-        Post(
-          s"$baseApiUrl/v2/searchextended",
-          HttpEntity(RdfMediaTypes.`application/sparql-query`, gravsearchQuery),
-        ),
-      )
-      val expected = testData("BooksNotPublishedOnDate.jsonld")
-      compareJSONLDForResourcesResponse(expected, actual)
-      // this is the negation of the query condition above, hence the size of the result set must be 19 (total of incunabula:book) minus 2 (number of results from query above)
-      checkSearchResponseNumberOfResults(actual, 17)
-    }
-
     "perform a Gravsearch query for books that have not been published on the first of March 1497 (Julian Calendar) 2" in {
       val gravsearchQuery =
         """PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/simple/v2#>
