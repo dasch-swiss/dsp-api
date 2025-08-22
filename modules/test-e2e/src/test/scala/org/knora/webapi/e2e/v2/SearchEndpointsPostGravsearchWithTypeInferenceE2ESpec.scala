41d6d6bf47ea18e34ffed7565f276d99148dab56
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-08-22 14:39:48 +0200

    move test to SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec

---
+++ modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2021 - 2025 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.e2e.v2
+
+import zio.*
+import zio.test.*
+import org.knora.webapi.E2EZSpec
+import org.knora.webapi.messages.store.triplestoremessages.RdfDataObject
+import org.knora.webapi.e2e.v2.SearchEndpointE2ESpecHelper.*
+
+object SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec extends E2EZSpec {
+
+  override lazy val rdfDataObjects: List[RdfDataObject] = SearchEndpointE2ESpecHelper.rdfDataObjects
+
+  override def e2eSpec = suite("SearchEndpoints POST /v2/searchextended (with type inference)")(
+    test(
+      "do a Gravsearch query in which 'rdf:type knora-api:Resource' is inferred from a more specific rdf:type (with type inference)",
+    ) {
+      val query =
+        """
+          |PREFIX beol: <http://0.0.0.0:3333/ontology/0801/beol/simple/v2#>
+          |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+          |PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+          |
+          |CONSTRUCT {
+          |    ?letter knora-api:isMainResource true .
+          |    ?letter beol:creationDate ?date .
+          |    ?letter ?linkingProp1 <http://rdfh.ch/0801/H7s3FmuWTkaCXa54eFANOA> .
+          |    <http://rdfh.ch/0801/H7s3FmuWTkaCXa54eFANOA> beol:hasFamilyName ?name .
+          |} WHERE {
+          |    ?letter a beol:letter .
+          |    ?letter beol:creationDate ?date .
+          |    beol:creationDate knora-api:objectType knora-api:Date .
+          |    ?date a knora-api:Date .
+          |    ?letter ?linkingProp1 <http://rdfh.ch/0801/H7s3FmuWTkaCXa54eFANOA> .
+          |    <http://rdfh.ch/0801/H7s3FmuWTkaCXa54eFANOA> a beol:person .
+          |    ?linkingProp1 knora-api:objectType knora-api:Resource .
+          |    <http://rdfh.ch/0801/H7s3FmuWTkaCXa54eFANOA> beol:hasFamilyName ?name .
+          |    beol:hasFamilyName knora-api:objectType xsd:string .
+          |    ?name a xsd:string .
+          |
+          |    FILTER(?linkingProp1 = beol:hasAuthor || ?linkingProp1 = beol:hasRecipient)
+          |} ORDER BY ?date
+          |""".stripMargin
+      verifyQueryResult(query, "letterWithPersonWithName2.jsonld")
+    },
+  )
+}
