56ebfb224543a7485fce2ede88716027143a03ea
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-08-22 15:38:27 +0200

    move test to SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec

--- modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec.scala
+++ modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec.scala
@@ -491,5 +491,32 @@ object SearchEndpointsPostGravsearchWithTypeInferenceE2ESpec extends E2EZSpec {
           |}""".stripMargin
       verifyQueryResult(query, "PageWithSeqnum10OnlySeqnuminAnswer.jsonld")
     },
+    test(
+      "perform a Gravsearch query for the pages of a book whose seqnum is lower than or equals 10 (with type inference)",
+    ) {
+      val query =
+        """PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/simple/v2#>
+          |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+          |
+          |CONSTRUCT {
+          |    ?page knora-api:isMainResource true .
+          |
+          |    ?page knora-api:isPartOf <http://rdfh.ch/0803/b6b5ff1eb703> .
+          |
+          |    ?page incunabula:seqnum ?seqnum .
+          |} WHERE {
+          |
+          |    ?page a incunabula:page .
+          |
+          |    ?page knora-api:isPartOf <http://rdfh.ch/0803/b6b5ff1eb703> .
+          |
+          |    ?page incunabula:seqnum ?seqnum .
+          |
+          |    FILTER(?seqnum <= 10)
+          |
+          |} ORDER BY ?seqnum
+          |""".stripMargin
+      verifyQueryResult(query, "PagesOfLatinNarrenschiffWithSeqnumLowerEquals10.jsonld")
+    },
   )
 }
