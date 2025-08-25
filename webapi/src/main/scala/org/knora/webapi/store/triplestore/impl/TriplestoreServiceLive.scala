b058e24638a16b3f9099be4c1a0460d7aff00645
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-08-25 14:34:31 +0200

    Clarify error message

--- webapi/src/main/scala/org/knora/webapi/store/triplestore/impl/TriplestoreServiceLive.scala
+++ webapi/src/main/scala/org/knora/webapi/store/triplestore/impl/TriplestoreServiceLive.scala
@@ -259,9 +259,7 @@ case class TriplestoreServiceLive(
     ZIO
       .ifZIO(checkTriplestoreInitialized())(
         ZIO.succeed(Available),
-        ZIO.succeed(
-          NotInitialized(s"None of the active datasets meet our requirement of name: ${fusekiConfig.repositoryName}"),
-        ),
+        ZIO.succeed(NotInitialized(s"Dataset `${fusekiConfig.repositoryName} not found in triplestore.")),
       )
       .catchAll(ex => ZIO.succeed(Unavailable(s"Triplestore not available: ${ex.getMessage}")))
 
