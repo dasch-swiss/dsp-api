215d03973b47748d4b9c491c41e52e61c19fae00
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-08-26 09:05:04 +0200

    Move test to SearchEndpointsPostGravsearchWithTypeInferenceComplexSchemaE2ESpec

--- modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchWithTypeInferenceComplexSchemaE2ESpec.scala
+++ modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchWithTypeInferenceComplexSchemaE2ESpec.scala
@@ -327,5 +327,30 @@ object SearchEndpointsPostGravsearchWithTypeInferenceComplexSchemaE2ESpec extend
           |""".stripMargin
       verifyQueryResult(query, "PagesOfLatinNarrenschiffWithSeqnumLowerEquals10.jsonld")
     },
+    test(
+      "perform a Gravsearch query for the pages of a book and return them ordered by their seqnum (submitting the complex schema)",
+    ) {
+      val query =
+        """PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/v2#>
+          |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/v2#>
+          |
+          |CONSTRUCT {
+          |    ?page knora-api:isMainResource true .
+          |
+          |    ?page knora-api:isPartOf <http://rdfh.ch/0803/b6b5ff1eb703> .
+          |
+          |    ?page incunabula:seqnum ?seqnum .
+          |} WHERE {
+          |
+          |    ?page a incunabula:page .
+          |
+          |    ?page knora-api:isPartOf <http://rdfh.ch/0803/b6b5ff1eb703> .
+          |
+          |    ?page incunabula:seqnum ?seqnum .
+          |
+          |} ORDER BY ?seqnum
+          |""".stripMargin
+      verifyQueryResult(query, "PagesOfNarrenschiffOrderedBySeqnum.jsonld")
+    },
   )
 }
