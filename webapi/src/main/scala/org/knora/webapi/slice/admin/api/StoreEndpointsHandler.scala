6783bc34fbad3d9545b4101bf5b826a5df719803
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-09-22 17:00:37 +0200

    align StoreEndpointsHandler

--- webapi/src/main/scala/org/knora/webapi/slice/admin/api/StoreEndpointsHandler.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/admin/api/StoreEndpointsHandler.scala
@@ -14,23 +14,17 @@ import org.knora.webapi.slice.common.api.HandlerMapper
 import org.knora.webapi.slice.common.api.PublicEndpointHandler
 
 final case class StoreEndpointsHandler(
-  endpoints: StoreEndpoints,
-  appConfig: AppConfig,
-  storesResponder: StoreRestService,
-  mapper: HandlerMapper,
+  private val appConfig: AppConfig,
+  private val endpoints: StoreEndpoints,
+  private val mapper: HandlerMapper,
+  private val restService: StoreRestService,
 ) {
-
-  private val postStoreResetTriplestoreContentHandler =
-    PublicEndpointHandler[(Option[List[RdfDataObject]], Boolean), MessageResponse](
-      endpoints.postStoreResetTriplestoreContent,
-      { case (rdfObjs: Option[List[RdfDataObject]], prependDefaults: Boolean) =>
-        storesResponder.resetTriplestoreContent(rdfObjs.getOrElse(List.empty), prependDefaults)
-      },
-    )
 
   val allHandlers = {
     val handlerIfConfigured =
-      if (appConfig.allowReloadOverHttp) Seq(postStoreResetTriplestoreContentHandler) else Seq.empty
+      if (appConfig.allowReloadOverHttp)
+        Seq(PublicEndpointHandler(endpoints.postStoreResetTriplestoreContent, restService.resetTriplestoreContent))
+      else Seq.empty
     handlerIfConfigured.map(mapper.mapPublicEndpointHandler)
   }
 }
