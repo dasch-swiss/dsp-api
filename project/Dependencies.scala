749f167e06fbdcaeb6c4510a91365fab17b7ed38
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-10-01 10:12:00 +0200

    downgrade pekko http as it is not yet released https://github.com/apache/pekko-http/tags

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val ScalaVersion = "3.3.6"
 
   val PekkoActorVersion = "1.2.1"
-  val PekkoHttpVersion  = "1.2.1"
+  val PekkoHttpVersion  = "1.2.0"
 
   val MonocleVersion = "3.3.0"
 
