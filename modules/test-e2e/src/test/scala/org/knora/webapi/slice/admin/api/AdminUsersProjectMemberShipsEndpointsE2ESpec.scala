69d9f5f7890b955b046657fd74932609c9441a0b
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-10-03 10:37:04 +0200

    add logging timing

--- modules/test-e2e/src/test/scala/org/knora/webapi/slice/admin/api/AdminUsersProjectMemberShipsEndpointsE2ESpec.scala
+++ modules/test-e2e/src/test/scala/org/knora/webapi/slice/admin/api/AdminUsersProjectMemberShipsEndpointsE2ESpec.scala
@@ -7,6 +7,7 @@ package org.knora.webapi.slice.admin.api
 
 import sttp.client4.UriContext
 import sttp.model.StatusCode
+import zio.*
 import zio.test.*
 
 import org.knora.webapi.*
@@ -18,8 +19,10 @@ import org.knora.webapi.sharedtestdata.SharedTestDataADM2
 import org.knora.webapi.slice.admin.api.model.Project
 import org.knora.webapi.slice.admin.api.service.UserRestService.UserResponse
 import org.knora.webapi.slice.admin.domain.model.*
+import org.knora.webapi.slice.admin.domain.model.KnoraProject.ProjectIri
 import org.knora.webapi.testservices.ResponseOps
 import org.knora.webapi.testservices.ResponseOps.assert200
+import org.knora.webapi.testservices.ResponseOps.assert400
 import org.knora.webapi.testservices.TestApiClient
 
 object AdminUsersProjectMemberShipsEndpointsE2ESpec extends E2EZSpec {
@@ -31,11 +34,7 @@ object AdminUsersProjectMemberShipsEndpointsE2ESpec extends E2EZSpec {
   )(
     suite("used to query project memberships")(
       test("return all projects the user is a member of") {
-        TestApiClient
-          .getJson[UserProjectMembershipsGetResponseADM](
-            uri"/admin/users/iri/$multiUserIri/project-memberships",
-            rootUser,
-          )
+        getProjectMemberships(multiUserIri)
           .flatMap(_.assert200)
           .map(result =>
             assertTrue(
@@ -48,96 +47,40 @@ object AdminUsersProjectMemberShipsEndpointsE2ESpec extends E2EZSpec {
     ),
     suite("used to modify project membership")(
       test("NOT add a user to project if the requesting user is not a SystemAdmin or ProjectAdmin") {
-        TestApiClient
-          .postJson[UserResponse, String](
-            uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-            "",
-            normalUser,
-          )
+        addUserToProject(normalUser.userIri, imagesProjectExternal.id, normalUser)
           .map(response => assertTrue(response.code == StatusCode.Forbidden))
       },
       test("add user to project") {
         for {
-          beforeResult <- TestApiClient
-                            .getJson[UserProjectMembershipsGetResponseADM](
-                              uri"/admin/users/iri/${normalUser.id}/project-memberships",
-                              rootUser,
-                            )
-                            .flatMap(_.assert200)
-          _ <- TestApiClient
-                 .postJson[UserResponse, String](
-                   uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                   "",
-                   rootUser,
-                 )
-                 .flatMap(_.assert200)
-          afterResult <- TestApiClient
-                           .getJson[UserProjectMembershipsGetResponseADM](
-                             uri"/admin/users/iri/${normalUser.id}/project-memberships",
-                             rootUser,
-                           )
-                           .flatMap(_.assert200)
+          beforeResult <- getProjectMemberships(normalUser.userIri).flatMap(_.assert200)
+          _            <- addUserToProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
+          afterResult  <- getProjectMemberships(normalUser.userIri).flatMap(_.assert200)
         } yield assertTrue(
           beforeResult.projects == Seq.empty,
-          afterResult.projects == Seq(SharedTestDataADM.imagesProjectExternal),
+          afterResult.projects == Seq(imagesProjectExternal),
         )
       },
       test("don't add user to project if user is already a member") {
         for {
-          beforeResult <- TestApiClient
-                            .getJson[UserProjectMembershipsGetResponseADM](
-                              uri"/admin/users/iri/${normalUser.id}/project-memberships",
-                              rootUser,
-                            )
-                            .flatMap(_.assert200)
-          response <- TestApiClient.postJson[UserResponse, String](
-                        uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                        "",
-                        rootUser,
-                      )
-          afterResult <- TestApiClient
-                           .getJson[UserProjectMembershipsGetResponseADM](
-                             uri"/admin/users/iri/${normalUser.id}/project-memberships",
-                             rootUser,
-                           )
-                           .flatMap(_.assert200)
-        } yield assertTrue(
-          response.code == StatusCode.BadRequest,
-          afterResult.projects == beforeResult.projects,
-        )
+          beforeResult <- getProjectMemberships(normalUser.userIri).flatMap(_.assert200)
+          _            <- addUserToProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert400)
+          afterResult  <- getProjectMemberships(normalUser.userIri).flatMap(_.assert200)
+        } yield assertTrue(afterResult.projects == beforeResult.projects)
       },
       test("remove user from project") {
         for {
-          beforeResult <- TestApiClient
-                            .getJson[UserProjectMembershipsGetResponseADM](
-                              uri"/admin/users/iri/${normalUser.id}/project-memberships",
-                              rootUser,
-                            )
-                            .flatMap(_.assert200)
-          response <- TestApiClient.deleteJson[UserResponse](
-                        uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                        rootUser,
-                      )
-          afterResult <- TestApiClient
-                           .getJson[UserProjectMembershipsGetResponseADM](
-                             uri"/admin/users/iri/${normalUser.id}/project-memberships",
-                             rootUser,
-                           )
-                           .flatMap(_.assert200)
+          beforeResult <- getProjectMemberships(normalUser.userIri).flatMap(_.assert200)
+          _            <- removeUserFromProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
+          afterResult  <- getProjectMemberships(normalUser.userIri).flatMap(_.assert200)
         } yield assertTrue(
-          beforeResult.projects == Seq(SharedTestDataADM.imagesProjectExternal),
-          response.code == StatusCode.Ok,
-          afterResult.projects == Seq.empty[Project],
+          beforeResult.projects == Seq(imagesProjectExternal),
+          afterResult.projects == Seq.empty,
         )
       },
     ),
     suite("used to query project admin group memberships")(
       test("return all projects the user is a member of the project admin group") {
-        TestApiClient
-          .getJson[UserProjectAdminMembershipsGetResponseADM](
-            uri"/admin/users/iri/$multiUserIri/project-admin-memberships",
-            rootUser,
-          )
+        getProjectAdminMemberships(multiUserIri)
           .flatMap(_.assert200)
           .map(result =>
             assertTrue(
@@ -151,101 +94,94 @@ object AdminUsersProjectMemberShipsEndpointsE2ESpec extends E2EZSpec {
     suite("used to modify project admin group membership")(
       test("add user to project admin group only if he is already member of that project") {
         for {
-          // add user as project admin to images project - returns a BadRequest because user is not member of the project
-          responseWithoutBeingMember <-
-            TestApiClient.postJson[UserResponse, String](
-              uri"/admin/users/iri/${normalUser.id}/project-admin-memberships/$imagesProjectIri",
-              "",
-              rootUser,
-            )
-          // add user as member to images project
-          responseAddUserToProject <- TestApiClient.postJson[UserResponse, String](
-                                        uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                                        "",
-                                        rootUser,
-                                      )
+          // add user as project admin to images project - should return BadRequest because user is not member of the project
+          _ <- addUserToProjectAsAdmin(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert400)
+          // add user as member to images project, must succeed
+          _ <- addUserToProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
           // verify that user is not yet project admin in images project
-          membershipsBeforeResponse <- TestApiClient.getJson[UserProjectAdminMembershipsGetResponseADM](
-                                         uri"/admin/users/iri/${normalUser.id}/project-admin-memberships",
-                                         rootUser,
-                                       )
-          membershipsBeforeResult <- membershipsBeforeResponse.assert200
+          membershipsBeforeResult <- getProjectAdminMemberships(normalUser.userIri).flatMap(_.assert200)
           // add user as project admin to images project
-          response <- TestApiClient.postJson[UserResponse, String](
-                        uri"/admin/users/iri/${normalUser.id}/project-admin-memberships/$imagesProjectIri",
-                        "",
-                        rootUser,
-                      )
+          _ <- addUserToProjectAsAdmin(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
           // verify that user has been added as project admin to images project
-          membershipsAfterResponse <- TestApiClient.getJson[UserProjectAdminMembershipsGetResponseADM](
-                                        uri"/admin/users/iri/${normalUser.id}/project-admin-memberships",
-                                        rootUser,
-                                      )
-          membershipsAfterResult <- membershipsAfterResponse.assert200
+          membershipsAfterResult <- getProjectAdminMemberships(normalUser.userIri).flatMap(_.assert200)
         } yield assertTrue(
-          responseWithoutBeingMember.code == StatusCode.BadRequest,
-          responseAddUserToProject.code == StatusCode.Ok,
-          membershipsBeforeResult.projects == Seq(),
-          response.code == StatusCode.Ok,
-          membershipsAfterResult.projects == Seq(SharedTestDataADM.imagesProjectExternal),
+          membershipsBeforeResult.projects == Seq.empty,
+          membershipsAfterResult.projects == Seq(imagesProjectExternal),
         )
       },
       test("remove user from project admin group") {
         for {
-          membershipsBeforeResponse <- TestApiClient.getJson[UserProjectAdminMembershipsGetResponseADM](
-                                         uri"/admin/users/iri/${normalUser.id}/project-admin-memberships",
-                                         rootUser,
-                                       )
-          membershipsBeforeResult <- membershipsBeforeResponse.assert200
-          response <- TestApiClient.deleteJson[UserResponse](
-                        uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                        rootUser,
-                      )
-          membershipsAfterResponse <- TestApiClient.getJson[UserProjectAdminMembershipsGetResponseADM](
-                                        uri"/admin/users/iri/${normalUser.id}/project-admin-memberships",
-                                        rootUser,
-                                      )
-          membershipsAfterResult <- membershipsAfterResponse.assert200
+          membershipsBefore <- getProjectAdminMemberships(normalUser.userIri).flatMap(_.assert200)
+          _                 <- removeUserFromProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
+          membershipsAfter  <- getProjectAdminMemberships(normalUser.userIri).flatMap(_.assert200)
         } yield assertTrue(
-          membershipsBeforeResult.projects == Seq(SharedTestDataADM.imagesProjectExternal),
-          response.code == StatusCode.Ok,
-          membershipsAfterResult.projects == Seq.empty,
+          membershipsBefore.projects == Seq(imagesProjectExternal),
+          membershipsAfter.projects == Seq.empty,
         )
       },
       test("remove user from project which also removes him from project admin group") {
         for {
           // add user as project admin to images project
-          _ <- TestApiClient
-                 .postJson[UserResponse, String](
-                   uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                   "",
-                   rootUser,
-                 )
-                 .flatMap(_.assert200) *>
-                 TestApiClient
-                   .postJson[UserResponse, String](
-                     uri"/admin/users/iri/${normalUser.id}/project-admin-memberships/$imagesProjectIri",
-                     "",
-                     rootUser,
-                   )
-                   .flatMap(_.assert200)
-
+          _ <- addUserToProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
+          _ <- addUserToProjectAsAdmin(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
           // remove user as project member from images project
-          _ <- TestApiClient
-                 .deleteJson[UserResponse](
-                   uri"/admin/users/iri/${normalUser.id}/project-memberships/$imagesProjectIri",
-                   rootUser,
-                 )
-                 .flatMap(_.assert200)
+          _ <- removeUserFromProject(normalUser.userIri, imagesProjectExternal.id).flatMap(_.assert200)
           // verify that user has also been removed as project admin from images project
-          projectAdminMembershipsAfterResult <- TestApiClient
-                                                  .getJson[UserProjectAdminMembershipsGetResponseADM](
-                                                    uri"/admin/users/iri/${normalUser.id}/project-admin-memberships",
-                                                    rootUser,
-                                                  )
-                                                  .flatMap(_.assert200)
+          projectAdminMembershipsAfterResult <- getProjectAdminMemberships(normalUser.userIri).flatMap(_.assert200)
         } yield assertTrue(projectAdminMembershipsAfterResult.projects == Seq.empty)
       },
     ),
   )
+
+  private def getProjectMemberships(userIri: UserIri, requestingUser: User = rootUser) =
+    addLogTiming("GET project-memberships") {
+      TestApiClient
+        .getJson[UserProjectMembershipsGetResponseADM](
+          uri"/admin/users/iri/$userIri/project-memberships",
+          requestingUser,
+        )
+    }
+
+  private def getProjectAdminMemberships(userIri: UserIri, requestingUser: User = rootUser) =
+    addLogTiming("GET project-admin-memberships") {
+      TestApiClient
+        .getJson[UserProjectAdminMembershipsGetResponseADM](
+          uri"/admin/users/iri/$userIri/project-admin-memberships",
+          requestingUser,
+        )
+    }
+
+  private def addUserToProject(userIri: UserIri, projectIri: ProjectIri, requestingUser: User = rootUser) =
+    addLogTiming("POST project-memberships") {
+      TestApiClient
+        .postJson[UserResponse, String](
+          uri"/admin/users/iri/$userIri/project-memberships/$projectIri",
+          "",
+          requestingUser,
+        )
+    }
+
+  private def addUserToProjectAsAdmin(userIri: UserIri, projectIri: ProjectIri, requestingUser: User = rootUser) =
+    addLogTiming("POST project-admin-memberships") {
+      TestApiClient
+        .postJson[UserResponse, String](
+          uri"/admin/users/iri/$userIri/project-admin-memberships/$projectIri",
+          "",
+          requestingUser,
+        )
+    }
+
+  private def removeUserFromProject(userIri: UserIri, projectIri: ProjectIri, requestingUser: User = rootUser) =
+    addLogTiming("DELETE project-memberships") {
+      TestApiClient
+        .deleteJson[UserResponse](
+          uri"/admin/users/iri/$userIri/project-memberships/$projectIri",
+          requestingUser,
+        )
+    }
+
+  private def addLogTiming[R, E, A](msg: String)(zio: ZIO[R, E, A]): ZIO[R, E, A] =
+    zio.timed.flatMap { case (duration, res) =>
+      ZIO.logWarning(s"$msg took: ${duration.toMillis} ms").as(res)
+    }
 }
