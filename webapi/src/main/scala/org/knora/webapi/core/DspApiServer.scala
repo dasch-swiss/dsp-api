30a8b83376164f4056dc1cf30c071c497e2208bf
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-10-03 14:31:31 +0200

    activate response compression

--- webapi/src/main/scala/org/knora/webapi/core/DspApiServer.scala
+++ webapi/src/main/scala/org/knora/webapi/core/DspApiServer.scala
@@ -13,6 +13,7 @@ import sttp.tapir.server.ziohttp.ZioHttpInterpreter
 import sttp.tapir.server.ziohttp.ZioHttpServerOptions
 import zio.*
 import zio.http.*
+import zio.http.Server.Config.ResponseCompressionConfig
 
 import org.knora.webapi.config.KnoraApi
 import org.knora.webapi.routing.Endpoints
@@ -47,7 +48,13 @@ object DspApiServer {
 
   private val serverLayer = ZLayer
     .service[KnoraApi]
-    .flatMap(cfg => Server.defaultWith(_.binding(cfg.get.internalHost, cfg.get.internalPort).enableRequestStreaming))
+    .flatMap(cfg =>
+      Server
+        .defaultWith(
+          _.binding(cfg.get.internalHost, cfg.get.internalPort).enableRequestStreaming
+            .responseCompression(ResponseCompressionConfig.default),
+        ),
+    )
     .orDie
 
   val layer = serverLayer >>> ZLayer.derive[DspApiServer]
