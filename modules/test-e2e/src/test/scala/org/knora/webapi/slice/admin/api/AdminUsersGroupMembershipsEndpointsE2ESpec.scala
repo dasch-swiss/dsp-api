0bbe546b7327017fcf18e5baa1b08fa9462170a7
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-10-13 11:57:43 +0200

    mark as flaky and refactor for readability

--- modules/test-e2e/src/test/scala/org/knora/webapi/slice/admin/api/AdminUsersGroupMembershipsEndpointsE2ESpec.scala
+++ modules/test-e2e/src/test/scala/org/knora/webapi/slice/admin/api/AdminUsersGroupMembershipsEndpointsE2ESpec.scala
@@ -6,7 +6,6 @@
 package org.knora.webapi.slice.admin.api
 
 import sttp.client4.UriContext
-import sttp.model.StatusCode
 import zio.*
 import zio.test.*
 
@@ -15,8 +14,11 @@ import org.knora.webapi.messages.admin.responder.usersmessages.UserGroupMembersh
 import org.knora.webapi.sharedtestdata.SharedTestDataADM
 import org.knora.webapi.sharedtestdata.SharedTestDataADM.*
 import org.knora.webapi.sharedtestdata.SharedTestDataADM2
+import org.knora.webapi.slice.admin.api.UsersEndpoints.Requests.UserCreateRequest
+import org.knora.webapi.slice.admin.api.model.UserDto
 import org.knora.webapi.slice.admin.api.service.UserRestService.UserResponse
 import org.knora.webapi.slice.admin.domain.model.*
+import org.knora.webapi.slice.common.domain.LanguageCode.DE
 import org.knora.webapi.testservices.ResponseOps
 import org.knora.webapi.testservices.ResponseOps.assert200
 import org.knora.webapi.testservices.TestApiClient
@@ -24,6 +26,25 @@ import org.knora.webapi.testservices.TestApiClient
 object AdminUsersGroupMembershipsEndpointsE2ESpec extends E2EZSpec {
 
   private val multiUserIri = UserIri.unsafeFrom(SharedTestDataADM2.multiuserUser.userData.user_id.get)
+
+  private def createNewUser =
+    val firstName = faker.name().firstName()
+    val lastName  = faker.name().lastName()
+    val req = UserCreateRequest(
+      id = None,
+      Username.unsafeFrom(s"$firstName.${faker.number().positive()}"),
+      Email.unsafeFrom(s"$firstName.$lastName@example.org"),
+      GivenName.unsafeFrom(firstName),
+      FamilyName.unsafeFrom(lastName),
+      Password.unsafeFrom(faker.credentials().password(8, 16)),
+      UserStatus.Active,
+      DE,
+      SystemAdmin.IsNotSystemAdmin,
+    )
+    TestApiClient
+      .postJson[UserResponse, UserCreateRequest](uri"/admin/users", req, rootUser)
+      .flatMap(_.assert200)
+      .map(_.user)
 
   override val e2eSpec = suite(
     "The Users Route ('admin/users/iri/:userIri/group-member-ships') ",
@@ -43,49 +64,44 @@ object AdminUsersGroupMembershipsEndpointsE2ESpec extends E2EZSpec {
     suite("used to modify group membership")(
       test("add user to group") {
         for {
-          _ <- TestApiClient
-                 .getJson[UserGroupMembershipsGetResponseADM](
-                   uri"/admin/users/iri/${normalUser.id}/group-memberships",
-                   rootUser,
-                 )
-                 .flatMap(_.assert200)
-                 .filterOrFail(_.groups.isEmpty)(IllegalStateException("User is already member of a group"))
-          response <- TestApiClient.postJson[UserResponse, String](
-                        uri"/admin/users/iri/${normalUser.id}/group-memberships/${imagesReviewerGroup.groupIri}",
-                        "",
-                        rootUser,
-                      )
-          membershipsAfterResult <- TestApiClient
-                                      .getJson[UserGroupMembershipsGetResponseADM](
-                                        uri"/admin/users/iri/${normalUser.id}/group-memberships",
-                                        rootUser,
-                                      )
-                                      .flatMap(_.assert200)
-        } yield assertTrue(membershipsAfterResult.groups == Seq(SharedTestDataADM.imagesReviewerGroupExternal))
+          newUser           <- createNewUser
+          _                 <- addUserToGroup(newUser, imagesReviewerGroupExternal)
+          actualMemberships <- getGroupMemberships(newUser)
+        } yield assertTrue(actualMemberships.groups == Seq(imagesReviewerGroupExternal))
       } @@ TestAspect.timeout(5.seconds) @@ TestAspect.flaky,
       test("remove user from group") {
         for {
-          membershipsBeforeResponse <-
-            TestApiClient.getJson[UserGroupMembershipsGetResponseADM](
-              uri"/admin/users/iri/${normalUser.id}/group-memberships",
-              rootUser,
-            )
-          membershipsBeforeResult <- membershipsBeforeResponse.assert200
-          response <- TestApiClient.deleteJson[UserResponse](
-                        uri"/admin/users/iri/${normalUser.id}/group-memberships/${imagesReviewerGroup.groupIri}",
-                        rootUser,
-                      )
-          membershipsAfterResponse <- TestApiClient.getJson[UserGroupMembershipsGetResponseADM](
-                                        uri"/admin/users/iri/${normalUser.id}/group-memberships",
-                                        rootUser,
-                                      )
-          membershipsAfterResult <- membershipsAfterResponse.assert200
-        } yield assertTrue(
-          membershipsBeforeResult.groups == Seq(SharedTestDataADM.imagesReviewerGroupExternal),
-          response.code == StatusCode.Ok,
-          membershipsAfterResult.groups == Seq.empty[Group],
-        )
-      },
+          newUser           <- createNewUser
+          _                 <- addUserToGroup(newUser, imagesReviewerGroupExternal)
+          _                 <- removeUserFromGroup(newUser, imagesReviewerGroupExternal)
+          actualMemberships <- getGroupMemberships(newUser)
+        } yield assertTrue(actualMemberships.groups == Seq.empty)
+      } @@ TestAspect.timeout(5.seconds) @@ TestAspect.flaky,
     ),
   )
+
+  private def removeUserFromGroup(user: UserDto, group: Group) =
+    TestApiClient
+      .deleteJson[UserResponse](
+        uri"/admin/users/iri/${user.id}/group-memberships/${group.groupIri}",
+        rootUser,
+      )
+      .flatMap(_.assert200)
+
+  private def getGroupMemberships(user: UserDto) =
+    TestApiClient
+      .getJson[UserGroupMembershipsGetResponseADM](
+        uri"/admin/users/iri/${user.id}/group-memberships",
+        rootUser,
+      )
+      .flatMap(_.assert200)
+
+  private def addUserToGroup(user: UserDto, group: Group) =
+    TestApiClient
+      .postJson[UserResponse, IRI](
+        uri"/admin/users/iri/${user.id}/group-memberships/${group.groupIri}",
+        "",
+        rootUser,
+      )
+      .flatMap(_.assert200)
 }
