331273c9862172a98c9417bb603cb0f0263255a0
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-10-28 13:27:16 +0100

    trying stuff out

--- modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchRecursiveBugE2ESpec.scala
+++ modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchRecursiveBugE2ESpec.scala
@@ -26,8 +26,6 @@ object SearchEndpointsPostGravsearchRecursiveBugE2ESpec extends E2EZSpec with Go
       ),
     )
 
-  (assertGolden _, ())
-
   override def e2eSpec = suite("SearchEndpoints POST /v2/searchextended")(
     test("do a Gravsearch query that nests resources infinitively recursively") {
       val query =
@@ -61,3 +59,16 @@ object SearchEndpointsPostGravsearchRecursiveBugE2ESpec extends E2EZSpec with Go
     },
   )
 }
+
+/*
+ConstructResponseUtilV2: 1 http://rdfh.ch/0803/ff17e5ef9601 -> http://rdfh.ch/0803/3f89a693a501/values/133ea4ce-b9a5-4c66-a3ab-38be07ccd201
+ConstructResponseUtilV2: 1 http://rdfh.ch/0803/ff17e5ef9601 -> http://rdfh.ch/0803/6240ce899801/values/e29da7bf-bfa5-4842-a4fc-e455c72dbb0a
+ConstructResponseUtilV2: 1 http://rdfh.ch/0803/ff17e5ef9601 -> http://rdfh.ch/0803/c568b7239a01/values/46951e64-8f06-47fb-a07a-c8b19c146447
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/2e0252679a35/values/498d07ec-dddb-4051-bfd1-62626f282fc5 -> http://rdfh.ch/0803/3f89a693a501
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/3f89a693a501/values/133ea4ce-b9a5-4c66-a3ab-38be07ccd201 -> http://rdfh.ch/0803/ff17e5ef9601
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/4e2223eb204c01/values/97188bdc-292a-48f0-9373-29e67afbbd81 -> http://rdfh.ch/0803/6240ce899801
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/6240ce899801/values/e29da7bf-bfa5-4842-a4fc-e455c72dbb0a -> http://rdfh.ch/0803/ff17e5ef9601
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/c2807e831a4c01/values/39c732db-d191-44b9-b091-d764e6e0eff8 -> http://rdfh.ch/0803/c568b7239a01
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/c4160576204c01/values/8f499fc5-2884-4876-8887-4aa83504d937 -> http://rdfh.ch/0803/6240ce899801
+ConstructResponseUtilV2: 2 http://rdfh.ch/0803/c568b7239a01/values/46951e64-8f06-47fb-a07a-c8b19c146447 -> http://rdfh.ch/0803/ff17e5ef9601
+*/
