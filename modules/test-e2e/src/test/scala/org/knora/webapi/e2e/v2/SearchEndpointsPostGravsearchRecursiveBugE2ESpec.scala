c6913536df027aee754b61056b4e895be96ddde3
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-10-28 12:00:10 +0100

    1

---
+++ modules/test-e2e/src/test/scala/org/knora/webapi/e2e/v2/SearchEndpointsPostGravsearchRecursiveBugE2ESpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2021 - 2025 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.knora.webapi.e2e.v2
+
+import zio.*
+import zio.test.*
+
+import org.knora.webapi.E2EZSpec
+import org.knora.webapi.e2e.v2.SearchEndpointE2ESpecHelper.*
+import org.knora.webapi.messages.store.triplestoremessages.RdfDataObject
+import org.knora.webapi.sharedtestdata.SharedTestDataADM.*
+import org.knora.webapi.GoldenTest
+
+object SearchEndpointsPostGravsearchRecursiveBugE2ESpec extends E2EZSpec with GoldenTest {
+  override val rewriteAll = false
+
+  override val rdfDataObjects: List[RdfDataObject] =
+    List(
+      RdfDataObject(
+        // manual derivation from incunabula-data.ttl
+        path = "test_data/project_data/incunabula-data-SearchEndpointsPostGravsearchRecursiveBug.ttl",
+        name = "http://www.knora.org/data/0803/incunabula",
+      ),
+    )
+
+  (assertGolden _, ())
+
+  override def e2eSpec = suite("SearchEndpoints POST /v2/searchextended")(
+    test("do a Gravsearch query that nests resources infinitively recursively") {
+      val query =
+        """
+          |PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/simple/v2#>
+          |PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+          |CONSTRUCT {
+          |    ?region knora-api:isMainResource true .
+          |    ?region knora-api:isRegionOf ?page .
+          |    ?page knora-api:isPartOf ?book .
+          |    ?book incunabula:title ?title .
+          |} WHERE {
+          |    ?region a knora-api:Resource .
+          |    ?region a knora-api:Region .
+          |    ?region knora-api:isRegionOf ?page .
+          |
+          |    ?page a knora-api:Resource .
+          |    ?page a incunabula:page .
+          |
+          |    ?page knora-api:isPartOf ?book .
+          |    ?book a knora-api:Resource .
+          |    ?book a incunabula:book .
+          |    ?book incunabula:title ?title .
+          |
+          |    incunabula:title knora-api:objectType xsd:string .
+          |    ?title a xsd:string .
+          |    FILTER(?title = "Zeitglöcklein des Lebens und Leidens Christi")
+          |}
+          |""".stripMargin
+      verifyQueryResultOrWrite(query, "SearchEndpointsPostGravsearchRecursiveBugE2ESpec.jsonld", incunabulaMemberUser)
+    },
+  )
+}
