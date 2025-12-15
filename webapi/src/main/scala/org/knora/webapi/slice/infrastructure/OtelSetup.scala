e5eeace8564e109b5511696e9c542322540249ca
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2025-12-15 10:09:51 +0100

    copyright header

--- webapi/src/main/scala/org/knora/webapi/slice/infrastructure/OtelSetup.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/infrastructure/OtelSetup.scala
@@ -1,4 +1,10 @@
+/*
+ * Copyright © 2021 - 2025 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.knora.webapi.slice.infrastructure
+
 import io.opentelemetry.api
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator
