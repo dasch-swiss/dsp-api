725857d3f63d960d0472596dcc227e4494631f74
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2026-02-10 11:43:27 +0100

    fix: Solve timeout problems with the FileValuePermissionsQuery
    The twirl template had an additional filter which improved performance. This got lost when migrating to rdf4j.
    
    https://github.com/dasch-swiss/dsp-api/pull/3930/files#diff-bf79f36534ac50be72b70f389b77a55bcb26b5243c1bcc36aee7eb4346a6e67f
    ``` FILTER(?objPred != knora-base:previousValue)```

--- webapi/src/test/scala/org/knora/webapi/slice/admin/repo/FileValuePermissionsQuerySpec.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/admin/repo/FileValuePermissionsQuerySpec.scala
@@ -14,30 +14,43 @@ object FileValuePermissionsQuerySpec extends ZIOSpecDefault {
 
   private val testFilename: InternalFilename = InternalFilename.unsafeFrom("0001/test-image.jp2")
 
-  // @formatter:off
-  private val expectedQuery: String =
-    """PREFIX knora-base: <http://www.knora.org/ontology/knora-base#>
-      |SELECT ?creator ?project ?permissions
-      |WHERE { ?fileValue knora-base:internalFilename "0001/test-image.jp2" ;
-      |    knora-base:attachedToUser ?creator .
-      |?currentFileValue knora-base:previousValue* ?fileValue ;
-      |    knora-base:hasPermissions ?permissions ;
-      |    knora-base:isDeleted false .
-      |?resource ?prop ?currentFileValue ;
-      |    knora-base:attachedToProject ?project ;
-      |    knora-base:isDeleted false . }
-      |""".stripMargin
-  // @formatter:on
-
   override def spec: Spec[TestEnvironment, Any] = suite("FileValuePermissionsQuerySpec")(
     test("should produce correct SELECT query for a filename") {
       val actual: SelectQuery = FileValuePermissionsQuery.build(testFilename)
-      assertTrue(actual.getQueryString == expectedQuery)
+      val expected            =
+        """PREFIX knora-base: <http://www.knora.org/ontology/knora-base#>
+          |SELECT ?creator ?project ?permissions
+          |WHERE { ?fileValue knora-base:internalFilename "0001/test-image.jp2" .
+          |?currentFileValue knora-base:previousValue* ?fileValue ;
+          |    knora-base:hasPermissions ?permissions ;
+          |    knora-base:attachedToUser ?creator .
+          |?resource ?prop ?currentFileValue ;
+          |    knora-base:attachedToProject ?project .
+          |{ ?fileValue ?objPred ?objObj .
+          |FILTER ( ?objPred != knora-base:previousValue ) }
+          |?currentFileValue knora-base:isDeleted false .
+          |?resource knora-base:isDeleted false . }
+          |""".stripMargin
+      assertTrue(actual.getQueryString == expected)
     },
     test("should produce correct SELECT query for a filename with special characters") {
       val specialFilename: InternalFilename = InternalFilename.unsafeFrom("0001/file-with-special_chars.jp2")
       val actual: SelectQuery               = FileValuePermissionsQuery.build(specialFilename)
-      assertTrue(actual.getQueryString.contains("\"0001/file-with-special_chars.jp2\""))
+      val expected: String                  =
+        """PREFIX knora-base: <http://www.knora.org/ontology/knora-base#>
+          |SELECT ?creator ?project ?permissions
+          |WHERE { ?fileValue knora-base:internalFilename "0001/file-with-special_chars.jp2" .
+          |?currentFileValue knora-base:previousValue* ?fileValue ;
+          |    knora-base:hasPermissions ?permissions ;
+          |    knora-base:attachedToUser ?creator .
+          |?resource ?prop ?currentFileValue ;
+          |    knora-base:attachedToProject ?project .
+          |{ ?fileValue ?objPred ?objObj .
+          |FILTER ( ?objPred != knora-base:previousValue ) }
+          |?currentFileValue knora-base:isDeleted false .
+          |?resource knora-base:isDeleted false . }
+          |""".stripMargin
+      assertTrue(actual.getQueryString == expected)
     },
   )
 }
