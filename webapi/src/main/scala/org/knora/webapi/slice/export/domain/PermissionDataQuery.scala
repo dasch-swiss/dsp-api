3569fa776f49b866edbc885a17d3f7dd4d6b8f3e
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2026-02-19 12:54:37 +0100

    only query the permissions data graph for the permissions

--- webapi/src/main/scala/org/knora/webapi/slice/export/domain/PermissionDataQuery.scala
+++ webapi/src/main/scala/org/knora/webapi/slice/export/domain/PermissionDataQuery.scala
@@ -4,9 +4,10 @@
  */
 
 package org.knora.webapi.slice.`export`.domain
+
 import org.eclipse.rdf4j.sparqlbuilder.core.query.ConstructQuery
 import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries
-
+import org.knora.webapi.slice.admin.AdminConstants.permissionsDataNamedGraph
 import org.knora.webapi.slice.admin.domain.model.KnoraProject.ProjectIri
 import org.knora.webapi.slice.common.QueryBuilderHelper
 import org.knora.webapi.slice.common.repo.rdf.Vocabulary.KnoraAdmin as KA
@@ -16,7 +17,10 @@ object PermissionDataQuery extends QueryBuilderHelper {
   def build(project: ProjectIri): ConstructQuery = {
     val (s, p, o)  = spo
     val projectIri = toRdfIri(project)
-    val where      = s.has(KA.forProject, projectIri).andHas(p, o)
+    val where      = s
+      .has(KA.forProject, projectIri)
+      .andHas(p, o)
+      .from(toRdfIri(permissionsDataNamedGraph))
     Queries.CONSTRUCT(s.has(p, o)).where(where).prefix(KA.NS)
   }
 }
