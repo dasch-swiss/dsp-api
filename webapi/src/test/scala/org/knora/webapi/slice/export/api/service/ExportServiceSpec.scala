79bbc48665003092e85b50c159da7f3699611775
Marcin Procyk <marcin.procyk@dasch.swiss>
2026-03-16 13:14:32 +0100

    test(export): add superclass export test and SubClass1 fixtures (DEV-5952)
    - Add SuperClass1 and SubClass1 (subClassOf SuperClass1) to test ontology
    - Add SubClass1 resource instance to test data
    - Add ExportServiceSpec test proving SubClass1 resource is returned when
      exporting with SuperClass1 selected
    - Regenerate golden files (unchanged for existing tests)

--- webapi/src/test/scala/org/knora/webapi/slice/export/api/service/ExportServiceSpec.scala
+++ webapi/src/test/scala/org/knora/webapi/slice/export/api/service/ExportServiceSpec.scala
@@ -55,6 +55,9 @@ object ExportServiceSpec extends ZIOSpecDefault with GoldenTest {
   def resourceClassIri: ResourceClassIri =
     ResourceClassIri.unsafeFrom("http://www.knora.org/ontology/1612/Data#Class1")(using sf)
 
+  def superClassIri: ResourceClassIri =
+    ResourceClassIri.unsafeFrom("http://www.knora.org/ontology/1612/Data#SuperClass1")(using sf)
+
   val user       = TestDataFactory.User.rootUser
   val projectIri = ProjectIri.unsafeFrom("http://rdfh.ch/projects/Vk0NruDmRyeZCZvOVwXOnw")
 
@@ -124,6 +127,25 @@ object ExportServiceSpec extends ZIOSpecDefault with GoldenTest {
             )
           csv <- exportService.toCsv(exportedCsv)
         } yield assertGolden(csv, "includeIrisFalse")
+      },
+      test("superclass export returns instances of subclasses") {
+        for {
+          _             <- ZIO.serviceWithZIO[TriplestoreService](_.insertDataIntoTriplestore(dataSets.toList, false))
+          _             <- ZIO.serviceWithZIO[OntologyCache](_.refreshCache())
+          project       <- ZIO.serviceWithZIO[KnoraProjectService](_.findById(projectIri)).map(_.get)
+          exportService <- ZIO.service[ExportService]
+          exportedCsv   <-
+            exportService.exportResources(
+              project,
+              superClassIri,
+              List.empty,
+              user,
+              LanguageCode.EN,
+              includeIris = true,
+              includeArkUrls = false,
+            )
+          csv <- exportService.toCsv(exportedCsv)
+        } yield assertGolden(csv, "superclassExport")
       },
       test("with includeArkUrls = true") {
         for {
