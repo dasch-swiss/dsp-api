6831f533abb4783ca317b9585c3e5bb8a8516bbe
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2026-05-11 13:26:41 +0200

    fix: Add missing import for BadRequestException in ConstructResponseUtilV2

--- webapi/src/main/scala/org/knora/webapi/messages/util/ConstructResponseUtilV2.scala
+++ webapi/src/main/scala/org/knora/webapi/messages/util/ConstructResponseUtilV2.scala
@@ -9,6 +9,7 @@ import zio.*
 
 import java.time.Instant
 import java.util.UUID
+
 import dsp.errors.BadRequestException
 import dsp.errors.InconsistentRepositoryDataException
 import dsp.errors.NotFoundException
