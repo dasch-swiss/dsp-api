53632370cd6a86565aa1614991458afbcd0a5ec9
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2026-09-04 10:24:19 +0200

    refactor(sparql): migrate graph and list queries

--- modules/webapi/src/main/scala/org/knora/webapi/slice/resources/repo/UpdateListInfoQuery.scala
+++ modules/webapi/src/main/scala/org/knora/webapi/slice/resources/repo/UpdateListInfoQuery.scala
@@ -4,19 +4,17 @@
  */
 
 package org.knora.webapi.slice.resources.repo
-import org.eclipse.rdf4j.model.vocabulary.RDF
-import org.eclipse.rdf4j.model.vocabulary.RDFS
-import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries
 
+import org.knora.sparqlbuilder.*
+import org.knora.webapi.messages.store.triplestoremessages.StringLiteralV2
 import org.knora.webapi.slice.admin.domain.model.KnoraProject
 import org.knora.webapi.slice.admin.domain.model.ListProperties.Comments
 import org.knora.webapi.slice.admin.domain.model.ListProperties.Labels
 import org.knora.webapi.slice.admin.domain.model.ListProperties.ListIri
 import org.knora.webapi.slice.admin.domain.model.ListProperties.ListName
-import org.knora.webapi.slice.common.QueryBuilderHelper
-import org.knora.webapi.slice.common.repo.rdf.Vocabulary.KnoraBase
+import org.knora.webapi.slice.admin.domain.service.ProjectService
 
-object UpdateListInfoQuery extends QueryBuilderHelper {
+object UpdateListInfoQuery {
 
   def build(
     project: KnoraProject,
@@ -24,57 +22,47 @@ object UpdateListInfoQuery extends QueryBuilderHelper {
     name: Option[ListName],
     labels: Option[Labels],
     comments: Option[Comments],
-  ) = {
-    val node               = toRdfIri(nodeIri)
-    val currentLabelsVar   = variable("currentLabels")
-    val currentNameVar     = variable("currentName")
-    val currentCommentsVar = variable("currentComments")
+  ): String = {
+    val dataGraph = Iri.unsafeFrom(ProjectService.projectDataNamedGraphV2(project).value)
+    val node      = Iri.unsafeFrom(nodeIri.value)
 
-    val deletePatterns = {
-      val labelsPattern =
-        if labels.isDefined then Seq(node.has(RDFS.LABEL, currentLabelsVar))
-        else Seq.empty
+    def literal(value: StringLiteralV2): Literal =
+      value.languageOption.fold(Literal.string(value.value))(language =>
+        Literal.langString(value.value, language.value),
+      )
 
-      val namePattern =
-        if name.isDefined then Seq(node.has(KnoraBase.listNodeName, currentNameVar))
-        else Seq.empty
-
-      val commentsPattern =
-        if comments.isDefined then Seq(node.has(RDFS.COMMENT, currentCommentsVar))
-        else Seq.empty
-
-      labelsPattern ++ namePattern ++ commentsPattern
-    }
-
-    val insertPatterns = {
-      val labelsPattern   = labels.toSeq.flatMap(_.value.map(toRdfLiteral).map(node.has(RDFS.LABEL, _)))
-      val namePattern     = name.map(_.value).map(node.has(KnoraBase.listNodeName, _)).toList
-      val commentsPattern = comments.toSeq.flatMap(_.value.map(toRdfLiteral).map(node.has(RDFS.COMMENT, _)))
-      labelsPattern ++ namePattern ++ commentsPattern
-    }
-
-    val wherePatterns = {
-      val labelsPattern =
-        if labels.isDefined then Seq(node.has(RDFS.LABEL, currentLabelsVar).optional)
-        else Seq.empty
-
-      val namePattern =
-        if name.isDefined then Seq(node.has(KnoraBase.listNodeName, currentNameVar).optional)
-        else Seq.empty
-
-      val commentsPattern =
-        if comments.isDefined then Seq(node.has(RDFS.COMMENT, currentCommentsVar).optional)
-        else Seq.empty
-
-      Seq(node.isA(KnoraBase.ListNode)) ++ labelsPattern ++ namePattern ++ commentsPattern
-    }
-
-    Queries
-      .MODIFY()
-      .prefix(RDF.NS, RDFS.NS, KnoraBase.NS)
-      .`with`(graphIri(project))
-      .delete(deletePatterns*)
-      .insert(insertPatterns*)
-      .where(wherePatterns*)
+    sparql"""|PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+              |PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+              |PREFIX knora-base: <http://www.knora.org/ontology/knora-base#>
+              |
+              |WITH $dataGraph
+              |DELETE {
+              |  ${sparql"$node rdfs:label ?currentLabels .".when(labels.isDefined)}
+              |  ${sparql"$node knora-base:listNodeName ?currentName .".when(name.isDefined)}
+              |  ${sparql"$node rdfs:comment ?currentComments .".when(comments.isDefined)}
+              |}
+              |INSERT {
+              |  ${Fragment.join(
+        labels.toSeq.flatMap(_.value).map(label => sparql"$node rdfs:label ${literal(label)} ."),
+        Fragment.raw("\n"),
+      )}
+              |  ${name.whenSome(value => sparql"$node knora-base:listNodeName ${Literal.string(value.value)} .")}
+              |  ${Fragment.join(
+        comments.toSeq.flatMap(_.value).map(comment => sparql"$node rdfs:comment ${literal(comment)} ."),
+        Fragment.raw("\n"),
+      )}
+              |}
+              |WHERE {
+              |  $node a knora-base:ListNode .
+              |  ${sparql"""|OPTIONAL {
+                              |  $node rdfs:label ?currentLabels .
+                              |}""".when(labels.isDefined)}
+              |  ${sparql"""|OPTIONAL {
+                              |  $node knora-base:listNodeName ?currentName .
+                              |}""".when(name.isDefined)}
+              |  ${sparql"""|OPTIONAL {
+                              |  $node rdfs:comment ?currentComments .
+                              |}""".when(comments.isDefined)}
+              |}""".render
   }
 }
