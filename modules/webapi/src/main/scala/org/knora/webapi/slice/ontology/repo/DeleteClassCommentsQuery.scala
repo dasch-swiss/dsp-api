873a7b9f6cad75eb9ed2bda1b13bfa35c1c64301
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2026-09-08 09:54:33 +0200

    refactor: migrate ontology comment queries to interpolated SPARQL DSL (DEV-7153) (#4290)
    ### Description
    
    Migrates the ontology comment-deletion query batch from RDF4J
    SparqlBuilder to the typed interpolated SPARQL DSL:
    
    - `DeleteOntologyCommentQuery`
    - `DeleteClassCommentsQuery`
    - `DeletePropertyCommentsQuery`
    
    The migration preserves graph scoping, timestamp checks and updates, and
    the optional link-value-property deletion. Focused specs compare the new
    output with the legacy queries through Jena canonical rendering.
    
    ### Verification
    
    - `bazel test //modules/webapi:test
    --test_filter=.*Delete(ClassComments|PropertyComments|OntologyComment)QuerySpec.*`
    (7 tests passed)
    - `just check`
    
    Fixes DEV-7153

--- modules/webapi/src/main/scala/org/knora/webapi/slice/ontology/repo/DeleteClassCommentsQuery.scala
+++ modules/webapi/src/main/scala/org/knora/webapi/slice/ontology/repo/DeleteClassCommentsQuery.scala
@@ -4,46 +4,46 @@
  */
 
 package org.knora.webapi.slice.ontology.repo
-import org.eclipse.rdf4j.model.vocabulary.OWL
-import org.eclipse.rdf4j.model.vocabulary.RDFS
-import org.eclipse.rdf4j.model.vocabulary.XSD
-import org.eclipse.rdf4j.sparqlbuilder.core.query.ModifyQuery
-import org.eclipse.rdf4j.sparqlbuilder.core.query.Queries
 import zio.*
 
+import org.knora.sparqlbuilder.*
 import org.knora.webapi.slice.api.v2.ontologies.LastModificationDate
 import org.knora.webapi.slice.common.KnoraIris.ResourceClassIri
-import org.knora.webapi.slice.common.QueryBuilderHelper
-import org.knora.webapi.slice.common.repo.rdf.Vocabulary.KnoraBase
 
-object DeleteClassCommentsQuery extends QueryBuilderHelper {
+object DeleteClassCommentsQuery {
 
   def build(
     resourceClassIri: ResourceClassIri,
     lastModificationDate: LastModificationDate,
-  ): UIO[ModifyQuery] = Clock.instant.map { now =>
-    val ontology      = toRdfIri(resourceClassIri.ontologyIri)
-    val resourceClass = toRdfIri(resourceClassIri)
+  ): UIO[String] = Clock.instant.map { now =>
+    val ontology      = Iri.unsafeFrom(resourceClassIri.ontologyIri.toInternalSchema.toIri)
+    val resourceClass = Iri.unsafeFrom(resourceClassIri.toInternalSchema.toIri)
+    val previousDate  = Literal.dateTime(lastModificationDate.value)
+    val currentDate   = Literal.dateTime(now)
 
-    val comments = variable("comments")
-
-    Queries
-      .MODIFY()
-      .prefix(XSD.NS, OWL.NS, RDFS.NS, KnoraBase.NS, NS(resourceClassIri.ontologyIri))
-      .delete(
-        resourceClass.has(RDFS.COMMENT, comments),
-        ontology.has(KnoraBase.lastModificationDate, toRdfLiteral(lastModificationDate)),
-      )
-      .from(ontology)
-      .insert(ontology.has(KnoraBase.lastModificationDate, toRdfLiteral(now)))
-      .into(ontology)
-      .where(
-        ontology
-          .isA(OWL.ONTOLOGY)
-          .andHas(KnoraBase.lastModificationDate, toRdfLiteral(lastModificationDate))
-          .and(resourceClass.has(RDFS.COMMENT, comments))
-          .from(ontology),
-      )
+    sparql"""|PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+              |PREFIX owl: <http://www.w3.org/2002/07/owl#>
+              |PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+              |PREFIX knora-base: <http://www.knora.org/ontology/knora-base#>
+              |
+              |DELETE {
+              |  GRAPH $ontology {
+              |    $resourceClass rdfs:comment ?comments .
+              |    $ontology knora-base:lastModificationDate $previousDate .
+              |  }
+              |}
+              |INSERT {
+              |  GRAPH $ontology {
+              |    $ontology knora-base:lastModificationDate $currentDate .
+              |  }
+              |}
+              |WHERE {
+              |  GRAPH $ontology {
+              |    $ontology a owl:Ontology ;
+              |      knora-base:lastModificationDate $previousDate .
+              |    $resourceClass rdfs:comment ?comments .
+              |  }
+              |}""".render
   }
 
 }
